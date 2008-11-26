8bc75896b1393748c55895f9a83c7a819fb8f501
Alexey Tarasov <tarasov@dodologics.com>
2008-11-27 06:16:17 +1000

    Changed sprintf() -> snprintf().
    Updated to snprintf(), additionaly fixed code style mixing per file.
    Removed typos.

--- debuggerd/utility.c
+++ debuggerd/utility.c
@@ -36,15 +36,15 @@ void get_remote_struct(int pid, void *src, void *dst, size_t size)
 {
     unsigned int i;
 
-    for (i = 0; i+4 <= size; i+=4) {
-        *(int *)(dst+i) = ptrace(PTRACE_PEEKTEXT, pid, src+i, NULL);
+    for (i = 0; i + 4 <= size; i += 4) {
+        *(int *)(dst + i) = ptrace(PTRACE_PEEKTEXT, pid, src + i, NULL);
     }
 
     if (i < size) {
         int val;
 
         assert((size - i) < 4);
-        val = ptrace(PTRACE_PEEKTEXT, pid, src+i, NULL);
+        val = ptrace(PTRACE_PEEKTEXT, pid, src + i, NULL);
         while (i < size) {
             ((unsigned char *)dst)[i] = val & 0xff;
             i++;
@@ -54,10 +54,10 @@ void get_remote_struct(int pid, void *src, void *dst, size_t size)
 }
 
 /* Map a pc address to the name of the containing ELF file */
-const char *map_to_name(mapinfo *mi, unsigned pc, const char* def)
+const char *map_to_name(mapinfo *mi, unsigned pc, const char *def)
 {
-    while(mi) {
-        if((pc >= mi->start) && (pc < mi->end)){
+    while (mi != NULL) {
+        if ((pc >= mi->start) && (pc < mi->end)) {
             return mi->name;
         }
         mi = mi->next;
@@ -68,8 +68,8 @@ const char *map_to_name(mapinfo *mi, unsigned pc, const char* def)
 /* Find the containing map info for the pc */
 const mapinfo *pc_to_mapinfo(mapinfo *mi, unsigned pc)
 {
-    while(mi) {
-        if((pc >= mi->start) && (pc < mi->end)){
+    while (mi != NULL) {
+        if ((pc >= mi->start) && (pc < mi->end)) {
             return mi;
         }
         mi = mi->next;
--- debuggerd/utility.h
+++ debuggerd/utility.h
@@ -33,7 +33,7 @@ typedef struct mapinfo {
     unsigned end;
     unsigned exidx_start;
     unsigned exidx_end;
-    char name[];
+    char *name;
 } mapinfo;
 
 /* Get a word from pid using ptrace. The result is the return value. */
@@ -48,7 +48,7 @@ extern void get_remote_struct(int pid, void *src, void *dst, size_t size);
 const mapinfo *pc_to_mapinfo (mapinfo *mi, unsigned pc);
 
 /* Map a pc address to the name of the containing ELF file */
-const char *map_to_name(mapinfo *mi, unsigned pc, const char* def);
+const char *map_to_name(mapinfo *mi, unsigned pc, const char *def);
 
 /* Log information onto the tombstone */
 extern void _LOG(int tfd, bool in_tombstone_only, const char *fmt, ...);
