e4d49849dd271e9ea5e261a422a62c4342aaedaa
Alexey Tarasov <tarasov@dodologics.com>
2009-08-12 15:08:04 +1100

    Changed sprintf() -> snprintf().
    Updated to snprintf(), additionaly fixed code style mixing per file.
    Removed typos.

--- debuggerd/getevent.c
+++ debuggerd/getevent.c
@@ -31,13 +31,14 @@ static int open_device(const char *device)
     if(fd < 0) {
         return -1;
     }
-    
+    /* XXX: unused { */
     if(ioctl(fd, EVIOCGVERSION, &version)) {
         return -1;
     }
     if(ioctl(fd, EVIOCGID, &id)) {
         return -1;
     }
+    /* XXX: unused } */
     name[sizeof(name) - 1] = '\0';
     location[sizeof(location) - 1] = '\0';
     idstr[sizeof(idstr) - 1] = '\0';
@@ -56,13 +57,13 @@ static int open_device(const char *device)
 
     new_ufds = realloc(ufds, sizeof(ufds[0]) * (nfds + 1));
     if(new_ufds == NULL) {
-        fprintf(stderr, "out of memory\n");
+        fprintf(stderr, "open_device(): failed to allocate memory for ufds\n");
         return -1;
     }
     ufds = new_ufds;
     new_device_names = realloc(device_names, sizeof(device_names[0]) * (nfds + 1));
     if(new_device_names == NULL) {
-        fprintf(stderr, "out of memory\n");
+        fprintf(stderr, "open_device(): failed to allocate memory for device names\n");
         return -1;
     }
     device_names = new_device_names;
@@ -81,7 +82,8 @@ int close_device(const char *device)
         if(strcmp(device_names[i], device) == 0) {
             int count = nfds - i - 1;
             free(device_names[i]);
-            memmove(device_names + i, device_names + i + 1, sizeof(device_names[0]) * count);
+            memmove(device_names + i, device_names + i + 1,
+	        sizeof(device_names[0]) * count);
             memmove(ufds + i, ufds + i + 1, sizeof(ufds[0]) * count);
             nfds--;
             return 0;
@@ -90,11 +92,11 @@ int close_device(const char *device)
     return -1;
 }
 
-static int read_notify(const char *dirname, int nfd)
+static int read_notify(const char *dirname, int nfd,
+    int allow_skip)
 {
     int res;
     char devname[PATH_MAX];
-    char *filename;
     char event_buf[512];
     int event_size;
     int event_pos = 0;
@@ -109,21 +111,29 @@ static int read_notify(const char *dirname, int nfd)
     }
     //printf("got %d bytes of event information\n", res);
 
-    strcpy(devname, dirname);
-    filename = devname + strlen(devname);
-    *filename++ = '/';
-
     while(res >= (int)sizeof(*event)) {
         event = (struct inotify_event *)(event_buf + event_pos);
         //printf("%d: %08x \"%s\"\n", event->wd, event->mask, event->len ? event->name : "");
-        if(event->len) {
-            strcpy(filename, event->name);
-            if(event->mask & IN_CREATE) {
-                open_device(devname);
-            }
-            else {
-                close_device(devname);
-            }
+        if(event->len > 0) {
+	    int result = snprintf(devname, sizeof(devname), "%s/%s", dirname,
+                             event->name);
+    
+            if (result >= (int)sizeof(devname)) {
+	        fprintf(stderr, "devname buffer size %zu was not big enough "
+		    "to store %d bytes\n", sizeof(devname), result);
+
+                if (allow_skip == 0)
+		    return 1;
+	    }
+	    else {
+	    
+                if(event->mask & IN_CREATE) {
+                    open_device(devname);
+                }
+                else {
+                    close_device(devname);
+                }
+	    }
         }
         event_size = sizeof(*event) + event->len;
         res -= event_size;
@@ -135,22 +145,23 @@ static int read_notify(const char *dirname, int nfd)
 static int scan_dir(const char *dirname)
 {
     char devname[PATH_MAX];
-    char *filename;
     DIR *dir;
     struct dirent *de;
     dir = opendir(dirname);
     if(dir == NULL)
         return -1;
-    strcpy(devname, dirname);
-    filename = devname + strlen(devname);
-    *filename++ = '/';
     while((de = readdir(dir))) {
         if(de->d_name[0] == '.' &&
            (de->d_name[1] == '\0' ||
             (de->d_name[1] == '.' && de->d_name[2] == '\0')))
             continue;
-        strcpy(filename, de->d_name);
-        open_device(devname);
+	int result = snprintf(devname, sizeof(devname), "%s/%s", dirname,
+                        de->d_name);
+        if (result < (int)sizeof(devname))
+            open_device(devname);
+	else
+	    fprintf(stderr, "scan_dir(): filename '%s' is too big, skipping\n",
+	        de->d_name);
     }
     closedir(dir);
     return 0;
@@ -166,7 +177,7 @@ int init_getevent()
     ufds[0].fd = inotify_init();
     ufds[0].events = POLLIN;
 
-	res = inotify_add_watch(ufds[0].fd, device_path, IN_DELETE | IN_CREATE);
+    res = inotify_add_watch(ufds[0].fd, device_path, IN_DELETE | IN_CREATE);
     if(res < 0) {
         return 1;
     }
@@ -188,7 +199,7 @@ void uninit_getevent()
     nfds = 0;
 }
 
-int get_event(struct input_event* event, int timeout)
+int get_event(struct input_event *event, int timeout)
 {
     int res;
     int i;
@@ -200,14 +211,14 @@ int get_event(struct input_event* event, int timeout)
             return 1;
         }
         if(ufds[0].revents & POLLIN) {
-            read_notify(device_path, ufds[0].fd);
+            read_notify(device_path, ufds[0].fd, 0);
         }
         for(i = 1; i < nfds; i++) {
             if(ufds[i].revents) {
                 if(ufds[i].revents & POLLIN) {
                     res = read(ufds[i].fd, event, sizeof(*event));
                     if(res < (int)sizeof(event)) {
-                        fprintf(stderr, "could not get event\n");
+                        fprintf(stderr, "get_event(): could not read event\n");
                         return -1;
                     }
                     return 0;
