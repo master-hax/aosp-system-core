55de73bb7d57fc1dccd616a43c3420084b9c8263
Al Sutton <alsutton@funkyandroid.com>
2011-11-24 19:08:32 +0000

    Sony Series S and P Tablets USB IDs
    This patch adds the Sony series S and P tablet USB IDs to the list of
    known USB devices

--- adb/usb_vendors.c
+++ adb/usb_vendors.c
@@ -111,6 +111,8 @@
 #define VENDOR_ID_ARCHOS        0x0E79
 // Positivo's USB Vendor ID
 #define VENDOR_ID_POSITIVO      0x1662
+// Sony's USB Vendor ID
+#define VENDOR_ID_SONY			0x054c
 
 
 /** built-in vendor list */
@@ -153,6 +155,7 @@ int builtInVendorIds[] = {
     VENDOR_ID_PEGATRON,
     VENDOR_ID_ARCHOS,
     VENDOR_ID_POSITIVO,
+    VENDOR_ID_SONY,
 };
 
 #define BUILT_IN_VENDOR_COUNT    (sizeof(builtInVendorIds)/sizeof(builtInVendorIds[0]))
