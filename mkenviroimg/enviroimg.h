79db0d9f9eb9e29c6e2e23a87374ece98c6b33b2
Dan Murphy <dmurphy@ti.com>
2012-05-03 10:25:01 -0500

    Add a mechanism to build an enviroment partition for a device
    This code adds a way that OEMs can create an enviroment partition
    for the target device.
    
    This partition can include:
    - a fastboot image for the bootloader to display an image during fastboot
    - a charger image for the bootloader to display an image during charging
    - a splash image the bootloader to display a splash screen
    - an enviroment area where persistent data can be stored (i.e lock status,
    boot reason or what ever the OEM decided.
    - an area for device tree so that OEMs can have a partition for the device tree.
    
    This will allow OEMs to have a single bootloader and a single kernel but a configurable
    partition for the device.
    
    Useage:
    mkenviroimg --dev_tree <filename> --dt_base <Load address of device tree>
    --enviroment <default enviroment data in binary format> --env_size <size>
    --board <board name limited to %i characters>
    --splash_img <splash image-filename>
    --fastboot_img <fastboot image-filename>
    --charger_img <charger image-filename>
    --fb_base <Framebuffer address for the image>
    -o | --output <filename>\n", ENVIRO_NAME_SIZE
    
    Change-Id: Ic8fe44a268e08db61481d02d9f4786cd2d7d6e27
    Signed-off-by: Dan Murphy <dmurphy@ti.com>

---
+++ mkenviroimg/enviroimg.h
@@ -0,0 +1,79 @@
+/* tools/mkenviroimg/enviromimg.h
+**
+** Copyright 2007, The Android Open Source Project
+**
+** Licensed under the Apache License, Version 2.0 (the "License"); 
+** you may not use this file except in compliance with the License. 
+** You may obtain a copy of the License at 
+**
+**     http://www.apache.org/licenses/LICENSE-2.0 
+**
+** Unless required by applicable law or agreed to in writing, software 
+** distributed under the License is distributed on an "AS IS" BASIS, 
+** WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+** See the License for the specific language governing permissions and 
+** limitations under the License.
+*/
+
+#ifndef _ENVIRO_IMAGE_H_
+#define _ENVIRO_IMAGE_H_
+
+typedef struct enviro_img_hdr enviro_img_hdr;
+
+#define ENVIRO_MAGIC "ANDROID_ENV!"
+#define ENVIRO_MAGIC_SIZE 12
+#define ENVIRO_NAME_SIZE 16
+#define ENVIRO_ARGS_SIZE 512
+
+struct enviro_img_hdr
+{
+    unsigned char magic[ENVIRO_MAGIC_SIZE];
+
+    unsigned dev_tree_size;  /* size in bytes */
+    unsigned dev_tree_addr;  /* physical load addr */
+
+    unsigned enviroment_size; /* size in bytes */
+
+    unsigned splash_img_size;  /* size in bytes */
+    unsigned splash_img_addr;  /* physical load addr */
+
+    unsigned fastboot_img_size;  /* size in bytes */
+    unsigned fastboot_img_addr;  /* physical load addr */
+
+    unsigned charger_img_size;  /* size in bytes */
+    unsigned charger_img_addr;  /* physical load addr */
+
+    unsigned page_size;    /* flash page size we assume */
+
+    unsigned char name[ENVIRO_NAME_SIZE]; /* asciiz product name */
+    
+    unsigned id[8]; /* timestamp / checksum / sha1 / etc */
+};
+
+/*
+** +-----------------+ 
+** | enviroment hdr  | 1 page
+** +-----------------+
+** | device tree     | n pages  
+** +-----------------+
+** | enviroment      | m pages  
+** +-----------------+
+** | boot splash     | o pages  
+** +-----------------+
+** | fastboot splash | p pages  
+** +-----------------+
+** | charger splash  | r pages  
+** +-----------------+
+**
+** n = (dev_tree_size + page_size - 1) / page_size
+** m = (enviroment_size + page_size - 1) / page_size
+** o = (splash_img_size + page_size - 1) / page_size
+** p = (fastboot_img_size + page_size - 1) / page_size
+** r = (charger_img_size + page_size - 1) / page_size
+**
+** 0. all entities are page_size aligned in flash
+** 1. Only device tree is required (size != 0)
+** 2. Splash screens are optional
+*/
+
+#endif
