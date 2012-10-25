f4db6d4265b271f793d3158f5ba6c7e75fb90cf7
Andrew Boie <andrew.p.boie@intel.com>
2012-10-25 14:10:07 -0700

    init: expand usage of property lookup
    Currently, the init.rc syntax ${prop.name} to substitute a value
    looked up from the property service is only supported for the
    setprop, write, and import commands. Here we generalize it so that
    the arguments for all commands and services allow for references like
    this.
    
    Substitutions are always done at the last possible moment; right
    when the command is executed or the service is started. If a service
    is started/stopped multiple times, the substitutions will always be
    re-done each time it starts.
    
    If any substitition fails, the command will not run/service will not
    start.
    
    The deprecated syntax $prop.name is no longer supported.
    
    Change-Id: I8560b70f48c0396832d9a7fadc546441e8e81ff9
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>

--- init/builtins.c
+++ init/builtins.c
@@ -543,15 +543,8 @@ int do_setprop(int nargs, char **args)
 {
     const char *name = args[1];
     const char *value = args[2];
-    char prop_val[PROP_VALUE_MAX];
-    int ret;
 
-    ret = expand_props(prop_val, value, sizeof(prop_val));
-    if (ret) {
-        ERROR("cannot expand '%s' while assigning to '%s'\n", value, name);
-        return -EINVAL;
-    }
-    property_set(name, prop_val);
+    property_set(name, value);
     return 0;
 }
 
@@ -635,15 +628,8 @@ int do_write(int nargs, char **args)
 {
     const char *path = args[1];
     const char *value = args[2];
-    char prop_val[PROP_VALUE_MAX];
-    int ret;
 
-    ret = expand_props(prop_val, value, sizeof(prop_val));
-    if (ret) {
-        ERROR("cannot expand '%s' while writing to '%s'\n", value, path);
-        return -EINVAL;
-    }
-    return write_file(path, prop_val);
+    return write_file(path, value);
 }
 
 int do_copy(int nargs, char **args)
