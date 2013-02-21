20efc25ee0ba818b5ea81b71563632bb4f6d3e8a
Wing_Tseng <Wing_Tseng@pegatroncorp.com>
2013-02-21 11:50:58 +0800

    Add usb vendor id for Harris
    Change-Id: I6ad7e455e0bfce8a424af1385da219308ba4aa6c
    Signed-off-by: Wing_Tseng <Wing_Tseng@pegatroncorp.com>

--- adb/usb_vendors.c
+++ adb/usb_vendors.c
@@ -131,6 +131,8 @@
 #define VENDOR_ID_KOBO          0x2237
 // Teleepoch's USB Vendor ID
 #define VENDOR_ID_TELEEPOCH     0x2340
+// Harris's USB Vendor ID
+#define VENDOR_ID_HARRIS     0x19A5
 
 
 /** built-in vendor list */
@@ -183,6 +185,7 @@ int builtInVendorIds[] = {
     VENDOR_ID_YULONG_COOLPAD,
     VENDOR_ID_KOBO,
     VENDOR_ID_TELEEPOCH,
+    VENDOR_ID_HARRIS,
 };
 
 #define BUILT_IN_VENDOR_COUNT    (sizeof(builtInVendorIds)/sizeof(builtInVendorIds[0]))
