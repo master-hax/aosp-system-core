5c892e44de23d4b924793e59182a7035e62722dd
esther <esther530@gmail.com>
2013-06-13 13:28:18 +0800

    Add Qisda usb config
    add Qisda usb config
    Signed-off-by: esther <esther530@gmail.com>
    
    Change-Id: I5b8a0e5bd2218ecdf417de357c9a7d9307c91932

--- adb/usb_vendors.c
+++ adb/usb_vendors.c
@@ -143,7 +143,8 @@
 #define VENDOR_ID_BYD           0x19D1
 // OUYA's USB Vendor ID
 #define VENDOR_ID_OUYA          0x2836
-
+// Qisda's USB Vendor ID
+#define VENDOR_ID_QISDA         0x1D45
 
 /** built-in vendor list */
 int builtInVendorIds[] = {
@@ -201,6 +202,7 @@ int builtInVendorIds[] = {
     VENDOR_ID_XIAOMI,
     VENDOR_ID_BYD,
     VENDOR_ID_OUYA,
+    VENDOR_ID_QISDA,
 };
 
 #define BUILT_IN_VENDOR_COUNT    (sizeof(builtInVendorIds)/sizeof(builtInVendorIds[0]))
