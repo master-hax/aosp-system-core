0d0814cd6c31706b3c51e4f36fdbb4fddd19d218
DanielMo <DanielMo@fih-foxconn.com>
2013-08-27 11:19:32 +0800

    adb: Add USB vendor ID for Rockchip
    Change-Id: Iea3f7cc6ed463684fe30a73c39445b3ca36854d9
    Signed-off-by: DanielMo <DanielMo@fih-foxconn.com>

--- adb/usb_vendors.c
+++ adb/usb_vendors.c
@@ -159,6 +159,8 @@
 #define VENDOR_ID_MSI           0x1462
 // Wacom's USB Vendor ID
 #define VENDOR_ID_WACOM         0x0531
+// Rockchip's USB Vendor ID
+#define VENDOR_ID_ROCKCHIP      0x2207
 
 /** built-in vendor list */
 int builtInVendorIds[] = {
@@ -224,6 +226,7 @@ int builtInVendorIds[] = {
     VENDOR_ID_ECS,
     VENDOR_ID_MSI,
     VENDOR_ID_WACOM,
+    VENDOR_ID_ROCKCHIP,
 };
 
 #define BUILT_IN_VENDOR_COUNT    (sizeof(builtInVendorIds)/sizeof(builtInVendorIds[0]))
