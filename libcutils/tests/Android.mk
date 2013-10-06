1cb5e1fd0a8cbc91e0429e88b3a3bc2caebcb47b
William Roberts <wroberts@tresys.com>
2013-10-06 17:52:04 -0400

    Add testing makefile
    Change-Id: I30d845f16e445a50d93c0d91eb8cef380ebd2df0

--- libcutils/tests/Android.mk
+++ libcutils/tests/Android.mk
@@ -1,1 +1,32 @@
+LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := libcutils_test
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := EXECUTABLES
+
+intermediates:= $(local-intermediates-dir)
+
+all_test_files := $(LOCAL_PATH)/string.c
+all_tests.c := $(intermediates)/all_tests.c
+$(all_tests.c): $(all_test_files) $(LOCAL_PATH)/cutest/CuTest.h $(LOCAL_PATH)/cutest/CuTest.c $(LOCAL_PATH)/cutest/make-tests.sh
+	mkdir -p $(dir $@)
+	system/core/libcutils/tests/cutest/make-tests.sh $(all_test_files) > $@
+
+LOCAL_GENERATED_SOURCES := \
+		$(all_tests.c)
+
+LOCAL_SRC_FILES := \
+	cutest/CuTest.c \
+	cutest/make-tests.sh \
+	string.c
+
+LOCAL_C_INCLUDES := \
+	$(LOCAL_PATH)/cutest
+
+LOCAL_SHARED_LIBRARIES := libcutils
+
+include $(BUILD_EXECUTABLE)
+
 include $(all-subdir-makefiles)
