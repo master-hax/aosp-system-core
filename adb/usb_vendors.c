a1d36609624508c5cfe5ca906520d5be593f23ba
DanielMo <DanielMo@fih-foxconn.com>
2013-12-17 16:25:42 +0800

    adb: Add USB vendor ID for TechFaith
    Change-Id: Ied42e4d011b736ee990ed75e7d81bc6a16db47f9

--- adb/usb_vendors.c
+++ adb/usb_vendors.c
@@ -159,6 +159,8 @@
 #define VENDOR_ID_MSI           0x0DB0
 // Wacom's USB Vendor ID
 #define VENDOR_ID_WACOM         0x0531
+// TechFaith's USB Vendor ID
+#define VENDOR_ID_TECHFAITH     0x1d09
 
 /** built-in vendor list */
 int builtInVendorIds[] = {
@@ -224,6 +226,7 @@ int builtInVendorIds[] = {
     VENDOR_ID_ECS,
     VENDOR_ID_MSI,
     VENDOR_ID_WACOM,
+    VENDOR_ID_TECHFAITH,
 };
 
 #define BUILT_IN_VENDOR_COUNT    (sizeof(builtInVendorIds)/sizeof(builtInVendorIds[0]))
