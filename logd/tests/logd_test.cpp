465b98967c66a56a96324eab80681332c8380b23
Mark Salyzyn <salyzyn@google.com>
2014-04-09 10:02:23 -0700

    logd: enable UID spam filter and test
    Change-Id: Ie9c7a744e341e6e64afa14973d4d095717c97933

--- logd/tests/logd_test.cpp
+++ logd/tests/logd_test.cpp
@@ -22,6 +22,7 @@
 #include <gtest/gtest.h>
 
 #include "cutils/sockets.h"
+#include "log/log.h"
 #include "log/logger.h"
 
 #define __unused __attribute__((__unused__))
@@ -77,8 +78,9 @@ static char *find_benchmark_spam(char *cp)
     //
     // main: UID/PID Total size/num   Now          UID/PID[?]  Total
     // 0           7500306/304207     71608/3183   0/4225?     7454388/303656
+    //    <wrap>                                                     93432/1012
     // -or-
-    // 0/gone      7454388/303656
+    // 0/gone      7454388/303656     93432/1012
     //
     // basically if we see a *large* number of 0/????? entries
     unsigned long value;
@@ -106,6 +108,15 @@ static char *find_benchmark_spam(char *cp)
             while (isdigit(*cp)) {
                 value = value * 10ULL + *cp - '0';
                 ++cp;
+            }
+            if (*cp != '/') {
+                value = 0;
+                continue;
+            }
+            while (isdigit(*++cp));
+            while (*cp == ' ') ++cp;
+            if (!isdigit(*cp)) {
+                value = 0;
             }
         }
     } while ((value < 900000ULL) && *cp);
@@ -607,5 +618,47 @@ TEST(logd, benchmark) {
     ASSERT_TRUE(NULL != buf);
     EXPECT_TRUE(find_benchmark_spam(buf) != NULL);
 
+    char *benchmark_statistics_found = find_benchmark_spam(buf);
+    EXPECT_TRUE(benchmark_statistics_found != NULL);
+
+    if (!benchmark_statistics_found) {
+        return;
+    }
+
+    // Check how effective the SPAM filter is, parse out Now size.
+    //             Total               Now
+    // 0/4225?     7454388/303656      31488/755
+    //                                 ^-- benchmark_statistics_found
+
+    unsigned long nowSize = atol(benchmark_statistics_found);
+
     delete [] buf;
+
+    ASSERT_NE(0UL, nowSize);
+
+    int sock = socket_local_client("logd",
+                                   ANDROID_SOCKET_NAMESPACE_RESERVED,
+                                   SOCK_STREAM);
+    unsigned long totalSize = 65536;
+    if (sock >= 0) {
+        static const char getSize[] = {
+            'g', 'e', 't', 'L', 'o', 'g', 'S', 'i', 'z', 'e', ' ',
+            LOG_ID_MAIN + '0', '\0'
+        };
+        if (write(sock, getSize, sizeof(getSize)) > 0) {
+            char buffer[80];
+            memset(buffer, 0, sizeof(buffer));
+            read(sock, buffer, sizeof(buffer));
+            totalSize = atol(buffer);
+            if (totalSize < 65536) {
+                totalSize = 65536;
+            }
+        }
+        close(sock);
+    }
+    // logd allows excursions to 110% of total size
+    totalSize = (totalSize * 11 ) / 10;
+
+    // 50% threshold for SPAM filter (<20% typical, lots of engineering margin)
+    ASSERT_GT(totalSize, nowSize * 2);
 }
