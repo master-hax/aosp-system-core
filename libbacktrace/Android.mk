e37bcba982b60515caf796a02baa965a9a419f42
Christopher Ferris <cferris@google.com>
2014-04-18 15:30:15 -0700

    Remove the arm64 special case.
    The stack-protector option is handled by the global makefiles.
    
    Change-Id: I99c4a1624e72ad91ac10eef27c0e3758fe9af8ba

--- libbacktrace/Android.mk
+++ libbacktrace/Android.mk
@@ -120,13 +120,6 @@ backtrace_test_cflags := \
 	-g \
 	-DGTEST_HAS_STD_STRING \
 
-ifneq ($(TARGET_ARCH),arm64)
-backtrace_test_cflags += -fstack-protector-all
-else
-  $(info TODO: $(LOCAL_PATH)/Android.mk -fstack-protector not yet available for the AArch64 toolchain)
-  common_cflags += -fno-stack-protector
-endif # arm64
-
 backtrace_test_cflags_target := \
 	-DGTEST_OS_LINUX_ANDROID \
 	-DENABLE_PSS_TESTS \
