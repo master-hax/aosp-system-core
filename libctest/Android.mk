e750710c3b56c1896aec31d1592bc27ea775ecad
Mark Salyzyn <salyzyn@google.com>
2014-05-15 15:16:46 -0700

    libctest: Turn on -Werror
    Change-Id: I28a017104f48cc4216df876eb9102ecd332d28e3

--- libctest/Android.mk
+++ libctest/Android.mk
@@ -1,7 +1,8 @@
-LOCAL_PATH:= $(call my-dir)
+LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_MODULE:= libctest
+LOCAL_MODULE := libctest
 LOCAL_SRC_FILES := ctest.c
+LOCAL_CFLAGS := -Werror
 
 include $(BUILD_SHARED_LIBRARY)
