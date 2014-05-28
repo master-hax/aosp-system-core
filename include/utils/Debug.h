44655320e5d39b19679d2955733fcd6565031cfa
Sreeram Ramachandran <sreeram@google.com>
2014-05-28 15:47:36 -0700

    Fix up Debug.h.
    The main purpose of this is to change 'class' to 'struct' in one instance, to
    avoid the following compilation error:
    
    error: class template 'CompileTimeAssert' was previously declared as a struct
    template [-Werror,-Wmismatched-tags]
    
    system/core/include/utils/Debug.h:30:14: note: expanded from macro
    'COMPILE_TIME_ASSERT'
        template class CompileTimeAssert< (_exp) >;
                 ^
    system/core/include/utils/Debug.h:27:23: note: previous use is here
    template<bool> struct CompileTimeAssert;
                          ^
    
    In the process, take the opportunity to also:
    + Remove unused includes.
    + Remove the __cplusplus guards. Everything in this file is C++, including
      "namespace" and "template". No C code includes this file (indeed if it had, it
      would've broken because the previous guards didn't guard all the C++ stuff).
    + Move the macros outside the namespace, and make them refer to the explicitly
      namespace-qualified type names. This is so clients don't have to be forced to
      use the macro only within the 'android' namespace.
    + Do other cosmetic changes (fix typo, whitespace, unnecessary semicolon, etc).
    
    Change-Id: I63c60c69243a8d4441c666d326ba05f5134a277f

--- include/utils/Debug.h
+++ include/utils/Debug.h
@@ -17,32 +17,22 @@
 #ifndef ANDROID_UTILS_DEBUG_H
 #define ANDROID_UTILS_DEBUG_H
 
-#include <stdint.h>
-#include <sys/types.h>
-
 namespace android {
-// ---------------------------------------------------------------------------
 
-#ifdef __cplusplus
 template<bool> struct CompileTimeAssert;
 template<> struct CompileTimeAssert<true> {};
-#define COMPILE_TIME_ASSERT(_exp) \
-    template class CompileTimeAssert< (_exp) >;
-#endif
-#define COMPILE_TIME_ASSERT_FUNCTION_SCOPE(_exp) \
-    CompileTimeAssert<( _exp )>();
 
-// ---------------------------------------------------------------------------
-
-#ifdef __cplusplus
-template<bool C, typename LSH, typename RHS> struct CompileTimeIfElse;
-template<typename LHS, typename RHS> 
+template<bool COND, typename LHS, typename RHS> struct CompileTimeIfElse;
+template<typename LHS, typename RHS>
 struct CompileTimeIfElse<true,  LHS, RHS> { typedef LHS TYPE; };
-template<typename LHS, typename RHS> 
+template<typename LHS, typename RHS>
 struct CompileTimeIfElse<false, LHS, RHS> { typedef RHS TYPE; };
-#endif
 
-// ---------------------------------------------------------------------------
-}; // namespace android
+}  // namespace android
 
-#endif // ANDROID_UTILS_DEBUG_H
+#define COMPILE_TIME_ASSERT(_exp) \
+    template struct android::CompileTimeAssert<(_exp)>;
+#define COMPILE_TIME_ASSERT_FUNCTION_SCOPE(_exp) \
+    android::CompileTimeAssert<(_exp)>();
+
+#endif  // ANDROID_UTILS_DEBUG_H
