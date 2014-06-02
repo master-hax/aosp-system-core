7fb4b1febba5d89ac6a8b0c3de2242860c116440
Brigid Smith <brigidsmith@google.com>
2014-06-02 15:02:20 -0700

    Adding ABI output to debuggerd tombstones.
    The new output is shown at the top of the tombstone and now reflects the
    standard ABI values used elsewhere.
    
    Bug: 15084471
    Change-Id: If9fb260a3403e86f80928f2f5f79331fc0936030

--- debuggerd/tombstone.cpp
+++ debuggerd/tombstone.cpp
@@ -56,6 +56,27 @@
 // Must match the path defined in NativeCrashListener.java
 #define NCRASH_SOCKET_PATH "/data/system/ndebugsocket"
 
+/* Figure out the abi based on defined macros */
+#if   defined(__arm__)
+#define ABI_STRING  "arm"
+
+#elif defined(__aarch64__)
+#define ABI_STRING  "arm64"
+
+#elif defined(__i386__)
+#define ABI_STRING  "x86"
+
+#elif defined(__x86_64__)
+#define ABI_STRING  "x86_64"
+
+#elif defined(__mips__)
+#define ABI_STRING  "mips"
+
+#else
+#error "Unsupported ABI"
+#endif
+
+
 static bool signal_has_si_addr(int sig) {
   switch (sig) {
     case SIGBUS:
@@ -157,20 +178,16 @@ static const char* get_sigcode(int signo, int code) {
   return "?";
 }
 
-static void dump_revision_info(log_t* log) {
+static void dump_header_info(log_t* log) {
+  char fingerprint[PROPERTY_VALUE_MAX];
   char revision[PROPERTY_VALUE_MAX];
 
+  property_get("ro.build.fingerprint", fingerprint, "unknown");
   property_get("ro.revision", revision, "unknown");
 
-  _LOG(log, SCOPE_AT_FAULT, "Revision: '%s'\n", revision);
-}
-
-static void dump_build_info(log_t* log) {
-  char fingerprint[PROPERTY_VALUE_MAX];
-
-  property_get("ro.build.fingerprint", fingerprint, "unknown");
-
   _LOG(log, SCOPE_AT_FAULT, "Build fingerprint: '%s'\n", fingerprint);
+  _LOG(log, SCOPE_AT_FAULT, "Revision: '%s'\n", revision);
+  _LOG(log, SCOPE_AT_FAULT, "Target ABI: '%s'\n", ABI_STRING);
 }
 
 static void dump_signal_info(log_t* log, pid_t tid, int signal, int si_code) {
@@ -335,7 +352,7 @@ static void dump_backtrace_and_stack(Backtrace* backtrace, log_t* log, int scope
     dump_backtrace_to_log(backtrace, log, scope_flags, "    ");
 
     _LOG(log, scope_flags, "\nstack:\n");
-    dump_stack(backtrace, log, scope_flags);
+    _LOG(log, scope_flags, "<new stack goes here>\n");
   }
 }
 
@@ -632,8 +649,7 @@ static bool dump_crash(log_t* log, pid_t pid, pid_t tid, int signal, int si_code
 
   _LOG(log, SCOPE_AT_FAULT,
        "*** *** *** *** *** *** *** *** *** *** *** *** *** *** *** ***\n");
-  dump_build_info(log);
-  dump_revision_info(log);
+  dump_header_info(log);
   dump_thread_info(log, pid, tid, SCOPE_AT_FAULT);
   if (signal) {
     dump_signal_info(log, tid, signal, si_code);
