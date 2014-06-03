302975dbc469685fe68fd78ec270becccdacce01
Mathieu Chartier <mathieuc@google.com>
2014-06-03 10:24:21 -0700

    Make host ashmem_create_region seed only once.
    Not seeding this each call should help reduce collisions when multiple
    threads are calling ashmem_create_region. Also cleaned up code by
    deleting gotos, and making formatting consistent.
    
    Bug: 15394258
    Change-Id: Iafdaea57b2317e0eb7c40d7b5595c523814fd88c

--- libcutils/ashmem-host.c
+++ libcutils/ashmem-host.c
@@ -22,6 +22,8 @@
 #include <errno.h>
 #include <fcntl.h>
 #include <limits.h>
+#include <pthread.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -36,20 +38,30 @@
 #define __unused __attribute__((__unused__))
 #endif
 
+static bool seeded = false;
+static pthread_mutex_t seed_mutex = PTHREAD_MUTEX_INITIALIZER;
+
 int ashmem_create_region(const char *ignored __unused, size_t size)
 {
 	static const char txt[] = "abcdefghijklmnopqrstuvwxyz"
-				  "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+			"ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	char name[64];
 	unsigned int retries = 0;
 	pid_t pid = getpid();
 	int fd;
-
-	srand(time(NULL) + pid);
-
-retry:
-	/* not beautiful, its just wolf-like loop unrolling */
-	snprintf(name, sizeof(name), "/tmp/android-ashmem-%d-%c%c%c%c%c%c%c%c",
+	if (pthread_mutex_lock(&seed_mutex) != 0) {
+		return -1;
+	}
+	if (!seeded) {
+		srand(time(NULL) + pid);
+		seeded = true;
+	}
+	if (pthread_mutex_unlock(&seed_mutex) != 0) {
+		return -1;
+	}
+	do {
+		/* not beautiful, its just wolf-like loop unrolling */
+		snprintf(name, sizeof(name), "/tmp/android-ashmem-%d-%c%c%c%c%c%c%c%c",
 		pid,
 		txt[(int) ((sizeof(txt) - 1) * (rand() / (RAND_MAX + 1.0)))],
 		txt[(int) ((sizeof(txt) - 1) * (rand() / (RAND_MAX + 1.0)))],
@@ -60,24 +72,19 @@ retry:
 		txt[(int) ((sizeof(txt) - 1) * (rand() / (RAND_MAX + 1.0)))],
 		txt[(int) ((sizeof(txt) - 1) * (rand() / (RAND_MAX + 1.0)))]);
 
-	/* open O_EXCL & O_CREAT: we are either the sole owner or we fail */
-	fd = open(name, O_RDWR | O_CREAT | O_EXCL, 0600);
-	if (fd == -1) {
-		/* unlikely, but if we failed because `name' exists, retry */
-		if (errno == EEXIST && ++retries < 6)
-			goto retry;
-		return -1;
-	}
-
+		/* open O_EXCL & O_CREAT: we are either the sole owner or we fail */
+		fd = open(name, O_RDWR | O_CREAT | O_EXCL, 0600);
+		if (fd == -1) {
+			/* unlikely, but if we failed because `name' exists, retry */
+			if (errno != EEXIST || ++retries >= 6) {
+				return -1;
+			}
+		}
+	} while (fd == -1);
 	/* truncate the file to `len' bytes */
-	if (ftruncate(fd, size) == -1)
-		goto error;
-
-	if (unlink(name) == -1)
-		goto error;
-
-	return fd;
-error:
+	if (ftruncate(fd, size) != -1 && unlink(name) != -1) {
+		return fd;
+	}
 	close(fd);
 	return -1;
 }
@@ -99,20 +106,20 @@ int ashmem_unpin_region(int fd __unused, size_t offset __unused, size_t len __un
 
 int ashmem_get_size_region(int fd)
 {
-        struct stat buf;
-        int result;
+	struct stat buf;
+	int result;
 
-        result = fstat(fd, &buf);
-        if (result == -1) {
-                return -1;
-        }
+	result = fstat(fd, &buf);
+	if (result == -1) {
+		return -1;
+	}
 
-        // Check if this is an "ashmem" region.
-        // TODO: This is very hacky, and can easily break. We need some reliable indicator.
-        if (!(buf.st_nlink == 0 && S_ISREG(buf.st_mode))) {
-                errno = ENOTTY;
-                return -1;
-        }
+	// Check if this is an "ashmem" region.
+	// TODO: This is very hacky, and can easily break. We need some reliable indicator.
+	if (!(buf.st_nlink == 0 && S_ISREG(buf.st_mode))) {
+		errno = ENOTTY;
+		return -1;
+	}
 
-        return (int)buf.st_size;  // TODO: care about overflow (> 2GB file)?
+	return (int)buf.st_size;	// TODO: care about overflow (> 2GB file)?
 }
