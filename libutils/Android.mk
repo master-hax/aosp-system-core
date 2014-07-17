6e01d2488dc540ed2a64de93e666b95075bba36b
Tim Murray <timmurray@google.com>
2014-07-17 16:36:46 -0700

    Update makefiles for host clang.
    bug 16172793
    
    Change-Id: If7484c5dbcccce7d925bec97bff0a3e4c30e9434

--- libutils/Android.mk
+++ libutils/Android.mk
@@ -43,7 +43,7 @@ commonSources:= \
 	VectorImpl.cpp \
 	misc.cpp
 
-host_commonCflags := -DLIBUTILS_NATIVE=1 $(TOOL_CFLAGS) -Werror
+host_commonCflags := -DLIBUTILS_NATIVE=1 $(TOOL_CFLAGS) -Werror -Wno-unused-parameter
 
 ifeq ($(HOST_OS),windows)
 ifeq ($(strip $(USE_CYGWIN),),)
