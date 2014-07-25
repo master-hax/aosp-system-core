389f4cf1c38789efbbe43295586e10264a6eacb8
Mark Salyzyn <salyzyn@google.com>
2014-07-25 12:25:48 -0700

    debuggerd: tombstone: suppress logger request for logd process
    Change-Id: I619880342ee6febd1a6cc98d2a4f536120efc1c8

--- debuggerd/utility.h
+++ debuggerd/utility.h
@@ -46,9 +46,11 @@ struct log_t{
     pid_t crashed_tid;
     // The tid of the thread we are currently working with.
     pid_t current_tid;
+    // if logcat should be retrieved
+    bool logcat;
 
     log_t()
-        : tfd(-1), amfd(-1), crashed_tid(-1), current_tid(-1) {}
+        : tfd(-1), amfd(-1), crashed_tid(-1), current_tid(-1), logcat(true) {}
 };
 
 // List of types of logs to simplify the logging decision in _LOG
