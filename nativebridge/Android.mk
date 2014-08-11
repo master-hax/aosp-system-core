746ea3caa82d670f579b29c53dd2ee01bc9016d9
Calin Juravle <calin@google.com>
2014-08-11 16:11:59 +0100

    Move native bridge lib in system/core
    Bug: 16884833
    Change-Id: I73aab8e212860ba5aee9444d801806d3da326a41

---
+++ nativebridge/Android.mk
@@ -0,0 +1,17 @@
+LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES:= \
+  native_bridge.cc
+
+LOCAL_MODULE:= nativebridge
+LOCAL_CPP_EXTENSION := .cc
+
+LOCAL_CFLAGS := -Werror
+LOCAL_CPPFLAGS := -std=gnu++11
+LOCAL_LDFLAGS := -ldl
+
+LOCAL_MULTILIB := both
+
+include $(BUILD_SHARED_LIBRARY)
