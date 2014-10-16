13654f072be2a9569bbdd87e548692dedf84757b
Alan Jeon <skyisle@gmail.com>
2014-10-16 17:05:25 +0900

    adb: Do not share memory between multiple thread
    When multiple client try to connect to other hosts, it failed because
    memory corruption. Allocate memory for each thread like other command did.
    
    Change-Id: I79959ce3dbfd11c7553736cd3f5a899815873584
    Signed-off-by: Alan Jeon <skyisle@gmail.com>

--- adb/services.c
+++ adb/services.c
@@ -620,6 +620,7 @@ static void connect_service(int fd, void* cookie)
     snprintf(resp, sizeof(resp), "%04x%s",(unsigned)strlen(buf), buf);
     writex(fd, resp, strlen(resp));
     adb_close(fd);
+    free(host);
 }
 #endif
 
@@ -648,6 +649,9 @@ asocket*  host_service_to_socket(const char*  name, const char *serial)
             sinfo->transport = kTransportAny;
             sinfo->state = CS_DEVICE;
         } else {
+            if (sinfo->serial) {
+                free(sinfo->serial);
+            }
             free(sinfo);
             return NULL;
         }
@@ -655,7 +659,7 @@ asocket*  host_service_to_socket(const char*  name, const char *serial)
         int fd = create_service_thread(wait_for_state, sinfo);
         return create_local_socket(fd);
     } else if (!strncmp(name, "connect:", 8)) {
-        const char *host = name + 8;
+        const char *host = strdup(name + 8);
         int fd = create_service_thread(connect_service, (void *)host);
         return create_local_socket(fd);
     }
