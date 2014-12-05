26522f05038476e6d8e15946be6d43efbec60ca6
Yongqin Liu <yongqin.liu@linaro.org>
2014-12-05 13:45:02 +0800

    bootchart: fix bootchart can not be triggered problem
    bootchart use the file in data partition to decide
    if it should start collect data for bootchart.
    but the data partition will be mounted by the mount_all
    command in the "on fs" section, and it will be only added
    into the action queue when command "trigger fs" is execute,
    and it is after the position of bootchart_init action which is
    added into the action queue just after late_init section.
    This causes the bootchart will not be started since the start flag
    file is not there before it's partition is mounted.
    
    This change makes bootchart_init a builtin command of init,
    and make it executed as the first command of "on post-fs" section
    which will be trigger after "on fs" section. This will let bootchart
    can collect as much data as possible.
    
    Change-Id: Ia74aa34ca5b785f51fcffdd383075a549b2a99d9
    Signed-off-by: Yongqin Liu <yongqin.liu@linaro.org>

--- init/init.c
+++ init/init.c
@@ -64,7 +64,6 @@ struct selabel_handle *sehandle_prop;
 static int property_triggers_enabled = 0;
 
 #if BOOTCHART
-static int   bootchart_count;
 static long long bootchart_time = 0;
 #endif
 
@@ -842,21 +841,6 @@ static int queue_property_triggers_action(int nargs, char **args)
     return 0;
 }
 
-#if BOOTCHART
-static int bootchart_init_action(int nargs, char **args)
-{
-    bootchart_count = bootchart_init();
-    if (bootchart_count < 0) {
-        ERROR("bootcharting init failure\n");
-    } else if (bootchart_count > 0) {
-        NOTICE("bootcharting started (period=%d ms)\n", bootchart_count*BOOTCHART_POLLING_MS);
-    } else {
-        NOTICE("bootcharting ignored\n");
-    }
-
-    return 0;
-}
-#endif
 
 static const struct selinux_opt seopts_prop[] = {
         { SELABEL_OPT_PATH, "/property_contexts" },
@@ -1098,11 +1082,6 @@ int main(int argc, char **argv)
 
     /* run all property triggers based on current state of the properties */
     queue_builtin_action(queue_property_triggers_action, "queue_property_triggers");
-
-
-#if BOOTCHART
-    queue_builtin_action(bootchart_init_action, "bootchart_init");
-#endif
 
     for(;;) {
         int nr, i, timeout = -1;
