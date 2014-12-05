2f2ed911225b0cfe077584dad355bb6c12e9b35d
Yongqin Liu <yongqin.liu@linaro.org>
2014-12-05 13:45:02 +0800

    bootchart: fix bootchart can not be triggered problem
    bootchart use the file in data partition to decide
    if it should start collect data for bootchart.
    but the data partition will be mounted by the mount_all
    command in the "on fs" section, and it will be only added
    into the action queue when command "trigger fs" is execute,
    and it is after the position of bootchart_init action which is
    added into the action queue just after late_init section.
    This causes the bootchart will not be started since the start flag
    file is not there before it's partition is mounted.
    
    This change makes bootchart_init a builtin command of init,
    and make it executed as the first command of "on post-fs" section
    which will be trigger after "on fs" section. This will let bootchart
    can collect as much data as possible.
    
    Change-Id: Ia74aa34ca5b785f51fcffdd383075a549b2a99d9
    Signed-off-by: Yongqin Liu <yongqin.liu@linaro.org>

--- init/init_parser.c
+++ init/init_parser.c
@@ -76,8 +76,12 @@ static struct {
 static int lookup_keyword(const char *s)
 {
     switch (*s++) {
+#if BOOTCHART
+    case 'b':
+        if (!strcmp(s, "ootchart_init")) return K_bootchart_init;
+#endif
     case 'c':
-    if (!strcmp(s, "opy")) return K_copy;
+        if (!strcmp(s, "opy")) return K_copy;
         if (!strcmp(s, "apability")) return K_capability;
         if (!strcmp(s, "hdir")) return K_chdir;
         if (!strcmp(s, "hroot")) return K_chroot;
