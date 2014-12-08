98f7cf29bf1de63ec2803158221d2a5fee43b50e
William Roberts <w2.roberts@samsung.com>
2014-12-08 08:04:27 -0800

    chcon: support -h Symlink labeling support
    Add the -h option to chcon. This is inline with the man page for
    chcon.
    
    Change-Id: Id60b240028d68bc727530d138f119606cb30fd47

--- toolbox/chcon.c
+++ toolbox/chcon.c
@@ -1,23 +1,53 @@
+#include <ctype.h>
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
 #include <selinux/selinux.h>
 
+typedef int (*pfnsetfilecon)(const char *path, const char *con);
+
+static void usage(const char *prog_name)
+{
+    fprintf(stderr, "usage:  %s [ -h Label symbolic links ] context path...\n",
+                   prog_name);
+}
+
 int chcon_main(int argc, char **argv)
 {
-    int rc, i;
+    int rc, i, c;
 
-    if (argc < 3) {
-        fprintf(stderr, "usage:  %s context path...\n", argv[0]);
+    pfnsetfilecon setfcon = setfilecon;
+
+    while ((c=getopt(argc, argv, "h")) != -1) {
+        switch (c) {
+        case 'h':
+            setfcon = lsetfilecon;
+        break;
+        case '?':
+            if (isprint(optopt))
+                fprintf(stderr, "Unknown option `-%c'.\n", optopt);
+            else
+                fprintf(stderr,
+                   "Unknown option character `\\x%x'.\n",
+                   optopt);
+            /* intentionally fall through to default */
+        default:
+            usage(argv[0]);
+            exit(1);
+        }
+    }
+
+    if (argc - optind < 2) {
+        usage(argv[0]);
         exit(1);
     }
 
-    for (i = 2; i < argc; i++) {
-        rc = setfilecon(argv[i], argv[1]);
+    for (i = optind + 1; i < argc; i++) {
+        rc = setfcon(argv[i], argv[optind]);
         if (rc < 0) {
             fprintf(stderr, "%s:  Could not label %s with %s:  %s\n",
-                    argv[0], argv[i], argv[1], strerror(errno));
+                    argv[0], argv[i], argv[optind], strerror(errno));
             exit(2);
         }
     }
