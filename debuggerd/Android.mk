a637eac5765ac4abe7d1a4ada9af26a6212b198e
Christopher Ferris <cferris@google.com>
2015-01-15 14:47:36 -0800

    Move 32 bit/64 bit check into debuggerd.
    On 64 bit systems, calls to dump_backtrace_to_file or dump_tombstone
    try and directly contact the correct debuggerd (32 bit vs 64 bit)
    by reading the elf information for the executable.
    Unfortunately, system_server makes a call to dump_backtrace_to_file
    and it doesn't have permissions to read the executable data, so it
    defaults to always contacting the 64 bit debuggerd.
    This CL changes the code so that all dump requests go to the 64 bit
    debuggerd, which reads the elf information and redirects requests for
    32 bit processes to the 32 bit debuggerd.
    
    Testing:
    
    - Forced the watchdog code in system_server to dump stacks and
      verified that all native stacks are dumped correctly.
    - Verified that dumpstate and bugreport still properly dump the native
      processes on a 64 bit and 32 bit system.
    - Intentionally forced the 64 bit to 32 bit redirect to write only a
      byte at a time and verified there are no errors, and no dropped data.
    - Used debuggerd and debuggerd64 to dump 32 bit and 64 bit processes
      seemlessly.
    - Used debuggerd on a 32 bit system to dump native stacks.
    
    Bug: https://code.google.com/p/android/issues/detail?id=97024
    Change-Id: Ie01945153bdc1c4ded696c7334b61d58575314d1

--- debuggerd/Android.mk
+++ debuggerd/Android.mk
@@ -22,6 +22,10 @@ LOCAL_CPPFLAGS := \
     -Wunused \
     -Werror \
 
+ifeq ($(TARGET_IS_64_BIT),true)
+LOCAL_CPPFLAGS += -DDEBUGGERD_64BIT
+endif
+
 LOCAL_SHARED_LIBRARIES := \
     libbacktrace \
     libcutils \
