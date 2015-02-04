aa7c282fa6166697ec1d0aedceecc4e64f764ee9
Mark Salyzyn <salyzyn@google.com>
2015-02-04 12:39:57 -0800

    liblog: Add android_set_log_frontend
    android_set_log_frontend allows one to select LOGGER_NORMAL or
    LOGGER_NULL which allows a runtime mechanism to turn logging on
    or off.
    
    Test: gTest liblog-unit-tests notably liblog.android_set_log_frontend
          and liblog-benchmarks before and after.
    Bug: 27405083
    Change-Id: I365dbee9f7d83112da0ef8d1cace748d247773c8

---
+++ liblog/include/log/log_frontend.h
@@ -0,0 +1,32 @@
+/*
+**
+** Copyright 2017, The Android Open Source Project
+**
+** This file is dual licensed.  It may be redistributed and/or modified
+** under the terms of the Apache 2.0 License OR version 2 of the GNU
+** General Public License.
+*/
+
+#ifndef _LIBS_LOG_FRONTEND_H
+#define _LIBS_LOG_FRONTEND_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*
+ * Logging frontends, bit mask to select features. Function returns selection.
+ */
+#define LOGGER_NORMAL 0x0
+#define LOGGER_KERNEL 0x1 /* Reserved/Deprecated */
+#define LOGGER_NULL   0x2 /* Does not release resources of other selections */
+
+/* both return the selected frontend flag mask, or a -errno */
+int android_set_log_frontend(unsigned frontend_flag);
+int android_get_log_frontend();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _LIBS_LOG_FRONTEND_H */
