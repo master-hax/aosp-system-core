1ff7d62810e47b494beef2210ebe172ee1940413
Christopher Ferris <cferris@google.com>
2015-02-27 13:39:47 -0800

    Print the build id of shared libraries if present.
    For every map that has a name, and if it's a mapped in shared library,
    print the build id.
    
    Also, refactor the way dump_all_maps logs data.
    
    Bug: 19371018
    Change-Id: Ic08d05a4b13f128925743936fb84d8059f7cb56f

---
+++ debuggerd/Elf.h
@@ -0,0 +1,57 @@
+/*
+ * Copyright (C) 2015 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef _DEBUGGERD_ELF_H
+#define _DEBUGGERD_ELF_H
+
+#include <stdint.h>
+#include <string>
+
+class Backtrace;
+
+class Elf {
+public:
+  Elf(Backtrace* backtrace, uintptr_t start_addr, uintptr_t end_addr)
+      : backtrace_(backtrace), start_addr_(start_addr), end_addr_(end_addr) {}
+  Elf(const Elf& elf)
+      : backtrace_(elf.backtrace_), start_addr_(elf.start_addr_), end_addr_(elf.end_addr_) {}
+  virtual ~Elf() {}
+
+  bool Read(uintptr_t, uint8_t*, size_t);
+
+  virtual bool GetBuildId(std::string*) { return false; }
+
+protected:
+  Backtrace* backtrace_;
+  uintptr_t start_addr_;
+  uintptr_t end_addr_;
+};
+
+template <typename HdrType, typename PhdrType, typename NhdrType>
+class ElfT : public Elf {
+public:
+  ElfT(const Elf&, uint8_t*);
+  virtual ~ElfT() {}
+
+  bool GetBuildId(std::string*) override;
+
+private:
+  HdrType hdr_;
+};
+
+bool ElfGetBuildId(Backtrace*, uintptr_t, uintptr_t, std::string*);
+
+#endif // _DEBUGGERD_ELF_H
