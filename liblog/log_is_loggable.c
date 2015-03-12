48b901abfc8ff1c16c95146d68b5fbaf0c521724
Mark Salyzyn <salyzyn@google.com>
2015-03-12 15:46:29 -0700

    liblog: __android_log_is_loggable support global properties
    - Add support for "log.tag" and "persist.log.tag" global
      logging properties, effectively a runtime default
    - Add a single level cache plus selective logic for the four
      properties being checked to help speed up logging decision.
    - A NULL or blank tag is no longer directed to return default,
      it will check the pair of global logging properties first.
    
    Bug: 19544788
    Change-Id: Ideb887755aa3f1fd14a2603bda1fe23cba49642c

--- liblog/log_is_loggable.c
+++ liblog/log_is_loggable.c
@@ -15,33 +15,90 @@
 */
 
 #include <ctype.h>
+#include <stdlib.h>
 #include <string.h>
-#include <sys/system_properties.h>
+#define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
+#include <sys/_system_properties.h>
 
 #include <android/log.h>
 
 static int __android_log_level(const char *tag, int def)
 {
-    char buf[PROP_VALUE_MAX];
+    static const char log_namespace[] = "persist.log.tag.";
+    const size_t taglen = (tag && *tag) ? strlen(tag) : 0;
+    char key[sizeof(log_namespace) + taglen];
+    static char *last_tag;
+    size_t i;
+    char c = 0;
+    /*
+     * Single cache of four properties. Priorities are:
+     *    log.tag.<tag>
+     *    persist.log.tag.<tag>
+     *    log.tag
+     *    persist.log.tag
+     * Where the missing tag matches all tags and becomes the
+     * system global default.
+     *
+     * The cache only speeds up reading an existing entry and
+     * subsequent tracking of changes. ToDo: skip N searches
+     * if not found to gain some speedup by being sloppy?
+     */
+    static struct {
+        const prop_info *pinfo;
+        uint32_t serial;
+        char c;
+    } cache[4] = {
+        { NULL, -1, 0 },
+        { NULL, -1, 0 },
+        { NULL, -1, 0 },
+        { NULL, -1, 0 }
+    };
 
-    if (!tag || !*tag) {
-        return def;
-    }
-    {
-        static const char log_namespace[] = "persist.log.tag.";
-        char key[sizeof(log_namespace) + strlen(tag)];
+    strcpy(key, log_namespace);
 
-        strcpy(key, log_namespace);
+    if (taglen) {
+        if (!last_tag || strcmp(last_tag, tag)) {
+            /* invalidate log.tag.<tag> cache */
+            for(i = 0; i < 2; ++i) {
+                cache[i].pinfo = NULL;
+                cache[i].serial = -1;
+                cache[i].c = '\0';
+            }
+            free(last_tag);
+            last_tag = NULL;
+        }
+        if (!last_tag) {
+            last_tag = strdup(tag);
+        }
         strcpy(key + sizeof(log_namespace) - 1, tag);
+    }
 
-        if (__system_property_get(key + 8, buf) <= 0) {
-            buf[0] = '\0';
+    for(i = taglen ? 0 : 2; i < (sizeof(cache) / sizeof(cache[0])); ++i) {
+        char buf[PROP_VALUE_MAX];
+
+        if (i == 2) {
+            /* clear '.' after log.tag */
+            key[sizeof(log_namespace) - 2] = '\0';
         }
-        if (!buf[0] && __system_property_get(key, buf) <= 0) {
-            buf[0] = '\0';
+
+        if (!cache[i].pinfo) {
+            cache[i].pinfo = __system_property_find((i & 1) ? key : (key + 8));
+        }
+        if (cache[i].pinfo) {
+            uint32_t serial = __system_property_serial(cache[i].pinfo);
+            if (serial != cache[i].serial) {
+                cache[i].serial = serial;
+                __system_property_read(cache[i].pinfo, 0, buf);
+                cache[i].c = buf[0];
+            }
+        }
+        if (cache[i].c) {
+            c = cache[i].c;
+            break;
         }
     }
-    switch (toupper(buf[0])) {
+
+    switch (toupper(c)) {
         case 'V': return ANDROID_LOG_VERBOSE;
         case 'D': return ANDROID_LOG_DEBUG;
         case 'I': return ANDROID_LOG_INFO;
