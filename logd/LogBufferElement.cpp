1aec5b78f03cbdd3c82abf47d4d88c3b1b6b586c
Mark Salyzyn <salyzyn@google.com>
2015-03-16 12:04:09 -0700

    logd: annotate worst-UID pruned entries
    - internal dropped entries are associated by prune by worst UID
      and are applied by UID and by PID
    - track dropped entries by rewriting them in place
    - merge similar dropped entries together for same UID(implied),
      PID and TID so that blame can more clearly be placed
    - allow aging of dropped entries by the general backgound pruning
    - report individual dropped entries formatted to reader
    - add statistics to track dropped entries by UID, the combination
      of statistics and dropped logging can track over-the-top Chattiest
      clients.
    
    Bug: 19608965
    Change-Id: Ibc68480df0c69c55703270cd70c6b26aea165853

--- logd/LogBufferElement.cpp
+++ logd/LogBufferElement.cpp
@@ -20,8 +20,10 @@
 #include <unistd.h>
 
 #include <log/logger.h>
+#include <private/android_logger.h>
 
 #include "LogBufferElement.h"
+#include "LogCommand.h"
 #include "LogReader.h"
 
 const uint64_t LogBufferElement::FLUSH_ERROR(0);
@@ -47,23 +49,78 @@ LogBufferElement::~LogBufferElement() {
 
 uint64_t LogBufferElement::flushTo(SocketClient *reader) {
     struct logger_entry_v3 entry;
+    struct iovec iovec[2];
+    char *buffer = NULL;
+    uint64_t retval;
+
     memset(&entry, 0, sizeof(struct logger_entry_v3));
+
+    iovec[0].iov_base = &entry;
+    iovec[0].iov_len = sizeof(struct logger_entry_v3);
+
     entry.hdr_size = sizeof(struct logger_entry_v3);
-    entry.len = mMsgLen;
     entry.lid = mLogId;
     entry.pid = mPid;
     entry.tid = mTid;
     entry.sec = mRealTime.tv_sec;
     entry.nsec = mRealTime.tv_nsec;
 
-    struct iovec iovec[2];
-    iovec[0].iov_base = &entry;
-    iovec[0].iov_len = sizeof(struct logger_entry_v3);
-    iovec[1].iov_base = mMsg;
-    iovec[1].iov_len = mMsgLen;
-    if (reader->sendDatav(iovec, 2)) {
-        return FLUSH_ERROR;
+    if (!mMsg) {
+        static const char format_uid[] = "uid=%u dropped=%u";
+#       define format (format_uid + 7)
+        static const char tag[] = "logd";
+
+        bool log = clientHasLogCredentials(reader);
+        size_t len;
+        if (log) {
+            len = snprintf(NULL, 0, format_uid, mUid, mMsgLen);
+        } else {
+            len = snprintf(NULL, 0, format, mMsgLen);
+        }
+
+        size_t hdrLen;
+        if (mLogId == LOG_ID_EVENTS) {
+            hdrLen = sizeof(android_log_event_string_t);
+        } else {
+            hdrLen = 1 + sizeof(tag);
+        }
+
+        buffer = static_cast<char *>(calloc(1, hdrLen + len + 1));
+        if (!buffer) {
+            return mSequence;
+        }
+
+        iovec[1].iov_base = buffer;
+
+        entry.len = hdrLen + len;
+        if (mLogId == LOG_ID_EVENTS) {
+            android_log_event_string_t *e = reinterpret_cast<android_log_event_string_t *>(buffer);
+
+            e->header.tag = htole32(LOGD_LOG_TAG);
+            e->type = EVENT_TYPE_STRING;
+            e->length = htole32(len);
+        } else {
+            ++entry.len;
+            buffer[0] = ANDROID_LOG_INFO;
+            strcpy(buffer + 1, tag);
+        }
+        if (log) {
+            snprintf(buffer + hdrLen, len + 1, format_uid, mUid, mMsgLen);
+        } else {
+            snprintf(buffer + hdrLen, len + 1, format, mMsgLen);
+        }
+#       undef format
+    } else {
+        iovec[1].iov_base = mMsg;
+        entry.len = mMsgLen;
+    }
+    iovec[1].iov_len = entry.len;
+
+    retval = reader->sendDatav(iovec, 2) ? FLUSH_ERROR : mSequence;
+
+    if (buffer) {
+        free(buffer);
     }
 
-    return mSequence;
+    return retval;
 }
