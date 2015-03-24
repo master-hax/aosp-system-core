8e7493615dc4c0962c143b56ddaf4d3aec95eb26
Andreas Gampe <agampe@google.com>
2015-03-23 21:46:55 -0700

    Libcutils: Partial mac build fix
    Change-Id: I2afd109fc8b45dc3a9b909a6fb2e45321b22d438

--- libcutils/threads.c
+++ libcutils/threads.c
@@ -21,6 +21,7 @@
 // For gettid.
 #if defined(__APPLE__)
 #include "AvailabilityMacros.h"  // For MAC_OS_X_VERSION_MAX_ALLOWED
+#include <stdint.h>
 #include <sys/syscall.h>
 #include <sys/time.h>
 #include <unistd.h>
