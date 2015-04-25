1122d3d00ea5cb6de91c634ab241650e389afa3e
Elliott Hughes <enh@google.com>
2015-04-25 11:49:04 -0700

    Improve toolbox SIGPIPE behavior.
    None of our tools -- except for top, which I'd fixed previously --
    handles SIGPIE correctly. Let's just handle SIGPIPE in the driver.
    
    Bug: https://code.google.com/p/android/issues/detail?id=157920
    Change-Id: I322ea411f53c71585a64118c217d54389f675d4e

--- toolbox/toolbox.c
+++ toolbox/toolbox.c
@@ -1,6 +1,8 @@
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 int main(int, char **);
 
@@ -31,10 +33,20 @@ static struct
     { 0, 0 },
 };
 
+static void SIGPIPE_handler(int signal) {
+    _exit(0);
+}
+
 int main(int argc, char **argv)
 {
     int i;
     char *name = argv[0];
+
+    // Let's assume that none of this code handles broken pipes. At least ls,
+    // ps, and top were broken (though I'd previously added this fix locally
+    // to top). We exit rather than use SIG_IGN because tools like top will
+    // just keep on writing to nowhere forever if we don't stop them.
+    signal(SIGPIPE, SIGPIPE_handler);
 
     if((argc > 1) && (argv[1][0] == '@')) {
         name = argv[1] + 1;
