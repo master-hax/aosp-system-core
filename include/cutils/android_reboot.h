e4ea68a20661167bfe2227091f9f481d0fca9b6c
Yusuke Sato <yusukes@google.com>
2015-07-08 14:57:07 -0700

    Use fsck.f2fs -a instead of -f for faster boot
    and run fsck with -f on clean shutdown instead.
    
    With -f, fsck.f2fs always performs a full scan of the /data
    partition regardless of whether the partition is clean or not.
    The full scan takes more than 2 seconds on volantis-userdebug
    and delays the OS boot.
    
    With -a, the command does almost nothing when the partition
    is clean and finishes within 20-30ms on volantis-userdebug.
    When the partition has an error or its check point has
    CP_FSCK_FLAG (aka "need_fsck"), the command does exactly the
    same full scan as -f to fix it.
    
    Bug: 21853106
    Change-Id: I126263caf34c0f5bb8f5e6794454d4e72526ce38

--- include/cutils/android_reboot.h
+++ include/cutils/android_reboot.h
@@ -28,6 +28,9 @@ __BEGIN_DECLS
 #define ANDROID_RB_PROPERTY "sys.powerctl"
 
 int android_reboot(int cmd, int flags, const char *arg);
+int android_reboot_with_callback(
+    int cmd, int flags, const char *arg,
+    void (*cb_on_remount)(const struct mntent*));
 
 __END_DECLS
 
