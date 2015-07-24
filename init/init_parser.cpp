88d984e5434f06d3ea45e3990be7b871d042226a
Lee Campbell <leecam@google.com>
2015-07-24 16:57:14 -0700

    init: Adding support to import directories
    Support added so init scripts can now import directories.
    
    BUG: 22721249
    Change-Id: I02b566bfb50ea84469f1ea0c6ad205435a1df286
    TEST: Tested importing a folder on arm64 emulator

--- init/init_parser.cpp
+++ init/init_parser.cpp
@@ -15,6 +15,7 @@
  */
 
 #include <ctype.h>
+#include <dirent.h>
 #include <errno.h>
 #include <fcntl.h>
 #include <inttypes.h>
@@ -32,6 +33,7 @@
 #include "property_service.h"
 #include "util.h"
 
+#include <base/stringprintf.h>
 #include <cutils/iosched_policy.h>
 #include <cutils/list.h>
 
@@ -426,15 +428,15 @@ static void parse_config(const char *fn, const std::string& data)
 parser_done:
     list_for_each(node, &import_list) {
          struct import* import = node_to_item(node, struct import, list);
-         if (!init_parse_config_file(import->filename)) {
+         if (!init_parse_config(import->filename)) {
              ERROR("could not import file '%s' from '%s': %s\n",
                    import->filename, fn, strerror(errno));
          }
     }
 }
 
-bool init_parse_config_file(const char* path) {
-    INFO("Parsing %s...\n", path);
+static bool init_parse_config_file(const char* path) {
+    INFO("Parsing file %s...\n", path);
     Timer t;
     std::string data;
     if (!read_file(path, &data)) {
@@ -447,6 +449,33 @@ bool init_parse_config_file(const char* path) {
 
     NOTICE("(Parsing %s took %.2fs.)\n", path, t.duration());
     return true;
+}
+
+static bool init_parse_config_dir(const char* path) {
+    INFO("Parsing directory %s...\n", path);
+    DIR *config_dir = opendir(path);
+    if (!config_dir)
+        return false;
+    struct dirent *current_file;
+    struct dirent entry;
+    while ((readdir_r(config_dir, &entry, &current_file) == 0) && current_file) {
+        std::string current_path =
+            android::base::StringPrintf("%s/%s", path, current_file->d_name);
+        // Ignore directories.
+        if(!is_dir(current_path.c_str())) {
+            if (!init_parse_config_file(current_path.c_str())) {
+                ERROR("could not import file '%s'\n", current_path.c_str());
+            }
+        }
+    }
+    closedir(config_dir);
+    return true;
+}
+
+bool init_parse_config(const char* path) {
+    if (is_dir(path))
+        return init_parse_config_dir(path);
+    return init_parse_config_file(path);
 }
 
 static int valid_name(const char *name)
