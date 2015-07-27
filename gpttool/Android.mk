278c098c2253cd97f458c4eddabb536d85934575
Andreas Gampe <agampe@google.com>
2015-07-27 15:32:29 -0700

    GPTTool: Ignore unused-result warning
    For build-system CFLAGS clean-up.
    
    Bug: 18632512
    Change-Id: I4809f5e166cb00c7147731b9ccbda40dc648b162

--- gpttool/Android.mk
+++ gpttool/Android.mk
@@ -7,6 +7,9 @@ LOCAL_SRC_FILES := gpttool.c
 LOCAL_STATIC_LIBRARIES := libz
 LOCAL_CFLAGS := -Werror
 
+# The code is ignoring read() results.
+LOCAL_CFLAGS += -Wno-unused-result
+
 LOCAL_MODULE := gpttool
 
 include $(BUILD_HOST_EXECUTABLE)
