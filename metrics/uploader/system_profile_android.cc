66ba985cf8784195f53e463612a264896490b3d8
Bertrand SIMONNET <bsimonnet@google.com>
2015-08-04 14:12:32 -0700

    metrics: Allow some metadata to be missing.
    Metrics can be collected by development system (for testing). Allow the
    version field to not be defined and tag the metrics with a 0.0.0.0
    version string. If the channel is not set either, use the unknown
    channel.
    
    BUG: 22879597
    
    Change-Id: I23dd14581fd4741713f272d60c607304ce7bb94f

--- metrics/uploader/system_profile_android.cc
+++ metrics/uploader/system_profile_android.cc
@@ -61,15 +61,16 @@ bool SystemProfileAndroid::Initialize() {
   CHECK(!initialized_)
       << "this should be called only once in the metrics_daemon lifetime.";
 
-  std::string channel;
-  if (!base::SysInfo::GetLsbReleaseValue("BRILLO_CHANNEL", &channel) ||
-      !base::SysInfo::GetLsbReleaseValue("BRILLO_VERSION", &profile_.version) ||
-      !base::SysInfo::GetLsbReleaseValue("BRILLO_BUILD_TARGET_ID",
+  if (!base::SysInfo::GetLsbReleaseValue("BRILLO_BUILD_TARGET_ID",
                                          &profile_.build_target_id)) {
     LOG(ERROR) << "Could not initialize system profile.";
     return false;
   }
 
+  std::string channel = "";
+  profile_.version = metrics::kDefaultVersion;
+  base::SysInfo::GetLsbReleaseValue("BRILLO_CHANNEL", &channel);
+  base::SysInfo::GetLsbReleaseValue("BRILLO_VERSION", &profile_.version);
   profile_.client_id =
       testing_ ? "client_id_test" :
       GetPersistentGUID(metrics::kMetricsGUIDFilePath);
