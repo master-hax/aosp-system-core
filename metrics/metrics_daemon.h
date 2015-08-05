99e8b301c7f5d196f3eeca008ce1fb06aa60d6d7
Bertrand SIMONNET <bsimonnet@google.com>
2015-08-05 16:04:14 -0700

    metrics: Add a CLI flag to disable dbus.
    DBus is not setup in most target and the metrics_daemon will crash if it
    cannot connect to DBus.
    Running metrics daemon without dbus allows us to test the daemon in any
    target.
    
    BUG: 22879597
    
    Change-Id: Ica69c8e96c3df794a210a0fa3d44f85a98a660f0

--- metrics/metrics_daemon.h
+++ metrics/metrics_daemon.h
@@ -31,6 +31,7 @@ class MetricsDaemon : public chromeos::DBusDaemon {
   // Initializes metrics class variables.
   void Init(bool testing,
             bool uploader_active,
+            bool dbus_enabled,
             MetricsLibraryInterface* metrics_lib,
             const std::string& diskstats_path,
             const std::string& vmstats_path,
@@ -289,6 +290,10 @@ class MetricsDaemon : public chromeos::DBusDaemon {
 
   // Whether the uploader is enabled or disabled.
   bool uploader_active_;
+
+  // Whether or not dbus should be used.
+  // If disabled, we will not collect the frequency of crashes.
+  bool dbus_enabled_;
 
   // Root of the configuration files to use.
   std::string config_root_;
