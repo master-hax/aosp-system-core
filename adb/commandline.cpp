aosp-mirror/platform_system_core

commit 28416d6d33267f92768116e269966d61eac01db9

Fix adb shell escape handling.
Start of input is, by definition, the start of a line. Give the states
names to make the error clearer. We should also swallow the escape
character until/unless it's proven not to have been part of an escape
sequence. Finally, implement an ssh(1)-like -e option so the user can
choose -- or, more importantly, disable -- escape character handling.

Change-Id: I28a91db47e15f3e7de96303c0b9a98d302b9c92e

adb/commandline.cpp
@@ -467,6 +467,7 @@ struct StdinReadArgs {
     int stdin_fd, write_fd;
     bool raw_stdin;
     std::unique_ptr<ShellProtocol> protocol;
+    char escape_char;
 };
 
 // Loops to read from stdin and push the data to the given FD.
@@ -474,7 +475,6 @@ struct StdinReadArgs {
 // will take ownership of the object and delete it when finished.
 static void* stdin_read_thread_loop(void* x) {
     std::unique_ptr<StdinReadArgs> args(reinterpret_cast<StdinReadArgs*>(x));
-    int state = 0;
 
 #if !defined(_WIN32)
     // Mask SIGTTIN in case we're in a backgrounded process.
@@ -496,13 +496,21 @@ static void* stdin_read_thread_loop(void* x) {
     // Set up the initial window size.
     send_window_size_change(args->stdin_fd, args->protocol);
 
-    char raw_buffer[1024];
+    char raw_buffer[BUFSIZ];
     char* buffer_ptr = raw_buffer;
     size_t buffer_size = sizeof(raw_buffer);
     if (args->protocol != nullptr) {
         buffer_ptr = args->protocol->data();
         buffer_size = args->protocol->data_capacity();
     }
+
+    // If we need to parse escape sequences, make life easy.
+    if (args->raw_stdin && args->escape_char != '\0') {
+        buffer_size = 1;
+    }
+
+    enum EscapeState { kMidFlow, kStartOfLine, kInEscape };
+    EscapeState state = kStartOfLine;
 
     while (true) {
         // Use unix_read() rather than adb_read() for stdin.
@@ -529,36 +537,36 @@ static void* stdin_read_thread_loop(void* x) {
             }
             break;
         }
-        // If we made stdin raw, check input for the "~." escape sequence. In
+        // If we made stdin raw, check input for escape sequences. In
         // this situation signals like Ctrl+C are sent remotely rather than
         // interpreted locally so this provides an emergency out if the remote
         // process starts ignoring the signal. SSH also does this, see the
         // "escape characters" section on the ssh man page for more info.
-        if (args->raw_stdin) {
-            for (int n = 0; n < r; n++) {
-                switch (buffer_ptr[n]) {
-                case '\n':
-                    state = 1;
-                    break;
-                case '\r':
-                    state = 1;
-                    break;
-                case '~':
-                    if (state == 1) {
-                        state++;
-                    } else {
-                        state = 0;
-                    }
-                    break;
-                case '.':
-                    if (state == 2) {
-                        fprintf(stderr,"\r\n* disconnect *\r\n");
+        if (args->raw_stdin && args->escape_char != '\0') {
+            char ch = buffer_ptr[0];
+            if (ch == args->escape_char) {
+                if (state == kStartOfLine) {
+                    state = kInEscape;
+                    // Swallow the escape character.
+                    continue;
+                } else {
+                    state = kMidFlow;
+                }
+            } else {
+                if (state == kInEscape) {
+                    if (ch == '.') {
+                        fprintf(stderr,"\r\n[ disconnected ]\r\n");
                         stdin_raw_restore();
                         exit(0);
+                    } else {
+                        // We swallowed an escape character that wasn't part of
+                        // a valid escape sequence; time to cough it up.
+                        buffer_ptr[0] = args->escape_char;
+                        buffer_ptr[1] = ch;
+                        ++r;
                     }
-                default:
-                    state = 0;
                 }
+                state = (ch == '\n' || ch == '\r') ? kStartOfLine : kMidFlow;
             }
         }
         if (args->protocol) {
@@ -603,6 +611,7 @@ static std::string ShellServiceString(bool use_shell_protocol,
 // On success returns the remote exit code if |use_shell_protocol| is true,
 // 0 otherwise. On failure returns 1.
 static int RemoteShell(bool use_shell_protocol, const std::string& type_arg,
+                       char escape_char,
                        const std::string& command) {
     std::string service_string = ShellServiceString(use_shell_protocol,
                                                     type_arg, command);
@@ -628,6 +637,7 @@ static int RemoteShell(bool use_shell_protocol, const std::string& type_arg,
     args->stdin_fd = STDIN_FILENO;
     args->write_fd = fd;
     args->raw_stdin = raw_stdin;
+    args->escape_char = escape_char;
     if (use_shell_protocol) {
         args->protocol.reset(new ShellProtocol(args->write_fd));
     }
@@ -689,8 +699,17 @@ static int adb_shell(int argc, const char** argv,
     --argc;
     ++argv;
     int t_arg_count = 0;
+    char escape_char = '~';
     while (argc) {
-        if (!strcmp(argv[0], "-T") || !strcmp(argv[0], "-t")) {
+        if (!strcmp(argv[0], "-e")) {
+            if (argc < 2 || !(strlen(argv[1]) == 1 || strcmp(argv[1], "none") == 0)) {
+                fprintf(stderr, "error: -e requires a single-character argument or 'none'\n");
+                return 1;
+            }
+            escape_char = (strcmp(argv[1], "none") == 0) ? 0 : argv[1][0];
+            argc -= 2;
+            argv += 2;
+        } else if (!strcmp(argv[0], "-T") || !strcmp(argv[0], "-t")) {
             if (!CanUseFeature(features, kFeatureShell2)) {
                 fprintf(stderr, "error: target doesn't support PTY args -Tt\n");
                 return 1;
@@ -746,7 +765,7 @@ static int adb_shell(int argc, const char** argv,
         command = android::base::Join(std::vector<const char*>(argv, argv + argc), ' ');
     }
 
-    return RemoteShell(use_shell_protocol, shell_type_arg, command);
+    return RemoteShell(use_shell_protocol, shell_type_arg, escape_char, command);
 }
 
 static int adb_download_buffer(const char *service, const char *fn, const void* data, unsigned sz,

master-hax/aosp-system-core

commit 716992a21bdbc45e977640901acd90f7e6c106ef

adb: win32: fix adb shell
28416d6d33267f92768116e269966d61eac01db9 broke adb shell on Windows
because it was calling unix_read() with a buffer size of 1 byte which
isn't supported by unix_read()/_console_read().

This change switches back to bigger reads by removing and inserting
characters from the bigger buffer when processing it.

Also modify _console_read() to CHECK() if the caller passes a very small
buffer size to detect this sort of issue faster in the future.

Change-Id: I8f0aed4fb9f6f2f5b9a6b68ce60d2e368fec81c7
Signed-off-by: Spencer Low <CompareAndSwap@gmail.com>

adb/commandline.cpp
@@ -505,10 +505,11 @@ static void* stdin_read_thread_loop(void* x) {
         buffer_size = args->protocol->data_capacity();
     }
 
-    // If we need to parse escape sequences, make life easy.
-    if (args->raw_stdin && args->escape_char != '\0') {
-        buffer_size = 1;
-    }
+    // If we need to parse escape sequences, and the previous read put us in the
+    // kInEscape state, we need one extra byte to write the escape character
+    // that we swallowed before.
+    const size_t read_size = (args->raw_stdin && args->escape_char != '\0') ?
+            (buffer_size - 1) : buffer_size;
 
     enum EscapeState { kMidFlow, kStartOfLine, kInEscape };
     EscapeState state = kStartOfLine;
@@ -518,14 +519,14 @@ static void* stdin_read_thread_loop(void* x) {
         D("stdin_read_thread_loop(): pre unix_read(fdi=%d,...)", args->stdin_fd);
 #if !defined(_WIN32)
 #undef read
-        int r = read(args->stdin_fd, buffer_ptr, buffer_size);
+        int r = read(args->stdin_fd, buffer_ptr, read_size);
         if (r == -1 && errno == EINTR) {
             send_window_size_change(args->stdin_fd, args->protocol);
             continue;
         }
 #define read ___xxx_read
 #else
-        int r = unix_read(args->stdin_fd, buffer_ptr, buffer_size);
+        int r = unix_read(args->stdin_fd, buffer_ptr, read_size);
 #endif
         D("stdin_read_thread_loop(): post unix_read(fdi=%d,...)", args->stdin_fd);
         if (r <= 0) {
@@ -544,30 +545,39 @@ static void* stdin_read_thread_loop(void* x) {
         // process starts ignoring the signal. SSH also does this, see the
         // "escape characters" section on the ssh man page for more info.
         if (args->raw_stdin && args->escape_char != '\0') {
-            char ch = buffer_ptr[0];
-            if (ch == args->escape_char) {
-                if (state == kStartOfLine) {
-                    state = kInEscape;
-                    // Swallow the escape character.
-                    continue;
-                } else {
-                    state = kMidFlow;
-                }
-            } else {
-                if (state == kInEscape) {
-                    if (ch == '.') {
-                        fprintf(stderr,"\r\n[ disconnected ]\r\n");
-                        stdin_raw_restore();
-                        exit(0);
+            for (int i = 0; i < r; ++i) {
+                const char ch = buffer_ptr[i];
+                if (ch == args->escape_char) {
+                    if (state == kStartOfLine) {
+                        state = kInEscape;
+                        // Swallow the escape character.
+                        memmove(&buffer_ptr[i], &buffer_ptr[i + 1],
+                                r - (i + 1));
+                        --r;
+                        --i;    // process any char after the escape character
                     } else {
-                        // We swallowed an escape character that wasn't part of
-                        // a valid escape sequence; time to cough it up.
-                        buffer_ptr[0] = args->escape_char;
-                        buffer_ptr[1] = ch;
-                        ++r;
+                        state = kMidFlow;
                     }
+                } else {
+                    if (state == kInEscape) {
+                        if (ch == '.') {
+                            fprintf(stderr, "\r\n[ disconnected ]\r\n");
+                            stdin_raw_restore();
+                            exit(0);
+                        } else {
+                            // We swallowed an escape character that wasn't part
+                            // of a valid escape sequence; time to cough it up.
+                            CHECK_LE(r + 1, static_cast<int>(buffer_size));
+                            memmove(&buffer_ptr[i + 1], &buffer_ptr[i], r - i);
+                            buffer_ptr[i] = args->escape_char;
+                            CHECK_EQ(ch, buffer_ptr[i + 1]);
+                            ++r;
+                            ++i;    // skip char that we just processed
+                        }
+                    }
+                    state = (ch == '\n' || ch == '\r') ?
+                            kStartOfLine : kMidFlow;
                 }
-                state = (ch == '\n' || ch == '\r') ? kStartOfLine : kMidFlow;
             }
         }
         if (args->protocol) {

adb/sysdeps_win32.cpp
@@ -2950,6 +2950,11 @@ size_t _escape_prefix(char* const buf, const size_t len) {
 // -1 on error. Never returns zero because Win32 consoles are never 'closed'
 // (as far as I can tell).
 static int _console_read(const HANDLE console, void* buf, size_t len) {
+    // The longest output sequence is 7 bytes, produced by input such as
+    // Ctrl-Shift-Alt F5. Instead of maintaining an internal buffer, we just
+    // make this minimum size part of the API.
+    CHECK_GE(len, 7u) << "buffer size is too small";
+
     for (;;) {
         KEY_EVENT_RECORD* const key_event = _get_key_event_record(console);
         if (key_event == NULL) {