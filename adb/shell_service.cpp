47e11774b252e2dc7b1f736b1d2a091bd51875c9
Josh Gao <jmgao@google.com>
2016-01-19 17:31:09 -0800

    adbd: use pty to determine whether a session is interactive.
    Change-Id: I8baa4009a2fbe9a4c93f6ef5350ce61161b7237d

--- adb/shell_service.cpp
+++ adb/shell_service.cpp
@@ -183,7 +183,7 @@ class Subprocess {
     ~Subprocess();
 
     const std::string& command() const { return command_; }
-    bool is_interactive() const { return command_.empty(); }
+    bool is_interactive() const { return type_ == SubprocessType::kPty; }
 
     int local_socket_fd() const { return local_socket_sfd_.fd(); }
 
@@ -332,7 +332,7 @@ bool Subprocess::ForkAndExec() {
         parent_error_sfd.Reset();
         close_on_exec(child_error_sfd.fd());
 
-        if (is_interactive()) {
+        if (command_.empty()) {
             execle(_PATH_BSHELL, _PATH_BSHELL, "-", nullptr, cenv.data());
         } else {
             execle(_PATH_BSHELL, _PATH_BSHELL, "-c", command_.c_str(), nullptr, cenv.data());
