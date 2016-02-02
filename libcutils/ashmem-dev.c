a24eff2e47f2760743d33ed0da00f938152594bd
Mark Salyzyn <salyzyn@google.com>
2016-02-02 09:20:31 -0800

    libcutils: abort for invalid fd
    Bug: 26871259
    Change-Id: Icf9aafc3631a80b9af6fe0fea03599d61e285d21

--- libcutils/ashmem-dev.c
+++ libcutils/ashmem-dev.c
@@ -113,7 +113,7 @@ static int __ashmem_is_ashmem(int fd)
     }
 
 failed:
-    ALOGE("illegal fd=%d mode=0%o rdev=%d:%d expected 0%o %d:%d",
+    LOG_ALWAYS_FATAL("illegal fd=%d mode=0%o rdev=%d:%d expected 0%o %d:%d",
         fd, st.st_mode, major(st.st_rdev), minor(st.st_rdev),
         S_IFCHR | S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IRGRP,
         major(rdev), minor(rdev));
