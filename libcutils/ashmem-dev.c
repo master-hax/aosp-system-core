9d157bf38168c0245b03e74e6a9ae52a83ecc03d
Mark Salyzyn <salyzyn@google.com>
2016-02-02 09:19:39 -0800

    libcutils: ashmem print error message for invalid fd
    Bug: 26871259
    Change-Id: I46249838850ae32063eb5b7d08c731c5bb0fbf6b

--- libcutils/ashmem-dev.c
+++ libcutils/ashmem-dev.c
@@ -19,6 +19,7 @@
  * ashmem-enabled kernel. See ashmem-sim.c for the "fake" tmp-based version,
  * used by the simulator.
  */
+#define LOG_TAG "ashmem"
 
 #include <errno.h>
 #include <fcntl.h>
@@ -32,6 +33,7 @@
 #include <linux/ashmem.h>
 
 #include <cutils/ashmem.h>
+#include <log/log.h>
 
 #define ASHMEM_DEVICE "/dev/ashmem"
 
@@ -86,6 +88,10 @@ static int __ashmem_is_ashmem(int fd)
     }
 
     if (!S_ISCHR(st.st_mode) || !st.st_rdev) {
+        rdev = atomic_load(&__ashmem_rdev); /* suppress uninitialized use */
+        if (!rdev) {
+            rdev = makedev(10, 50); /* low cost WAG, as long as not zero */
+        }
         goto failed;
     }
 
@@ -107,6 +113,11 @@ static int __ashmem_is_ashmem(int fd)
     }
 
 failed:
+    ALOGE("illegal fd=%d mode=0%o rdev=%d:%d expected 0%o %d:%d",
+        fd, st.st_mode, major(st.st_rdev), minor(st.st_rdev),
+        S_IFCHR | S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IRGRP,
+        major(rdev), minor(rdev));
+
     ret = -ENOTTY;
 
 error:
