a6058e6b193d10a6e034682594fe8c310dc849a8
Mattias Nissler <mnissler@google.com>
2016-02-22 13:12:15 +0100

    Prepare mincrypt build for move to external/libmincrypt
    This introduces a build variable that lets us control whether mincrypt
    will build from system/core or from the new external/libmincrypt
    repository. Once both locations are prepared, we'll switch the
    variable to build mincrypt from the new location and afterwards remove
    the conditionals.
    
    BUG=27287913
    
    Change-Id: I2c45a1b5bd14cbc1c71824e606783314221b16b3

--- libmincrypt/Android.mk
+++ libmincrypt/Android.mk
@@ -2,6 +2,16 @@
 #
 LOCAL_PATH := $(call my-dir)
 
+# Determine whether to build mincrypt from system/core or from
+# external/libmincrypt. The mincrypt source is temporarily present in both
+# locations during the process of moving mincrypt to external/libmincrypt.
+# TODO(mnissler): Remove this after the transition is complete.
+ifndef MINCRYPT_STANDALONE
+MINCRYPT_STANDALONE := false
+endif
+
+ifeq ($(MINCRYPT_STANDALONE),false)
+
 include $(CLEAR_VARS)
 LOCAL_MODULE := libmincrypt
 LOCAL_SRC_FILES := dsa_sig.c p256.c p256_ec.c p256_ecdsa.c rsa.c sha.c sha256.c
@@ -15,3 +25,5 @@ LOCAL_CFLAGS := -Wall -Werror
 include $(BUILD_HOST_STATIC_LIBRARY)
 
 include $(LOCAL_PATH)/test/Android.mk
+
+endif  # MINCRYPT_STANDALONE == false
