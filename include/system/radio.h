eb60b69366df4c7833c660dbc13d6d01f954d61b
Franck Lenormand <franckx.lenormand@intel.com>
2016-02-24 15:01:22 +0100

    Add Volume buttons events support to FM framework
    Add the events for volume buttons pressing in the list of
    events supported aligned on the values used by
    RadioModule.java
    
    Change-Id: Id9753f387692288649a28206725a686aebd8ff9a
    Signed-off-by: Franck Lenormand <franckx.lenormand@intel.com>
    Signed-off-by: Jin Xue <jin.xue@intel.com>

--- include/system/radio.h
+++ include/system/radio.h
@@ -184,6 +184,8 @@ enum {
     RADIO_EVENT_METADATA    = 4,  /* New meta data received */
     RADIO_EVENT_TA          = 5,  /* Traffic announcement start or stop */
     RADIO_EVENT_AF_SWITCH   = 6,  /* Switch to Alternate Frequency */
+    RADIO_EVENT_VOL_BUT_DOWN= 7,  /* Volume Down */
+    RADIO_EVENT_VOL_BUT_UP  = 8,  /* Volume Up */
     // begin framework only events
     RADIO_EVENT_CONTROL     = 100, /* loss/gain of tuner control */
     RADIO_EVENT_SERVER_DIED = 101, /* radio service died */
@@ -195,7 +197,8 @@ typedef struct radio_hal_event {
     radio_event_type_t  type;       /* event type */
     int                 status;     /* used by RADIO_EVENT_CONFIG, RADIO_EVENT_TUNED */
     union {
-        bool                    on;     /* RADIO_EVENT_ANTENNA, RADIO_EVENT_TA */
+        bool                    on;     /* RADIO_EVENT_ANTENNA, RADIO_EVENT_TA,
+                                         * RADIO_EVENT_VOL_BUT_DOWN, RADIO_EVENT_VOL_BUT_UP */
         radio_hal_band_config_t config; /* RADIO_EVENT_CONFIG */
         radio_program_info_t    info;   /* RADIO_EVENT_TUNED, RADIO_EVENT_AF_SWITCH */
         radio_metadata_t        *metadata; /* RADIO_EVENT_METADATA */
