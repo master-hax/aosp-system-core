330336a2ea469c662a8adbd8dda9e908b3c7cae8
Chih-Hung Hsieh <chh@google.com>
2016-03-03 14:38:10 -0800

    Use unique_ptr to free objects when ASSERT* fail.
    BUG: 27479198
    Change-Id: Ib0f2279de85c6259330d5436257f6b978420601a

--- libion/tests/allocate_test.cpp
+++ libion/tests/allocate_test.cpp
@@ -14,6 +14,7 @@
  * limitations under the License.
  */
 
+#include <memory>
 #include <sys/mman.h>
 
 #include <gtest/gtest.h>
@@ -90,7 +91,8 @@ TEST_F(Allocate, RepeatedAllocate)
 
 TEST_F(Allocate, Zeroed)
 {
-    void *zeroes = calloc(4096, 1);
+    std::unique_ptr<char[]> zeroes_ptr(new char[4096]());
+    void *zeroes = zeroes_ptr.get();
 
     for (unsigned int heapMask : m_allHeaps) {
         SCOPED_TRACE(::testing::Message() << "heap " << heapMask);
