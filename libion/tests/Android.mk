e8b2956e666d9af6d45fb59a314b52d40e0fc569
Chih-Hung Hsieh <chh@google.com>
2016-03-03 14:38:10 -0800

    Use unique_ptr to free objects when ASSERT* fail.
    Now need c++14 and clang to compile std::make_unique.
    
    BUG: 27479198
    Change-Id: Ib0f2279de85c6259330d5436257f6b978420601a

--- libion/tests/Android.mk
+++ libion/tests/Android.mk
@@ -17,8 +17,9 @@
 LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
+LOCAL_CALNG := true
 LOCAL_MODULE := ion-unit-tests
-LOCAL_CFLAGS += -g -Wall -Werror -std=gnu++11 -Wno-missing-field-initializers
+LOCAL_CFLAGS += -g -Wall -Werror -std=c++14 -Wno-missing-field-initializers
 LOCAL_SHARED_LIBRARIES += libion
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../kernel-headers
 LOCAL_SRC_FILES := \
