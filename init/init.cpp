7b8ddd175f65d0aea52d2c13fef5554d350b5b3b
Kees Cook <keescook@google.com>
2016-03-22 16:45:11 -0700

    init: improve mmap ASLR failure reporting
    This should help people track down the reason for entropy setting
    failures.
    
    Bug: 27681085
    Change-Id: I6bde342ca0215e32c1f851365a1723630c7d4372

--- init/init.cpp
+++ init/init.cpp
@@ -296,6 +296,17 @@ static void security_failure() {
     while (true) { pause(); }  // never reached
 }
 
+/*
+ * For mmap_rnd_bits support, the following upstream commits are needed:
+ *
+ * d07e22597d1d mm: mmap: add new /proc tunable for mmap_base ASLR
+ * e0c25d958f78 arm: mm: support ARCH_MMAP_RND_BITS
+ * 8f0d3aa9de57 arm64: mm: support ARCH_MMAP_RND_BITS
+ * 9e08f57d684a x86: mm: support ARCH_MMAP_RND_BITS
+ * ec9ee4acd97c drivers: char: random: add get_random_long()
+ * 5ef11c35ce86 mm: ASLR: use get_random_long()
+ */
+
 #define MMAP_RND_PATH "/proc/sys/vm/mmap_rnd_bits"
 #define MMAP_RND_COMPAT_PATH "/proc/sys/vm/mmap_rnd_compat_bits"
 
@@ -310,6 +321,7 @@ static bool __attribute__((unused)) set_mmap_rnd_bits_min(int start, int min, bo
     }
     std::ifstream inf(path, std::fstream::in);
     if (!inf) {
+        ERROR("Cannot open for reading: %s!\n", path.c_str());
         return false;
     }
     while (start >= min) {
@@ -317,6 +329,7 @@ static bool __attribute__((unused)) set_mmap_rnd_bits_min(int start, int min, bo
         std::string str_val = std::to_string(start);
         std::ofstream of(path, std::fstream::out);
         if (!of) {
+            ERROR("Cannot open for writing: %s!\n", path.c_str());
             return false;
         }
         of << str_val << std::endl;
@@ -332,7 +345,12 @@ static bool __attribute__((unused)) set_mmap_rnd_bits_min(int start, int min, bo
         start--;
     }
     inf.close();
-    return (start >= min);
+    if (start < min) {
+        ERROR("Unable to set minimum required entropy %d in %s!\n",
+              min, path.c_str());
+        return false;
+    }
+    return true;
 }
 
 /*
