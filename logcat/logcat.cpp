14fd960a2706a4ebbf82db014e17694e52de1764
Mark Salyzyn <salyzyn@google.com>
2016-03-30 12:38:29 -0700

    logcat: support --regex on binary buffers as-is
    Bug: 27706081
    Change-Id: Ie647fd62d027b01004177559d68b8e5a346705e6

--- logcat/logcat.cpp
+++ logcat/logcat.cpp
@@ -149,14 +149,10 @@ void printBinary(struct log_msg *buf)
     TEMP_FAILURE_RETRY(write(g_outFD, buf, size));
 }
 
-static bool regexOk(const AndroidLogEntry& entry, log_id_t id)
+static bool regexOk(const AndroidLogEntry& entry)
 {
-    if (! g_regex) {
+    if (!g_regex) {
         return true;
-    }
-
-    if (id == LOG_ID_EVENTS || id == LOG_ID_SECURITY) {
-        return false;
     }
 
     std::string messageString(entry.message, entry.messageLen);
@@ -193,7 +189,7 @@ static void processBuffer(log_device_t* dev, struct log_msg *buf)
     }
 
     if (android_log_shouldPrintLine(g_logformat, entry.tag, entry.priority) &&
-        regexOk(entry, buf->id())) {
+            regexOk(entry)) {
         bytesWritten = android_log_printLogLine(g_logformat, g_outFD, &entry);
 
         g_printCount++;
@@ -1185,7 +1181,7 @@ int main(int argc, char **argv)
     dev = NULL;
     log_device_t unexpected("unexpected", false);
 
-    while (!g_maxCount || g_printCount < g_maxCount) {
+    while (!g_maxCount || (g_printCount < g_maxCount)) {
         struct log_msg log_msg;
         log_device_t* d;
         int ret = android_logger_list_read(logger_list, &log_msg);
