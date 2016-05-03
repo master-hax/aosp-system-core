ce5d3a806f90006ed37fb937844da57756493785
Alex Vakulenko <avakulenko@google.com>
2016-05-03 12:00:00 -0700

    Add "cpuset" service option to init language.
    - Add a new option to specify the cpuset to run the service in. The
      "cpuset" option takes as a parameter the cpuset path relative to the
      root cpuset in /dev/cpuset.
    
      Example:
        service foo /system/bin/foo
            class main
            user system
            group system
            cpuset /system/background
    
    - Use the value in ro.cpuset.default as the default cpuset for services,
      when not specified using the cpuset option. If the default is not set,
      services are started in the root cpuset, which is the default Android
      behavior.
    
    Bug:28550814
    Test: `m -j32` succeeds, phone boots.
    Change-Id: I501727fa5ee3f4bb7a938fa104b81a404b616633

--- init/service.cpp
+++ init/service.cpp
@@ -51,6 +51,11 @@ using android::base::ParseInt;
 using android::base::StringPrintf;
 using android::base::WriteStringToFile;
 
+namespace {
+const char kCpuSetPathPrefix[] = "/dev/cpuset";
+const size_t kCpuSetPathPrefixLength = sizeof(kCpuSetPathPrefix) - 1;
+}  // anonymous namespace
+
 static std::string ComputeContextFromExecutable(std::string& service_name,
                                                 const std::string& service_path) {
     std::string computed_context;
@@ -509,6 +514,36 @@ bool Service::ParseUser(const std::vector<std::string>& args, std::string* err) 
 
 bool Service::ParseWritepid(const std::vector<std::string>& args, std::string* err) {
     writepid_files_.assign(args.begin() + 1, args.end());
+    // See if we used "writepid" to write to any files under /dev/cpuset since
+    // the "cpuset" option should be used for this instead.
+    // If this is the case, issue a warning and pretend that we have used "cpuset"
+    // option.
+    auto filter_cpuset_path = [](const std::string& path) {
+        return path.substr(0, kCpuSetPathPrefixLength) == kCpuSetPathPrefix;
+    };
+    auto first_cpuset_path_iter = std::remove_if(
+            writepid_files_.begin(), writepid_files_.end(), filter_cpuset_path);
+    if (first_cpuset_path_iter != writepid_files_.end()) {
+        std::string cpuset_path = first_cpuset_path_iter->substr(kCpuSetPathPrefixLength);
+        LOG(WARNING) << "Using 'writepid' to write to cpuset '" << cpuset_path
+                     << "'. Use 'cpuset' option instead";
+        writepid_files_.erase(first_cpuset_path_iter, writepid_files_.end());
+        cpuset_tasks_fd_ = OpenCpuSetFd(cpuset_path);
+        if (cpuset_tasks_fd_.get() == -1) {
+            *err = "Unable to write to cpuset " + cpuset_path;
+            return false;
+        }
+    }
+    return true;
+}
+
+bool Service::ParseCpuSet(const std::vector<std::string>& args, std::string* err) {
+    cpuset_tasks_fd_ = OpenCpuSetFd(args[1]);
+    // Fail parsing if a cpuset was requested for the service but we can't open it.
+    if (cpuset_tasks_fd_.get() == -1) {
+        *err = "Unable to write to cpuset " + args[1];
+        return false;
+    }
     return true;
 }
 
@@ -527,6 +562,7 @@ Service::OptionParserMap::Map& Service::OptionParserMap::map() const {
                         {1,     kMax, &Service::ParseCapabilities}},
         {"class",       {1,     1,    &Service::ParseClass}},
         {"console",     {0,     1,    &Service::ParseConsole}},
+        {"cpuset",      {1,     1,    &Service::ParseCpuSet}},
         {"critical",    {0,     0,    &Service::ParseCritical}},
         {"disabled",    {0,     0,    &Service::ParseDisabled}},
         {"group",       {1,     NR_SVC_SUPP_GIDS + 1, &Service::ParseGroup}},
@@ -621,6 +657,21 @@ bool Service::Start() {
     }
 
     if (pid == 0) {
+        // Attach the process to the cpuset specified in the service or use
+        // the system default if one is not specified. When no system default
+        // is set, fallback to leaving the process in the root cpuset.
+        if (cpuset_tasks_fd_.get() == -1) {
+            cpuset_tasks_fd_ = OpenCpuSetFd(property_get("ro.cpuset.default"));
+        }
+
+        if (cpuset_tasks_fd_.get() != -1) {
+            std::string pid = std::to_string(getpid());
+            if (!android::base::WriteStringToFd(pid, cpuset_tasks_fd_.get())) {
+                PLOG(ERROR) << "Failed to write pid to cpuset tasks";
+            }
+            cpuset_tasks_fd_.reset();
+        }
+
         umask(077);
 
         if (namespace_flags_ & CLONE_NEWPID) {
@@ -669,6 +720,8 @@ bool Service::Start() {
 
         _exit(127);
     }
+
+    cpuset_tasks_fd_.reset();
 
     if (pid < 0) {
         PLOG(ERROR) << "failed to fork for '" << name_ << "'";
@@ -807,6 +860,36 @@ void Service::OpenConsole() const {
     dup2(fd, 1);
     dup2(fd, 2);
     close(fd);
+}
+
+// Opens the cpuset 'tasks' file. The cpuset is specified relative to the
+// standard mount point for the cpuset cgroup at /dev/cpuset, regardless of
+// leading "/" chars.
+android::base::unique_fd Service::OpenCpuSetFd(const std::string& subpath) {
+    android::base::unique_fd tasks_fd;
+    std::string path = "./" + subpath;
+    android::base::unique_fd base_fd{open(kCpuSetPathPrefix,
+                                          O_CLOEXEC | O_RDONLY | O_DIRECTORY)};
+    if (base_fd.get() == -1) {
+        PLOG(ERROR) << "Failed to open root cpuset";
+        return tasks_fd;
+    }
+
+    android::base::unique_fd cpuset_fd{openat(
+        base_fd.get(), path.c_str(), O_CLOEXEC | O_RDONLY | O_DIRECTORY)};
+    if (cpuset_fd.get() == -1) {
+        PLOG(ERROR) << "Failed to open cpuset \"" << subpath.c_str() << "\"";
+        return tasks_fd;
+    }
+
+    // Do not use O_CLOEXEC here since this fd will be passed to the child
+    // process.
+    tasks_fd.reset(openat(cpuset_fd.get(), "tasks", O_WRONLY));
+    if (tasks_fd.get() == -1) {
+        PLOG(ERROR) << "Failed to open cpuset tasks";
+    }
+
+    return tasks_fd;
 }
 
 int ServiceManager::exec_count_ = 0;
