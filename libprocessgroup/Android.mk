f5054756ab9ede19d9da175289d1db1d60a631d5
Collin Mulliner <collinrm@squareup.com>
2016-06-01 21:03:55 +0000

    use process groups for processes started by init
    Put every service into a process group, kill the process group
    and all child processes created within the group when killing the
    service.
    Bug: 25355957
    Change-Id: Ieed60ec41579f638ab9b1e66a7e6330ed578ab05
    Signed-off-by: Collin Mulliner <collinrm@squareup.com>

--- libprocessgroup/Android.mk
+++ libprocessgroup/Android.mk
@@ -3,6 +3,15 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES := processgroup.cpp
 LOCAL_MODULE := libprocessgroup
+LOCAL_STATIC_LIBRARIES := liblog libutils
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/include
+LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)/include
+LOCAL_CFLAGS := -Wall -Werror -DSTATIC
+include $(BUILD_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := processgroup.cpp
+LOCAL_MODULE := libprocessgroup
 LOCAL_SHARED_LIBRARIES := liblog libutils
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/include
 LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)/include
