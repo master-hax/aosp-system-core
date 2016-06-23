d23cfa3a03baaf345a798b87eff3ca84a3d60357
Casey Dahlin <sadmac@google.com>
2016-06-23 14:19:37 -0700

    Add mDNS device discovery for adb client
    Test: Was able to discover a raspberry pi.
    Bug: 28074466
    (cherry picked from e292cd16760321fccc99c8c261cb92fa4b6462ab)
    
    Change-Id: Id9571576457a4a0a078e48a274a4e8eac78bfe2b

--- adb/daemon/mdns.cpp
+++ adb/daemon/mdns.cpp
@@ -14,6 +14,7 @@
  * limitations under the License.
  */
 
+#include "adb_mdns.h"
 #include "sysdeps.h"
 
 #include <dns_sd.h>
@@ -76,7 +77,7 @@ static void setup_mdns_thread(void* /* unused */) {
 
     property_get("ro.serialno", hostname + 4, "unidentified");
 
-    auto error = DNSServiceRegister(&mdns_ref, 0, 0, hostname, "_adb._tcp",
+    auto error = DNSServiceRegister(&mdns_ref, 0, 0, hostname, kADBServiceType,
                                     nullptr, nullptr, htobe16((uint16_t)port),
                                     0, nullptr, mdns_callback, nullptr);
 
