fd15126ce7b08cb1f83aebb6f7c057cb742939e2
xinglong.zhu <xinglong.zhu@spreadtrum.com>
2016-08-05 15:04:27 +0800

    Bug #570023 Recovery mode UI flicker
    [root cause  ] recovery miniui need a libsync static library
    [changes     ] libsync
    [side effects] ui show
    [self test   ] sc9850-2 general operation has not display abnormal
    [reviewers   ] xinglong.zhu
    
    Signed-off-by: xinglong.zhu <xinglong.zhu@spreadtrum.com>
    [change_type ] AOB --> google_original
    [tag_product ] common
    
    Change-Id: Ia640d3c5d2d7ba6d58aa1de97dcdb3cc8bfd4b97

--- libsync/Android.mk
+++ libsync/Android.mk
@@ -11,6 +11,16 @@ LOCAL_CFLAGS := -Werror
 include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
+LOCAL_SRC_FILES := sync.c
+LOCAL_MODULE := libsync
+LOCAL_MODULE_TAGS := optional
+LOCAL_STATIC_LIBRARIES := liblog
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/include
+LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)/include
+LOCAL_CFLAGS := -Werror
+include $(BUILD_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
 LOCAL_SRC_FILES := sync.c sync_test.c
 LOCAL_MODULE := sync_test
 LOCAL_MODULE_TAGS := optional tests
