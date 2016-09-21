d0788d9c287c1fc45ab743a80cbc4ef33fd5af11
Elliott Hughes <enh@google.com>
2016-09-21 16:53:15 -0700

    Add std::string system property functions.
    Makes it easier to write correct code in a world where the maximum
    property key/value lengths change.
    
    Bug: http://b/23102347
    Test: libbase_test64
    Change-Id: I100f00904221bbcef9e8786a4e6e30428039bb49

---
+++ base/properties_test.cpp
@@ -0,0 +1,51 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "android-base/properties.h"
+
+#include <gtest/gtest.h>
+
+#include <string>
+
+TEST(properties, smoke) {
+  android::base::SetProperty("debug.libbase.property_test", "hello");
+
+  std::string s;
+  android::base::GetProperty("debug.libbase.property_test", &s);
+  ASSERT_EQ("hello", s);
+
+  android::base::SetProperty("debug.libbase.property_test", "world");
+  android::base::GetProperty("debug.libbase.property_test", &s);
+  ASSERT_EQ("world", s);
+
+  s = "";
+  android::base::GetProperty("this.property.does.not.exist", &s);
+  ASSERT_EQ("", s);
+
+  s = "default";
+  android::base::GetProperty("this.property.does.not.exist", &s);
+  ASSERT_EQ("default", s);
+}
+
+TEST(properties, empty) {
+  // Because you can't delete a property, people "delete" them by
+  // setting them to the empty string. In that case we'd want to
+  // keep the default value (like cutils' property_get did).
+  android::base::SetProperty("debug.libbase.property_test", "");
+  std::string s = "default";
+  android::base::GetProperty("debug.libbase.property_test", &s);
+  ASSERT_EQ("default", s);
+}
