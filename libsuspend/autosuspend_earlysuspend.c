5a22965a931754e72626ddee2d079094cf4de3d9
Mark Salyzyn <salyzyn@google.com>
2016-09-28 10:07:20 -0700

    system/core Replace cutils/log.h with android/log.h
    Should use android/log.h instead of cutils/log.h as a good
    example to all others.
    
    Test: Compile
    Bug: 26552300
    Bug: 31289077
    Change-Id: I2c9cbbbd64d8dccf2d44356361d9742e4a9b9031

--- libsuspend/autosuspend_earlysuspend.c
+++ libsuspend/autosuspend_earlysuspend.c
@@ -14,6 +14,8 @@
  * limitations under the License.
  */
 
+#define LOG_TAG "libsuspend"
+
 #include <errno.h>
 #include <fcntl.h>
 #include <pthread.h>
@@ -24,8 +26,7 @@
 #include <sys/stat.h>
 #include <unistd.h>
 
-#define LOG_TAG "libsuspend"
-#include <cutils/log.h>
+#include <android/log.h>
 
 #include "autosuspend_ops.h"
 
