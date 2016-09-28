2ac859d1c199ab2f3be9875731b35603fdcc9976
Mark Salyzyn <salyzyn@google.com>
2016-09-28 09:58:56 -0700

    liblog: deprecate log/logd.h & cutils/log.h
    Add a warning when deprecated headers are included.
    cutils/log.h is a soft warning.
    
    Test: compile
    Bug: 26552300
    Bug: 31289077
    Change-Id: If24df0fc9f42196e7047e5aa8f01bb1f5adae0ac

--- include/cutils/log.h
+++ include/cutils/log.h
@@ -1,1 +1,56 @@
-#include <android/log.h>
+/*
+ * Copyright (C) 2005-2014 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef _LIBS_CUTIL_LOG_H
+#define _LIBS_CUTIL_LOG_H
+
+/* We do not know if developer wanted log/log.h or subset android/log.h */
+#include <log/log.h>
+
+#if defined(__GNUC__)
+#ifdef __USE_MINGW_ANSI_STDIO
+#if __USE_MINGW_ANSI_STDIO
+#else
+#pragma GCC diagnostic push
+#pragma GCC diagnostic warning "-W#warnings"
+#endif
+#else
+#pragma GCC diagnostic push
+#pragma GCC diagnostic warning "-W#warnings"
+#endif
+#endif
+#if defined(__clang__)
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wpedantic"
+#endif
+
+#warning "Deprecated: don't include cutils/log.h, use either android/log.h or log/log.h"
+
+#if defined(__clang__)
+#pragma clang diagnostic pop
+#endif
+#if defined(__GNUC__)
+#ifdef __USE_MINGW_ANSI_STDIO
+#if __USE_MINGW_ANSI_STDIO
+#else
+#pragma GCC diagnostic pop
+#endif
+#else
+#pragma GCC diagnostic pop
+#endif
+#endif
+
+#endif /* _LIBS_CUTIL_LOG_H */
--- include/log/logd.h
+++ include/log/logd.h
@@ -1,1 +1,3 @@
 #include <android/log.h>
+
+#warning "Deprecated: do not include log/logd.h, use log/log.h instead"
