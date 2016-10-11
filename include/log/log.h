14f7dbcd82bf51d24a08b34a5d4e2c6b730d91b7
Mark Salyzyn <salyzyn@google.com>
2016-10-11 07:34:52 -0700

    liblog: android/log.h feature flags
    Add __ANDROID_USE_LIBLOG_<feature>_INTERFACE that are controlled by
    individual ANDROID_NATIVE_API_LEVELs. ANDROID_NATIVE_API_LEVEL is
    defaulted to __ANDROID_API__. Correct liblog ndk symbols.
    
    Test: compile
    Bug: 31992412
    Change-Id: Id3731496fa226e8c170305d0d2a1859e8cf67e14

--- include/log/log.h
+++ include/log/log.h
@@ -1,1 +1,41 @@
+/*
+ * Copyright (C) 2005-2014 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef _LIBS_LOG_LOG_H
+#define _LIBS_LOG_LOG_H
+
+/*
+ * Special case for __ANDROID_USE_LIBLOG_MACRO_INTERFACE since available
+ * since epoch internally.
+ */
+
+#ifdef __ANDROID_USE_LIBLOG_MACRO_INTERFACE
+#if __ANDROID_USE_LIBLOG_MACRO_INTERFACE
+/* Already included android/log.h correctly */
+#else
+/* Re-include android/log.h with a correction */
+#undef __ANDROID_USE_LIBLOG_MACRO_INTERFACE
+#define __ANDROID_USE_LIBLOG_MACRO_INTERFACE 1
+#undef _ANDROID_LOG_H
 #include <android/log.h>
+#endif
+#else
+/* Including android/log.h for first time */
+#define __ANDROID_USE_LIBLOG_MACRO_INTERFACE 1
+#include <android/log.h>
+#endif
+
+#endif /* _LIBS_LOG_LOG_H */
