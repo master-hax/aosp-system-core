b0bbd60553ae4951c6df93effc11d41ac3d4defb
Mark Salyzyn <salyzyn@google.com>
2016-10-27 08:04:48 -0700

    libcutils: add android_get_control_socket() test
    android_get_control_socket() checks if the resulting file descriptor
    is valid, open and matches the socket bound name reference, which on
    purpose will fail if a symbolic link is in the path rather than using
    a fully qualified path.  Add gTest unit test.
    
    Test: gTest libcutils_test --gtest_filter=SocketTest.android_get_control_socket
    Bug: 32450474
    Change-Id: I27a6419012033ef8bd6ca04f3e479d01264d8c49

--- libcutils/sockets.cpp
+++ libcutils/sockets.cpp
@@ -29,11 +29,22 @@
 // This file contains socket implementation that can be shared between
 // platforms as long as the correct headers are included.
 
-#include <cutils/sockets.h>
-
+#include <errno.h>
+#include <fcntl.h>
+#include <limits.h>
 #if !defined(_WIN32)
 #include <netinet/in.h>
 #endif
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <unistd.h>
+
+#include <string>
+
+#include <cutils/sockets.h>
 
 int socket_get_local_port(cutils_socket_t sock) {
     sockaddr_storage addr;
@@ -47,22 +58,41 @@ int socket_get_local_port(cutils_socket_t sock) {
 }
 
 int android_get_control_socket(const char* name) {
-    char key[64];
-    snprintf(key, sizeof(key), ANDROID_SOCKET_ENV_PREFIX "%s", name);
+    char *key = NULL;
+    if (asprintf(&key, ANDROID_SOCKET_ENV_PREFIX "%s", name) < 0) return -1;
+    if (!key) return -1;
 
     const char* val = getenv(key);
-    if (!val) {
-        return -1;
-    }
+    free(key);
+    if (!val) return -1;
 
     errno = 0;
-    long ret = strtol(val, NULL, 10);
-    if (errno) {
-        return -1;
-    }
-    if (ret < 0 || ret > INT_MAX) {
-        return -1;
-    }
+    long fd = strtol(val, NULL, 10);
+    if (errno) return -1;
 
-    return static_cast<int>(ret);
+    // validity checking
+    if ((fd < 0) || (fd > INT_MAX)) return -1;
+#if defined(_SC_OPEN_MAX)
+    if (fd >= sysconf(_SC_OPEN_MAX)) return -1;
+#elif defined(_POSIX_OPEN_MAX)
+    if (fd >= _POSIX_OPEN_MAX) return -1;
+#elif defined(OPEN_MAX)
+    if (fd >= OPEN_MAX) return -1;
+#endif
+
+    if (TEMP_FAILURE_RETRY(fcntl(fd, F_GETFD)) < 0) return -1;
+
+    struct sockaddr_un addr;
+    socklen_t addrlen = sizeof(addr);
+    int ret = TEMP_FAILURE_RETRY(getsockname(fd, (struct sockaddr *)&addr, &addrlen));
+    if (ret < 0) return -1;
+    char *path = NULL;
+    if (asprintf(&path, ANDROID_SOCKET_DIR"/%s", name) < 0) return -1;
+    if (!path) return -1;
+    int cmp = strcmp(addr.sun_path, path);
+    free(path);
+    if (cmp != 0) return -1;
+
+    // It is what we think it is
+    return static_cast<int>(fd);
 }
