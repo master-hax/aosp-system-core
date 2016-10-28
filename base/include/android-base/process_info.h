7d4ceb1435968ce109bd0787132cc920159fc715
Josh Gao <jmgao@google.com>
2016-10-28 15:23:25 -0700

    base: add process info helpers.
    Add helpers to parse /proc/<pid>/status and /proc/<pid>/task.
    
    Bug: http://b/30705528
    Test: libbase_test32/64 on host and bullhead
    Change-Id: I5757514c0aede8a9d75834b55aae42a5cf762b95

---
+++ base/include/android-base/process_info.h
@@ -0,0 +1,80 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <dirent.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#include <memory>
+#include <type_traits>
+
+#include "logging.h"
+#include "parseint.h"
+
+namespace android {
+namespace base {
+
+#if defined(__linux__)
+
+struct ProcessInfo {
+  pid_t tid;
+  pid_t pid;
+  pid_t ppid;
+  pid_t tracer;
+  uid_t uid;
+  uid_t gid;
+};
+
+// Parse the contents of /proc/<tid>/status into |process_info|.
+bool GetProcessInfo(ProcessInfo* process_info, pid_t tid);
+
+// Parse the contents of <fd>/status into |process_info|.
+// |fd| should be an fd pointing at a /proc/<pid> directory.
+bool GetProcessInfoFromProcPidFd(ProcessInfo* process_info, int fd);
+
+template <typename Collection>
+auto GetProcessTids(Collection* out, pid_t pid)
+    -> std::enable_if_t<sizeof(typename Collection::value_type) >= sizeof(pid_t),
+                        bool> {
+  char task_path[PATH_MAX];
+  if (snprintf(task_path, PATH_MAX, "/proc/%d/task", pid) >= PATH_MAX) {
+    LOG(ERROR) << "task path overflow (pid = " << pid << ")";
+    return false;
+  }
+
+  std::unique_ptr<DIR, int (*)(DIR*)> dir(opendir(task_path), closedir);
+  struct dirent* dent;
+  while ((dent = readdir(dir.get()))) {
+    if (strcmp(dent->d_name, ".") != 0 && strcmp(dent->d_name, "..") != 0) {
+      pid_t tid;
+      if (!ParseInt(dent->d_name, &tid, 1, std::numeric_limits<pid_t>::max())) {
+        LOG(ERROR) << "failed to parse task id: " << dent->d_name;
+        return false;
+      }
+
+      out->insert(out->end(), tid);
+    }
+  }
+
+  return true;
+}
+
+#endif
+
+} /* namespace base */
+} /* namespace android */
