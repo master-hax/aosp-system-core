2056162182b910a822164d35020d68097af76383
Hung-ying Tyan <tyanh@google.com>
2016-12-20 16:58:23 +0800

    init: add ro.boot.script
    SoC vendors and ODMs need a way to run different boot scripts under
    different boot modes. This patch adds a new ro.boot.script kernel
    cmdline argument to support this.
    
    Bug: 26639863
    Test: Tested on bullhead with
      androidboot.script = {non-existent .rc file}
      and/or
      androidboot.mode = {none empty string}
      both of which lead to expected boot failures.
    Change-Id: Ie5f08c3914f2f825ad3c401fde6741459668c523

--- init/init.cpp
+++ init/init.cpp
@@ -800,7 +800,13 @@ int main(int argc, char** argv) {
     parser.AddSectionParser("service",std::make_unique<ServiceParser>());
     parser.AddSectionParser("on", std::make_unique<ActionParser>());
     parser.AddSectionParser("import", std::make_unique<ImportParser>());
-    parser.ParseConfig("/init.rc");
+
+    std::string bootscript = property_get("ro.boot.script");
+    if (bootscript.empty()) {
+        parser.ParseConfig("/init.rc");
+    } else {
+        parser.ParseConfig(bootscript);
+    }
 
     ActionManager& am = ActionManager::GetInstance();
 
@@ -825,7 +831,7 @@ int main(int argc, char** argv) {
     std::string bootmode = property_get("ro.bootmode");
     if (bootmode == "charger") {
         am.QueueEventTrigger("charger");
-    } else {
+    } else if (bootmode == "unknown") {
         am.QueueEventTrigger("late-init");
     }
 
