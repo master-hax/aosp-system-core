a76151b37fa9d9b7e7e37b131c7fdc7d7795e39c
Nick Kralevich <nnk@google.com>
2017-01-03 10:35:34 -0800

    LogAudit.cpp: Delete newlines in audit messages
    Some kernels have a bug which causes a newline to show up in audit
    messages. This causes logd to inject two events instead of one into
    logcat / events log.
    
    Delete any newlines which may be present in an audit message.
    
    Test: create an audit message with a newline, and watch it be cleaned up.
    Bug: 27878170
    Change-Id: Id90c29ab9e10d3be96f51403b0293622d782422a

--- logd/LogAudit.cpp
+++ logd/LogAudit.cpp
@@ -98,6 +98,13 @@ int LogAudit::logPrint(const char *fmt, ...) {
         memmove(cp, cp + 1, strlen(cp + 1) + 1);
     }
 
+    // Work around kernels missing
+    // https://github.com/torvalds/linux/commit/b8f89caafeb55fba75b74bea25adc4e4cd91be67
+    // Such kernels improperly add newlines inside audit messages.
+    while ((cp = strstr(str, "\n"))) {
+        memmove(cp, cp + 1, strlen(cp + 1) + 1);
+    }
+
     bool info = strstr(str, " permissive=1") || strstr(str, " policy loaded ");
     if ((fdDmesg >= 0) && initialized) {
         struct iovec iov[3];
