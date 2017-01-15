LineageOS/android_system_core_old

fastboot: add command 'dump'
this command implements the filedump protocol implemented by Motorolas custom
version of fastboot(mfastboot).

instead of 'fastboot oem partition dump boot.img' you would use:
'fastboot dump boot.img oem partition moto-dump boot'

Change-Id: I3662e5a450e8b67c29f8387984ee21827e07f35d

fastboot/fastboot.h
@@ -38,6 +38,7 @@ int fb_command(usb_handle *usb, const char *cmd);
 int fb_command_response(usb_handle *usb, const char *cmd, char *response);
 int fb_download_data(usb_handle *usb, const void *data, unsigned size);
 int fb_download_data_sparse(usb_handle *usb, struct sparse_file *s);
+int fb_pull_file(usb_handle *usb, const char *file_name);
 char *fb_get_error(void);
 
 #define FB_COMMAND_SZ 64
@@ -58,6 +59,7 @@ void fb_queue_reboot(void);
 void fb_queue_command(const char *cmd, const char *msg);
 void fb_queue_download(const char *name, void *data, unsigned size);
 void fb_queue_notice(const char *notice);
+void fb_queue_dump(char *filename);
 int fb_execute_queue(usb_handle *usb);
 int fb_queue_is_empty(void);
 

master-hax/aosp-system-core

fastboot/fastboot.h
@@ -43,6 +43,7 @@ int fb_command(Transport* transport, const char* cmd);
 int fb_command_response(Transport* transport, const char* cmd, char* response);
 int fb_download_data(Transport* transport, const void* data, uint32_t size);
 int fb_download_data_sparse(Transport* transport, struct sparse_file* s);
+int fb_pull_file(Transport* transport, const char *file_name);
 const std::string fb_get_error();
 
 #define FB_COMMAND_SZ 64
@@ -64,6 +65,7 @@ void fb_queue_command(const char *cmd, const char *msg);
 void fb_queue_download(const char *name, void *data, uint32_t size);
 void fb_queue_notice(const char *notice);
 void fb_queue_wait_for_disconnect(void);
+void fb_queue_dump(char *filename);
 int fb_execute_queue(Transport* transport);
 void fb_set_active(const char *slot);
 