b8209a858ebe68b5c310fb3d881cdeeeb5c9e7a8
Niranjan Pendharkar <npendhar@codeaurora.org>
2017-01-16 16:41:50 -0800

    init: create new 'IPA' uid/gid
    Test: make with marlin success
    
    Bug:34361337
    
    Change-Id: I4d3f2c5ec6ed546c78c3f2458d208fb7ee1c0c4d

--- include/private/android_filesystem_config.h
+++ include/private/android_filesystem_config.h
@@ -128,6 +128,7 @@
 #define AID_MEDIA_IMAGE   1057 /* GID for image files on internal media storage */
 #define AID_TOMBSTONED    1058  /* tombstoned user */
 #define AID_MEDIA_OBB     1059 /* GID for OBB files on internal media storage */
+#define AID_IPA           1060 /* IPA subsystem */
 /* Changes to this file must be made in AOSP, *not* in internal branches. */
 
 #define AID_SHELL         2000  /* adb and debug shell user */
