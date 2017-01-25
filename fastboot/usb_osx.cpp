0b3facbc85a6ebec0355c1013c687ea9eea32665
Keith Mok <ek9852@gmail.com>
2017-01-25 15:07:31 -0800

    fastboot: Workaround for touchbar hang on MBP 2016
    fastboot will try to set usb configuration on connected
    usb devices. When it tries to change the usb configuration
    on iBridge device on MBP 2016 model, it hangs, and frozen
    the touchbar.  Workaround the system problem by skip usb
    device with apple vendor id.

--- fastboot/usb_osx.cpp
+++ fastboot/usb_osx.cpp
@@ -323,6 +323,16 @@ static int try_device(io_service_t device, usb_handle *handle) {
         goto error;
     }
 
+    /*
+     * Apple touchbar will hang when we called SetConfiguration later,
+     * skip all apple vendor device id for now.
+     */
+    if (handle->info.dev_vendor == 0x5ac) {
+        handle->success = 0;
+        ERR("Skip Apple device");
+        goto out;
+    }
+
     kr = (*dev)->GetDeviceProduct(dev, &handle->info.dev_product);
     if (kr != 0) {
         ERR("GetDeviceProduct");
@@ -436,7 +446,7 @@ static int init_usb(ifc_match_func callback, std::unique_ptr<usb_handle>* handle
         ERR("Could not create iterator.");
         return -1;
     }
-
+  
     for (;;) {
         if (! IOIteratorIsValid(iterator)) {
             /*
