b5286ed7cd38d7174a52d99e3ec791cbe49e61a3
Jocelyn Bohr <bohr@google.com>
2017-01-26 19:20:53 -0800

    fastboot: Add 'stage' command
    (cherry-picked from internal nyc-iot-dev to AOSP)
    
    New user-level command usage:
    
     * fastboot stage [ <infile> ]
       Sends the contents of <infile> to the device to stage for use in the
       next command.
    
    This enables OEM commands to use data downloaded from host to device.
    
    Bug: 35811075
    Test: Manual test on imx6ul
    Change-Id: I483a18f9f4205d3289ee524656b9d741b16e9fe6
    (cherry-picked from commit 001c75c6c0fe6a70a1db2a65253ab3c43ec17d46)

--- fastboot/fastboot.cpp
+++ fastboot/fastboot.cpp
@@ -360,6 +360,10 @@ static void usage() {
             "  continue                                 Continue with autoboot.\n"
             "  reboot [bootloader|emergency]            Reboot device [into bootloader or emergency mode].\n"
             "  reboot-bootloader                        Reboot device into bootloader.\n"
+            "  oem <parameter1> ... <parameterN>        Executes oem specific command.\n"
+            "  stage <infile>                           Sends contents of <infile> to stage for\n"
+            "                                           the next command. Supported only on\n"
+            "                                           devices with relevant oem commands\n"
             "  help                                     Show this help message.\n"
             "\n"
             "options:\n"
@@ -1785,6 +1789,16 @@ int main(int argc, char **argv)
             }
             fb_set_active(slot.c_str());
             skip(2);
+        } else if(!strcmp(*argv, "stage")) {
+            require(2);
+            char *infile = argv[1];
+            struct fastboot_buffer buf;
+            skip(2);
+
+            if (load_buf(transport, infile, &buf)) {
+                die("cannot load '%s'", infile);
+            }
+            fb_queue_download(infile, buf.data, buf.sz);
         } else if(!strcmp(*argv, "oem")) {
             argc = do_oem_command(argc, argv);
         } else if(!strcmp(*argv, "flashing")) {
