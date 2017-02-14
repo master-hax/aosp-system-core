07681b5f653d51af9848473ed7d937b680fc1fc0
Po-Chien Hsueh <pchsueh@google.com>
2017-02-14 09:39:45 +0800

    fs_config: Move hostapd to vendor partition
    Move hostapd to /vendor/bin/ because it's only used by WIFI HAL.
    
    Bug: 34236942
    Bug: 34237659
    Test: Hotspot works fine. Integration test.
    
    Merged-In: I8d9f51ed85a0614bf0141461dabeddae094ad4e0
    Change-Id: I8d9f51ed85a0614bf0141461dabeddae094ad4e0

--- libcutils/fs_config.c
+++ libcutils/fs_config.c
@@ -158,6 +158,10 @@ static const struct fs_path_config android_files[] = {
                                            CAP_MASK_LONG(CAP_NET_RAW),
                                               "system/bin/hostapd" },
 
+    { 00755, AID_WIFI,      AID_WIFI,      CAP_MASK_LONG(CAP_NET_ADMIN) |
+                                           CAP_MASK_LONG(CAP_NET_RAW),
+                                              "vendor/bin/hostapd" },
+
     /* Support wifi_hal_legacy administering a network interface. */
     { 00755, AID_WIFI,      AID_WIFI,      CAP_MASK_LONG(CAP_NET_ADMIN) |
                                            CAP_MASK_LONG(CAP_NET_RAW),
