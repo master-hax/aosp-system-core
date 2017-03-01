4f42fe0b24f765be483932cd7b463df2331e3737
Bowgo Tsai <bowgotsai@google.com>
2017-03-01 18:24:46 +0800

    init: skip early mount in recovery mode
    We don't need early mount in recovery mode for security considerations,
    e.g., users should explicitly select 'mount /system' from the recovery
    menu. This CL checks the existence of file "/sbin/recovery" and skip
    early mount when it is found.
    
    Bug: 35853576
    Test: early mount /vendor without dm-verity on sailfish
    Test: early mount not happen in recovery mode on sailfish
    Change-Id: I69cc96f6fd0de6ce493082921738a958dd571115

--- init/init.cpp
+++ init/init.cpp
@@ -788,8 +788,16 @@ static bool get_early_partitions(const std::vector<fstab_rec*>& early_fstab_recs
     return true;
 }
 
+#define RECOVERY_BIN_PATH "/sbin/recovery"
+
 /* Early mount vendor and ODM partitions. The fstab is read from device-tree. */
 static bool early_mount() {
+    // skip early mount if we're in recovery mode
+    if (access(RECOVERY_BIN_PATH, F_OK) == 0) {
+        LOG(INFO) << "Early mount skipped (recovery mode)";
+        return true;
+    }
+
     // first check if device tree fstab entries are compatible
     if (!is_dt_fstab_compatible()) {
         LOG(INFO) << "Early mount skipped (missing/incompatible fstab in device tree)";
