b5edd07c362642f485b501f71a20b16006dc9bc0
Mark Salyzyn <salyzyn@google.com>
2017-03-03 10:21:23 -0800

    logd: ensure LogBufferElement mSequence is monotonic
    - Improves accuracy of -t/-T '<timestamp>' behavior when out of order
      arrival of entries messes with mSequence as the list will now have
      monotonic sequence numbers enforced.
    - Out of order time entries still remain because of reader requiring
      the ability to receive newly arrived old entries.
    - -t/-T '<timestamp>' can still quit backward search prematurely
      because an old entry lands later in the list.
    - Adjust insert in place algorithm from two loops of fine place and
      then place against watermark, into one that does all of that plus
      iteratively swap update the sequence numbers to set monotonicity.
      Side effect will be that the read lock (which is actually the
      LogTimes lock) will be held longer while we search for the a place
      above the youngest LogTimes watermark.  We need to hold the read
      lock because we may be altering the sequence numbers affecting
      -t/-T '<timestamp>' search.
    
    Test: gTest logd-unit-tests liblog-unit-tests logcat-unit-tests
    Bug: 35373582
    Change-Id: I79a385fc149bac2179128b53d4c8f71e429181ae

--- logd/LogBufferElement.h
+++ logd/LogBufferElement.h
@@ -41,7 +41,7 @@ class LogBufferElement {
     const uint32_t mUid;
     const uint32_t mPid;
     const uint32_t mTid;
-    const uint64_t mSequence;
+    uint64_t mSequence;
     log_time mRealTime;
     char *mMsg;
     union {
