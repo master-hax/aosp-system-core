7df9c668a0ff5006fa38da90d3aab5eeb518c86c
Keun-young Park <keunyoung@google.com>
2017-03-27 16:07:02 -0700

    refactor handling of critical services for shutdown
    - keep shutdown_critical class services during the whole shutdown.
      This class of service should not hold files under /data and should work
      even if /data is gone.
    - keep logd and adbd until all normal processes are killed to provide logcat
      up to that point.
    
    bug: 36526187
    Test: many reboots
    
    Change-Id: I758f700a622c6005f3df9f29de2b55270055ad4d

--- init/reboot.cpp
+++ init/reboot.cpp
@@ -26,6 +26,7 @@
 #include <sys/wait.h>
 
 #include <memory>
+#include <set>
 #include <string>
 #include <thread>
 #include <vector>
@@ -248,8 +249,9 @@ static bool UmountPartitions(std::vector<MountEntry>* partitions, int maxRetry, 
                                               flags);
                 } else {
                     umountDone = false;
-                    PLOG(WARNING) << StringPrintf("cannot umount %s, flags:0x%x",
-                                                  entry.mnt_fsname().c_str(), flags);
+                    PLOG(WARNING) << StringPrintf("cannot umount %s, mnt_dir %s, flags:0x%x",
+                                                  entry.mnt_fsname().c_str(),
+                                                  entry.mnt_dir().c_str(), flags);
                 }
             }
         }
@@ -351,26 +353,28 @@ void DoReboot(unsigned int cmd, const std::string& reason, const std::string& re
     }
     LOG(INFO) << "Shutdown timeout: " << shutdownTimeout;
 
-    static const constexpr char* shutdown_critical_services[] = {"vold", "watchdogd"};
-    for (const char* name : shutdown_critical_services) {
-        Service* s = ServiceManager::GetInstance().FindServiceByName(name);
-        if (s == nullptr) {
-            LOG(WARNING) << "Shutdown critical service not found:" << name;
-            continue;
-        }
-        s->Start();  // make sure that it is running.
-        s->SetShutdownCritical();
-    }
+    const std::set<std::string> firstToKills{"tombstoned"};
+    const std::set<std::string> killAfterApps{"logd", "adbd"};
+    // watchdogd is a vendor specific component but should be alive to complete shutdown safely.
+    const std::set<std::string> toStarts{"watchdogd", "vold"};
+    ServiceManager::GetInstance().ForEachService(
+        [&firstToKills, &killAfterApps, &toStarts](Service* s) {
+            if (firstToKills.count(s->name())) {
+                s->Stop();
+            } else if (killAfterApps.count(s->name())) {
+                s->SetShutdownCritical();
+            } else if (toStarts.count(s->name())) {
+                s->Start();
+                s->SetShutdownCritical();
+            } else if (s->classnames().count("service_critical")) {
+                s->SetShutdownCritical();
+            }
+        });
+
     // optional shutdown step
     // 1. terminate all services except shutdown critical ones. wait for delay to finish
     if (shutdownTimeout > 0) {
         LOG(INFO) << "terminating init services";
-        // tombstoned can write to data when other services are killed. so finish it first.
-        static const constexpr char* first_to_kill[] = {"tombstoned"};
-        for (const char* name : first_to_kill) {
-            Service* s = ServiceManager::GetInstance().FindServiceByName(name);
-            if (s != nullptr) s->Stop();
-        }
 
         // Ask all services to terminate except shutdown critical ones.
         ServiceManager::GetInstance().ForEachService([](Service* s) {
@@ -409,8 +413,8 @@ void DoReboot(unsigned int cmd, const std::string& reason, const std::string& re
 
     // minimum safety steps before restarting
     // 2. kill all services except ones that are necessary for the shutdown sequence.
-    ServiceManager::GetInstance().ForEachService([](Service* s) {
-        if (!s->IsShutdownCritical()) s->Stop();
+    ServiceManager::GetInstance().ForEachService([&killAfterApps](Service* s) {
+        if (!s->IsShutdownCritical() || killAfterApps.count(s->name())) s->Stop();
     });
     ServiceManager::GetInstance().ReapAnyOutstandingChildren();
 
