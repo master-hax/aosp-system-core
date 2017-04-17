23d68eadef73add1321e82291e6c54a54a23b8cb
Mark Salyzyn <salyzyn@google.com>
2017-04-17 09:20:18 -0700

    logd: iterator corruption paranoia
    Add checking for impossible(tm) scenarios within LogBuffer::flushTo:
    
    1) When dealing with logcat -t/-T, or any tail reading, cap the search
       backwards to the lowest reader watermark to ensure that we do not
       collide with prune that removes the oldest entries.
    2) When iterating through the log entries, check if the iterator
       returns two identical element references and break out of the loop.
    3) Cap the maximum number of log entries we will skip while holding
       the iterator lock at 4194304, break out of the loop.
    
    For #2 and #3 we print a message to the kernel logs if we hit them.
    
    Added some instrumentation into the logd unit test.
    
    ToDo: Remove this paranoia, root cause always needs investigation.
    
    Test: gTest liblog-unit-tests logcat-unit-tests and logd-unit-tests
    Bug: 37378309
    Change-Id: I789594649db14093238828b9f6d1daeca8b780c2

--- logd/LogBuffer.cpp
+++ logd/LogBuffer.cpp
@@ -1090,13 +1090,35 @@ log_time LogBuffer::flushTo(SocketClient* reader, const log_time& start,
         // client wants to start from the beginning
         it = mLogElements.begin();
     } else {
-        LogBufferElementCollection::iterator last;
         // 3 second limit to continue search for out-of-order entries.
         log_time min = start - log_time(3, 0);
+
+        // Check if it is below the oldest watermark (stay away from prune)
+        LogTimeEntry::lock();
+
+        LogTimeEntry* oldest = nullptr;
+        LastLogTimes::iterator times = mTimes.begin();
+        while (times != mTimes.end()) {
+            LogTimeEntry* entry = (*times);
+            if (entry->owned_Locked() &&
+                (!oldest || (oldest->mStart > entry->mStart) ||
+                 ((oldest->mStart == entry->mStart) &&
+                  (entry->mTimeout.tv_sec || entry->mTimeout.tv_nsec)))) {
+                oldest = entry;
+            }
+            times++;
+        }
+
+        if (oldest && (min < oldest->mStart)) min = oldest->mStart;
+
+        LogTimeEntry::unlock();
+
         // Cap to 300 iterations we look back for out-of-order entries.
         size_t count = 300;
+
         // Client wants to start from some specified time. Chances are
         // we are better off starting from the end of the time sorted list.
+        LogBufferElementCollection::iterator last;
         for (last = it = mLogElements.end(); it != mLogElements.begin();
              /* do nothing */) {
             --it;
@@ -1112,8 +1134,21 @@ log_time LogBuffer::flushTo(SocketClient* reader, const log_time& start,
 
     log_time max = start;
 
+    LogBufferElement* lastElement = nullptr;  // iterator corruption paranoia
+    static const size_t maxSkip = 4194304;    // maximum entries to skip
+    size_t skip = maxSkip;
     for (; it != mLogElements.end(); ++it) {
         LogBufferElement* element = *it;
+
+        if (!--skip) {
+            android::prdebug("reader.per: too many elements skipped");
+            break;
+        }
+        if (element == lastElement) {
+            android::prdebug("reader.per: identical elements");
+            break;
+        }
+        lastElement = element;
 
         if (!privileged && (element->getUid() != uid)) {
             continue;
@@ -1159,6 +1194,7 @@ log_time LogBuffer::flushTo(SocketClient* reader, const log_time& start,
             return max;
         }
 
+        skip = maxSkip;
         pthread_mutex_lock(&mLogElementsLock);
     }
     pthread_mutex_unlock(&mLogElementsLock);
