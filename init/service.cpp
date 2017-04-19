b66ae452037e220f93d9f479a139cf6679149ff6
Tom Cherry <tomcherry@google.com>
2017-04-19 16:18:50 -0700

    init: create action_tests.cpp
    Start a action_tests.cpp file for testing the Action* classes.  This
    currently contains two end-to-end tests that parse small init script
    segments and verify that they act as expected.  In particular, one
    test ensures the order of triggers within a file, as was recently
    fixed.
    
    Test: Boot bullhead, run unit tests
    
    Change-Id: Ic446c02605ab796fd41e0596ce1fd381aee80ce0

--- init/service.cpp
+++ init/service.cpp
@@ -538,10 +538,10 @@ public:
     OptionParserMap() {
     }
 private:
-    Map& map() const override;
+  const Map& map() const override;
 };
 
-Service::OptionParserMap::Map& Service::OptionParserMap::map() const {
+const Service::OptionParserMap::Map& Service::OptionParserMap::map() const {
     constexpr std::size_t kMax = std::numeric_limits<std::size_t>::max();
     // clang-format off
     static const Map option_parsers = {
