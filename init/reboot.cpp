89f1b6a78427d1a608a0dea2c16e5f63f63dc108
Tom Cherry <tomcherry@google.com>
2017-04-20 16:38:54 -0700

    init: fix last_reboot_reason string
    This got moved when refactoring the reboot commands.
    
    Bug: 37540660
    Test: verify bullhead's last_reboot_reason is correct
    Change-Id: I3b86496fc469ca41645df7e7ba8bb51dd25b6b38

--- init/reboot.cpp
+++ init/reboot.cpp
@@ -442,12 +442,16 @@ bool HandlePowerctlMessage(const std::string& command) {
             // The shutdown reason is PowerManager.SHUTDOWN_USER_REQUESTED.
             // Run fsck once the file system is remounted in read-only mode.
             run_fsck = true;
-            reason_string = cmd_params[1];
         }
     } else if (cmd_params[0] == "reboot") {
         cmd = ANDROID_RB_RESTART2;
         if (cmd_params.size() >= 2) {
             reboot_target = cmd_params[1];
+            if (command.size() > cmd_params[0].size() + 1) {
+                // As long as command contains more than simply "reboot," reason_string
+                // is everything that follows the "reboot,"
+                reason_string = command.substr(cmd_params[0].size() + 1);
+            }
             // When rebooting to the bootloader notify the bootloader writing
             // also the BCB.
             if (reboot_target == "bootloader") {
