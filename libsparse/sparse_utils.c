782cb1423c3f8d8b04abd69a8016376fe2e7b2f8
Chris Fries <cfries@google.com>
2017-04-20 11:45:22 -0500

    libsparse: Add sparse utils into libsparse
    Add library functions for img2simg, simg2img, append2simg and simg2simg
    utilities into libsparse.
    
    Test: sparse_test
    Bug: 37339998
    Change-Id: Ibdcab1d0ad4ec752596a339847b95c2f9764c9df

--- libsparse/sparse_utils.c
+++ libsparse/sparse_utils.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2012 The Android Open Source Project
+ * Copyright (C) 2017 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,8 +16,12 @@
 
 #define _FILE_OFFSET_BITS 64
 #define _LARGEFILE64_SOURCE 1
+#define _GNU_SOURCE /* for asprintf */
 
+#include <assert.h>
+#include <errno.h>
 #include <fcntl.h>
+#include <sparse/sparse.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -26,325 +30,336 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-#include <sparse/sparse.h>
+#include "backed_block.h"
+#include "sparse_file.h"
 
 #ifndef O_BINARY
 #define O_BINARY 0
 #endif
+
+#define MAX_PATH 4096
 
 #if defined(__APPLE__) && defined(__MACH__)
 #define lseek64 lseek
 #define off64_t off_t
 #endif
 
-void usage() {
-    fprintf(stderr, "Usage: img2simg <raw_image_file> <sparse_image_file> [<block_size>]\n");
-}
-
 /* todo: migrate img2simg.c */
 
-int main(int argc, char* argv[]) {
+int img2simg(const char* input, const char* output) {
+    return img2simg_size(input, output, 4096);
+}
+
+int img2simg_size(const char* input, const char* output, unsigned int block_size) {
     int in;
     int out;
     int ret;
-    struct sparse_file* s;
-    unsigned int block_size = 4096;
-    off64_t len;
 
-    if (argc < 3 || argc > 4) {
-        usage();
-        exit(-1);
-    }
-
-    if (argc == 4) {
-        block_size = atoi(argv[3]);
-    }
-
-    if (block_size < 1024 || block_size % 4 != 0) {
-        usage();
-        exit(-1);
-    }
-
-    if (strcmp(argv[1], "-") == 0) {
+    if (strcmp(input, "-") == 0) {
         in = STDIN_FILENO;
     } else {
-        in = open(argv[1], O_RDONLY | O_BINARY);
+        in = open(input, O_RDONLY | O_BINARY);
         if (in < 0) {
-            fprintf(stderr, "Cannot open input file %s\n", argv[1]);
-            exit(-1);
+            fprintf(stderr, "Cannot open input file %s\n", input);
+            return -1;
         }
     }
 
-    if (strcmp(argv[2], "-") == 0) {
+    if (strcmp(output, "-") == 0) {
         out = STDOUT_FILENO;
     } else {
-        out = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC | O_BINARY, 0664);
+        out = open(output, O_WRONLY | O_CREAT | O_TRUNC | O_BINARY, 0664);
         if (out < 0) {
-            fprintf(stderr, "Cannot open output file %s\n", argv[2]);
-            exit(-1);
+            fprintf(stderr, "Cannot open output file %s\n", output);
+            if (in != STDIN_FILENO) {
+                close(in);
+            }
+            return -1;
         }
     }
 
-    len = lseek64(in, 0, SEEK_END);
+    ret = img2simg_size_fd(in, out, block_size);
+
+    if (in != STDIN_FILENO) {
+        close(in);
+    }
+    if (out != STDOUT_FILENO) {
+        close(out);
+    }
+
+    return ret;
+}
+
+int img2simg_size_fd(int in, int out, int block_size) {
+    struct sparse_file* s;
+    off64_t len = lseek64(in, 0, SEEK_END);
+    int ret;
+
     lseek64(in, 0, SEEK_SET);
+
+    assert(block_size >= 1024);
+    assert(block_size % 4 == 0);
 
     s = sparse_file_new(block_size, len);
     if (!s) {
-        fprintf(stderr, "Failed to create sparse file\n");
-        exit(-1);
+       fprintf(stderr, "Failed to create sparse file\n");
+       return -1;
     }
 
     sparse_file_verbose(s);
     ret = sparse_file_read(s, in, false, false);
-    if (ret) {
+    if (!ret) {
+        ret = sparse_file_write(s, out, false, true, false);
+        if (ret) {
+            fprintf(stderr, "Failed to write sparse file\n");
+        }
+    } else {
         fprintf(stderr, "Failed to read file\n");
-        exit(-1);
     }
 
-    ret = sparse_file_write(s, out, false, true, false);
-    if (ret) {
-        fprintf(stderr, "Failed to write sparse file\n");
-        exit(-1);
-    }
+    sparse_file_destroy(s);
 
-    close(in);
-    close(out);
-
-    exit(0);
-}
-
-void usage() {
-    fprintf(stderr, "Usage: simg2img <sparse_image_files> <raw_image_file>\n");
+    return ret;
 }
 
 /* todo: migrate simg2img.c */
 
-int main(int argc, char* argv[]) {
-    int in;
-    int out;
+int simg2img(int num_input, const char* input[], const char* output) {
+    int* ifd = malloc(num_input * sizeof(*ifd));
+    int ofd = -1;
     int i;
-    struct sparse_file* s;
+    int ret = -1;
 
-    if (argc < 3) {
-        usage();
-        exit(-1);
-    }
-
-    out = open(argv[argc - 1], O_WRONLY | O_CREAT | O_TRUNC | O_BINARY, 0664);
-    if (out < 0) {
-        fprintf(stderr, "Cannot open output file %s\n", argv[argc - 1]);
-        exit(-1);
-    }
-
-    for (i = 1; i < argc - 1; i++) {
-        if (strcmp(argv[i], "-") == 0) {
-            in = STDIN_FILENO;
+    assert(ifd != NULL);
+    for (i = 0; i < num_input; i++) {
+        if (strcmp(input[i], "-") == 0) {
+            ifd[i] = STDIN_FILENO;
         } else {
-            in = open(argv[i], O_RDONLY | O_BINARY);
-            if (in < 0) {
-                fprintf(stderr, "Cannot open input file %s\n", argv[i]);
-                exit(-1);
-            }
+            ifd[i] = open(input[i], O_RDONLY | O_BINARY);
         }
-
-        s = sparse_file_import(in, true, false);
-        if (!s) {
-            fprintf(stderr, "Failed to read sparse file\n");
-            exit(-1);
+        if (ifd[i] < 0) {
+            fprintf(stderr, "Cannot open %s\n", input[i]);
+            goto out;
         }
-
-        if (lseek(out, 0, SEEK_SET) == -1) {
-            perror("lseek failed");
-            exit(EXIT_FAILURE);
-        }
-
-        if (sparse_file_write(s, out, false, false, false) < 0) {
-            fprintf(stderr, "Cannot write output file\n");
-            exit(-1);
-        }
-        sparse_file_destroy(s);
-        close(in);
     }
 
-    close(out);
+    ofd = open(output, O_WRONLY | O_CREAT | O_TRUNC | O_BINARY, 0664);
+    if (ofd < 0) {
+        fprintf(stderr, "Cannot open output file %s\n", output);
+        goto out;
+    }
 
-    exit(0);
+    ret = simg2img_fd(num_input, ifd, ofd);
+
+out:
+    for (i = 0; i < num_input && ifd[i] >= 0; i++) {
+        if (ifd[i] != STDIN_FILENO) {
+            close(ifd[i]);
+        }
+    }
+    free(ifd);
+
+    if (ofd >= 0) {
+        close(ofd);
+    }
+    return ret;
 }
 
-void usage() {
-    fprintf(stderr, "Usage: append2simg <output> <input>\n");
+int simg2img_fd(int num_input, int* ifd, int ofd) {
+    int i;
+
+    for (i = 0; i < num_input; i++) {
+        struct sparse_file* s = sparse_file_import(ifd[i], true, false);
+        if (lseek(ofd, 0, SEEK_SET) < 0) {
+            fprintf(stderr, "seek failed\n");
+            sparse_file_destroy(s);
+            return -1;
+        }
+
+        if (sparse_file_write(s, ofd, false, false, false) < 0) {
+            fprintf(stderr, "Cannot write output file\n");
+            sparse_file_destroy(s);
+            return -1;
+        }
+        sparse_file_destroy(s);
+    }
+
+    return 0;
 }
 
 /* todo: migrate append2simg.c */
 
-int main(int argc, char* argv[]) {
-    int output;
-    int output_block;
-    char* output_path;
-    struct sparse_file* sparse_output;
+int append2simg(const char* output, const char* input) {
+    int ofd;
+    int ifd = -1;
+    int tmpfd = -1;
+    char* tmp_path = NULL;
+    int ret = -1;
 
-    int input;
-    char* input_path;
-    off64_t input_len;
-
-    int tmp_fd;
-    char* tmp_path;
-
-    int ret;
-
-    if (argc == 3) {
-        output_path = argv[1];
-        input_path = argv[2];
-    } else {
-        usage();
-        exit(-1);
+    ofd = open(output, O_RDWR | O_BINARY);
+    if (ofd < 0) {
+        fprintf(stderr, "Couldn't open output file (%s)\n", strerror(errno));
+        return ret;
     }
 
-    ret = asprintf(&tmp_path, "%s.append2simg", output_path);
+    ifd = open(input, O_RDONLY | O_BINARY);
+    if (ifd < 0) {
+        fprintf(stderr, "Couldn't open input file (%s)\n", strerror(errno));
+        goto fail;
+    }
+
+    ret = asprintf(&tmp_path, "%s.append2simg", output);
     if (ret < 0) {
         fprintf(stderr, "Couldn't allocate filename\n");
-        exit(-1);
+        goto fail;
     }
-
-    output = open(output_path, O_RDWR | O_BINARY);
-    if (output < 0) {
-        fprintf(stderr, "Couldn't open output file (%s)\n", strerror(errno));
-        exit(-1);
+    tmpfd = open(tmp_path, O_WRONLY | O_CREAT | O_BINARY, 0664);
+    if (tmpfd < 0) {
+        fprintf(stderr, "Couldn't open temporary file (%s)\n", strerror(errno));
+        ret = -1;
+        goto fail;
     }
+    ret = append2simg_fd(ofd, ifd, tmpfd);
 
-    sparse_output = sparse_file_import_auto(output, false, true);
-    if (!sparse_output) {
+fail:
+    if (tmpfd >= 0) {
+       close(tmpfd);
+    }
+    if (ifd >= 0) {
+       close(ifd);
+    }
+    close(ofd);
+
+    if (!ret) {
+        ret = rename(tmp_path, output);
+        if (ret < 0) {
+            fprintf(stderr, "Failed to rename temporary file (%s)\n", strerror(errno));
+        }
+    } else {
+        unlink(tmp_path);
+    }
+    if (tmp_path) {
+        free(tmp_path);
+    }
+    return ret;
+}
+int append2simg_fd(int ofd, int ifd, int tmpfd) {
+    struct sparse_file* s;
+    off64_t input_len;
+    int output_block;
+    int ret;
+
+    s = sparse_file_import_auto(ofd, false, true);
+    if (!s) {
         fprintf(stderr, "Couldn't import output file\n");
-        exit(-1);
+        return -1;
     }
 
-    input = open(input_path, O_RDONLY | O_BINARY);
-    if (input < 0) {
-        fprintf(stderr, "Couldn't open input file (%s)\n", strerror(errno));
-        exit(-1);
-    }
-
-    input_len = lseek64(input, 0, SEEK_END);
+    input_len = lseek64(ifd, 0, SEEK_END);
     if (input_len < 0) {
         fprintf(stderr, "Couldn't get input file length (%s)\n", strerror(errno));
-        exit(-1);
-    } else if (input_len % sparse_output->block_size) {
-        fprintf(stderr, "Input file is not a multiple of the output file's block size");
-        exit(-1);
+        sparse_file_destroy(s);
+        return -1;
+    } else if (input_len % s->block_size) {
+        fprintf(stderr, "Input file is not a multiple of the output file's block size\n");
+        sparse_file_destroy(s);
+        return -1;
     }
-    lseek64(input, 0, SEEK_SET);
+    lseek64(ifd, 0, SEEK_SET);
 
-    output_block = sparse_output->len / sparse_output->block_size;
-    if (sparse_file_add_fd(sparse_output, input, 0, input_len, output_block) < 0) {
+    output_block = s->len / s->block_size;
+    if (sparse_file_add_fd(s, ifd, 0, input_len, output_block) < 0) {
         fprintf(stderr, "Couldn't add input file\n");
-        exit(-1);
+        sparse_file_destroy(s);
+        return -1;
     }
-    sparse_output->len += input_len;
+    s->len += input_len;
 
-    tmp_fd = open(tmp_path, O_WRONLY | O_CREAT | O_BINARY, 0664);
-    if (tmp_fd < 0) {
-        fprintf(stderr, "Couldn't open temporary file (%s)\n", strerror(errno));
-        exit(-1);
-    }
-
-    lseek64(output, 0, SEEK_SET);
-    if (sparse_file_write(sparse_output, tmp_fd, false, true, false) < 0) {
-        fprintf(stderr, "Failed to write sparse file\n");
-        exit(-1);
-    }
-
-    sparse_file_destroy(sparse_output);
-    close(tmp_fd);
-    close(output);
-    close(input);
-
-    ret = rename(tmp_path, output_path);
+    lseek64(ofd, 0, SEEK_SET);
+    ret = sparse_file_write(s, tmpfd, false, true, false);
     if (ret < 0) {
-        fprintf(stderr, "Failed to rename temporary file (%s)\n", strerror(errno));
-        exit(-1);
+        fprintf(stderr, "Failed to write sparse file\n");
     }
 
-    free(tmp_path);
+    sparse_file_destroy(s);
 
-    exit(0);
-}
-
-void usage() {
-    fprintf(stderr, "Usage: simg2simg <sparse image file> <sparse_image_file> <max_size>\n");
+    return ret;
 }
 
 /* todo: migrate simg2simg.c */
 
-int main(int argc, char* argv[]) {
+int simg2simg(const char* input, const char* output, int64_t max_size) {
     int in;
     int out;
     int i;
-    int ret;
-    struct sparse_file* s;
-    int64_t max_size;
-    struct sparse_file** out_s;
+    int ret = -1;
+    struct sparse_file* s = NULL;
+    struct sparse_file** out_s = NULL;
     int files;
-    char filename[4096];
+    char filename[MAX_PATH];
 
-    if (argc != 4) {
-        usage();
-        exit(-1);
-    }
-
-    max_size = atoll(argv[3]);
-
-    in = open(argv[1], O_RDONLY | O_BINARY);
+    in = open(input, O_RDONLY | O_BINARY);
     if (in < 0) {
-        fprintf(stderr, "Cannot open input file %s\n", argv[1]);
-        exit(-1);
+        fprintf(stderr, "Cannot open input file %s\n", input);
+        return -1;
     }
 
     s = sparse_file_import(in, true, false);
     if (!s) {
         fprintf(stderr, "Failed to import sparse file\n");
-        exit(-1);
+        goto fail;
     }
 
     files = sparse_file_resparse(s, max_size, NULL, 0);
     if (files < 0) {
         fprintf(stderr, "Failed to resparse\n");
-        exit(-1);
+        goto fail;
     }
 
     out_s = calloc(sizeof(struct sparse_file*), files);
-    if (!out_s) {
-        fprintf(stderr, "Failed to allocate sparse file array\n");
-        exit(-1);
-    }
+    assert(out_s != NULL);
 
     files = sparse_file_resparse(s, max_size, out_s, files);
     if (files < 0) {
         fprintf(stderr, "Failed to resparse\n");
-        exit(-1);
+        goto fail;
     }
 
     for (i = 0; i < files; i++) {
-        ret = snprintf(filename, sizeof(filename), "%s.%d", argv[2], i);
+        ret = snprintf(filename, sizeof(filename), "%s.%d", output, i);
         if (ret >= (int)sizeof(filename)) {
             fprintf(stderr, "Filename too long\n");
-            exit(-1);
+            break;
         }
 
         out = open(filename, O_WRONLY | O_CREAT | O_TRUNC | O_BINARY, 0664);
         if (out < 0) {
-            fprintf(stderr, "Cannot open output file %s\n", argv[2]);
-            exit(-1);
+            fprintf(stderr, "Cannot open output file %s\n", filename);
+            break;
         }
 
         ret = sparse_file_write(out_s[i], out, false, true, false);
+        close(out);
         if (ret) {
             fprintf(stderr, "Failed to write sparse file\n");
-            exit(-1);
+            break;
         }
-        close(out);
     }
 
+    for (i = 0; i < files; i++) {
+        if (out_s[i]) {
+            sparse_file_destroy(out_s[i]);
+        }
+    }
+
+fail:
+    if(out_s) {
+        free(out_s);
+    }
+    if (s) {
+        sparse_file_destroy(s);
+    }
     close(in);
 
-    exit(0);
+    return ret;
 }
