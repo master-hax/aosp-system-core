e8e6b89106fad4e000009be61594c85749958ee7
Chris Fries <cfries@google.com>
2017-04-21 12:22:33 -0500

    libsparse: Convert utilities to use libsparse utility functions
    Also, readd simg2simg which was lost.
    
    Test: run img2simg, simg2img, append2simg, simg2simg
    Bug: 37339998
    Change-Id: I66ca531db858cb0acc7bf80c7c6f5c595c989f57

---
+++ libsparse/simg2simg.cpp
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) 2012 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <private/sparse/sparse_utils.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void usage() {
+    fprintf(stderr, "Usage: simg2simg <input sparse file> <output sparse basename> <max_size>\n");
+}
+
+int main(int argc, char* argv[]) {
+    if (argc != 4) {
+        usage();
+        return -1;
+    }
+
+    return simg2simg(argv[1], argv[2], atoll(argv[3]));
+}
