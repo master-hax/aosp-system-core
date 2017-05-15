36828b3e2721db98328a8bed057dd14a6345bcec
Narayan Kamath <narayan@google.com>
2017-05-15 15:59:30 +0100

    WIP: Support java trace dumps in tombstoned.
    A rough (but fully) functional draft of the proposed changes to tombstoned.
    It is now responsible for managing java ANR dumps in addition to native crashes.
    
    The protocol is exactly the same, the type of request is inferred from the socket
    it's received on. Having different sockets allows us to install different sets of
    rules for each of those sockets; very few processes should need to write
    or connect to the native crash socket.
    
    TODO: Verify the provided PID with SO_PEERCRED
    
    Bug: 32064548
    Test: TBD
    
    Change-Id: I1d62cc7a7035fd500c3e2b831704a2934d725e35

--- debuggerd/tombstoned/tombstoned.cpp
+++ debuggerd/tombstoned/tombstoned.cpp
@@ -50,6 +50,49 @@ enum CrashStatus {
   kCrashStatusQueued,
 };
 
+struct Crash;
+
+class CrashType {
+ public:
+  const std::string file_name_prefix;
+  const std::string dir_path;
+
+  const size_t max_artifacts;
+  const size_t max_concurrent_dumps;
+
+  int dir_fd;
+
+  int next_artifact;
+  size_t num_concurrent_dumps;
+
+  std::deque<Crash*> queued_requests;
+
+  static CrashType tombstone;
+  static CrashType java_trace;
+
+ private:
+  CrashType(const std::string& dir_path, const std::string& file_name_prefix, size_t max_artifacts,
+            size_t max_concurrent_dumps)
+      : file_name_prefix(file_name_prefix),
+        dir_path(dir_path),
+        max_artifacts(max_artifacts),
+        max_concurrent_dumps(max_concurrent_dumps),
+        next_artifact(0),
+        num_concurrent_dumps(0) {
+    // NOTE: The success of this call is being tested externally for now.
+    // This is a kludge.
+    dir_fd = open(dir_path.c_str(), O_DIRECTORY | O_RDONLY | O_CLOEXEC);
+  }
+
+  DISALLOW_COPY_AND_ASSIGN(CrashType);
+};
+
+/* static */ CrashType CrashType::tombstone("/data/tombstones", "tombstone_" /* file_name_prefix */,
+                                            10 /* max_artifacts */, 1 /* max_concurrent_dumps */);
+
+/* static */ CrashType CrashType::java_trace("/data/anr", "anr_" /* file_name_prefix */,
+                                             64 /* max_artifacts */, 4 /* max_concurrent_dumps */);
+
 // Ownership of Crash is a bit messy.
 // It's either owned by an active event that must have a timeout, or owned by
 // queued_requests, in the case that multiple crashes come in at the same time.
@@ -61,29 +104,23 @@ struct Crash {
   unique_fd crash_fd;
   pid_t crash_pid;
   event* crash_event = nullptr;
+
+  // Not owned by |Crash|.
+  CrashType* crash_type = nullptr;
 };
-
-static constexpr char kTombstoneDirectory[] = "/data/tombstones/";
-static constexpr size_t kTombstoneCount = 10;
-static int tombstone_directory_fd = -1;
-static int next_tombstone = 0;
-
-static constexpr size_t kMaxConcurrentDumps = 1;
-static size_t num_concurrent_dumps = 0;
-
-static std::deque<Crash*> queued_requests;
 
 // Forward declare the callbacks so they can be placed in a sensible order.
 static void crash_accept_cb(evconnlistener* listener, evutil_socket_t sockfd, sockaddr*, int, void*);
 static void crash_request_cb(evutil_socket_t sockfd, short ev, void* arg);
 static void crash_completed_cb(evutil_socket_t sockfd, short ev, void* arg);
 
-static void find_oldest_tombstone() {
+static void find_oldest_artifact(CrashType* type) {
   size_t oldest_tombstone = 0;
   time_t oldest_time = std::numeric_limits<time_t>::max();
 
-  for (size_t i = 0; i < kTombstoneCount; ++i) {
-    std::string path = android::base::StringPrintf("%stombstone_%02zu", kTombstoneDirectory, i);
+  for (size_t i = 0; i < type->max_artifacts; ++i) {
+    std::string path = android::base::StringPrintf("%s/%s%02zu", type->dir_path.c_str(),
+                                                   type->file_name_prefix.c_str(), i);
     struct stat st;
     if (stat(path.c_str(), &st) != 0) {
       if (errno == ENOENT) {
@@ -101,35 +138,34 @@ static void find_oldest_tombstone() {
     }
   }
 
-  next_tombstone = oldest_tombstone;
+  type->next_artifact = oldest_tombstone;
 }
 
-static unique_fd get_tombstone_fd() {
+static unique_fd get_tombstone_fd(CrashType* type) {
   // If kMaxConcurrentDumps is greater than 1, then theoretically the same
   // filename could be handed out to multiple processes. Unlink and create the
   // file, instead of using O_TRUNC, to avoid two processes interleaving their
   // output.
   unique_fd result;
   char buf[PATH_MAX];
-  snprintf(buf, sizeof(buf), "tombstone_%02d", next_tombstone);
-  if (unlinkat(tombstone_directory_fd, buf, 0) != 0 && errno != ENOENT) {
-    PLOG(FATAL) << "failed to unlink tombstone at " << kTombstoneDirectory << buf;
+  snprintf(buf, sizeof(buf), "%s%02d", type->file_name_prefix.c_str(), type->next_artifact);
+  if (unlinkat(type->dir_fd, buf, 0) != 0 && errno != ENOENT) {
+    PLOG(FATAL) << "failed to unlink tombstone at " << type->dir_path << buf;
   }
 
-  result.reset(
-    openat(tombstone_directory_fd, buf, O_CREAT | O_EXCL | O_WRONLY | O_APPEND | O_CLOEXEC, 0640));
+  result.reset(openat(type->dir_fd, buf, O_CREAT | O_EXCL | O_WRONLY | O_APPEND | O_CLOEXEC, 0640));
   if (result == -1) {
-    PLOG(FATAL) << "failed to create tombstone at " << kTombstoneDirectory << buf;
+    PLOG(FATAL) << "failed to create tombstone at " << type->dir_path << buf;
   }
 
-  next_tombstone = (next_tombstone + 1) % kTombstoneCount;
+  type->next_artifact = (type->next_artifact + 1) % type->max_artifacts;
   return result;
 }
 
 static void perform_request(Crash* crash) {
   unique_fd output_fd;
   if (!intercept_manager->GetIntercept(crash->crash_pid, &output_fd)) {
-    output_fd = get_tombstone_fd();
+    output_fd = get_tombstone_fd(crash->crash_type);
   }
 
   TombstonedCrashPacket response = {
@@ -152,15 +188,16 @@ static void perform_request(Crash* crash) {
     event_add(crash->crash_event, &timeout);
   }
 
-  ++num_concurrent_dumps;
+  ++crash->crash_type->num_concurrent_dumps;
   return;
 
 fail:
   delete crash;
 }
 
-static void dequeue_requests() {
-  while (!queued_requests.empty() && num_concurrent_dumps < kMaxConcurrentDumps) {
+static void dequeue_requests(CrashType* type) {
+  std::deque<Crash*>& queued_requests = type->queued_requests;
+  while (!queued_requests.empty() && (type->num_concurrent_dumps < type->max_concurrent_dumps)) {
     Crash* next_crash = queued_requests.front();
     queued_requests.pop_front();
     perform_request(next_crash);
@@ -168,7 +205,7 @@ static void dequeue_requests() {
 }
 
 static void crash_accept_cb(evconnlistener* listener, evutil_socket_t sockfd, sockaddr*, int,
-                            void*) {
+                            void* artifact_type) {
   event_base* base = evconnlistener_get_base(listener);
   Crash* crash = new Crash();
 
@@ -176,12 +213,15 @@ static void crash_accept_cb(evconnlistener* listener, evutil_socket_t sockfd, so
   event* crash_event = event_new(base, sockfd, EV_TIMEOUT | EV_READ, crash_request_cb, crash);
   crash->crash_fd.reset(sockfd);
   crash->crash_event = crash_event;
+  crash->crash_type = reinterpret_cast<CrashType*>(artifact_type);
   event_add(crash_event, &timeout);
 }
 
 static void crash_request_cb(evutil_socket_t sockfd, short ev, void* arg) {
   ssize_t rc;
   Crash* crash = static_cast<Crash*>(arg);
+  CrashType* type = crash->crash_type;
+
   TombstonedCrashPacket request = {};
 
   if ((ev & EV_TIMEOUT) != 0) {
@@ -211,9 +251,9 @@ static void crash_request_cb(evutil_socket_t sockfd, short ev, void* arg) {
   crash->crash_pid = request.packet.dump_request.pid;
   LOG(INFO) << "received crash request for pid " << crash->crash_pid;
 
-  if (num_concurrent_dumps == kMaxConcurrentDumps) {
+  if (type->num_concurrent_dumps == type->max_concurrent_dumps) {
     LOG(INFO) << "enqueueing crash request for pid " << crash->crash_pid;
-    queued_requests.push_back(crash);
+    type->queued_requests.push_back(crash);
   } else {
     perform_request(crash);
   }
@@ -229,7 +269,7 @@ static void crash_completed_cb(evutil_socket_t sockfd, short ev, void* arg) {
   Crash* crash = static_cast<Crash*>(arg);
   TombstonedCrashPacket request = {};
 
-  --num_concurrent_dumps;
+  --crash->crash_type->num_concurrent_dumps;
 
   if ((ev & EV_READ) == 0) {
     goto fail;
@@ -252,10 +292,11 @@ static void crash_completed_cb(evutil_socket_t sockfd, short ev, void* arg) {
   }
 
 fail:
+  CrashType* type = crash->crash_type;
   delete crash;
 
   // If there's something queued up, let them proceed.
-  dequeue_requests();
+  dequeue_requests(type);
 }
 
 int main(int, char* []) {
@@ -269,22 +310,29 @@ int main(int, char* []) {
   };
   debuggerd_register_handlers(&action);
 
-  tombstone_directory_fd = open(kTombstoneDirectory, O_DIRECTORY | O_RDONLY | O_CLOEXEC);
-  if (tombstone_directory_fd == -1) {
+  if (CrashType::tombstone.dir_fd == -1) {
     PLOG(FATAL) << "failed to open tombstone directory";
   }
 
-  find_oldest_tombstone();
+  // TODO: Figure out a good way to flag guard this.
+  if (CrashType::java_trace.dir_fd == -1) {
+    PLOG(WARNING) << "failed to open tombstone directory";
+  }
+
+  find_oldest_artifact(&CrashType::tombstone);
+  find_oldest_artifact(&CrashType::java_trace);
 
   int intercept_socket = android_get_control_socket(kTombstonedInterceptSocketName);
   int crash_socket = android_get_control_socket(kTombstonedCrashSocketName);
+  int java_trace_socket = android_get_control_socket(kTombstonedJavaTraceSocketName);
 
-  if (intercept_socket == -1 || crash_socket == -1) {
+  if (intercept_socket == -1 || crash_socket == -1 || java_trace_socket == -1) {
     PLOG(FATAL) << "failed to get socket from init";
   }
 
   evutil_make_socket_nonblocking(intercept_socket);
   evutil_make_socket_nonblocking(crash_socket);
+  evutil_make_socket_nonblocking(java_trace_socket);
 
   event_base* base = event_base_new();
   if (!base) {
@@ -293,10 +341,16 @@ int main(int, char* []) {
 
   intercept_manager = new InterceptManager(base, intercept_socket);
 
-  evconnlistener* listener =
-    evconnlistener_new(base, crash_accept_cb, nullptr, -1, LEV_OPT_CLOSE_ON_FREE, crash_socket);
-  if (!listener) {
-    LOG(FATAL) << "failed to create evconnlistener";
+  evconnlistener* tombstone_listener = evconnlistener_new(
+      base, crash_accept_cb, &CrashType::tombstone, -1, LEV_OPT_CLOSE_ON_FREE, crash_socket);
+  if (!tombstone_listener) {
+    LOG(FATAL) << "failed to create evconnlistener for tombstones.";
+  }
+
+  evconnlistener* java_trace_listener = evconnlistener_new(
+      base, crash_accept_cb, &CrashType::java_trace, -1, LEV_OPT_CLOSE_ON_FREE, java_trace_socket);
+  if (!java_trace_listener) {
+    LOG(FATAL) << "failed to create evconnlistener for java traces.";
   }
 
   LOG(INFO) << "tombstoned successfully initialized";
