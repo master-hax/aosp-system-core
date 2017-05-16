13f1babf72ce41c9a2af5e174ebd97234bb9bbc9
Tom Cherry <tomcherry@google.com>
2017-05-16 15:35:41 -0700

    ueventd: parallelize uevent handling
    fork() subprocesses to handle uevents in parallel.
    
    This reduces coldboot time on bullhead from ~720ms to ~280ms.
    This reduces coldboot time on sailfish from ~690ms to ~360ms.
    
    Bug: 33785894
    
    Test: boot bullhead x40, observe no major differences in /dev and /sys
    Test: boot sailfish x40, observe no major differences in /dev and /sys
    Test: boottime tests on bullhead and sailfish
    Test: init unit tests
    
    Change-Id: Ie2f63e000b8af78d187477d31fe109f20304d749

--- init/devices.cpp
+++ init/devices.cpp
@@ -171,7 +171,7 @@ void DeviceHandler::FixupSysPermissions(const std::string& upath,
         if (s.MatchWithSubsystem(path, subsystem)) s.SetPermissions(path);
     }
 
-    if (access(path.c_str(), F_OK) == 0) {
+    if (!is_booting_ && access(path.c_str(), F_OK) == 0) {
         LOG(VERBOSE) << "restorecon_recursive: " << path;
         if (selinux_android_restorecon(path.c_str(), SELINUX_ANDROID_RESTORECON_RECURSE) != 0) {
             PLOG(ERROR) << "selinux_android_restorecon(" << path << ") failed";
@@ -463,12 +463,13 @@ void DeviceHandler::HandleDeviceEvent(const Uevent& uevent) {
 
 DeviceHandler::DeviceHandler(std::vector<Permissions> dev_permissions,
                              std::vector<SysfsPermissions> sysfs_permissions,
-                             std::vector<Subsystem> subsystems)
+                             std::vector<Subsystem> subsystems, bool is_booting)
     : dev_permissions_(std::move(dev_permissions)),
       sysfs_permissions_(std::move(sysfs_permissions)),
       subsystems_(std::move(subsystems)),
-      sehandle_(selinux_android_file_context_handle()) {}
+      sehandle_(selinux_android_file_context_handle()),
+      is_booting_(is_booting) {}
 
 DeviceHandler::DeviceHandler()
     : DeviceHandler(std::vector<Permissions>{}, std::vector<SysfsPermissions>{},
-                    std::vector<Subsystem>{}) {}
+                    std::vector<Subsystem>{}, false) {}
