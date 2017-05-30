eb2360c57782652e8e266fca87b6e66f0900150b
Sebastian Pop <s.pop@samsung.com>
2017-05-30 09:14:20 -0500

    add a performance benchmark for libziparchive
    This patch adds two benchmarks that measure the performance of some
    operations of libziparchive.
    
    Both benchmarks are creating a temporary zip file containing file
    names of uniformly distributed lengths.  The creation of the zip
    file is not timed in the benchmarks.
    
    - FindEntry_no_match tries to find an inexisting entry in the files
      of the zip archive, in order to force the code to examine all the
      files in the archive.
    
    - Iterate_all_files uses the iterate function to list all the files
      in the archive.
    
    To build the benchmark and to run on a device:
    
    $ source build/envsetup.sh
    $ lunch hikey-userdebug
    $ cd system/core/libziparchive
    $ mma &> build.log
    $ cd -
    $ adb push out/target/product/hikey/data/nativetest64/ziparchive-benchmarks/ziparchive-benchmarks /data/
    $ adb shell /data/ziparchive-benchmarks
    Run on (8 X 2.4 MHz CPU s)
    1970-01-01 00:08:50
    ***WARNING*** CPU scaling is enabled, the benchmark real time measurements may be noisy and will incur extra overhead.
    Benchmark                   Time           CPU Iterations
    ---------------------------------------------------------
    FindEntry_no_match   12853318 ns   12853321 ns         54
    Iterate_all_files    23822639 ns   23804554 ns         30
    
    Change-Id: Ibdb524ba1c5ae55caddf0416ebbc09f8b6df0021

---
+++ libziparchive/zip_archive_perf.cc
@@ -0,0 +1,87 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "benchmark/benchmark.h"
+#include <string>
+#include <cstring>
+#include <cstdlib>
+#include <cstdio>
+#include <iostream>
+#include <vector>
+#include <tuple>
+
+#include <ziparchive/zip_archive.h>
+#include <ziparchive/zip_writer.h>
+#include <ziparchive/zip_archive_stream_entry.h>
+#include <android-base/test_utils.h>
+
+static TemporaryFile* createZip() {
+  TemporaryFile* temp_file_ = new TemporaryFile();
+  int fd_ = temp_file_->fd;
+  FILE* file_ = fdopen(fd_, "w");
+
+  ZipWriter writer(file_);
+  std::string lastName = "file";
+  for (int i = 0; i < 1000; i++) {
+    // Make file names longer and longer.
+    lastName = lastName + std::to_string(i);
+    writer.StartEntry(lastName.c_str(), ZipWriter::kCompress);
+    writer.WriteBytes("helo", 4);
+    writer.FinishEntry();
+  }
+  writer.Finish();
+  fclose(file_);
+
+  return temp_file_;
+}
+
+static void FindEntry_no_match(benchmark::State& state) {
+  // Create a temporary zip archive.
+  std::unique_ptr<TemporaryFile> temp_file_(createZip());
+  ZipArchiveHandle handle;
+  ZipEntry data;
+
+  // In order to walk through all file names in the archive, look for a name
+  // that does not exist in the archive.
+  ZipString name("thisFileNameDoesNotExist");
+
+  // Start the benchmark.
+  while (state.KeepRunning()) {
+    OpenArchive(temp_file_->path, &handle);
+    FindEntry(handle, name, &data);
+    CloseArchive(handle);
+  }
+}
+BENCHMARK(FindEntry_no_match);
+
+static void Iterate_all_files(benchmark::State& state) {
+  std::unique_ptr<TemporaryFile> temp_file_(createZip());
+  ZipArchiveHandle handle;
+  void* iteration_cookie;
+  ZipEntry data;
+  ZipString name;
+
+  while (state.KeepRunning()) {
+    OpenArchive(temp_file_->path, &handle);
+    StartIteration(handle, &iteration_cookie, nullptr, nullptr);
+    while (0 == Next(iteration_cookie, &data, &name)) {}
+    EndIteration(iteration_cookie);
+    CloseArchive(handle);
+  }
+}
+BENCHMARK(Iterate_all_files);
+
+BENCHMARK_MAIN()
