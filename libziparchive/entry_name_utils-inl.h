66278cf07fa4757135227b29154f15e203fc701f
Sebastian Pop <s.pop@samsung.com>
2017-06-05 17:06:38 -0500

    ziparchive: vectorize IsValidEntryName
    improve the performance of IsValidEntryName by vectorizing the code.
    Patch written by Vikas Sinha and Sebastian Pop.
    
    The following experiments were conducted on a HiKey aarch64 A53.
    
    No changes:
    $ adb push out/target/product/hikey/data/benchmarktest64/ziparchive-benchmarks /data
    $ adb shell /data/ziparchive-benchmarks --benchmark_repetitions=5
    -----------------------------------------------------------------
    Benchmark                          Time           CPU Iterations
    -----------------------------------------------------------------
    FindEntry_no_match          82279629 ns   81759676 ns          9
    FindEntry_no_match          81648056 ns   81627431 ns          9
    FindEntry_no_match          81384074 ns   81369057 ns          9
    FindEntry_no_match          89618889 ns   82437755 ns          9
    FindEntry_no_match          81811389 ns   81785261 ns          9
    FindEntry_no_match_mean     83348407 ns   81795836 ns          9
    FindEntry_no_match_stddev    3520421 ns     394962 ns          0
    Iterate_all_files          137622000 ns  137589032 ns          5
    Iterate_all_files          139666333 ns  138409469 ns          5
    Iterate_all_files          150070000 ns  140883697 ns          5
    Iterate_all_files          138600667 ns  138540646 ns          5
    Iterate_all_files          137599833 ns  137567438 ns          5
    Iterate_all_files_mean     140711767 ns  138598056 ns          5
    Iterate_all_files_stddev     5299929 ns    1354928 ns          0
    
    $ cd system/core/libziparchive
    $ git am 0001-ziparchive-vectorize-IsValidEntryName.patch
    $ mma
    $ cd -
    $ adb push out/target/product/hikey/data/benchmarktest64/ziparchive-benchmarks /data
    $ adb shell /data/ziparchive-benchmarks --benchmark_repetitions=5
    -----------------------------------------------------------------
    Benchmark                          Time           CPU Iterations
    -----------------------------------------------------------------
    FindEntry_no_match          45218611 ns   45218636 ns         15
    FindEntry_no_match          45387889 ns   45382090 ns         15
    FindEntry_no_match          45745111 ns   45418330 ns         15
    FindEntry_no_match          46739667 ns   46022570 ns         15
    FindEntry_no_match          45780611 ns   45769372 ns         15
    FindEntry_no_match_mean     45774378 ns   45562199 ns         15
    FindEntry_no_match_stddev     589688 ns     326357 ns          0
    Iterate_all_files          102928333 ns  102206109 ns          7
    Iterate_all_files          101769405 ns  101769130 ns          7
    Iterate_all_files          103544643 ns  103485313 ns          7
    Iterate_all_files          102281548 ns  102254986 ns          7
    Iterate_all_files          102378690 ns  101690038 ns          7
    Iterate_all_files_mean     102580524 ns  102281115 ns          7
    Iterate_all_files_stddev      677963 ns     719010 ns          0
    
    Change-Id: I38279f887ae5c75832b047a7e8a4de1ee649ab41

--- libziparchive/entry_name_utils-inl.h
+++ libziparchive/entry_name_utils-inl.h
@@ -20,9 +20,8 @@
 #include <stddef.h>
 #include <stdint.h>
 
-// Check if |length| bytes at |entry_name| constitute a valid entry name.
-// Entry names must be valid UTF-8 and must not contain '0'.
-inline bool IsValidEntryName(const uint8_t* entry_name, const size_t length) {
+// Default implementation of IsValidEntryName to validate entry_name byte by byte.
+inline bool IsValidEntryNameByteByByte(const uint8_t* entry_name, const size_t length) {
   for (size_t i = 0; i < length; ++i) {
     const uint8_t byte = entry_name[i];
     if (byte == 0) {
@@ -53,6 +52,104 @@ inline bool IsValidEntryName(const uint8_t* entry_name, const size_t length) {
   }
 
   return true;
+}
+
+// This is a specialized version of IsValidEntryName when the name contains
+// sequences longer than 15 bytes.
+inline bool IsValidEntryNameLenGe16(const uint8_t* entry_name, size_t length) {
+  uint64_t chunk8[2];
+  constexpr unsigned sz16 = 2 * sizeof(uint64_t);
+
+  // Make a 8x vector with 0x01 and 0x80 in each byte.
+  constexpr uint64_t kVdup8_01 = 0x0101010101010101;
+  constexpr uint64_t kVdup8_80 = 0x8080808080808080;
+
+  // First bytes to align length to 16.
+  size_t incr = (length % sz16) ? (length % sz16) : sz16;
+
+  while (length > 0) {
+    // Load 128bit.
+    __builtin_memcpy(chunk8, entry_name, sz16);
+
+    // Is there any element in vector with value >127 (multiple byte sequence)?
+    // ((byte & 0x80 ) != 0).
+    if (chunk8[0] & kVdup8_80 || chunk8[1] & kVdup8_80)
+      // Validate the rest of this entry with the UTF-8 validator.
+      return IsValidEntryNameByteByByte(entry_name, length);
+
+    // Is there any element in vector with value = 0 (NUL)?
+    // Full zero byte check for 0-255:
+    // (X - 1) & (~X) & 0x80 != 0 ==> at least one byte is zero across word.
+    // Since we already check across vector that all bytes <128
+    // zero check for 0-127: (X - 1) & 0x80 != 0 ==> at least one byte is zero
+    // across word.
+    if ((chunk8[0] - kVdup8_01) & kVdup8_80 ||
+        (chunk8[1] - kVdup8_01) & kVdup8_80)
+      return false;
+
+    // None of the bytes are zero OR >127
+    // all of them are single byte UTF-8 sequence.
+    entry_name += incr;
+    length -= incr;
+    incr = sz16;
+  }
+
+  // All bytes matched single byte sequence.
+  return true;
+}
+
+// Copy of the >= 16 length function to optimize length=8-15 case.
+inline bool IsValidEntryNameLenGe8(const uint8_t* entry_name, size_t length) {
+  uint64_t chunk8;
+  constexpr unsigned sz8 = sizeof(chunk8);
+
+  // Make a 8x vector with 0x01 and 0x80 in each byte.
+  constexpr uint64_t kVdup8_01 = 0x0101010101010101;
+  constexpr uint64_t kVdup8_80 = 0x8080808080808080;
+
+  // First bytes to align length to 8.
+  size_t incr = (length % sz8) ? (length % sz8) : sz8;
+
+  while (length > 0) {
+    // Load 64bit.
+    __builtin_memcpy(&chunk8, entry_name, sz8);
+
+    // Is there any element in vector with value >127 (multiple byte sequence)?
+    // ((byte & 0x80 ) != 0).
+    if (chunk8 & kVdup8_80)
+      // Validate the rest of this entry with the UTF-8 validator.
+      return IsValidEntryNameByteByByte(entry_name, length);
+
+    // Is there any element in vector with value = 0 (NUL)?
+    // Full zero byte check for 0-255:
+    // (X - 1) & (~X) & 0x80 != 0 ==> at least one byte is zero across word.
+    // Since we already check across vector that all bytes <128
+    // zero check for 0-127: (X - 1) & 0x80 != 0 ==> at least one byte is zero
+    // across word.
+    if ((chunk8 - kVdup8_01) & kVdup8_80)
+      return false;
+
+    // None of the bytes are zero OR >127
+    // all of them single byte UTF-8 sequence.
+    entry_name += incr;
+    length -= incr;
+    incr = sz8;
+  }
+
+  // All bytes matched single byte sequence.
+  return true;
+}
+
+// Check if |length| bytes at |entry_name| constitute a valid entry name.
+// Entry names must be valid UTF-8 and must not contain '0'.
+inline bool IsValidEntryName(const uint8_t* entry_name, const size_t length) {
+  if (length >= 16)
+    return IsValidEntryNameLenGe16(entry_name, length);
+
+  if (length >= 8)
+    return IsValidEntryNameLenGe8(entry_name, length);
+
+  return IsValidEntryNameByteByByte(entry_name, length);
 }
 
 #endif  // LIBZIPARCHIVE_ENTRY_NAME_UTILS_INL_H_
