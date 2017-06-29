fb4e7acff888ec46af20334aa50a4999c05603cf
Luis Hector Chavez <lhchavez@google.com>
2017-06-29 09:50:30 -0700

    init: Reland "Terminate gracefully when CAP_SYS_BOOT is absent"
    This change makes it possible for Android running in a container to
    terminate cleanly instead of calling abort() when requested to shut
    down.
    
    Bug: 62388055
    Test: `adb reboot` on bullhead causes no kernel panics
    Test: `adb reboot` on a system without CAP_SYS_BOOT makes init terminate
           nicely
    
    Change-Id: I36b2298610f5b4a2bf8b05103d04804883df2c88

--- init/capabilities.h
+++ init/capabilities.h
@@ -15,15 +15,17 @@
 #ifndef _INIT_CAPABILITIES_H
 #define _INIT_CAPABILITIES_H
 
-#include <linux/capability.h>
+#include <sys/capability.h>
 
 #include <bitset>
 #include <string>
+#include <type_traits>
 
 namespace android {
 namespace init {
 
 using CapSet = std::bitset<CAP_LAST_CAP + 1>;
+using ScopedCaps = std::unique_ptr<std::remove_pointer<cap_t>::type, decltype(&cap_free)>;
 
 int LookupCap(const std::string& cap_name);
 bool CapAmbientSupported();
