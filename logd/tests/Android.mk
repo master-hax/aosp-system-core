df0d8d8b3c42f6c8fc0cb272ded00faf9205c07a
Jayant Chowdhary <jchowdhary@google.com>
2017-08-10 15:03:34 -0700

    Remove sysutils global includes.
    Test: make -j64.
    
    Bug: 33241851
    
    Change-Id: Ie61e8880952d582225129c540a1b15cea1534cb1

--- logd/tests/Android.mk
+++ logd/tests/Android.mk
@@ -47,6 +47,7 @@ LOCAL_MODULE := $(test_module_prefix)unit-tests
 LOCAL_MODULE_TAGS := $(test_tags)
 LOCAL_CFLAGS += $(test_c_flags)
 LOCAL_SHARED_LIBRARIES := libbase libcutils liblog libselinux
+LOCAL_HEADER_LIBRARIES := sysutils_headers
 LOCAL_SRC_FILES := $(test_src_files)
 include $(BUILD_NATIVE_TEST)
 
@@ -63,6 +64,7 @@ LOCAL_MODULE_STEM_32 := $(LOCAL_MODULE)32
 LOCAL_MODULE_STEM_64 := $(LOCAL_MODULE)64
 LOCAL_SHARED_LIBRARIES := libbase libcutils liblog libselinux
 LOCAL_STATIC_LIBRARIES := libgtest libgtest_main
+LOCAL_HEADER_LIBRARIES := sysutils_headers
 LOCAL_COMPATIBILITY_SUITE := cts
 LOCAL_CTS_TEST_PACKAGE := android.core.logd
 include $(BUILD_CTS_EXECUTABLE)
@@ -80,6 +82,7 @@ LOCAL_MODULE_STEM_32 := $(LOCAL_MODULE)
 LOCAL_MODULE_STEM_64 := $(LOCAL_MODULE)64
 LOCAL_CXX_STL := libc++
 LOCAL_SHARED_LIBRARIES := libbase libcutils liblog
+LOCAL_HEADER_LIBRARIES := sysutils_headers
 LOCAL_ADDITIONAL_DEPENDENCIES := $(LOCAL_PATH)/Android.mk
 include $(BUILD_HOST_NATIVE_TEST)
 
