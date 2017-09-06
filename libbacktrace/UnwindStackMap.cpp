4f9876710a797f6aabaa41319cac2f48b5aa25bb
Josh Gao <jmgao@google.com>
2017-09-06 15:39:14 -0700

    libbacktrace: return the process memory in BacktraceMap::CreateNew.
    Test: mma
    Change-Id: Icd2b891b121b90d55e3ac45037a59c24221a2496

--- libbacktrace/UnwindStackMap.cpp
+++ libbacktrace/UnwindStackMap.cpp
@@ -75,10 +75,32 @@ void UnwindStackMap::FillIn(uintptr_t addr, backtrace_map_t* map) {
   map->load_bias = elf->GetLoadBias();
 }
 
+std::string UnwindStackMap::GetFunctionName(uintptr_t pc, uintptr_t* offset) {
+  *offset = 0;
+  unwindstack::Maps* maps = stack_maps();
+
+  // Get the map for this
+  unwindstack::MapInfo* map_info = maps->Find(pc);
+  if (map_info == nullptr || map_info->flags & PROT_DEVICE_MAP) {
+    return "";
+  }
+
+  unwindstack::Elf* elf = map_info->GetElf(process_memory(), true);
+
+  std::string name;
+  uint64_t func_offset;
+  if (!elf->GetFunctionName(elf->GetRelPc(pc, map_info), &name, &func_offset)) {
+    return "";
+  }
+  *offset = func_offset;
+  return name;
+}
+
 //-------------------------------------------------------------------------
 // BacktraceMap create function.
 //-------------------------------------------------------------------------
-BacktraceMap* BacktraceMap::CreateNew(pid_t pid, bool uncached) {
+BacktraceMap* BacktraceMap::CreateNew(pid_t pid, bool uncached,
+                                      std::shared_ptr<unwindstack::Memory>* process_memory) {
   BacktraceMap* map;
 
   if (uncached) {
@@ -93,5 +115,10 @@ BacktraceMap* BacktraceMap::CreateNew(pid_t pid, bool uncached) {
     delete map;
     return nullptr;
   }
+
+  if (!uncached && process_memory) {
+    *process_memory = static_cast<UnwindStackMap*>(map)->process_memory();
+  }
+
   return map;
 }
