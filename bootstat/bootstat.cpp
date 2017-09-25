b8e202fe48aa44cffe93a0b5b49b05ff740147e0
Mark Salyzyn <salyzyn@google.com>
2017-09-25 13:10:11 -0700

    init: support suspend and hibernation
    Create pseudo shutdown,suspend and shutdown,hibernate requests, making
    init the hardware abstraction layer to request these operations and to
    perform a forced operation with any necessary low-level cleanup.  The
    request to the kernel to perform the suspend or hibernate operations
    are gated with "ro.support.suspend" and "ro.support.hibernate"
    properties respectively, it is the responsibility of the platform to
    enable these if required by setting the property to a value of 1,
    otherwise they default to disabled.
    
    Test: compile
    Bug: 63736262
    Bug: 38446744
    Bug: 66912053
    Change-Id: I1f3911269e869a24d700e11c03a838a18da3b3a9

--- bootstat/bootstat.cpp
+++ bootstat/bootstat.cpp
@@ -205,6 +205,8 @@ const std::map<std::string, int32_t> kBootReasonMap = {
     {"reboot,adb", 67},
     {"reboot,userrequested", 68},
     {"shutdown,sigterm", 69},
+    {"shutdown,suspend", 70},    // Suspend to RAM
+    {"shutdown,hibernate", 71},  // Suspend to disk
 };
 
 // Converts a string value representing the reason the system booted to an
