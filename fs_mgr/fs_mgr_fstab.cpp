aa90bb133343bd27f52e0c81a21dd6742fbf8c65
Bowgo Tsai <bowgotsai@google.com>
2017-09-27 18:00:10 +0800

    fs_mgr_fstab: removing fs_mgr_get_entry_for_mount_point_after()
    No one is using fs_mgr_get_entry_for_mount_point_after(), which supports
    matching a mount point iteratively by specifying a starting entry in the
    fstab. Rename the function, set it as static and cleans up unneeded sources
    related to the starting entry.
    
    Also re-formats other functions to comply with .clang-format.
    
    Bug: None
    Test: boot sailfish, manually test fs_mgr_get_entry_for_mount_point().
    
    Change-Id: I38e18fa6bd9485b6cd31b55474406a511df3f506

--- fs_mgr/fs_mgr_fstab.cpp
+++ fs_mgr/fs_mgr_fstab.cpp
@@ -641,6 +641,24 @@ static struct fstab *in_place_merge(struct fstab *a, struct fstab *b)
     return a;
 }
 
+/*
+ * Returns the fstab_rec* whose mount_point is path.
+ * Returns nullptr if not found.
+ */
+static struct fstab_rec* do_get_entry_for_mount_point(struct fstab* fstab, const std::string& path) {
+    if (!fstab) {
+        return nullptr;
+    }
+
+    for (int i = 0; i < fstab->num_entries; i++) {
+        if (fstab->recs[i].mount_point && path == fstab->recs[i].mount_point) {
+            return &fstab->recs[i];
+        }
+    }
+
+    return nullptr;
+}
+
 struct fstab *fs_mgr_read_fstab(const char *fstab_path)
 {
     FILE *fstab_file;
@@ -778,100 +796,51 @@ int fs_mgr_add_entry(struct fstab *fstab,
      return 0;
 }
 
-/*
- * Returns the 1st matching fstab_rec that follows the start_rec.
- * start_rec is the result of a previous search or NULL.
- */
-struct fstab_rec* fs_mgr_get_entry_for_mount_point_after(struct fstab_rec* start_rec,
-                                                         struct fstab* fstab,
-                                                         const std::string& path) {
-    int i;
-    if (!fstab) {
-        return nullptr;
-    }
-
-    if (start_rec) {
-        for (i = 0; i < fstab->num_entries; i++) {
-            if (&fstab->recs[i] == start_rec) {
-                i++;
-                break;
-            }
-        }
-    } else {
-        i = 0;
-    }
-
-    for (; i < fstab->num_entries; i++) {
-        if (fstab->recs[i].mount_point && path == fstab->recs[i].mount_point) {
-            return &fstab->recs[i];
-        }
-    }
-
-    return nullptr;
+struct fstab_rec* fs_mgr_get_entry_for_mount_point(struct fstab* fstab, const char* path) {
+    return do_get_entry_for_mount_point(fstab, path);
 }
 
-/*
- * Returns the 1st matching mount point.
- * There might be more. To look for others, use fs_mgr_get_entry_for_mount_point_after()
- * and give the fstab_rec from the previous search.
- */
-struct fstab_rec *fs_mgr_get_entry_for_mount_point(struct fstab *fstab, const char *path)
-{
-    return fs_mgr_get_entry_for_mount_point_after(NULL, fstab, path);
-}
-
-int fs_mgr_is_voldmanaged(const struct fstab_rec *fstab)
-{
+int fs_mgr_is_voldmanaged(const struct fstab_rec* fstab) {
     return fstab->fs_mgr_flags & MF_VOLDMANAGED;
 }
 
-int fs_mgr_is_nonremovable(const struct fstab_rec *fstab)
-{
+int fs_mgr_is_nonremovable(const struct fstab_rec* fstab) {
     return fstab->fs_mgr_flags & MF_NONREMOVABLE;
 }
 
-int fs_mgr_is_verified(const struct fstab_rec *fstab)
-{
+int fs_mgr_is_verified(const struct fstab_rec* fstab) {
     return fstab->fs_mgr_flags & MF_VERIFY;
 }
 
-int fs_mgr_is_avb(const struct fstab_rec *fstab)
-{
+int fs_mgr_is_avb(const struct fstab_rec* fstab) {
     return fstab->fs_mgr_flags & MF_AVB;
 }
 
-int fs_mgr_is_verifyatboot(const struct fstab_rec *fstab)
-{
+int fs_mgr_is_verifyatboot(const struct fstab_rec* fstab) {
     return fstab->fs_mgr_flags & MF_VERIFYATBOOT;
 }
 
-int fs_mgr_is_encryptable(const struct fstab_rec *fstab)
-{
+int fs_mgr_is_encryptable(const struct fstab_rec* fstab) {
     return fstab->fs_mgr_flags & (MF_CRYPT | MF_FORCECRYPT | MF_FORCEFDEORFBE);
 }
 
-int fs_mgr_is_file_encrypted(const struct fstab_rec *fstab)
-{
+int fs_mgr_is_file_encrypted(const struct fstab_rec* fstab) {
     return fstab->fs_mgr_flags & MF_FILEENCRYPTION;
 }
 
-void fs_mgr_get_file_encryption_modes(const struct fstab_rec *fstab,
-                                      const char **contents_mode_ret,
-                                      const char **filenames_mode_ret)
-{
+void fs_mgr_get_file_encryption_modes(const struct fstab_rec* fstab, const char** contents_mode_ret,
+                                      const char** filenames_mode_ret) {
     *contents_mode_ret = flag_to_encryption_mode(file_contents_encryption_modes,
                                                  fstab->file_contents_mode);
     *filenames_mode_ret = flag_to_encryption_mode(file_names_encryption_modes,
                                                   fstab->file_names_mode);
 }
 
-int fs_mgr_is_convertible_to_fbe(const struct fstab_rec *fstab)
-{
+int fs_mgr_is_convertible_to_fbe(const struct fstab_rec* fstab) {
     return fstab->fs_mgr_flags & MF_FORCEFDEORFBE;
 }
 
-int fs_mgr_is_noemulatedsd(const struct fstab_rec *fstab)
-{
+int fs_mgr_is_noemulatedsd(const struct fstab_rec* fstab) {
     return fstab->fs_mgr_flags & MF_NOEMULATEDSD;
 }
 
