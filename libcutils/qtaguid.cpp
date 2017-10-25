d83e618cc993448bfac9d811c7ef00b11fffe675
Chenbo Feng <fengc@google.com>
2017-10-25 12:31:43 -0700

    Redirect qtaguid native call to netd fwmark client
    In order to replace qtaguid module with new eBPF network monitoring
    module. We firstly move the current qtaguid userspace implementation
    into netd and hide the detail from other processes. The current API will
    talk to netd fwmark client to pass down the qtaguid related request from
    high level framework and netd will use the proper method to complete the
    request.
    
    Test: Current TrafficStats CTS tests should not fail.
    Bug: 30950746
    Change-Id: Ie90c28f3594ab2877746b2372a1b6944768bfb18

---
+++ libcutils/qtaguid.cpp
@@ -0,0 +1,140 @@
+/*
+** Copyright 2017, The Android Open Source Project
+**
+** Licensed under the Apache License, Version 2.0 (the "License");
+** you may not use this file except in compliance with the License.
+** You may obtain a copy of the License at
+**
+**     http://www.apache.org/licenses/LICENSE-2.0
+**
+** Unless required by applicable law or agreed to in writing, software
+** distributed under the License is distributed on an "AS IS" BASIS,
+** WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+** See the License for the specific language governing permissions and
+** limitations under the License.
+*/
+
+// #define LOG_NDEBUG 0
+
+#define LOG_TAG "qtaguid"
+
+#include <dlfcn.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <inttypes.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <cutils/qtaguid.h>
+#include <log/log.h>
+
+/*
+ * One per proccess.
+ * Once the device is open, this process will have its socket tags tracked.
+ * And on exit or untimely death, all socket tags will be removed.
+ * A process can only open /dev/xt_qtaguid once.
+ * It should not close it unless it is really done with all the socket tags.
+ * Failure to open it will be visible when socket tagging will be attempted.
+ */
+pthread_once_t resTrackInitDone = PTHREAD_ONCE_INIT;
+
+/* Load and link the libnetd_client method with qtaguid module. It is called
+ * once per process.
+ * TODO: Maybe we can find a way to do this one time and all processes can
+ * directly use it later?
+ */
+
+class netdHandler {
+  public:
+    int (*netdTagSocket)(int, uint32_t, uid_t);
+    int (*netdUntagSocket)(int);
+    int (*netdSetCounterSet)(uint32_t, uid_t);
+    int (*netdDeleteTagData)(uint32_t, uid_t);
+};
+
+netdHandler init(void) {
+    char* error;
+    netdHandler handler;
+    void* netdClientHandle = dlopen("libnetd_client.so", RTLD_NOW);
+    if (!netdClientHandle) {
+        error = dlerror();
+        ALOGE("Failed to open libnetd_client.so: %s", error);
+        return handler;
+    }
+
+    dlerror();
+    handler.netdTagSocket = (int (*)(int, uint32_t, uid_t))dlsym(netdClientHandle, "tagSocket");
+    if (!handler.netdTagSocket) {
+        error = dlerror();
+        ALOGE("load netdTagSocket handler failed: %s\n", error);
+    }
+
+    handler.netdUntagSocket = (int (*)(int))dlsym(netdClientHandle, "untagSocket");
+    if (!handler.netdUntagSocket) {
+        error = dlerror();
+        ALOGE("load netdUntagSocket handler failed: %s\n", error);
+    }
+
+    handler.netdSetCounterSet = (int (*)(uint32_t, uid_t))dlsym(netdClientHandle, "setCounterSet");
+    if (!handler.netdSetCounterSet) {
+        error = dlerror();
+        ALOGE("load netdSetCounterSet handler failed: %s\n", error);
+    }
+
+    handler.netdDeleteTagData = (int (*)(uint32_t, uid_t))dlsym(netdClientHandle, "deleteTagData");
+    if (!handler.netdDeleteTagData) {
+        error = dlerror();
+        ALOGE("load netdDeleteTagData handler failed: %s\n", error);
+    }
+    return handler;
+}
+
+static netdHandler qtaguidHandler = init();
+
+int qtaguid_tagSocket(int sockfd, int tag, uid_t uid) {
+    int res;
+
+    // Check the socket fd passed to us is still valid before we load the netd
+    // client. Pass a already closed socket fd to netd client may let netd open
+    // the unix socket with the same fd number and pass it to server for
+    // tagging.
+    res = fcntl(sockfd, F_GETFD);
+    if (res < 0) return res;
+
+    ALOGV("Tagging socket %d with tag %u for uid %d", sockfd, tag, uid);
+
+    res = qtaguidHandler.netdTagSocket(sockfd, tag, uid);
+
+    return res;
+}
+
+int qtaguid_untagSocket(int sockfd) {
+    int res;
+
+    ALOGV("Untagging socket %d", sockfd);
+
+    res = qtaguidHandler.netdUntagSocket(sockfd);
+
+    return res;
+}
+
+int qtaguid_setCounterSet(int counterSetNum, uid_t uid) {
+    int res;
+
+    ALOGV("Setting counters to set %d for uid %d", counterSetNum, uid);
+
+    res = qtaguidHandler.netdSetCounterSet(counterSetNum, uid);
+    return res;
+}
+
+int qtaguid_deleteTagData(int tag, uid_t uid) {
+    int res;
+
+    ALOGV("Deleting tag data with tag %u for uid %d", tag, uid);
+
+    res = qtaguidHandler.netdDeleteTagData(tag, uid);
+
+    return res;
+}
