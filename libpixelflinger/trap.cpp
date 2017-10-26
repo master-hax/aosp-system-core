247ab8824cfaf84c460132057e26cee9e67b79cf
Chih-Hung Hsieh <chh@google.com>
2017-10-26 11:19:43 -0700

    Use -Werror in system/core/libpixelflinger
    * Remove unused local variables.
    * Suppress/fix warning of unused functions, conditionally used variables.
    
    Bug: 66996870
    Test: build with WITH_TIDY=1
    Change-Id: If92bc79f15a0486364da0389defbb2d43b3bd6ea

--- libpixelflinger/trap.cpp
+++ libpixelflinger/trap.cpp
@@ -350,6 +350,7 @@ void linex_validate(void *con, const GGLcoord* v0, const GGLcoord* v1, GGLcoord 
 static void linex(void *con, const GGLcoord* v0, const GGLcoord* v1, GGLcoord width)
 {
     GGL_CONTEXT(c, con);
+    (void)c;  // suppress unused warning
     GGLcoord v[4][2];
     v[0][0] = v0[0];    v[0][1] = v0[1];
     v[1][0] = v1[0];    v[1][1] = v1[1];
@@ -378,6 +379,7 @@ static void linex(void *con, const GGLcoord* v0, const GGLcoord* v1, GGLcoord wi
 static void aa_linex(void *con, const GGLcoord* v0, const GGLcoord* v1, GGLcoord width)
 {
     GGL_CONTEXT(c, con);
+    (void)c;  // suppress unused warning
     GGLcoord v[4][2];
     v[0][0] = v0[0];    v[0][1] = v0[1];
     v[1][0] = v1[0];    v[1][1] = v1[1];
