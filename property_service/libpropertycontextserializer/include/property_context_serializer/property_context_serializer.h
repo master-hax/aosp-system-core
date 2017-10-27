f9fdec8d616166a4be641ff95f33103f33987afa
Tom Cherry <tomcherry@google.com>
2017-10-27 15:18:02 -0700

    Parse property contexts via a serialized trie
    Currently, whenever a new program starts, libc initializes two data
    structures related to properties from the raw property_context files.
    There are two problems here,
    1) This takes roughly 1.2ms on a trivial program to generate contents
       that could otherwise be cached.
    2) One of the data structures is a descending list of prefixes, each
       of which needs to be checked, whereas a trie would be more
       efficient.
    
    This change introduces two libraries,
    1) libpropertycontextserializer meant to be used by property_service
       to create a serialized trie containing all of the property
       contexts.
    2) libpropertycontextparser meant to be used by libc's property
       functions to parse this serialized trie during property lookup.
    
    This new trie also contains the ability to have exact matches instead
    of prefix matches for properties, which was not possible before.
    
    Bug: 36001741
    Change-Id: I42324f04c4d995a0e055e9685d79f40393dfca51

---
+++ property_service/libpropertycontextserializer/include/property_context_serializer/property_context_serializer.h
@@ -0,0 +1,33 @@
+//
+// Copyright (C) 2017 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+#ifndef PROPERTY_CONTEXT_SERIALIZER_H
+#define PROPERTY_CONTEXT_SERIALIZER_H
+
+#include <string>
+#include <vector>
+
+namespace android {
+namespace properties {
+
+bool BuildTrie(const std::vector<std::pair<std::string, std::string>>& prefixes_with_context,
+               const std::vector<std::pair<std::string, std::string>>& exact_matches_with_context,
+               const std::string& default_context, std::string* serialized_trie, std::string* error);
+
+}  // namespace properties
+}  // namespace android
+
+#endif
---
+++ property_service/libpropertycontextserializer/property_context_serializer.cpp
@@ -0,0 +1,57 @@
+//
+// Copyright (C) 2017 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+#include "property_context_serializer/property_context_serializer.h"
+
+#include "property_context_parser/property_context_parser.h"
+
+#include <set>
+
+#include "trie_builder.h"
+#include "trie_serializer.h"
+
+namespace android {
+namespace properties {
+
+bool BuildTrie(const std::vector<std::pair<std::string, std::string>>& prefixes_with_context,
+               const std::vector<std::pair<std::string, std::string>>& exact_matches_with_context,
+               const std::string& default_context, std::string* serialized_trie,
+               std::string* error) {
+  // Check that names are legal first
+
+  auto builder_root = TrieBuilderNode("root");
+  auto contexts = std::set<std::string>();
+  contexts.emplace(default_context);
+  builder_root.set_context(&(*contexts.find(default_context)));
+
+  for (const auto& [prefix, context] : prefixes_with_context) {
+    if (!AddToTrie(&builder_root, &contexts, prefix, context, false, error)) {
+      return false;
+    }
+  }
+  for (const auto& [exact_match, context] : exact_matches_with_context) {
+    if (!AddToTrie(&builder_root, &contexts, exact_match, context, true, error)) {
+      return false;
+    }
+  }
+
+  auto trie_serializer = TrieSerializer();
+  *serialized_trie = trie_serializer.SerializeTrie(builder_root, contexts);
+  return true;
+}
+
+}  // namespace properties
+}  // namespace android
