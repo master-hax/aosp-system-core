ede3267cea73f63e3be0d5868f34770370b2345e
Tom Cherry <tomcherry@google.com>
2017-10-27 15:18:02 -0700

    Parse property contexts via a serialized trie
    Currently, whenever a new program starts, libc initializes two data
    structures related to properties from the raw property_context files.
    There are two problems here,
    1) This takes roughly 1.2ms on a trivial program to generate contents
       that could otherwise be cached.
    2) One of the data structures is a descending list of prefixes, each
       of which needs to be checked, whereas a trie would be more
       efficient.
    
    This change introduces two libraries,
    1) libpropertycontextserializer meant to be used by property_service
       to create a serialized trie containing all of the property
       contexts.
    2) libpropertycontextparser meant to be used by libc's property
       functions to parse this serialized trie during property lookup.
    
    This new trie also contains the ability to have exact matches instead
    of prefix matches for properties, which was not possible before.
    
    Bug: 36001741
    Change-Id: I42324f04c4d995a0e055e9685d79f40393dfca51

---
+++ property_service/libpropertycontextserializer/trie_serializer.h
@@ -0,0 +1,65 @@
+//
+// Copyright (C) 2017 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+#ifndef PROPERTY_CONTEXT_SERIALIZER_TRIE_SERIALIZER_H
+#define PROPERTY_CONTEXT_SERIALIZER_TRIE_SERIALIZER_H
+
+#include <string>
+#include <vector>
+
+#include "property_context_parser/property_context_parser.h"
+
+#include "trie_builder.h"
+#include "trie_node_arena.h"
+
+namespace android {
+namespace properties {
+
+// A serialized Trie consists of the following
+// Version ?
+// Offset to ContextsArray
+// Offset to RootNode of Trie
+class TrieSerializer {
+ public:
+  TrieSerializer();
+
+  std::string SerializeTrie(const TrieBuilderNode& builder_root,
+                            const std::set<std::string>& contexts);
+
+ private:
+  void SerializeContexts(const std::set<std::string>& contexts);
+  template <auto size_array, auto match_offset_array, auto match_size_array_offset_pointer,
+            auto context_offset_array>
+  void WriteMatchArray(const std::vector<std::pair<std::string, const std::string*>>& sorted_matches,
+                       uint32_t trie_offset);
+  void WriteTriePrefixMatches(const TrieBuilderNode& builder_node, uint32_t trie_offset);
+  void WriteTrieExactMatches(const TrieBuilderNode& builder_node, uint32_t trie_offset);
+
+  // Creates a new TrieNode within arena, and recursively creates its children.
+  // Returns the offset within arena.
+  uint32_t CreateTrieNode(const TrieBuilderNode& builder_node);
+
+  const PropertyContextArea* contexts() const {
+    return reinterpret_cast<const PropertyContextArea*>(arena_->data().data());
+  }
+
+  std::unique_ptr<TrieNodeArena> arena_;
+};
+
+}  // namespace properties
+}  // namespace android
+
+#endif
