5ba729e3ee4864484451c908c0275517d1a4e8b1
Justin Yun <justinyun@google.com>
2017-11-27 17:04:14 +0900

    Load versioned llndk.libraries.txt and vndksp.libraries.txt
    When ro.vndk.version has a specific VNDK version in it, use the
    llndk.libraries.txt and vndksp.libraries.txt files with the version
    suffix in the file names.
    If ro.vndk.version is "current" or not set, the version suffix will
    not be added.
    This is to use a proper VNDK snapshot version configuration for a
    vendor patition.
    
    Bug: 69531793
    Test: In system/etc directory of a Pixel2 device,
      Change llndk.libraries.txt to llndk.libraries.27.1.0.txt
      Change vndksp.libraries.txt to vndksp.libraries.27.1.0.txt
      Set ro.vndk.version to 27.1.0 in vendor/default.prop
      reboot and check if vendor apks work.
    
    Change-Id: I82d83b6805799ea71cc88d1e0297d265a40c0061

--- libnativeloader/native_loader.cpp
+++ libnativeloader/native_loader.cpp
@@ -110,6 +110,24 @@ static bool is_debuggable() {
   return std::string(debuggable) == "1";
 }
 
+static std::string vndk_version_str(const char delimiter = '-') {
+  char version[1 + PROP_VALUE_MAX];
+  if (property_get("ro.vndk.version", version + 1, "") > 0 && strcmp(version + 1, "current") != 0) {
+    version[0] = delimiter;
+  } else {
+    version[0] = '\0';
+  }
+  return std::string(version);
+}
+
+static void insert_vndk_version_str(std::string& file_name) {
+  size_t insert_pos = file_name.find_last_of(".");
+  if (insert_pos == std::string::npos) {
+    insert_pos = file_name.length();
+  }
+  file_name.insert(insert_pos, vndk_version_str('.'));
+}
+
 class LibraryNamespaces {
  public:
   LibraryNamespaces() : initialized_(false) { }
@@ -318,6 +336,10 @@ class LibraryNamespaces {
                         "Error reading public native library list from \"%s\": %s",
                         public_native_libraries_system_config.c_str(), error_msg.c_str());
 
+    // Insert VNDK version to llndk and vndksp config file names.
+    insert_vndk_version_str(llndk_native_libraries_system_config);
+    insert_vndk_version_str(vndksp_native_libraries_system_config);
+
     // For debuggable platform builds use ANDROID_ADDITIONAL_PUBLIC_LIBRARIES environment
     // variable to add libraries to the list. This is intended for platform tests only.
     if (is_debuggable()) {
@@ -347,11 +369,11 @@ class LibraryNamespaces {
     system_public_libraries_ = base::Join(sonames, ':');
 
     sonames.clear();
-    ReadConfig(kLlndkNativeLibrariesSystemConfigPathFromRoot, &sonames);
+    ReadConfig(llndk_native_libraries_system_config, &sonames);
     system_llndk_libraries_ = base::Join(sonames, ':');
 
     sonames.clear();
-    ReadConfig(kVndkspNativeLibrariesSystemConfigPathFromRoot, &sonames);
+    ReadConfig(vndksp_native_libraries_system_config, &sonames);
     system_vndksp_libraries_ = base::Join(sonames, ':');
 
     sonames.clear();
