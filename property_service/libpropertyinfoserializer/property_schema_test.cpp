581c6a1b1c49cc929e5f76c51a211497724a8f10
Tom Cherry <tomcherry@google.com>
2017-12-11 01:40:07 -0800

    Introduce property schemas
    Properties right now can take any format, but that makes it hard to
    specify an API for these properties as Treble intends to do.
    Therefore this change introduces the idea of property schemas.  There
    are 6 possible schemas defined below:
    
    1) 'string' this is the default schema and allows any property to be set.
    2) 'bool' this allows only boolean values (true|false|1|0)
    3) 'int' and 'uint' these allow signed and unsigned integer values
    respectively.  These optionally take a set of parameters to specify a
    min and max value that can be set, e.g. 'int -3 3' or 'uint 0 1'.
    4) 'double' this functions similarly to int and uint above but works
    with floating point numbers.
    5) 'size' this allows for strings matching [0-9]+[gkm].
    6) 'enum' this allows only a specific set of space deliminated values
    to be set, e.g. 'enum allow these strings' only allows one of 'allow',
    'these', or 'strings' to be set.
    
    Bug: 70858511
    Test: unit tests, test that properties are only set if their schema matches
    Change-Id: I7a6b00fb43ec630d1f56c9e9a1f1b61d3914f603

---
+++ property_service/libpropertyinfoserializer/property_schema_test.cpp
@@ -0,0 +1,184 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "property_info_serializer/property_schema.h"
+
+#include <gtest/gtest.h>
+
+namespace android {
+namespace properties {
+
+TEST(property_schema, CheckSchema_string) {
+  EXPECT_TRUE(CheckSchema("string", ""));
+  EXPECT_TRUE(CheckSchema("string", "-234"));
+  EXPECT_TRUE(CheckSchema("string", "234"));
+  EXPECT_TRUE(CheckSchema("string", "true"));
+  EXPECT_TRUE(CheckSchema("string", "false"));
+  EXPECT_TRUE(CheckSchema("string", "45645634563456345634563456"));
+  EXPECT_TRUE(CheckSchema("string", "some other string"));
+}
+
+TEST(property_schema, CheckSchema_int) {
+  EXPECT_FALSE(CheckSchema("int", ""));
+  EXPECT_FALSE(CheckSchema("int", "abc"));
+  EXPECT_FALSE(CheckSchema("int", "-abc"));
+  EXPECT_TRUE(CheckSchema("int", "0"));
+  EXPECT_TRUE(CheckSchema("int", std::to_string(std::numeric_limits<int64_t>::min())));
+  EXPECT_TRUE(CheckSchema("int", std::to_string(std::numeric_limits<int64_t>::max())));
+  EXPECT_TRUE(CheckSchema("int", "123"));
+  EXPECT_TRUE(CheckSchema("int", "-123"));
+
+  EXPECT_FALSE(CheckSchema("int -10 10", ""));
+  EXPECT_FALSE(CheckSchema("int -10 10", "abc"));
+  EXPECT_FALSE(CheckSchema("int -10 10", "-abc"));
+  EXPECT_FALSE(CheckSchema("int -10 10", std::to_string(std::numeric_limits<int64_t>::min())));
+  EXPECT_FALSE(CheckSchema("int -10 10", std::to_string(std::numeric_limits<int64_t>::max())));
+  EXPECT_FALSE(CheckSchema("int -10 10", "-11"));
+  EXPECT_FALSE(CheckSchema("int -10 10", "11"));
+  EXPECT_TRUE(CheckSchema("int -10 10", "-10"));
+  EXPECT_TRUE(CheckSchema("int -10 10", "-1"));
+  EXPECT_TRUE(CheckSchema("int -10 10", "0"));
+  EXPECT_TRUE(CheckSchema("int -10 10", "1"));
+  EXPECT_TRUE(CheckSchema("int -10 10", "10"));
+
+  EXPECT_FALSE(CheckSchema("int -500 -500", "-499"));
+  EXPECT_TRUE(CheckSchema("int -500 -500", "-500"));
+  EXPECT_FALSE(CheckSchema("int -500 -500", "-501"));
+  EXPECT_FALSE(CheckSchema("int -500 -500", "500"));
+}
+
+TEST(property_schema, CheckSchema_uint) {
+  EXPECT_FALSE(CheckSchema("uint", ""));
+  EXPECT_FALSE(CheckSchema("uint", "abc"));
+  EXPECT_FALSE(CheckSchema("uint", "-abc"));
+  EXPECT_TRUE(CheckSchema("uint", "0"));
+  EXPECT_TRUE(CheckSchema("uint", std::to_string(std::numeric_limits<uint64_t>::max())));
+  EXPECT_TRUE(CheckSchema("uint", "123"));
+  EXPECT_FALSE(CheckSchema("uint", "-123"));
+
+  EXPECT_FALSE(CheckSchema("uint 0 1", ""));
+  EXPECT_FALSE(CheckSchema("uint 0 1", "abc"));
+  EXPECT_FALSE(CheckSchema("uint 0 1", "-abc"));
+  EXPECT_FALSE(CheckSchema("uint 0 1", std::to_string(std::numeric_limits<uint64_t>::max())));
+  EXPECT_FALSE(CheckSchema("uint 0 1", "-1"));
+  EXPECT_FALSE(CheckSchema("uint 0 1", "2"));
+  EXPECT_TRUE(CheckSchema("uint 0 1", "0"));
+  EXPECT_TRUE(CheckSchema("uint 0 1", "1"));
+
+  EXPECT_FALSE(CheckSchema("uint 500 500", "-500"));
+  EXPECT_FALSE(CheckSchema("uint 500 500", "499"));
+  EXPECT_TRUE(CheckSchema("uint 500 500", "500"));
+  EXPECT_FALSE(CheckSchema("uint 500 500", "501"));
+}
+
+TEST(property_schema, CheckSchema_double) {
+  EXPECT_FALSE(CheckSchema("double", ""));
+  EXPECT_FALSE(CheckSchema("double", "abc"));
+  EXPECT_FALSE(CheckSchema("double", "-abc"));
+  EXPECT_TRUE(CheckSchema("double", "0.0"));
+  EXPECT_TRUE(CheckSchema("double", std::to_string(std::numeric_limits<double>::min())));
+  EXPECT_TRUE(CheckSchema("double", std::to_string(std::numeric_limits<double>::max())));
+  EXPECT_TRUE(CheckSchema("double", "123.1"));
+  EXPECT_TRUE(CheckSchema("double", "-123.1"));
+
+  EXPECT_FALSE(CheckSchema("double -10.1 10.1", ""));
+  EXPECT_FALSE(CheckSchema("double -10.1 10.1", "abc"));
+  EXPECT_FALSE(CheckSchema("double -10.1 10.1", "-abc"));
+  EXPECT_FALSE(CheckSchema("double -10.1 10.1", "-11.1"));
+  EXPECT_FALSE(CheckSchema("double -10.1 10.1", "11.1"));
+  EXPECT_TRUE(CheckSchema("double -10.1 10.1", "-1.1"));
+  EXPECT_TRUE(CheckSchema("double -10.1 10.1", "0"));
+  EXPECT_TRUE(CheckSchema("double -10.1 10.1", "1.1"));
+}
+
+TEST(property_schema, CheckSchema_size) {
+  EXPECT_FALSE(CheckSchema("size", ""));
+  EXPECT_FALSE(CheckSchema("size", "ab"));
+  EXPECT_FALSE(CheckSchema("size", "abcd"));
+  EXPECT_FALSE(CheckSchema("size", "0"));
+
+  EXPECT_TRUE(CheckSchema("size", "512g"));
+  EXPECT_TRUE(CheckSchema("size", "512k"));
+  EXPECT_TRUE(CheckSchema("size", "512m"));
+
+  EXPECT_FALSE(CheckSchema("size", "512gggg"));
+  EXPECT_FALSE(CheckSchema("size", "512mgk"));
+  EXPECT_FALSE(CheckSchema("size", "g"));
+  EXPECT_FALSE(CheckSchema("size", "m"));
+}
+
+TEST(property_schema, CheckSchema_enum) {
+  EXPECT_FALSE(CheckSchema("enum abc", ""));
+  EXPECT_FALSE(CheckSchema("enum abc", "ab"));
+  EXPECT_FALSE(CheckSchema("enum abc", "abcd"));
+  EXPECT_FALSE(CheckSchema("enum 123 456 789", "0"));
+
+  EXPECT_TRUE(CheckSchema("enum abc", "abc"));
+  EXPECT_TRUE(CheckSchema("enum 123 456 789", "123"));
+  EXPECT_TRUE(CheckSchema("enum 123 456 789", "456"));
+  EXPECT_TRUE(CheckSchema("enum 123 456 789", "789"));
+}
+
+TEST(property_schema, IsSchemaValid) {
+  EXPECT_FALSE(IsSchemaValid("not"));
+  EXPECT_FALSE(IsSchemaValid("valid"));
+
+  EXPECT_TRUE(IsSchemaValid("string"));
+  EXPECT_TRUE(IsSchemaValid("bool"));
+  EXPECT_TRUE(IsSchemaValid("size"));
+  EXPECT_FALSE(IsSchemaValid("string other"));
+  EXPECT_FALSE(IsSchemaValid("bool other"));
+  EXPECT_FALSE(IsSchemaValid("size other"));
+
+  EXPECT_TRUE(IsSchemaValid("int"));
+  EXPECT_TRUE(IsSchemaValid("int -10 10"));
+  EXPECT_TRUE(IsSchemaValid("int 0 10"));
+  EXPECT_TRUE(IsSchemaValid("int 10 10"));
+  EXPECT_TRUE(IsSchemaValid("int 0 " + std::to_string(std::numeric_limits<int64_t>::max())));
+  EXPECT_TRUE(IsSchemaValid("int " + std::to_string(std::numeric_limits<int64_t>::min()) + " 0"));
+  EXPECT_FALSE(IsSchemaValid("int 10"));
+  EXPECT_FALSE(IsSchemaValid("int 10 9"));
+  EXPECT_FALSE(IsSchemaValid("int 10 -11"));
+  EXPECT_FALSE(IsSchemaValid("int 0 " + std::to_string(std::numeric_limits<int64_t>::min())));
+  EXPECT_FALSE(IsSchemaValid("int " + std::to_string(std::numeric_limits<int64_t>::max()) + " 0"));
+  EXPECT_FALSE(IsSchemaValid("int 0 1" + std::to_string(std::numeric_limits<int64_t>::max())));
+  EXPECT_FALSE(IsSchemaValid("int " + std::to_string(std::numeric_limits<int64_t>::min()) + "9 0"));
+
+  EXPECT_TRUE(IsSchemaValid("uint"));
+  EXPECT_FALSE(IsSchemaValid("uint -10 10"));
+  EXPECT_TRUE(IsSchemaValid("uint 0 10"));
+  EXPECT_TRUE(IsSchemaValid("uint 10 10"));
+  EXPECT_TRUE(IsSchemaValid("uint 0 " + std::to_string(std::numeric_limits<uint64_t>::max())));
+  EXPECT_FALSE(IsSchemaValid("uint 10"));
+  EXPECT_FALSE(IsSchemaValid("uint 10 9"));
+  EXPECT_FALSE(IsSchemaValid("uint 10 -11"));
+  EXPECT_FALSE(IsSchemaValid("uint " + std::to_string(std::numeric_limits<int64_t>::max()) + " 0"));
+  EXPECT_FALSE(IsSchemaValid("uint 0 1" + std::to_string(std::numeric_limits<int64_t>::max())));
+
+  EXPECT_TRUE(IsSchemaValid("double"));
+  EXPECT_TRUE(IsSchemaValid("double -10.1 10.1"));
+  EXPECT_TRUE(IsSchemaValid("double 0.0 10.2"));
+  EXPECT_FALSE(IsSchemaValid("double 10.1"));
+  EXPECT_FALSE(IsSchemaValid("double 10.1 9.1"));
+  EXPECT_FALSE(IsSchemaValid("double 10.1 -11.1"));
+
+  EXPECT_FALSE(IsSchemaValid("enum"));
+  EXPECT_TRUE(IsSchemaValid("enum other"));
+  EXPECT_TRUE(IsSchemaValid("enum other and another"));
+}
+
+}  // namespace properties
+}  // namespace android
