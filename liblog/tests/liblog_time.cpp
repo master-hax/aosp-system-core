63134cb8e70485a2b23129b21a64b67ffa3100ef
Mark Salyzyn <salyzyn@google.com>
2018-01-12 15:20:49 -0800

    liblog: test: add liblog.time_drift
    Confirm that MONOTONIC_RAW has no drift per se, and that MONOTONIC,
    REALTIME and BOOTTIME have very similar direction of drift.  Sample
    for 20 seconds, and at each end grab 32 time values for each to
    average them out to compensate for variance in durations to collect
    the time information.  We print the calculated drift information in
    estimated seconds per year (0.032 ppms).
    
    The accuracy of the information collected was expected to be within
    10% only, after performance compensation.  Accuracy based on
    experimentation on a device (marlin) without a fixed CPU clock rate
    and varying conditions to achieve that goal with a 50% engineering
    margin to help ensure the test is not flaky.  If a more accurate
    result is required, you can lock the cores or select performance.
    
    Test: liblog-unit-tests --gtest_filter=liblog.time_drift
    Bug: 69423514
    Change-Id: I2715dd439d2cfa05f83ff130746a6da64696c9a6

--- liblog/tests/liblog_time.cpp
+++ liblog/tests/liblog_time.cpp
@@ -26,7 +26,9 @@
 #include <unordered_map>
 
 #include <android-base/file.h>
+#include <android-base/macros.h>
 #include <gtest/gtest.h>
+#include <log/log_time.h>
 #include <private/android_filesystem_config.h>
 
 #define INCLUDED_IN_GTEST
@@ -282,6 +284,10 @@ long getBenchmark_BM_time_clock_gettime_MONOTONIC_RAW() {
   return gettime_period;
 }
 
+unsigned long long ns(const timespec& ts) {
+  return (static_cast<unsigned long long>(ts.tv_sec) * NS_PER_SEC) + ts.tv_nsec;
+}
+
 }  // namespace
 
 TEST(liblog, time_clock_gettime_MONOTONIC_is_vdso) {
@@ -369,4 +375,138 @@ TEST(liblog, time_time_is_vdso) {
   ASSERT_GT(BM_time_time, 0);
   // generally much faster, but at least 10% faster is good stuff.
   ASSERT_GT(BM_time_clock_gettime_FASTEST, BM_time_time * 11 / 10);
+}
+
+TEST(liblog, time_drift) {
+  // Adjust following to affect expected accuracy of the results
+  static constexpr unsigned duration = 20;
+  static constexpr size_t sums = 32;
+  struct spec {
+    clockid_t id;
+    const char* name;
+    unsigned long long start;
+    unsigned long long end;
+    unsigned long long diff;
+  } spec[] = {
+    // clang-format off
+    { CLOCK_MONOTONIC_RAW, "MONOTONIC_RAW", 0, 0, 0 },  // first, no drift adj.
+    { CLOCK_MONOTONIC,     "MONOTONIC",     0, 0, 0 },
+    { CLOCK_REALTIME,      "REALTIME",      0, 0, 0 },
+    { CLOCK_BOOTTIME,      "BOOTTIME",      0, 0, 0 },
+    // clang-format on
+  };
+  timespec ts;
+
+  setAffinityCurrent();
+
+  clock_gettime(CLOCK_MONOTONIC_RAW, &ts);
+  unsigned long long start_span = ns(ts);
+  for (size_t j = 0; j < sums; ++j) {
+    for (size_t i = 0; i < arraysize(spec); ++i) {
+      clock_gettime(spec[i].id, &ts);
+      spec[i].start += ns(ts);
+    }
+  }
+  clock_gettime(CLOCK_MONOTONIC_RAW, &ts);
+  start_span = ns(ts) - start_span;
+  sleep(duration);
+  clock_gettime(CLOCK_MONOTONIC_RAW, &ts);
+  unsigned long long end_span = ns(ts);
+  for (size_t j = 0; j < sums; ++j) {
+    for (size_t i = 0; i < arraysize(spec); ++i) {
+      clock_gettime(spec[i].id, &ts);
+      spec[i].end += ns(ts);
+    }
+  }
+  clock_gettime(CLOCK_MONOTONIC_RAW, &ts);
+  end_span = ns(ts) - end_span;
+
+  // Compensate for difference in processor performance between acquisition
+  // runs.  This is to allow us to run this test without cpu frequency locked.
+  // Assumptions: time-to-collect is identical for all clock ids.
+  for (size_t i = 0; i < arraysize(spec); ++i) {
+    // major correction for established average.
+    spec[i].start += start_span / 2;
+    spec[i].end -= end_span / 2;
+    // minor additional correction for variation in performance.
+    //   2/3 of difference experimentally determined approximates the
+    //   affect, feel free to explore the ratio between start_span and
+    //   end_span to refine in the future.  If you _need_ an accurate
+    //   result, you _must_ lock the clocks.
+    long long diff = end_span - start_span;
+    diff *= i * 2;
+    diff /= (arraysize(spec) - 1) * 3;
+    if (diff < 0) diff = -diff;
+    spec[i].end -= diff;
+  }
+
+  unsigned long long max = 0, min = ULLONG_MAX;
+  for (size_t i = 0; i < arraysize(spec); ++i) {
+    unsigned long long diff = spec[i].end - spec[i].start;
+    spec[i].diff = diff;
+    if (diff > max) max = diff;
+    if (diff < min) min = diff;
+  }
+
+  // If drift collected is silly, either too small (no corrections ever
+  // applied) or too large (something is contributing to a large time
+  // correction during the test), then do not check refined values.
+
+  bool positive = false, negative = false;
+  long long sum = 0;
+  unsigned long long abs = 0;
+  std::cerr << info << "Kernel drift results in seconds/year (0.032ppm):\n";
+  for (size_t i = 0; i < arraysize(spec); ++i) {
+    long long drift = spec[i].diff - spec[0].diff;
+    // Diagnostic information print is seconds per year (~0.032ppm)
+    std::cerr << info << std::setw(13) << std::left << spec[i].name << ": ";
+    std::streamsize prec = std::cerr.precision(3);
+    std::cerr << 60.0 * 60.0 * 24.0 * 365.25 * drift / spec[0].diff << '\n';
+    std::cerr.precision(prec);
+    if (i > 0) {
+      if (drift > 0) {
+        positive = true;
+        abs += drift;
+      } else if (drift < 0) {
+        negative = true;
+        abs -= drift;
+      }
+      sum += drift;
+    }
+  }
+
+  releaseAffinityCurrent();
+
+  // If drift is > 1%, our calculations are likely to be meaningless.
+  if ((max - min) > (sums * duration * NS_PER_SEC / 100)) {
+    std::cerr << warning << "large drift: " << std::dec
+              << (max - min) * 100 / sums / duration / NS_PER_SEC << "%\n";
+    return;
+  }
+
+  // if values are small, our calculations are likely to be meaningless.
+  if (abs < (2 * sums * duration * (arraysize(spec) - 1))) {
+    std::cerr << warning << "negligable drift: " << std::dec
+              << abs / (arraysize(spec) - 1) / sums << "ns/" << duration
+              << "s\n";
+    return;
+  }
+
+  // This is the key, they are all in the same direction
+  EXPECT_NE(positive, negative);
+  if (negative) sum = -sum;
+  // We need a minimum threshold for sum / sums / duration. Concern
+  // here is that a device may in fact have a _perfect_ clock and
+  // the drift is zero, or the test is performed before any clock
+  // correction software has accumulated the appropriate drift
+  // corrections.
+  for (size_t i = 1; i < arraysize(spec); ++i) {
+    long long diff = spec[i].diff - spec[0].diff;
+    if (negative) diff = -diff;
+    // all within a broad (+200%/-50%) range of each other to
+    // confirm expected accuracy of the above results.
+    EXPECT_NE(diff, 0) << spec[i].name;
+    EXPECT_LE(sum, diff * 3 * 3) << "          for " << spec[i].name;
+    EXPECT_GE(sum, diff * 3 / 2) << "          for " << spec[i].name;
+  }
 }
