f2fd49debd5385fd8c0e1d626d298b71a483f21b
David Srbecky <dsrbecky@google.com>
2018-01-17 13:14:47 +0000

    WIP: Load dex files to resolve the java method names.
    Bug: 66919073
    Test: testrunner.py --host --interpreter -b -t 137
    Change-Id: I1d30c9e13f1b3b6cdc557e49d4f6da6f16cfed79

--- libunwindstack/include/unwindstack/Unwinder.h
+++ libunwindstack/include/unwindstack/Unwinder.h
@@ -22,6 +22,7 @@
 
 #include <memory>
 #include <string>
+#include <unordered_map>
 #include <vector>
 
 #include <unwindstack/Maps.h>
@@ -83,6 +84,7 @@ class Unwinder {
   std::vector<FrameData> frames_;
   std::shared_ptr<Memory> process_memory_;
   JitDebug* jit_debug_ = nullptr;
+  std::unordered_map<uint64_t, std::vector<uint8_t>> loaded_dexfiles_;
 };
 
 }  // namespace unwindstack
