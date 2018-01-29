f9f8f4ce2b5668145261371d39f403b2676c288e
Idries Hamadi <idries@google.com>
2018-01-29 16:30:36 +0000

    DO NOT MERGE
    Integrate adb with fastdeploy
    
    Test: mm ; adb install -r ~/example_apks/example.apk
    Change-Id: Ia1c2160f87ea584656f8fdd67e314a260d39d607

---
+++ adb/fastdeploy.h
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef FASTDEPLOY_H
+#define FASTDEPLOY_H
+
+#include "adb.h"
+
+typedef enum EFastDeploy_AgentUpdateStrategy {
+    FastDeploy_AgentUpdateAlways,
+    FastDeploy_AgentUpdateNewerTimeStamp,
+    FastDeploy_AgentUpdateDifferentVersion
+} FastDeploy_AgentUpdateStrategy;
+
+bool update_agent(FastDeploy_AgentUpdateStrategy agentUpdateStrategy);
+int extract_metadata(const char* apkPath, FILE* outputFp);
+int create_patch(const char* apkPath, const char* metadatapath, const char* patchPath);
+int apply_patch_on_device(const char* apkPath, const char* patchPath, const char* outputPath);
+int install_patch(const char* apkPath, const char* patchPath);
+int delete_device_patch_file(const char* apkPath);
+
+#endif
