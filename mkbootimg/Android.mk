3936b53cc2c4748f7bd6270c9bc82e6d2d181359
Hridya Valsaraju <hridya@google.com>
2018-03-21 22:18:19 -0700

    Add a script to unpack boot image
    The script prints out the boot image header
    contents and extracts the kernel, ramdisk,
    second bootloader and recovery DTBO images.
    
    Bug: 74763691
    Test: extract_bootimg --boot_img $OUT/recovery.img
    
    Change-Id: Iadbca81c157d9e4607f808a14468ab5542347507

--- mkbootimg/Android.mk
+++ mkbootimg/Android.mk
@@ -9,3 +9,12 @@ LOCAL_IS_HOST_MODULE := true
 LOCAL_MODULE := mkbootimg
 
 include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := extract_bootimg
+LOCAL_MODULE_CLASS := EXECUTABLES
+LOCAL_IS_HOST_MODULE := true
+
+LOCAL_MODULE := extract_bootimg
+
+include $(BUILD_PREBUILT)
