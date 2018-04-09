734c388c7fa397f8f79da5603aa73415c946dce9
Mark Salyzyn <salyzyn@google.com>
2018-04-09 09:50:32 -0700

    lmkd: limit capability set to minimum
    Set F() capability set via libcutils/fs_config.cpp and 'drop' lmkd
    from AID_ROOT to AID_LMKD uid.
    
    Test: check /proc/`pidof lmkd`/status for capability set
    Test: lmkd_unit_test
    Bug: 77650566
    Change-Id: I986081a0434cf6e842b63a55726380205b30a3ea

--- libcutils/fs_config.cpp
+++ libcutils/fs_config.cpp
@@ -178,11 +178,18 @@ static const struct fs_path_config android_files[] = {
     // in user builds.
     { 00700, AID_SYSTEM,    AID_SHELL,     CAP_MASK_LONG(CAP_BLOCK_SUSPEND),
                                               "system/bin/inputflinger" },
+    { 00550, AID_LMKD,      AID_LMKD,      CAP_MASK_LONG(CAP_DAC_OVERRIDE) |
+                                           CAP_MASK_LONG(CAP_KILL) |
+                                           CAP_MASK_LONG(CAP_IPC_LOCK) |
+                                           CAP_MASK_LONG(CAP_SYS_NICE) |
+                                           CAP_MASK_LONG(CAP_SYS_PTRACE) |
+                                           CAP_MASK_LONG(CAP_SYS_RESOURCE),
+                                              "system/bin/lmkd" },
     { 00550, AID_LOGD,      AID_LOGD,      CAP_MASK_LONG(CAP_SYSLOG) |
                                            CAP_MASK_LONG(CAP_AUDIT_CONTROL) |
                                            CAP_MASK_LONG(CAP_SETGID),
                                               "system/bin/logd" },
-    { 00550, AID_SYSTEM,    AID_LOG,      CAP_MASK_LONG(CAP_SYSLOG),
+    { 00550, AID_SYSTEM,    AID_LOG,       CAP_MASK_LONG(CAP_SYSLOG),
                                               "system/bin/bootstat" },
     { 00750, AID_ROOT,      AID_SHELL,     CAP_MASK_LONG(CAP_SETUID) |
                                            CAP_MASK_LONG(CAP_SETGID),
