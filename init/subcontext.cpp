f3e04ce547bc38a92001032340f7c73c8e758361
Tom Cherry <tomcherry@google.com>
2018-04-11 15:50:00 -0700

    init: do not impose vendor_init restrictions on old vendor images
    Do not restrict vendor_init restrictions on vendor images that were
    built before P, as they will not have the correct permissions.
    
    Bug: 77732028
    Test: test new devices and see vendor_init still works
    Change-Id: I636a07b54fbfb248e1d1a68a8f3c4d047fd5a9e9

--- init/subcontext.cpp
+++ init/subcontext.cpp
@@ -23,6 +23,7 @@
 
 #include <android-base/file.h>
 #include <android-base/logging.h>
+#include <android-base/properties.h>
 #include <android-base/strings.h>
 #include <selinux/android.h>
 
@@ -37,6 +38,7 @@
 #endif
 
 using android::base::GetExecutablePath;
+using android::base::GetIntProperty;
 using android::base::Join;
 using android::base::Socketpair;
 using android::base::Split;
@@ -354,8 +356,10 @@ Result<std::vector<std::string>> Subcontext::ExpandArgs(const std::vector<std::s
 static std::vector<Subcontext> subcontexts;
 
 std::vector<Subcontext>* InitializeSubcontexts() {
-    for (const auto& [path_prefix, secontext] : paths_and_secontexts) {
-        subcontexts.emplace_back(path_prefix, secontext);
+    if (GetIntProperty("ro.vndk.version", 28) >= 28) {
+        for (const auto& [path_prefix, secontext] : paths_and_secontexts) {
+            subcontexts.emplace_back(path_prefix, secontext);
+        }
     }
     return &subcontexts;
 }
