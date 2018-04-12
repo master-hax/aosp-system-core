df7249de56df7cbe58da90e6b3cde271d45c8f93
Josh Gao <jmgao@google.com>
2018-04-12 13:51:55 -0700

    adb: add ServiceSocket.
    Test: none
    Change-Id: Ifb8101a9acefc65e0af4ba3e2dbcca2f10a561c9

--- adb/services.h
+++ adb/services.h
@@ -17,8 +17,53 @@
 #ifndef SERVICES_H_
 #define SERVICES_H_
 
+#include <atomic>
+#include <mutex>
+#include <thread>
+
+#include <android-base/thread_annotations.h>
+
+#include "adb_unique_fd.h"
+#include "fdevent.h"
+#include "socket.h"
+#include "types.h"
+
 constexpr char kShellServiceArgRaw[] = "raw";
 constexpr char kShellServiceArgPty[] = "pty";
 constexpr char kShellServiceArgShellProtocol[] = "v2";
+
+struct ServiceSocket : public asocket {
+    ServiceSocket();
+
+    virtual ~ServiceSocket();
+
+    virtual bool HandleInput() EXCLUDES(mutex_) = 0;
+    virtual bool InputEmpty();
+    virtual bool ReadInput(IOVector* out, size_t length) EXCLUDES(mutex_);
+
+    void Write(IOVector data) EXCLUDES(mutex_);
+
+  private:
+    void Close();
+
+    // Wake up the service thread.
+    void Notify() EXCLUDES(mutex_);
+
+    // Wait to be notified.
+    void Wait() EXCLUDES(mutex_);
+
+    std::atomic<bool> closed_;
+    std::thread thread_;
+    unique_fd thread_notify_read_;
+    unique_fd thread_notify_write_;
+
+  protected:
+    std::mutex mutex_;
+    IOVector input_queue_ GUARDED_BY(mutex_);
+
+  private:
+    bool ready_ GUARDED_BY(mutex_) = false;
+    IOVector output_queue_ GUARDED_BY(mutex_);
+};
 
 #endif  // SERVICES_H_
