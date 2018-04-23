e11a5b81977d4fd865cfa514770c143ea5565656
Tao Bao <tbao@google.com>
2018-04-23 15:15:40 -0700

    libsparse: Use 'size_t' for the 'len' parameter in callbacks.
    This CL updates the callback function signature in
    sparse_file_callback() and sparse_file_foreach_chunk().
    
    Before:
    int sparse_file_callback(
        struct sparse_file *s, bool sparse, bool crc,
        int (*write)(void *priv, const void *data, int len), void *priv);
    
    int sparse_file_foreach_chunk(
        struct sparse_file *s, bool sparse, bool crc,
        int (*write)(
            void *priv, const void *data, int len, unsigned int block,
            unsigned int nr_blocks),
        void *priv);
    
    After:
    int sparse_file_callback(
        struct sparse_file *s, bool sparse, bool crc,
        int (*write)(void *priv, const void *data, size_t len), void *priv);
    
    int sparse_file_foreach_chunk(
        struct sparse_file *s, bool sparse, bool crc,
        int (*write)(
            void *priv, const void *data, size_t len, unsigned int block,
            unsigned int nr_blocks),
        void *priv);
    
    The length (i.e. 'len') comes from the size of a chunk, which could be
    legitimately larger than INT_MAX. Prior to this CL, callers (e.g.
    write_sparse_data_chunk()) were already passing unsigned int to the
    callbacks. When a chunk size exceeds INT_MAX, the callback would see a
    negative value, which could lead to undesired behavior. For example,
    out_counter_write(), as one of the internal callbacks in libsparse,
    gives a wrong sum of chunk sizes, which in turn fails the fastboot
    flashing when given a huge sparse image.
    
    Bug: 78432315
    Test: `m dist` (with matching changes to all the clients)
    Test: Build fastboot and successfully flash a previously failing (huge)
          sparse image.
    Change-Id: Iac4bcf7b57039d08af3c57f4be00d75f6b693d93

--- libsparse/include/sparse/sparse.h
+++ libsparse/include/sparse/sparse.h
@@ -18,6 +18,7 @@
 #define _LIBSPARSE_SPARSE_H_
 
 #include <stdbool.h>
+#include <stddef.h>
 #include <stdint.h>
 
 #ifdef	__cplusplus
@@ -201,7 +202,7 @@ unsigned int sparse_file_block_size(struct sparse_file *s);
  * Returns 0 on success, negative errno on error.
  */
 int sparse_file_callback(struct sparse_file *s, bool sparse, bool crc,
-		int (*write)(void *priv, const void *data, int len), void *priv);
+		int (*write)(void *priv, const void *data, size_t len), void *priv);
 
 /**
  * sparse_file_foreach_chunk - call a callback for data blocks in sparse file
@@ -218,7 +219,7 @@ int sparse_file_callback(struct sparse_file *s, bool sparse, bool crc,
  * Returns 0 on success, negative errno on error.
  */
 int sparse_file_foreach_chunk(struct sparse_file *s, bool sparse, bool crc,
-	int (*write)(void *priv, const void *data, int len, unsigned int block,
+	int (*write)(void *priv, const void *data, size_t len, unsigned int block,
 		     unsigned int nr_blocks),
 	void *priv);
 /**
