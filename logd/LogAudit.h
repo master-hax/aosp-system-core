b87c883ca6a5d324c3b671c65ec4f60dc6ffd93b
Jeff Vander Stoep <jeffv@google.com>
2018-05-03 14:57:39 -0700

    Logd: include app package name in selinux metadata
    Note: denial metadata is currently only included on debug builds.
    
    avc: denied { read } for comm="getprop" name="u:object_r:net_dns_prop:s0"
    dev="tmpfs" ino=19605 scontext=u:r:untrusted_app:s0:c164,c256,c512,c768
    tcontext=u:object_r:net_dns_prop:s0 tclass=file permissive=0 b/12345678
    android.selinuxtargetsdkcurrent.cts
    
    Bug: 9496886
    Bug: 68016944
    Test: cts-tradefed run cts -m CtsSelinuxTargetSdkCurrentTestCases
    Change-Id: I1700722a45b75d22f17ba4edc7b95cbaf99fdda8

--- logd/LogAudit.h
+++ logd/LogAudit.h
@@ -48,7 +48,7 @@ class LogAudit : public SocketListener {
     std::map<std::string, std::string> populateDenialMap();
     std::string denialParse(const std::string& denial, char terminator,
                             const std::string& search_term);
-    void logParse(const std::string& string, std::string* bug_num);
+    void auditParse(const std::string& string, std::string* bug_num, uid_t uid);
     int logPrint(const char* fmt, ...)
         __attribute__((__format__(__printf__, 2, 3)));
 };
