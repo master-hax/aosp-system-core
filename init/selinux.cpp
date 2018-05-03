82ffa663d36e856f280823f9c73b8ce9b4f2e863
Logan Chien <loganchien@google.com>
2018-05-03 14:33:52 +0800

    init: Use sepolicy version instead
    This commit uses vendor sepolicy file version (defined in
    `/vendor/etc/selinux/plat_sepolicy_vers.txt`) to determine whether the
    source context should be set as `u:r:vendor_init:s0`.
    
    Before this commit, the criterion was `ro.vndk.version` >= 28.  However,
    the check in `property_service.cpp` will always be true because
    `ro.vndk.version` hasn't been loaded from `/vendor/default.prop`.
    
    Furthermore, under some circumstances, `ro.vndk.version` may be
    different from `plat_sepolicy_vers.txt` (e.g. O-MR1 vendor does not
    define `ro.vndk.version`).
    
    Bug: 78605339  # high-level bug to combine O-MR1 and P GSI
    Bug: 79135481  # the usage of `ro.vndk.version` in init
    Test: vts-tradefed run vts -m VtsTrebleVintfTest  # tetheroffload
    Change-Id: Ied46e9346b4ca7931aa4dcf1c9dbc11de0e12d93

--- init/selinux.cpp
+++ init/selinux.cpp
@@ -55,12 +55,14 @@
 #include <android-base/chrono_utils.h>
 #include <android-base/file.h>
 #include <android-base/logging.h>
+#include <android-base/parseint.h>
 #include <android-base/unique_fd.h>
 #include <selinux/android.h>
 
 #include "log.h"
 #include "util.h"
 
+using android::base::ParseInt;
 using android::base::Timer;
 using android::base::unique_fd;
 
@@ -451,6 +453,29 @@ void SelinuxSetupKernelLogging() {
     selinux_callback cb;
     cb.func_log = selinux_klog_callback;
     selinux_set_callback(SELINUX_CB_LOG, cb);
+}
+
+// This function checks whether the sepolicy supports vendor init.
+bool SelinuxHasVendorInit() {
+    if (!IsSplitPolicyDevice()) {
+        // If this device does not split sepolicy files, vendor_init will be available in the latest
+        // monolithic sepolicy file.
+        return true;
+    }
+
+    std::string version;
+    if (!GetVendorMappingVersion(&version)) {
+        // Return true as default if we cannot load vendor sepolicy file version.
+        return true;
+    }
+
+    int major_version;
+    if (!ParseInt(version.substr(0, version.find('.')), &major_version)) {
+        // Return true as default if we cannot parse the version number.
+        return true;
+    }
+
+    return major_version >= 28;
 }
 
 // selinux_android_file_context_handle() takes on the order of 10+ms to run, so we want to cache
