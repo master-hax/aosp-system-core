ef6e7e7094eff4c7f5eafc1f140d671714de5992
Mark Salyzyn <salyzyn@google.com>
2018-05-18 15:25:15 -0700

    init: turn keychords into a standalone class
    Test: init_tests
    Bug: 64114943
    Change-Id: Ie760e4a3a661260ed2aabb8a92fe65fc93521cb5

--- init/keychords.h
+++ init/keychords.h
@@ -18,6 +18,8 @@
 #define _INIT_KEYCHORDS_H_
 
 #include <functional>
+#include <map>
+#include <string>
 #include <vector>
 
 #include "epoll.h"
@@ -25,8 +27,67 @@
 namespace android {
 namespace init {
 
-void KeychordInit(Epoll* init_epoll, std::function<void(int)> handler);
-int GetKeychordId(const std::vector<int>& keycodes);
+class Keychords {
+  private:
+    int count;
+    Epoll* epoll;
+    std::function<void(int)> handler;
+
+    struct Entry {
+        const std::vector<int> keycodes;
+        bool notified;
+        int id;
+
+        Entry(const std::vector<int>& keycodes, int id);
+    };
+    std::vector<Entry> entries;
+
+    int inotify_fd;
+
+    static constexpr char kDevicePath[] = "/dev/input";
+
+    std::map<std::string, int> registration;
+
+    // Bit management
+    class Mask {
+      private:
+        typedef unsigned int mask_t;
+        std::vector<mask_t> bits;
+        static constexpr size_t bits_per_byte = 8;
+
+      public:
+        explicit Mask(size_t bit = 0);
+        void SetBit(size_t bit, bool value = true);
+        bool GetBit(size_t bit) const;
+        size_t bytesize() const;
+        void* data();
+        size_t size() const;
+        void resize(size_t bit);
+
+        operator bool() const;
+        Mask operator&(const Mask& rval) const;
+        void operator|=(const Mask& rval);
+    };
+    Mask current;
+
+    void LambdaCheck();
+    void LambdaHandler(int fd);
+
+    bool GeteventEnable(int fd);
+    void GeteventOpenDevice(const std::string& device);
+    void GeteventCloseDevice(const std::string& device);
+
+    void InotifyHandler();
+
+    void GeteventOpenDevice();
+
+  public:
+    Keychords();
+    ~Keychords();
+
+    int GetId(const std::vector<int>& keycodes);
+    void Start(Epoll* init_epoll, std::function<void(int)> init_handler);
+};
 
 }  // namespace init
 }  // namespace android
