5836b2cd85c43678efa744bb58a98b2b8ef1a2af
Bernie Innocenti <codewiz@google.com>
2018-05-23 19:02:52 +0900

    SocketListener: use poll() instead of select()
    FD_SET in Android is hard-coded to 1000 to file descriptors, which
    causes processes with too many open files or connections to crash:
    
      Abort message: 'FORTIFY: FD_ISSET: file descriptor 1024 >= FD_SETSIZE 128'
    
    The fix we used elsewhere is replacing select() with poll(), but in the
    case of SocketListener we additionally need to replace the SocketClient
    list with a map indexed by fd in order to avoid quadratic behavior on
    each poll() wakeup.
    
    Bug: 79838856
    Test: DO NOT SUBMIT - UNTESTED
    Change-Id: I19ca4be675e9638104c0e7acf4a4bc62085e8ecd

--- libsysutils/include/sysutils/SocketListener.h
+++ libsysutils/include/sysutils/SocketListener.h
@@ -18,6 +18,8 @@
 
 #include <pthread.h>
 
+#include <unordered_map>
+
 #include <sysutils/SocketClient.h>
 #include "SocketClientCommand.h"
 
@@ -25,7 +27,7 @@ class SocketListener {
     bool                    mListen;
     const char              *mSocketName;
     int                     mSock;
-    SocketClientCollection  *mClients;
+    std::unordered_map<int, SocketClient*> mClients;
     pthread_mutex_t         mClientsLock;
     int                     mCtrlPipe[2];
     pthread_t               mThread;
