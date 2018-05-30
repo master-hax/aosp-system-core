e382c126cf37c30d790afebfa0badd6c8fd6682b
Sandeep Patil <sspatil@google.com>
2018-05-30 13:46:55 -0700

    fs_mgr: device mapper: Add libdm and 'dmctl' tool to use it.
    libdm is small static library that is intended to be the one source to
    control device mapper. It is intended to have APIs to create, control
    and destroy device mapper targets. Eventually, all fs_mgr implementation
    that talks to device mapper will be moved to using libdm APIs.
    
    Added 'dmctl', a command line tool that lists the registered device
    mapper targets and their corresponding version. The tool will continue
    to get new features to exemplify new APIs implemented in libdm.
    
    The library is intentionally static and folds into libfs_mgr. All
    clients must link to libfs_mgr as a result.
    
    Test: dmctl list
    Bug: 110035986
    
    Change-Id: I71e7146073a74e7523524bf3b20d0be6d06f9fad
    Signed-off-by: Sandeep Patil <sspatil@google.com>

---
+++ fs_mgr/libdm/include/dm.h
@@ -0,0 +1,87 @@
+/*
+ *  Copyright 2018 Google, Inc
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+#ifndef _LIBDM_DM_H_
+#define _LIBDM_DM_H_
+
+#include <errno.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+#include <memory>
+
+#include <android-base/logging.h>
+
+#include <dm_device.h>
+
+#define DM_ALIGN_MASK (7)
+#define DM_ALIGN(x) ((x + DM_ALIGN_MASK) & ~DM_ALIGN_MASK)
+
+namespace android {
+namespace dm {
+
+class DeviceMapper final {
+  public:
+    // Creates a device mapper device with given name. If a device mapper device
+    // with the name already exists, return a 'nullptr', otherwise returns a
+    // DmDevice object that is suspended.
+    std::unique_ptr<DmDevice> CreateDevice(const std::string& name);
+
+    // Removes a device mapper device with the given name.
+    // Returns 'true' on success, false otherwise.
+    bool DeleteDevice(DmDevice* dev);
+
+    // Returns true if a list of available device mapper targets registered in the kernel was
+    // successfully read and stored in 'targets'. Returns 'false' otherwise.
+    bool GetAvailableTargets(std::vector<DmTarget>* targets);
+
+    // Returns the path to the device mapper device node in '/dev' corresponding to
+    // 'name'.
+    std::string GetDmDevicePathByName(const std::string& name);
+
+    // Same as 'GetDmDevicePathByName', except this returns a handle to the 'DmDevice'
+    // object corresponding to 'name'.
+    std::unique_ptr<DmDevice> Find(const std::string& name);
+
+    // The only way to create a DeviceMapper object.
+    static DeviceMapper& Instance();
+
+    ~DeviceMapper() {
+        if (fd_ != -1) {
+            ::close(fd_);
+        }
+    }
+
+  private:
+    DeviceMapper() : fd_(-1) {
+        fd_ = TEMP_FAILURE_RETRY(open("/dev/device-mapper", O_RDWR | O_CLOEXEC));
+        if (fd_ < 0) {
+            PLOG(ERROR) << "Failed to open device-mapper";
+        }
+    }
+
+    int fd_;
+    // Non-copyable & Non-movable
+    DeviceMapper(const DeviceMapper&) = delete;
+    DeviceMapper& operator=(const DeviceMapper&) = delete;
+    DeviceMapper& operator=(DeviceMapper&&) = delete;
+    DeviceMapper(DeviceMapper&&) = delete;
+};
+
+}  // namespace dm
+}  // namespace android
+
+#endif /* _LIBDM_DM_H_ */
