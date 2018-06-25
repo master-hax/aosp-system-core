f3fcc26f364d1488e4aee0e727918cdb1f447cd3
David Anderson <dvander@google.com>
2018-06-25 16:12:21 -0700

    fs_mgr: Use libdm for fs_mgr_verity.
    This change uses libdm to replace the hand-coded ioctl code in fs_mgr_verity.
    Since AVB2 is now preferred to AVB1, this is a surgical change to allow
    us to remove fs_mgr_dm_ioctl. Rather than refactor the target string
    construction code, we instead provide a DmTargetVerityString class which
    passes a raw parameter string directly to DmTable.
    
    Bug: 110035986
    Test: AVB1 device still boots
    Change-Id: Iad88993a98abb388a12cec9e6d4543dfb4157414

--- fs_mgr/libdm/include/libdm/dm_target.h
+++ fs_mgr/libdm/include/libdm/dm_target.h
@@ -128,6 +128,22 @@ class DmTargetVerity final : public DmTarget {
     bool valid_;
 };
 
+// This is the same as DmTargetVerity, but the table may be specified as a raw
+// string. This code exists only for fs_mgr_verity and should be avoided. Use
+// DmTargetVerity for new code instead.
+class DmTargetVerityString final : public DmTarget {
+  public:
+    DmTargetVerityString(uint64_t start, uint64_t length, const std::string& string)
+        : DmTarget(start, length), string_(string) {}
+
+    std::string name() const override { return "verity"; }
+    std::string GetParameterString() const override { return string_; }
+    bool Valid() const override { return true; }
+
+  private:
+    std::string string_;
+};
+
 }  // namespace dm
 }  // namespace android
 
