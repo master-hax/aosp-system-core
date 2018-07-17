002413e5c2dc9845a16432d80e545ad65f8c8056
Hridya Valsaraju <hridya@google.com>
2018-07-17 11:14:01 -0700

    Add fastbootd.
    Fastbootd is started by recovery, and communicates
    over usb with the fastboot protocol.
    
    Add additional fastboot commands to move in and out of
    fastbootd. "fastboot reboot recovery" and "fastboot reboot fastboot"
    
    Bug: 78793464
    Test: fastboot reboot recovery, fastboot reboot fastboot
    Change-Id: Iffb77b2d3ceefb7335c38aa9fba7bd17821484f0

---
+++ fastboot/device/commands.h
@@ -0,0 +1,50 @@
+/*
+ * Copyright (C) 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#pragma once
+
+#include <functional>
+#include <string>
+#include <vector>
+
+class FastbootDevice;
+
+enum class FastbootResult {
+    OKAY,
+    FAIL,
+    INFO,
+    DATA,
+};
+
+// Send the given status as 4 bytes along with an informative message.
+using status_cb_t = std::function<void(FastbootResult, std::string)>;
+// Read or write the entirety of the given vector to the transport.
+using data_cb_t = std::function<bool(std::vector<char>&, bool)>;
+
+// Execute a command with the given arguments (possibly empty).
+// The command is responsible for reporting its own status through status_cb,
+// and can retrieve data through data_cb.
+using command_handler = std::function<void(FastbootDevice*, const std::vector<std::string>&,
+                                           status_cb_t, data_cb_t)>;
+
+void DownloadHandler(FastbootDevice* device, const std::vector<std::string>& args,
+                     status_cb_t status_cb, data_cb_t data_cb);
+void SetActiveHandler(FastbootDevice* device, const std::vector<std::string>& args,
+                      status_cb_t status_cb);
+void ShutDownHandler(FastbootDevice* device, status_cb_t status_cb);
+void RebootHandler(FastbootDevice* device, status_cb_t status_cb);
+void RebootBootloaderHandler(FastbootDevice* device, status_cb_t status_cb);
+void RebootFastbootHandler(FastbootDevice* device, status_cb_t status_cb);
+void RebootRecoveryHandler(FastbootDevice* device, status_cb_t status_cb);
