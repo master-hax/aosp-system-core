9b4a1b66dc87437e2e5768aec94ab18db638d0a6
Suren Baghdasaryan <surenb@google.com>
2018-10-17 16:42:47 -0700

    rootdir: Add task profiles description file
    Add task_profiles.json file to be places under /etc/init/ on the device
    to describe task profiles that processes can request. This adds additional
    layer of abstraction between what a process wants to achieve and how
    system implements that request.
    
    Bug: 111307099
    test: builds, boots
    
    Change-Id: If92f011230ef5d24750e184bad776f30ba226f86
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -28,6 +28,17 @@ LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/init
 include $(BUILD_PREBUILT)
 
 #######################################
+# task_profiles.json
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := task_profiles.json
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)
+
+include $(BUILD_PREBUILT)
+
+#######################################
 # asan.options
 ifneq ($(filter address,$(SANITIZE_TARGET)),)
 
