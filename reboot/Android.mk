84c40a3006138b981528e2797235c20e1432ef46
Andrii Chepurnyi <andrii_chepurnyi@epam.com>
2018-10-29 14:36:09 +0200

    Added poweroff command to comply orderly shutdown
    The Linux kernel has orderly_poweroff which allows
    the kernel to initiate a graceful shutdown of userspace,
    by running /sbin/poweroff(please refer to kernel source
    kernel/reboot.c __orderly_poweroff()). This will be used
    for shutdown initiated by a system controller or hypervisor
    on platforms that do not use ACPI.
    
    Change-Id: I4bf4c2ddc531de22f8101b943909b4343bee0516
    Signed-off-by: Andrii Chepurnyi <andrii_chepurnyi@epam.com>

---
+++ reboot/Android.mk
@@ -0,0 +1,8 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+LOCAL_MODULE := poweroff
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := EXECUTABLES
+LOCAL_SRC_FILES := poweroff
+LOCAL_MODULE_PATH := $(TARGET_ROOT_OUT)/sbin
+include $(BUILD_PREBUILT)
