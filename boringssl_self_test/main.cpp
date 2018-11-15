77773dce20ae4d950ac5970c91151743678755c5
Branden Archer <brarcher@google.com>
2018-11-15 08:43:48 -0800

    Create BoringSSL test program
    NIAP certification requires that all cryptographic functions
    undergo a self-test during startup to demonstrate correct
    operation. This new program, boringssl_self_test, performs
    this check.
    
    If the check fails, the new program needs to prevent the device
    from booting. To this end, it will cause the device to boot into
    the bootloader.
    
    Bug: 119826244
    Test: Built for walleye. When run it reports that BoringSSL's
          self test is successful. Forcing the self check to fail
          causes the device to reboot into the boot loader.
    
    Change-Id: I07a5dc73a314502c87de566bb26f4d73499d2675

---
+++ boringssl_self_test/main.cpp
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <android-base/logging.h>
+#include <cutils/android_reboot.h>
+#include <openssl/crypto.h>
+#include <unistd.h>
+
+int main(void) {
+    if (BORINGSSL_self_test() != 1) {
+        LOG(INFO) << "BoringSSL crypto self tests failed";
+        sync();
+
+        // This check has failed, so the device should refuse
+        // to boot. Rebooting to bootloader to wait for
+        // further action from the user.
+        int result = android_reboot(ANDROID_RB_RESTART2, 0, "bootloader");
+        if (result != 0) {
+            LOG(ERROR) << "Failed to reboot into bootloader";
+        }
+
+        return 1;
+    }
+
+    return 0;
+}
