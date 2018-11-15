52002d1ab499b5b45bebf7cedf6a05bbcca3c7b1
Martin Stjernholm <mast@google.com>
2018-11-15 23:36:07 +0000

    Clean up some compile time diffs for coping with lack of dex file support in libunwindstack.
    They're not necessary now. The dex file functionality will still do nothing
    if libdexfile isn't linked in, though.
    
    Test: mmma system/core/{libunwindstack,libbacktrace} && out/host/linux-x86/nativetest*/*_test
    Test: Make image, flash, and reboot device.
    Bug: 119632407
    Change-Id: If7e3bd3b44cbac0b04175ee23f9147db39c4e6c2

--- libbacktrace/UnwindStackMap.cpp
+++ libbacktrace/UnwindStackMap.cpp
@@ -46,9 +46,7 @@ bool UnwindStackMap::Build() {
   // Create a JitDebug object for getting jit unwind information.
   std::vector<std::string> search_libs_{"libart.so", "libartd.so"};
   jit_debug_.reset(new unwindstack::JitDebug(process_memory_, search_libs_));
-#if !defined(NO_LIBDEXFILE_SUPPORT)
   dex_files_.reset(new unwindstack::DexFiles(process_memory_, search_libs_));
-#endif
 
   if (!stack_maps_->Parse()) {
     return false;
