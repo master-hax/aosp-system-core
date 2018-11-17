5bb23cf4ad7776248d4c9a15e607a02fa7ae758b
David Anderson <dvander@google.com>
2018-11-16 21:37:34 -0800

    liblp: Auto-suffix group names.
    This is needed for update_engine to properly clean old partitions on
    retrofit devices.
    
    Bug: 119687874
    Test: liblp_test gtest
    Change-Id: Ida9483ad3c127e357f45789540ebbedc9d3d3883

--- fs_mgr/liblp/include/liblp/metadata_format.h
+++ fs_mgr/liblp/include/liblp/metadata_format.h
@@ -38,7 +38,7 @@ extern "C" {
 #define LP_METADATA_HEADER_MAGIC 0x414C5030
 
 /* Current metadata version. */
-#define LP_METADATA_MAJOR_VERSION 9
+#define LP_METADATA_MAJOR_VERSION 10
 #define LP_METADATA_MINOR_VERSION 0
 
 /* Attributes for the LpMetadataPartition::attributes field.
@@ -269,7 +269,16 @@ typedef struct LpMetadataPartitionGroup {
 
     /* 36: Maximum size in bytes. If 0, the group has no maximum size. */
     uint64_t maximum_size;
+
+    /* 44: Flags (see LP_GROUP_*). */
+    uint32_t flags;
 } LpMetadataPartitionGroup;
+
+/* This flag is only intended to be used with super_empty.img and super.img on
+ * retrofit devices. If set, the group needs a slot suffix to be interpreted
+ * correctly. The suffix is automatically applied by ReadMetadata().
+ */
+#define LP_GROUP_SLOT_SUFFIXED (1 << 0)
 
 /* This struct defines an entry in the block_devices table. There must be at
  * least one device, and the first device must represent the partition holding
