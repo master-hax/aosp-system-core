4893e75dd5db32c8dd044fa2209c54b50df1972c
Henry Daitx <daitx@google.com>
2018-11-30 18:02:43 +0000

    Fix adb not correctly reading return code from DeployAgent
    Certain error conditions were getting lost because adb was wrongly
    reading DeployAgent's return code as always 0.
    
    Test: mm -j 72
    Test: adb install -r --fastdeploy --force-agent --local-agent /mnt/raid/boat-attack-apk/boat-attack-swappy.apk
    
    Bug: 120197330
    Change-Id: If835fd6ca2051be8e5ff6c957e08b1e458053989

--- adb/client/fastdeploy.cpp
+++ adb/client/fastdeploy.cpp
@@ -232,7 +232,9 @@ void extract_metadata(const char* apkPath, FILE* outputFp) {
     DeployAgentFileCallback cb(outputFp, &extractErrorBuffer, &statusCode);
     int returnCode = send_shell_command(extractCommand, false, &cb);
     if (returnCode != 0) {
-        error_exit("Executing %s returned %d", extractCommand.c_str(), returnCode);
+        fprintf(stderr, "Executing %s returned %d\n", extractCommand.c_str(), returnCode);
+        fprintf(stderr, "%*s\n", int(extractErrorBuffer.size()), extractErrorBuffer.data());
+        error_exit("Aborting");
     }
 }
 
--- adb/client/fastdeploycallbacks.cpp
+++ adb/client/fastdeploycallbacks.cpp
@@ -86,6 +86,7 @@ void DeployAgentFileCallback::OnStderr(const char* buffer, int length) {
 int DeployAgentFileCallback::Done(int status) {
     if (mpStatusCode != NULL) {
         *mpStatusCode = status;
+        return *mpStatusCode;
     }
     return 0;
 }
@@ -113,6 +114,7 @@ void DeployAgentBufferCallback::OnStderr(const char* buffer, int length) {
 int DeployAgentBufferCallback::Done(int status) {
     if (mpStatusCode != NULL) {
         *mpStatusCode = status;
+        return *mpStatusCode;
     }
     return 0;
 }
