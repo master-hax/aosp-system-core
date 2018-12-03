424fc64d694f8f90c7ff1e7b256dd37905ccc8ff
Yifan Hong <elsk@google.com>
2018-12-03 13:46:56 -0800

    Move parts of roots.cpp from librecovery
    Move some mounting functionalities from librecovery so that
    other modules can also mount logical partitions correctly.
    
    Test: run recovery tests
    Test: vintf_object_recovery_test
    
    Bug: 118634720
    Bug: 113182233
    Change-Id: Ie02c3a941ed801fdae5818099faf5f3863444296

---
+++ fs_mgr/include/fs_mgr_roots.h
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <string>
+
+#include <fs_mgr.h>
+
+// Finds the volume specified by the given path. fs_mgr_get_entry_for_mount_point() does exact match
+// only, so it attempts the prefixes recursively (e.g. "/cache/recovery/last_log",
+// "/cache/recovery", "/cache", "/" for a given path of "/cache/recovery/last_log") and returns the
+// first match or nullptr.
+struct fstab_rec* fs_mgr_fstab_rec_for_path(struct fstab* fstab, const char* path);
+
+// Make sure that the volume 'path' is on is mounted.
+// If 'mount_point' is nullptr, use mount point in fstab.
+// Returns 0 on success (volume is mounted).
+int fs_mgr_ensure_path_mounted(struct fstab* fstab, const char* path,
+                               const char* mount_point = nullptr);
+
+// Return "/system" if it is in default fstab, otherwise "/".
+std::string fs_mgr_get_system_root();
+
+// Return true iff logical partitions are mapped when partitions are mounted via ensure_path_mounted
+// functions.
+bool fs_mgr_logical_partitions_mapped();
+
+// Make sure that the volume 'path' is on is unmounted.  Returns 0 on
+// success (volume is unmounted);
+int fs_mgr_ensure_path_unmounted(struct fstab* fstab, const char* path);
