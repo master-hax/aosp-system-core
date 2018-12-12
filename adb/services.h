c1a7b4bbee1c098f5570d0a3f8fbeecb88e08583
Alex Buynytskyy <alexbuy@google.com>
2018-12-12 10:48:50 -0800

    Adding Android Binder Bridge (abb) utility launched from adbd.
    Once launched, abb will listen for incoming Binder cli requests.
    Executing in-process provides 6x latency improvement (125ms vs 25ms on
    PixelXL) for commands like 'package path'. These are required by Android
    Studio for fast deployment of APKs.
    
    Test: manual
    
    Change-Id: Ica84eb2ec9628efa441ecd627b119f3361feaf9f

--- adb/services.h
+++ adb/services.h
@@ -24,4 +24,5 @@ constexpr char kShellServiceArgPty[] = "pty";
 constexpr char kShellServiceArgShellProtocol[] = "v2";
 
 unique_fd create_service_thread(const char* service_name, std::function<void(unique_fd)> func);
+
 #endif  // SERVICES_H_
