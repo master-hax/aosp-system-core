3100b8c51384d55db017bd33b545e4efcd47331a
Pirama Arumuga Nainar <pirama@google.com>
2018-12-21 14:13:07 -0800

    Use '__attribute__((always_inline))' instead of 'inline'
    Bug: http://b/116873221
    
    'inline' does not guarantee an external visible symbol, which causes an
    issue with -O0.  For enabling coverage, we also need to build with -O0.
    Use __attribute__((always_inline)) instead.
    
    Test: Build lmkd with coverage enabled:
          m NATIVE_COVERAGE=true COVERAGE_PATHS="system/core/lmkd"
    
    Change-Id: I2880119834f764164a1aac10b696e76a2b462b71

--- lmkd/include/lmkd.h
+++ lmkd/include/lmkd.h
@@ -50,7 +50,7 @@ enum lmk_cmd {
 typedef int LMKD_CTRL_PACKET[CTRL_PACKET_MAX_SIZE / sizeof(int)];
 
 /* Get LMKD packet command */
-inline enum lmk_cmd lmkd_pack_get_cmd(LMKD_CTRL_PACKET pack) {
+__attribute__((always_inline)) enum lmk_cmd lmkd_pack_get_cmd(LMKD_CTRL_PACKET pack) {
     return (enum lmk_cmd)ntohl(pack[0]);
 }
 
@@ -64,8 +64,8 @@ struct lmk_target {
  * For LMK_TARGET packet get target_idx-th payload.
  * Warning: no checks performed, caller should ensure valid parameters.
  */
-inline void lmkd_pack_get_target(LMKD_CTRL_PACKET packet,
-                                 int target_idx, struct lmk_target *target) {
+__attribute__((always_inline)) void lmkd_pack_get_target(LMKD_CTRL_PACKET packet, int target_idx,
+                                                         struct lmk_target* target) {
     target->minfree = ntohl(packet[target_idx * 2 + 1]);
     target->oom_adj_score = ntohl(packet[target_idx * 2 + 2]);
 }
@@ -74,9 +74,9 @@ inline void lmkd_pack_get_target(LMKD_CTRL_PACKET packet,
  * Prepare LMK_TARGET packet and return packet size in bytes.
  * Warning: no checks performed, caller should ensure valid parameters.
  */
-inline size_t lmkd_pack_set_target(LMKD_CTRL_PACKET packet,
-                                   struct lmk_target *targets,
-                                   size_t target_cnt) {
+__attribute__((always_inline)) size_t lmkd_pack_set_target(LMKD_CTRL_PACKET packet,
+                                                           struct lmk_target* targets,
+                                                           size_t target_cnt) {
     int idx = 0;
     packet[idx++] = htonl(LMK_TARGET);
     while (target_cnt) {
@@ -99,8 +99,8 @@ struct lmk_procprio {
  * For LMK_PROCPRIO packet get its payload.
  * Warning: no checks performed, caller should ensure valid parameters.
  */
-inline void lmkd_pack_get_procprio(LMKD_CTRL_PACKET packet,
-                                   struct lmk_procprio *params) {
+__attribute__((always_inline)) void lmkd_pack_get_procprio(LMKD_CTRL_PACKET packet,
+                                                           struct lmk_procprio* params) {
     params->pid = (pid_t)ntohl(packet[1]);
     params->uid = (uid_t)ntohl(packet[2]);
     params->oomadj = ntohl(packet[3]);
@@ -110,8 +110,8 @@ inline void lmkd_pack_get_procprio(LMKD_CTRL_PACKET packet,
  * Prepare LMK_PROCPRIO packet and return packet size in bytes.
  * Warning: no checks performed, caller should ensure valid parameters.
  */
-inline size_t lmkd_pack_set_procprio(LMKD_CTRL_PACKET packet,
-                                   struct lmk_procprio *params) {
+__attribute__((always_inline)) size_t lmkd_pack_set_procprio(LMKD_CTRL_PACKET packet,
+                                                             struct lmk_procprio* params) {
     packet[0] = htonl(LMK_PROCPRIO);
     packet[1] = htonl(params->pid);
     packet[2] = htonl(params->uid);
@@ -128,8 +128,8 @@ struct lmk_procremove {
  * For LMK_PROCREMOVE packet get its payload.
  * Warning: no checks performed, caller should ensure valid parameters.
  */
-inline void lmkd_pack_get_procremove(LMKD_CTRL_PACKET packet,
-                                   struct lmk_procremove *params) {
+__attribute__((always_inline)) void lmkd_pack_get_procremove(LMKD_CTRL_PACKET packet,
+                                                             struct lmk_procremove* params) {
     params->pid = (pid_t)ntohl(packet[1]);
 }
 
@@ -137,8 +137,8 @@ inline void lmkd_pack_get_procremove(LMKD_CTRL_PACKET packet,
  * Prepare LMK_PROCREMOVE packet and return packet size in bytes.
  * Warning: no checks performed, caller should ensure valid parameters.
  */
-inline size_t lmkd_pack_set_procremove(LMKD_CTRL_PACKET packet,
-                                   struct lmk_procprio *params) {
+__attribute__((always_inline)) size_t lmkd_pack_set_procremove(LMKD_CTRL_PACKET packet,
+                                                               struct lmk_procprio* params) {
     packet[0] = htonl(LMK_PROCREMOVE);
     packet[1] = htonl(params->pid);
     return 2 * sizeof(int);
@@ -148,7 +148,7 @@ inline size_t lmkd_pack_set_procremove(LMKD_CTRL_PACKET packet,
  * Prepare LMK_PROCPURGE packet and return packet size in bytes.
  * Warning: no checks performed, caller should ensure valid parameters.
  */
-inline size_t lmkd_pack_set_procpurge(LMKD_CTRL_PACKET packet) {
+__attribute__((always_inline)) size_t lmkd_pack_set_procpurge(LMKD_CTRL_PACKET packet) {
     packet[0] = htonl(LMK_PROCPURGE);
     return sizeof(int);
 }
@@ -163,8 +163,8 @@ struct lmk_getkillcnt {
  * For LMK_GETKILLCNT packet get its payload.
  * Warning: no checks performed, caller should ensure valid parameters.
  */
-inline void lmkd_pack_get_getkillcnt(LMKD_CTRL_PACKET packet,
-                                   struct lmk_getkillcnt *params) {
+__attribute__((always_inline)) void lmkd_pack_get_getkillcnt(LMKD_CTRL_PACKET packet,
+                                                             struct lmk_getkillcnt* params) {
     params->min_oomadj = ntohl(packet[1]);
     params->max_oomadj = ntohl(packet[2]);
 }
@@ -173,8 +173,8 @@ inline void lmkd_pack_get_getkillcnt(LMKD_CTRL_PACKET packet,
  * Prepare LMK_GETKILLCNT packet and return packet size in bytes.
  * Warning: no checks performed, caller should ensure valid parameters.
  */
-inline size_t lmkd_pack_set_getkillcnt(LMKD_CTRL_PACKET packet,
-                                       struct lmk_getkillcnt *params) {
+__attribute__((always_inline)) size_t lmkd_pack_set_getkillcnt(LMKD_CTRL_PACKET packet,
+                                                               struct lmk_getkillcnt* params) {
     packet[0] = htonl(LMK_GETKILLCNT);
     packet[1] = htonl(params->min_oomadj);
     packet[2] = htonl(params->max_oomadj);
@@ -185,7 +185,8 @@ inline size_t lmkd_pack_set_getkillcnt(LMKD_CTRL_PACKET packet,
  * Prepare LMK_GETKILLCNT reply packet and return packet size in bytes.
  * Warning: no checks performed, caller should ensure valid parameters.
  */
-inline size_t lmkd_pack_set_getkillcnt_repl(LMKD_CTRL_PACKET packet, int kill_cnt) {
+__attribute__((always_inline)) size_t lmkd_pack_set_getkillcnt_repl(LMKD_CTRL_PACKET packet,
+                                                                    int kill_cnt) {
     packet[0] = htonl(LMK_GETKILLCNT);
     packet[1] = htonl(kill_cnt);
     return 2 * sizeof(int);
