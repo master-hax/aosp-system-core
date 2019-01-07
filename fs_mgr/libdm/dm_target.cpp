df0273becac0bbe015cc7d25b5647654d1fc42a5
Bowgo Tsai <bowgotsai@google.com>
2019-01-07 15:30:27 +0800

    Support host build for libdm and libfstab
    The host builds for both libs are needed for libfs_avb host unit test.
    Also replaces strlcat()/strlcpy() with snprintf() because the former
    doesn't have a glibc version.
    
    Bug: 112103720
    Bug: 117960205
    Test: boot a device
    Test: run libdm_test on the device
    Change-Id: Id9b92b5286b8ed9ab0d80f18ab5802dcfeb83dfa

--- fs_mgr/libdm/dm_target.cpp
+++ fs_mgr/libdm/dm_target.cpp
@@ -42,7 +42,11 @@ std::string DmTarget::Serialize() const {
     struct dm_target_spec* spec = reinterpret_cast<struct dm_target_spec*>(&data[0]);
     spec->sector_start = start();
     spec->length = size();
+#ifdef HOST
+    snprintf(spec->target_type, sizeof(spec->target_type), "%s", name().c_str());
+#else
     strlcpy(spec->target_type, name().c_str(), sizeof(spec->target_type));
+#endif
     spec->next = (uint32_t)data.size();
     return data;
 }
