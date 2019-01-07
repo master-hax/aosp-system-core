6b7db04a6065aca8a510c346afb225b38e1e59ff
Zimuzo <zezeozue@google.com>
2019-01-07 10:19:02 +0000

    Set system property if 'updatable' process crashes too frequently
    Critical processes currently cause the device to reboot into recovery when they crash 4 times in 4mins.
    We extend this feature to a new 'updatable' class of init services. If these services crash 4 times in 4mins, instead of rebooting into recovery, the ro.update_status to 'failure'. apexd and update_verifier will listen to that property and try to abort and rollback updates, staged apex modules and new slots respectively.
    
    Test: Tested manually by marking zygote as updatable and killing zygote frequently, verified that property is set.
    Bug: 120598832
    Change-Id: I7d47ea1223f7792a834981c729694242ab3f28c9

--- init/service.cpp
+++ init/service.cpp
@@ -369,10 +369,17 @@ void Service::Reap(const siginfo_t& siginfo) {
 
     // If we crash > 4 times in 4 minutes, reboot into bootloader.
     boot_clock::time_point now = boot_clock::now();
-    if ((flags_ & SVC_CRITICAL) && !(flags_ & SVC_RESTART)) {
+    if (((flags_ & SVC_CRITICAL) || classnames_.count("updatable")) && !(flags_ & SVC_RESTART)) {
         if (now < time_crashed_ + 4min) {
             if (++crash_count_ > 4) {
-                LOG(FATAL) << "critical process '" << name_ << "' exited 4 times in 4 minutes";
+                if (flags_ & SVC_CRITICAL) {
+                    // Aborts into recovery
+                    LOG(FATAL) << "critical process '" << name_ << "' exited 4 times in 4 minutes";
+                } else {
+                    LOG(ERROR) << "updatable process '" << name_ << "' exited 4 times in 4 minutes";
+                    // Notifies update_verifier and apexd
+                    property_set("ro.update_status", "failure");
+                }
             }
         } else {
             time_crashed_ = now;
