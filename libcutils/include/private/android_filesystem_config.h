13577ea228f605d2e2d90389b5bbadcc3fcd3c4a
Martijn Coenen <maco@google.com>
2019-01-10 08:42:32 +0100

    Update AID_ISOLATED_START to include new range.
    The AppZygote allocates UIDs out of the [90000..98999] range.
    
    Additionally, introduce new constants for the isolated UIDs used by the
    app zygote vs the regular zygote; this allows callers that care to
    differentiate (eg getpwnam() in bionic) do that.
    
    Bug: 111434506
    Test: builds
    Change-Id: I7846a81077636f6f9aff2880f1c9ee01c6f98257

--- libcutils/include/private/android_filesystem_config.h
+++ libcutils/include/private/android_filesystem_config.h
@@ -190,8 +190,17 @@
  */
 #define AID_OVERFLOWUID 65534 /* unmapped user in the user namespace */
 
-#define AID_ISOLATED_START 99000 /* start of uids for fully isolated sandboxed processes */
+/* use the ranges below to determine whether a process is isolated */
+#define AID_ISOLATED_START 90000 /* start of uids for fully isolated sandboxed processes */
 #define AID_ISOLATED_END 99999   /* end of uids for fully isolated sandboxed processes */
+
+/* isolated processes in this range are spawned from an application zygote */
+#define AID_APP_ZYGOTE_ISOLATED_START 90000
+#define AID_APP_ZYGOTE_ISOLATED_END 98999
+
+/* isolated processes in this range are spawned from the regular zygote */
+#define AID_REGULAR_ZYGOTE_ISOLATED_START 99000
+#define AID_REGULAR_ZYGOTE_ISOLATED_END 99999
 
 #define AID_USER 100000        /* TODO: switch users over to AID_USER_OFFSET */
 #define AID_USER_OFFSET 100000 /* offset for uid ranges for each user */
