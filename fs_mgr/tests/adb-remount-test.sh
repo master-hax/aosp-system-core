c331fe2d6a9db0ccb173fecd60eabc332524864d
Jiyong Park <jiyong@google.com>
2019-01-17 23:26:22 +0900

    Fix: bionic files are unabled to be pushed to the device
    This CL fixes a problem that `adb push` and `adb sync` do not work for
    the bionic files that were moved to the runtime APEX.
    
    With [1], the bionic libraries and the dynanic linker are moved to the
    runtime APEX. However, in order to provide the same path, e.g.,
    /system/lib/libc.so, to all processes, the files in the runtime APEX are
    bind-mounted to the old paths. This is causing problem when pushing
    files to the paths which are now mountpoints and not regular files.
    
    The solution to this problem are two folds:
    
    1) Init no longer bind mounts the bootstrap bionic to the mountpoints.
    Instead, it makes following three paths which are the parent directories
    of the bionic files as private mount points
    - system/lib
    - system/lib64
    - system/bin
    
    Bionic from the runtime APEX are bind-mounted to the ordinary paths
    (/system/lib/libc.so, etc.) as before.
    
    2) when adbd executes the file sync service service, it temporarily
    creates a new mount namespace and unmounts the new private mount points
    to reveal the underlying raw filesystem. The sync is done on top of the
    raw filesystem.
    
    In addition, since we are introducing mount points inside the system
    partition, they are also explicitly remounted during `adb remount`.
    
    [1] https://android-review.googlesource.com/q/topic:move_bionic_to_apex
    Bug: 122737045
    Test: adb root; adb remount; adb sync
    Test: ./system/core/fs_mgr/tests/adb-remount-test.sh passed
    Change-Id: I628f510c62df68709b7b57e54e44671b9a02a49e

--- fs_mgr/tests/adb-remount-test.sh
+++ fs_mgr/tests/adb-remount-test.sh
@@ -194,6 +194,11 @@ fastboot_getvar() {
   echo ${O} >&2
 }
 
+# Do nothing. Should be redefined when necessary.
+cleanup() {
+  :
+}
+
 [ "USAGE: die [-d|-t <epoch>] [message] >/dev/stderr
 
 If -d, or -t <epoch> argument is supplied, dump logcat.
@@ -212,6 +217,7 @@ die() {
     shift 2
   fi
   echo "${RED}[  FAILED  ]${NORMAL} ${@}" >&2
+  cleanup
   exit 1
 }
 
@@ -532,6 +538,18 @@ B="`adb_cat /vendor/hello`" ||
   die "vendor hello"
 check_eq "${A}" "${B}" /vendor before reboot
 
+# download libc.so, append some gargage, push back, and check if the file is updated.
+tempdir="`mktemp -d`"
+cleanup() {
+  rm -rf ${tempdir}
+}
+adb pull /system/lib/libc.so ${tempdir} || die "pull libc.so from device"
+garbage="`hexdump -n 16 -e '4/4 "%08X" 1 "\n"' /dev/random`"
+echo ${garbage} >> ${tempdir}/libc.so
+adb push ${tempdir}/libc.so /system/lib/libc.so || die "push libc.so to device"
+adb pull /system/lib/libc.so ${tempdir}/libc.so.fromdevice || die "pull libc.so from device"
+diff ${tempdir}/libc.so ${tempdir}/libc.so.fromdevice > /dev/null || die "libc.so differ"
+
 echo "${GREEN}[ RUN      ]${NORMAL} reboot to confirm content persistent" >&2
 
 adb_reboot &&
@@ -568,6 +586,14 @@ adb_root &&
   die "re-read /vendor/hello after reboot"
 check_eq "${A}" "${B}" vendor after reboot
 echo "${GREEN}[       OK ]${NORMAL} /vendor content remains after reboot" >&2
+
+# check if the updated libc.so is persistent after reboot
+adb_root &&
+  adb pull /system/lib/libc.so ${tempdir}/libc.so.fromdevice ||
+  die "pull libc.so from device"
+diff ${tempdir}/libc.so ${tempdir}/libc.so.fromdevice > /dev/null || die "libc.so differ"
+rm -r ${tempdir}
+echo "${GREEN}[       OK ]${NORMAL} /system/lib/libc.so content remains after reboot" >&2
 
 echo "${GREEN}[ RUN      ]${NORMAL} flash vendor, confirm its content disappears" >&2
 
