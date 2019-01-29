55cd0a8538c60bbc8de20b9d95492670dfcfa509
Erick Reyes <erickreyes@google.com>
2019-01-28 19:53:30 -0800

    meminfo: handle multiple buffer references in dmabufinfo
    Change the dmabufinfo library to handle multiple references to the same
    buffer across PIDs by doing these changes:
    
    - Do not clear the supplied DmaBuffer vector when calling ReadDmaBufInfo
    for a specific PID
    - Change the way fd/map references to a buffer belonging to a PID are
    stored in DmaBuffer from vector<pid_t> to unorderedmap<pid_t, int>
    - When parsing proc/%pid/{maps|fds}, look up the buffer by inode and
    update the data if found
    
    Test: builds
    Bug: 63860998
    
    Change-Id: If22feea772f182086ae3446b46d626a750674b1f
    Signed-off-by: Erick Reyes <erickreyes@google.com>

--- libmeminfo/libdmabufinfo/include/dmabufinfo/dmabufinfo.h
+++ libmeminfo/libdmabufinfo/include/dmabufinfo/dmabufinfo.h
@@ -21,6 +21,7 @@
 
 #include <string>
 #include <vector>
+#include <unordered_map>
 
 namespace android {
 namespace dmabufinfo {
@@ -33,20 +34,27 @@ struct DmaBuffer {
     ~DmaBuffer() = default;
 
     // Adds one file descriptor reference for the given pid
-    void AddFdRef(pid_t pid) { fdrefs_.emplace_back(pid); }
+    void AddFdRef(pid_t pid) {
+        AddRefToPidMap(fdrefs_, pid);
+    }
 
     // Adds one map reference for the given pid
-    void AddMapRef(pid_t pid) { maprefs_.emplace_back(pid); }
+    void AddMapRef(pid_t pid) {
+        AddRefToPidMap(maprefs_, pid);
+    }
 
     // Getters for each property
     uint64_t size() { return size_; }
-    const std::vector<pid_t>& fdrefs() const { return fdrefs_; }
-    const std::vector<pid_t>& maprefs() const { return maprefs_; }
+    const std::unordered_map<pid_t, int>& fdrefs() const { return fdrefs_; }
+    const std::unordered_map<pid_t, int>& maprefs() const { return maprefs_; }
     ino_t inode() const { return inode_; }
     uint64_t total_refs() const { return fdrefs_.size() + maprefs_.size(); }
     uint64_t count() const { return count_; };
     const std::string& name() const { return name_; }
     const std::string& exporter() const { return exporter_; }
+    void SetName(const std::string& name) { name_ = name; }
+    void SetExporter(const std::string& exporter) { exporter_ = exporter; }
+    void SetCount(uint64_t count) { count_ = count; }
 
   private:
     ino_t inode_;
@@ -54,19 +62,32 @@ struct DmaBuffer {
     uint64_t count_;
     std::string exporter_;
     std::string name_;
-    std::vector<pid_t> fdrefs_;
-    std::vector<pid_t> maprefs_;
+    std::unordered_map<pid_t, int> fdrefs_;
+    std::unordered_map<pid_t, int> maprefs_;
+    void AddRefToPidMap(std::unordered_map<pid_t, int>& map, pid_t pid) {
+        auto [it, inserted] = map.insert(std::make_pair(pid, 1));
+        if (!inserted) {
+            auto& existing = it->second;
+            existing++;
+        }
+    }
 };
 
 // Read and return current dma buf objects from
 // DEBUGFS/dma_buf/bufinfo. The references to each dma buffer are not
 // populated here and will return an empty vector.
+// The vector will be cleared before parsing.
+//
 // Returns false if something went wrong with the function, true otherwise.
 bool ReadDmaBufInfo(std::vector<DmaBuffer>* dmabufs,
                     const std::string& path = "/sys/kernel/debug/dma_buf/bufinfo");
 
 // Read and return dmabuf objects for a given process without the help
 // of DEBUGFS
+// Any references found will be appended to the supplied vector in order to
+// allow incremental parsing (For example, to read multiple PIDs)
+//
+// Returns false if something went wrong with the function, true otherwise.
 bool ReadDmaBufInfo(pid_t pid, std::vector<DmaBuffer>* dmabufs);
 
 }  // namespace dmabufinfo
