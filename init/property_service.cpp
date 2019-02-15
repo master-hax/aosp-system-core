3b261f721a1c0f79e6fa5f394316d5f1cea1ee8b
Jinguang Dong <dongjinguang@huawei.com>
2019-02-15 16:56:28 +0800

    load selinux property_contexts in /odm and /product
    Commit I458abc23275ecdfadcabacd611b288449196efa0 make property_service no longer
    load selinux property_contexts by function selinux_android_prop_context_handle,
    So we should config /odm and /product property_contexts loading path in
    property_service.cpp but not in libselinux.
    
    Test: property_service can load property_contexts in /odm and /product
    Change-Id: Ifa94b87180c4867ecbe8dea347ad02bb37958043

--- init/property_service.cpp
+++ init/property_service.cpp
@@ -907,6 +907,12 @@ void CreateSerializedPropertyInfo() {
             // Fallback to nonplat_* if vendor_* doesn't exist.
             LoadPropertyInfoFromFile("/vendor/etc/selinux/nonplat_property_contexts",
                                      &property_infos);
+            if (access("/product/etc/selinux/product_property_contexts", R_OK) != -1)
+                LoadPropertyInfoFromFile("/product/etc/selinux/product_property_contexts",
+                                         &property_infos);
+        }
+        if (access("/odm/etc/selinux/odm_property_contexts", R_OK) != -1) {
+            LoadPropertyInfoFromFile("/odm/etc/selinux/odm_property_contexts", &property_infos);
         }
     } else {
         if (!LoadPropertyInfoFromFile("/plat_property_contexts", &property_infos)) {
@@ -916,6 +922,8 @@ void CreateSerializedPropertyInfo() {
             // Fallback to nonplat_* if vendor_* doesn't exist.
             LoadPropertyInfoFromFile("/nonplat_property_contexts", &property_infos);
         }
+        LoadPropertyInfoFromFile("/product_property_contexts", &property_infos);
+        LoadPropertyInfoFromFile("/odm_property_contexts", &property_infos);
     }
 
     auto serialized_contexts = std::string();
