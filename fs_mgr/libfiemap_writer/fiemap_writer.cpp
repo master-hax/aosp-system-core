4254ded767d841debf38f189b9c3ae9244f8f605
David Anderson <dvander@google.com>
2019-02-28 21:00:51 -0800

    Remove the block size alignment restriction.
    Rather than require block-size alignment, instead bump the requested
    file size to the necessary alignment. This ensures that the final block
    is usable without placing onerous restrictions on the caller to figure
    out the file system's block size.
    
    This will require callers (namely, gsid) to track the actual desired
    image size separately from the flie size.
    
    This patch also updates tests to use the actual filesize of the
    filesystem, rather than hardcoded 4096.
    
    Bug: 126230649
    Test: fiemap_writer_test gtest
    Change-Id: I000cca274718c3ceac526d7c3392fe3a23bb42bc

--- fs_mgr/libfiemap_writer/fiemap_writer.cpp
+++ fs_mgr/libfiemap_writer/fiemap_writer.cpp
@@ -200,12 +200,6 @@ static bool PerformFileChecks(const std::string& file_path, uint64_t file_size, 
         return false;
     }
 
-    if (file_size % sfs.f_bsize) {
-        LOG(ERROR) << "File size " << file_size << " is not aligned to optimal block size "
-                   << sfs.f_bsize << " for file " << file_path;
-        return false;
-    }
-
     // Check if the filesystem is of supported types.
     // Only ext4 and f2fs are tested and supported.
     if ((sfs.f_type != EXT4_SUPER_MAGIC) && (sfs.f_type != F2FS_SUPER_MAGIC)) {
@@ -498,6 +492,11 @@ FiemapUniquePtr FiemapWriter::Open(const std::string& file_path, uint64_t file_s
         LOG(ERROR) << "Failed to validate file or file system for file:" << abs_path;
         cleanup(abs_path, create);
         return nullptr;
+    }
+
+    // Align up to the nearest block size.
+    if (file_size % blocksz) {
+        file_size += blocksz - (file_size % blocksz);
     }
 
     if (create) {
