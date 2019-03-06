7b9a6f77e55c36c61b634c7007969abe27efcc36
Mark Salyzyn <salyzyn@google.com>
2019-03-06 15:53:21 -0800

    fs_mgr: init: Add fs_mgr_set_sleep_for
    Provide a replacement for std::this_thread::sleep_for() function
    used internally.
    
    Also provide EpollSleepManager::sleep_for to fs_mgr_set_sleep_for().
    
    Test: compile
    Bug: 124072565
    Change-Id: I62457b580c6254795289a69d9f8fb14c83954c43

--- init/init.cpp
+++ init/init.cpp
@@ -40,6 +40,7 @@
 #include <android-base/strings.h>
 #include <cutils/android_reboot.h>
 #include <fs_avb/fs_avb.h>
+#include <fs_mgr.h>
 #include <fs_mgr_vendor_overlay.h>
 #include <keyutils.h>
 #include <libavb/libavb.h>
@@ -687,6 +688,7 @@ int SecondStageMain(int argc, char** argv) {
     fs_mgr_vendor_overlay_mount_all();
     export_oem_lock_status();
     EpollSleepManager epollSleepManager(&epoll);
+    fs_mgr_set_sleep_for(epollSleepManager.sleep_for);
     StartPropertyService(&epoll);
     MountHandler mount_handler(&epoll);
     set_usb_controller();
