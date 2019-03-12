45dc06d8625f60670e1c3bbd614cd5a2e612b552
Casey Dahlin <sadmac@google.com>
2019-03-12 13:55:06 -0700

    Split out debug data decompression
    Fuchsia doesn't need support for this and doesn't have the libraries
    involved, so we need to separate it out so Fuchsia has the option to not
    build it.
    
    Test: Unit tests pass
    Change-Id: If73453c5fe03472668ae08ca905abf59af794460

--- libunwindstack/ElfInterface.cpp
+++ libunwindstack/ElfInterface.cpp
@@ -21,10 +21,6 @@
 #include <string>
 #include <utility>
 
-#include <7zCrc.h>
-#include <Xz.h>
-#include <XzCrc64.h>
-
 #include <unwindstack/DwarfError.h>
 #include <unwindstack/DwarfSection.h>
 #include <unwindstack/ElfInterface.h>
@@ -32,6 +28,7 @@
 #include <unwindstack/Memory.h>
 #include <unwindstack/Regs.h>
 
+#include "DebugDataMemory.h"
 #include "DwarfDebugFrame.h"
 #include "DwarfEhFrame.h"
 #include "DwarfEhFrameWithHdr.h"
@@ -74,53 +71,15 @@ Memory* ElfInterface::CreateGnuDebugdataMemory() {
     return nullptr;
   }
 
-  // TODO: Only call these initialization functions once.
-  CrcGenerateTable();
-  Crc64GenerateTable();
+  Memory* ret =
+      ::unwindstack::CreateGnuDebugdataMemory(memory_, gnu_debugdata_offset_, gnu_debugdata_size_);
 
-  std::vector<uint8_t> src(gnu_debugdata_size_);
-  if (!memory_->ReadFully(gnu_debugdata_offset_, src.data(), gnu_debugdata_size_)) {
+  if (!ret) {
     gnu_debugdata_offset_ = 0;
     gnu_debugdata_size_ = static_cast<uint64_t>(-1);
-    return nullptr;
   }
 
-  ISzAlloc alloc;
-  CXzUnpacker state;
-  alloc.Alloc = [](ISzAllocPtr, size_t size) { return malloc(size); };
-  alloc.Free = [](ISzAllocPtr, void* ptr) { return free(ptr); };
-
-  XzUnpacker_Construct(&state, &alloc);
-
-  std::unique_ptr<MemoryBuffer> dst(new MemoryBuffer);
-  int return_val;
-  size_t src_offset = 0;
-  size_t dst_offset = 0;
-  ECoderStatus status;
-  dst->Resize(5 * gnu_debugdata_size_);
-  do {
-    size_t src_remaining = src.size() - src_offset;
-    size_t dst_remaining = dst->Size() - dst_offset;
-    if (dst_remaining < 2 * gnu_debugdata_size_) {
-      dst->Resize(dst->Size() + 2 * gnu_debugdata_size_);
-      dst_remaining += 2 * gnu_debugdata_size_;
-    }
-    return_val = XzUnpacker_Code(&state, dst->GetPtr(dst_offset), &dst_remaining, &src[src_offset],
-                                 &src_remaining, true, CODER_FINISH_ANY, &status);
-    src_offset += src_remaining;
-    dst_offset += dst_remaining;
-  } while (return_val == SZ_OK && status == CODER_STATUS_NOT_FINISHED);
-  XzUnpacker_Free(&state);
-  if (return_val != SZ_OK || !XzUnpacker_IsStreamWasFinished(&state)) {
-    gnu_debugdata_offset_ = 0;
-    gnu_debugdata_size_ = static_cast<uint64_t>(-1);
-    return nullptr;
-  }
-
-  // Shrink back down to the exact size.
-  dst->Resize(dst_offset);
-
-  return dst.release();
+  return ret;
 }
 
 template <typename AddressType>
