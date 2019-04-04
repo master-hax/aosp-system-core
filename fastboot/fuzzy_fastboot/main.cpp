24423ab97d9baa8b395fe23234eb476f5b2c2146
Hridya Valsaraju <hridya@google.com>
2019-04-04 13:43:29 -0700

    Test is-logical command for vendor and boot partitions.
    Vendor must be a logical partition and boot must not be a logical
    partition.
    
    Test: fuzzy_fastboot --gtest_filter=*Logical*
    Bug: 117220134
    Change-Id: Ifc6f2f715ca92cd1fe779e8fce2d6a10a1f140b9

--- fastboot/fuzzy_fastboot/main.cpp
+++ fastboot/fuzzy_fastboot/main.cpp
@@ -201,18 +201,29 @@ TEST_F(LogicalPartitionCompliance, GetVarIsLogical) {
     ASSERT_TRUE(UserSpaceFastboot());
     std::string has_slot;
     EXPECT_EQ(fb->GetVar("has-slot:system", &has_slot), SUCCESS) << "getvar has-slot:system failed";
-    std::string is_logical_cmd;
+    std::string is_logical_cmd_system, is_logical_cmd_vendor, is_logical_cmd_boot;
     if (has_slot == "yes") {
         std::string current_slot;
         EXPECT_EQ(fb->GetVar("current-slot", &current_slot), SUCCESS)
                 << "getvar current-slot failed";
-        is_logical_cmd = "is-logical:system_" + current_slot;
+        is_logical_cmd_system = "is-logical:system_" + current_slot;
+        is_logical_cmd_vendor = "is-logical:vendor_" + current_slot;
+        is_logical_cmd_boot = "is-logical:boot_" + current_slot;
     } else {
-        is_logical_cmd = "is-logical:system";
+        is_logical_cmd_system = "is-logical:system";
+        is_logical_cmd_vendor = "is-logical:vendor";
+        is_logical_cmd_boot = "is-logical:boot";
     }
     std::string is_logical;
-    EXPECT_EQ(fb->GetVar(is_logical_cmd, &is_logical), SUCCESS) << "getvar is-logical failed";
+    EXPECT_EQ(fb->GetVar(is_logical_cmd_system, &is_logical), SUCCESS)
+            << "getvar is-logical:system failed";
     ASSERT_EQ(is_logical, "yes");
+    EXPECT_EQ(fb->GetVar(is_logical_cmd_vendor, &is_logical), SUCCESS)
+            << "getvar is-logical:vendor failed";
+    ASSERT_EQ(is_logical, "yes");
+    EXPECT_EQ(fb->GetVar(is_logical_cmd_boot, &is_logical), SUCCESS)
+            << "getvar is-logical:boot failed";
+    ASSERT_EQ(is_logical, "no");
 }
 
 TEST_F(LogicalPartitionCompliance, FastbootRebootTest) {
