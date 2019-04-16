154e556f785f78c6b767e0299a4e5dcff831c83e
Steve Muckle <smuckle@google.com>
2019-04-15 17:43:02 -0700

    create libmodprobe
    Modprobe functionality is required both within first stage init and also
    as a standalone binary. Create a library for this using and extending
    the logic in modalias_handler.cpp.
    
    Bug: 129780532
    Change-Id: Ie3582358fd839c2f64e1b386b30ed551a86aef5d

---
+++ libmodprobe/include/modprobe/modprobe.h
@@ -0,0 +1,55 @@
+/*
+ * Copyright (C) 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+class Modprobe {
+  public:
+    Modprobe(std::vector<std::string>);
+    virtual ~Modprobe() = default;
+
+    void HandleUevent(const std::string& modalias);
+    bool LoadListedModules();
+
+  private:
+    std::string MakeCanonical(const std::string& module_path);
+    bool InsmodWithDeps(const std::string& module_name);
+    bool Insmod(const std::string& path_name);
+    std::vector<std::string> GetDependencies(const std::string& module);
+    bool ModuleExists(const std::string& module_name);
+    bool LoadWithAliases(const std::string& module_name, bool strict);
+
+    bool ParseDepCallback(std::vector<std::string>&& args);
+    bool ParseAliasCallback(std::vector<std::string>&& args);
+    bool ParseSoftdepCallback(std::vector<std::string>&& args);
+    bool ParseLoadCallback(std::vector<std::string>&& args);
+    bool ParseOptionsCallback(std::vector<std::string>&& args);
+    void ParseCfg(const std::string& cfg,
+                  std::function<bool(std::vector<std::string>&&)> f);
+    std::string base_path;
+    std::vector<std::pair<std::string, std::string>> module_aliases_;
+    std::unordered_map<std::string, std::vector<std::string>> module_deps_;
+    std::vector<std::pair<std::string, std::string>> module_pre_softdep_;
+    std::vector<std::pair<std::string, std::string>> module_post_softdep_;
+    std::vector<std::string> module_load_;
+    std::unordered_map<std::string, std::string> module_options_;
+};
+
+typedef bool (Modprobe::*ModprobeParseFn)(std::vector<std::string>&&);
