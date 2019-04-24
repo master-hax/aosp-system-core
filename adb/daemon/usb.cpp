0c8714b80ecf929796a4721ecdfd9507b57735c4
Josh Gao <jmgao@google.com>
2019-04-24 14:28:25 -0700

    adbd: reduce queue depths.
    Each operation we submit results in a contiguous allocation in the
    kernel which can trigger OOM-killing. Tune the queue depths to reduce
    the chances of this happening, with a minimal impact on performance.
    
    Bug: http://b/126582877
    Test: ./benchmark_device.py with blueline
    Change-Id: I4b0f83d54a9a9a69f0ab48322d78fb137c94dd35

--- adb/daemon/usb.cpp
+++ adb/daemon/usb.cpp
@@ -58,10 +58,10 @@ using android::base::StringPrintf;
 static std::optional<bool> gFfsAioSupported;
 
 // Not all USB controllers support operations larger than 16k, so don't go above that.
-static constexpr size_t kUsbReadQueueDepth = 32;
+static constexpr size_t kUsbReadQueueDepth = 8;
 static constexpr size_t kUsbReadSize = 4 * PAGE_SIZE;
 
-static constexpr size_t kUsbWriteQueueDepth = 32;
+static constexpr size_t kUsbWriteQueueDepth = 8;
 static constexpr size_t kUsbWriteSize = 4 * PAGE_SIZE;
 
 static const char* to_string(enum usb_functionfs_event_type type) {
