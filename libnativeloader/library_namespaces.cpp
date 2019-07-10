87df059cade658e352e4b09a35aacfd88b58c1e9
Przemyslaw Szczepaniak <pszczepaniak@google.com>
2019-07-10 12:08:57 +0100

    ld.config for com.android.nnapi APEX.
    Makes updated libneuralnetworks.so in /apex/com.android.nnapi/{LIB}
    loadable from all relevant namespaces.
    
    Test: Verified that right library is being loaded using lsof and
    Test: verbose debug logs from bionic/ld.
    Bug: 137320025
    Change-Id: I38140bb9915a3f85f10456e15ed0f492dbdba802

--- libnativeloader/library_namespaces.cpp
+++ libnativeloader/library_namespaces.cpp
@@ -43,6 +43,7 @@ namespace {
 constexpr const char* kVendorNamespaceName = "sphal";
 constexpr const char* kVndkNamespaceName = "vndk";
 constexpr const char* kRuntimeNamespaceName = "runtime";
+constexpr const char* kNnapiNamespaceName = "nnapi";
 
 // classloader-namespace is a linker namespace that is created for the loaded
 // app. To be specific, it is created for the app classloader. When
@@ -236,6 +237,13 @@ NativeLoaderNamespace* LibraryNamespaces::Create(JNIEnv* env, uint32_t target_sd
       *error_msg = app_ns.GetError();
       return nullptr;
     }
+  }
+
+  // Give access to NNAPI libraries (apex-updated LLNDK library).
+  auto nnapi_ns = NativeLoaderNamespace::GetExportedNamespace(kNnapiNamespaceName, is_bridged);
+  if (!app_ns.Link(nnapi_ns, "libneuralnetworks.so")) {
+    *error_msg = app_ns.GetError();
+    return nullptr;
   }
 
   // Give access to VNDK-SP libraries from the 'vndk' namespace.
