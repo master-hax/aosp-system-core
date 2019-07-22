d38c8a24853a21dbdc4d59b2c321f8696a20cffe
Jiyong Park <jiyong@google.com>
2019-07-22 20:29:08 +0900

    add nopreload option in public.libraries.txt
    A lib with 'nopreload' option in public.libraries.txt is not preloaded
    during zygote. This is useful for seldom used public libraries; they
    don't contribute to the zygote startup time and only affect the apps
    that they are used.
    
    Bug: 132911956
    Test: libnativeloader_test (WIP)
    Change-Id: I6f97c90e6721aec7f2f96c8fc7b963b34f8edd3e

--- libnativeloader/native_loader_test.cpp
+++ libnativeloader/native_loader_test.cpp
@@ -288,7 +288,7 @@ class NativeLoaderTest : public ::testing::TestWithParam<bool> {
 
   void SetExpectations() {
     std::vector<std::string> default_public_libs =
-        android::base::Split(default_public_libraries(), ":");
+        android::base::Split(default_public_libraries(/* for_preload */ true), ":");
     for (auto l : default_public_libs) {
       EXPECT_CALL(*mock, dlopen(StrEq(l.c_str()), RTLD_NOW | RTLD_NODELETE))
           .WillOnce(Return(any_nonnull));
@@ -340,11 +340,13 @@ class NativeLoaderTest_Create : public NativeLoaderTest {
   bool expected_link_with_sphal_ns = true;
   bool expected_link_with_vndk_ns = false;
   bool expected_link_with_default_ns = false;
-  std::string expected_shared_libs_to_platform_ns = default_public_libraries();
+  std::string expected_shared_libs_to_platform_ns =
+      default_public_libraries(/* for_preload */ false);
   std::string expected_shared_libs_to_runtime_ns = runtime_public_libraries();
   std::string expected_shared_libs_to_sphal_ns = vendor_public_libraries();
   std::string expected_shared_libs_to_vndk_ns = vndksp_libraries();
-  std::string expected_shared_libs_to_default_ns = default_public_libraries();
+  std::string expected_shared_libs_to_default_ns =
+      default_public_libraries(/* for_preload */ false);
 
   void SetExpectations() {
     NativeLoaderTest::SetExpectations();
