f98133cf0f3b63ca4ad7470029d19bf95da68ecb
Kaiwen Szu <kwsz@google.com>
2019-07-31 14:43:04 +0800

    Construct the super_vbmeta image
    This commit constructs the super_vbmeta image to eliminate the
    need of adding /vbmeta_system and/or /vbmeta_vendor when AVB
    chain partition is used with Android Dynamic Partition.
    
    See BOARD_AVB_VBMETA_SYSTEM under the link:
    https://android.googlesource.com/platform/external/avb/#build-system-integration
    
    The structure of super_vbmeta :
    
    |     VBMeta Table    | (fixed-length 2KiB)
    | Backup VBMeta Table | (fixed-length 2KiB)
    |     VBMeta Images   | (fixed-length 64KiB each)
    
    The structure of VBMeta Table :
    
    | Super VBMeta Header | (fixed-length 128B)
    |  VBMeta Descriptors | (variable-length)
    
    The VBMeta Table records the slot number of each
    vbmeta image within the /super_vbmeta partition.
    
    Bug: 137054296
    Test: m libvbmeta_test
    Test: ./out/host/linux-x86/nativetest/libvbmeta_test/libvbmeta_test
    Change-Id: I01aeadd850750ae87d9125484c1b1f570bb84756

---
+++ fs_mgr/libvbmeta/builder.h
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <map>
+#include <string>
+
+#include <android-base/result.h>
+
+#include "super_vbmeta_format.h"
+
+namespace android {
+namespace fs_mgr {
+
+class SuperVBMetaBuilder {
+  public:
+    SuperVBMetaBuilder();
+    SuperVBMetaBuilder(const int super_vbmeta_fd,
+                       const std::map<std::string, std::string>& images_path);
+    android::base::Result<void> Build();
+    android::base::Result<std::string> ReadVBMetaImageFromFile(const std::string& file);
+    android::base::Result<uint8_t> /* slot number */ AddVBMetaImage(const std::string& vbmeta_name);
+    void DeleteVBMetaImage(const std::string& vbmeta_name);
+    std::unique_ptr<VBMetaTable> ExportVBMetaTable();
+    android::base::Result<void> ExportVBMetaTableToFile();
+    android::base::Result<void> ExportVBMetaImageToFile(const uint8_t vbmeta_slot,
+                                                        const std::string& vbmeta_image);
+
+  private:
+    android::base::Result<uint8_t> GetEmptySlot();
+
+    int super_vbmeta_fd_;
+    VBMetaTable table_;
+    std::map<std::string /* vbmeta image name */, std::string /* vbmeta image file path */>
+            images_path_;
+};
+
+}  // namespace fs_mgr
+}  // namespace android
