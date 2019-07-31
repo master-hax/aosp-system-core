01063791ddcad7291aedb50db33006845a700fc4
Kaiwen Szu <kwsz@google.com>
2019-07-31 14:43:04 +0800

    Construct the vbmeta_table image
    This commit constructs the vbmeta_table image to eliminate the
    need of adding /vbmeta_system and/or /vbmeta_vendor when AVB
    chain partition is used with Android Dynamic Partition.
    
    See BOARD_AVB_VBMETA_SYSTEM under the link:
    https://android.googlesource.com/platform/external/avb/#build-system-integration
    
    |    VBMeta Table   | (fixed-length 2KiB)
    |Backup VBMeta Table| (fixed-length 2KiB)
    
    The VBMeta Table records the physical offset and the size of each
    logical partition's vbmeta within the /super partition.
    
    Bug: 137054296
    Test: m libvbmeta_test
    Test: ./out/host/linux-x86/nativetest/libvbmeta_test/libvbmeta_test
    Change-Id: I01aeadd850750ae87d9125484c1b1f570bb84756

---
+++ fs_mgr/libvbmeta/reader.cpp
@@ -0,0 +1,106 @@
+/*
+ * Copyright (C) 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "reader.h"
+
+#include <android-base/file.h>
+
+using android::base::ErrnoError;
+using android::base::Error;
+using android::base::Result;
+
+namespace android {
+namespace fs_mgr {
+
+Result<bool> LoadAndVerifyVBMetaTableHeader(const void* buffer, VBMetaTableHeader* header) {
+    memcpy(header, buffer, sizeof(*header));
+
+    // Do basic validation of vbmeta table.
+    if (header->magic != VBMETA_TABLE_MAGIC) {
+        return Error() << "VBMeta Table has invalid magic value";
+    }
+
+    // Check that the version is compatible.
+    if (header->major_version != VBMETA_TABLE_MAJOR_VERSION ||
+        header->minor_version > VBMETA_TABLE_MINOR_VERSION) {
+        return Error() << "VBMeta Table has incompatible version";
+    }
+
+    return true;
+}
+
+void LoadVBMetaDescriptors(const void* buffer, uint32_t size,
+                           std::vector<InternalVBMetaDescriptor>* descriptors) {
+    for (int p = 0; p < size;) {
+        InternalVBMetaDescriptor descriptor;
+        memcpy(&descriptor, (char*)buffer + p, VBMETA_TABLE_DESCRIPTOR_SIZE);
+        p += VBMETA_TABLE_DESCRIPTOR_SIZE;
+
+        descriptor.partition_name =
+                std::string((char*)buffer + p, descriptor.partition_name_length);
+        p += descriptor.partition_name_length;
+
+        descriptors->emplace_back(std::move(descriptor));
+    }
+}
+
+Result<bool> ReadVBMetaTable(int fd, uint64_t offset, VBMetaTable* table) {
+    std::unique_ptr<uint8_t[]> header_buffer =
+            std::make_unique<uint8_t[]>(VBMETA_TABLE_HEADER_SIZE);
+    if (!android::base::ReadFullyAtOffset(fd, header_buffer.get(), VBMETA_TABLE_HEADER_SIZE,
+                                          offset)) {
+        return ErrnoError() << "Couldn't read vbmeta table header at offset " << offset;
+    }
+
+    Result<bool> rv_header = LoadAndVerifyVBMetaTableHeader(header_buffer.get(), &table->header);
+    if (!rv_header) {
+        return rv_header.error();
+    }
+
+    const uint64_t descriptors_offset = offset + table->header.header_size;
+    std::unique_ptr<uint8_t[]> descriptors_buffer =
+            std::make_unique<uint8_t[]>(table->header.descriptors_size);
+    if (!android::base::ReadFullyAtOffset(fd, descriptors_buffer.get(),
+                                          table->header.descriptors_size, descriptors_offset)) {
+        return ErrnoError() << "Couldn't read vbmeta table descriptors at offset "
+                            << descriptors_offset;
+    }
+
+    LoadVBMetaDescriptors(descriptors_buffer.get(), table->header.descriptors_size,
+                          &table->descriptors);
+    return true;
+}
+
+Result<bool> ReadPrimaryVBMetaTable(int fd, VBMetaTable* table) {
+    uint64_t offset = PRIMARY_VBMETA_TABLE_OFFSET;
+    Result<bool> read_vbmeta_table = ReadVBMetaTable(fd, offset, table);
+    if (!read_vbmeta_table) {
+        return read_vbmeta_table.error();
+    }
+    return true;
+}
+
+Result<bool> ReadBackupVBMetaTable(int fd, VBMetaTable* table) {
+    uint64_t offset = BACKUP_VBMETA_TABLE_OFFSET;
+    Result<bool> read_vbmeta_table = ReadVBMetaTable(fd, offset, table);
+    if (!read_vbmeta_table) {
+        return read_vbmeta_table.error();
+    }
+    return true;
+}
+
+}  // namespace fs_mgr
+}  // namespace android
---
+++ fs_mgr/libvbmeta/utility.h
@@ -0,0 +1,62 @@
+/*
+ * Copyright (C) 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <android-base/logging.h>
+#include <android-base/result.h>
+#include <libavb/libavb.h>
+#include <liblp/liblp.h>
+
+#include "vbmeta_table_format.h"
+
+#define VBMETA_TAG "[libvbmeta]"
+#define LWARN LOG(WARNING) << VBMETA_TAG
+#define LINFO LOG(INFO) << VBMETA_TAG
+#define LERROR LOG(ERROR) << VBMETA_TAG
+#define PWARNING PLOG(WARNING) << VBMETA_TAG
+#define PERROR PLOG(ERROR) << VBMETA_TAG
+
+namespace android {
+namespace fs_mgr {
+
+uint64_t ComputePartitionSize(const LpMetadata& lpmetadata, const LpMetadataPartition& partition);
+
+uint64_t ComputeSuperSize(const LpMetadata& lpmetadata);
+
+// Helper to parse partition AVB Footer
+android::base::Result<bool> ParsePartitionAvbFooter(const void* buffer, AvbFooter* footer);
+
+// Helper to load avb footer, it reads the partition file by |partition_fd|
+// and seek to the position of avbfooter by |partition_size|.
+android::base::Result<std::unique_ptr<AvbFooter>> LoadAvbFooter(const int partition_fd,
+                                                                const uint64_t partition_size);
+
+// Helper to build physical VBMetaInfo, it use avbfooter to get the logical address and the size of
+// vbmeta and calculate the physical address by |lpmetadata| and |lppartition|.
+android::base::Result<std::unique_ptr<VBMetaInfo>> BuildPhysicalVBMetaInfo(
+        const LpMetadata& lpmetadata, const LpMetadataPartition& lppartition,
+        const AvbFooter& avbfooter);
+
+// Helper to validate vbmeta (equal to AVB_VBMETA_VERIFY_RESULT_OK), which is |size| bytes at
+// |offset| in file |fd|.
+android::base::Result<bool> ValidateVBMeta(int fd, uint64_t offset, uint64_t size);
+
+// Helper to validate vbmeta (equal to result), which is |size| bytes at |offset| in file |fd|.
+android::base::Result<bool> ValidateVBMetaWithResult(int fd, uint64_t offset, uint64_t size,
+                                                     AvbVBMetaVerifyResult result);
+}  // namespace fs_mgr
+}  // namespace android
