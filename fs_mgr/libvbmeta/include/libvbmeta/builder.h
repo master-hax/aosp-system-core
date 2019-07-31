8afa7ee05879a6612d95a97b31a885b270f96da3
Kaiwen Szu <kwsz@google.com>
2019-07-31 14:43:04 +0800

    Construct the super_vbmeta image
    This commit constructs the super_vbmeta image to eliminate the
    need of adding /vbmeta_system and/or /vbmeta_vendor when AVB
    chain partition is used with Android Dynamic Partition.
    
    See BOARD_AVB_VBMETA_SYSTEM under the link:
    https://android.googlesource.com/platform/external/avb/#build-system-integration
    
    |    Super VBMeta   | (variable-length)
    |Backup Super VBMeta| (variable-length)
    
    The Super VBMeta records the physical offset of each
    logical partition's vbmeta within the /super partition.
    The size of the super vbmeta is a multiple of 4K and
    Primary/Backup is half and half.
    
    Bug: 137054296
    Test: m libvbmeta_test
    Test: ./out/host/linux-x86/nativetest/libvbmeta_test/libvbmeta_test
    Change-Id: I01aeadd850750ae87d9125484c1b1f570bb84756

---
+++ fs_mgr/libvbmeta/include/libvbmeta/builder.h
@@ -0,0 +1,51 @@
+/*
+ * Copyright (C) 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <map>
+#include <string>
+
+#include <liblp/liblp.h>
+
+#include <libvbmeta/super_vbmeta_format.h>
+
+namespace android {
+namespace fs_mgr {
+
+class SuperVBMetaBuilder {
+public:
+  SuperVBMetaBuilder();
+  SuperVBMetaBuilder(const LpMetadata &metadata,
+                     const std::map<std::string, std::string> &images);
+  SuperVBMetaBuilder(const SuperVBMeta &footer);
+  bool Build();
+  bool AddPartitionImage(const LpMetadataPartition &partition,
+                         const std::string &file);
+  bool Add(const std::string &partition_name, uint64_t vbmeta_offset,
+           uint64_t vbmeta_size);
+  bool Delete(const std::string &partition_name);
+  std::unique_ptr<SuperVBMeta> Export();
+  bool Export(const std::string &file);
+
+private:
+  std::map<std::string, std::pair<uint64_t, uint64_t>> vbmetas_;
+  LpMetadata metadata_;
+  std::map<std::string, std::string> images_;
+};
+
+} // namespace fs_mgr
+} // namespace android
---
+++ fs_mgr/libvbmeta/include/libvbmeta/super_vbmeta_format.h
@@ -0,0 +1,66 @@
+/*
+ * Copyright (C) 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#ifdef __cplusplus
+#include <vector>
+#endif
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Magic signature for SuperVBMeta. */
+#define SUPER_VBMETA_MAGIC 0x61766266
+
+/* Current metadata version. */
+#define SUPER_VBMETA_MAJOR_VERSION 1
+#define SUPER_VBMETA_MINOR_VERSION 0
+
+/* SuperVBMeta size */
+#define SUPER_VBMETA_HEADER_SIZE 52
+#define SUPER_VBMETA_DESCRIPTOR_SIZE 64
+#define SUPER_VBMETA_TOTAL_SIZE 4096
+
+struct SuperVBMetaHeader {
+  uint32_t magic;
+  uint16_t major_version;
+  uint16_t minor_version;
+  uint32_t header_size;
+  uint32_t total_size;
+  uint8_t checksum[32];
+  uint32_t descriptors_size;
+} __attribute__((packed));
+
+struct SuperVBMetaDescriptor {
+  uint64_t vbmeta_offset;
+  uint32_t vbmeta_size;
+  uint32_t partition_name_length;
+  uint8_t reserved[48];
+  char *partition_name;
+} __attribute__((packed));
+
+struct SuperVBMeta {
+  SuperVBMetaHeader header;
+  std::vector<SuperVBMetaDescriptor> descriptors;
+};
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
