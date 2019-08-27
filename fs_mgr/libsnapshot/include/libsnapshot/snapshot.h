909ed81010644633fbb377b6bee34ec3ff7ce42c
Yifan Hong <elsk@google.com>
2019-08-27 14:25:31 -0700

    libsnapshot: Also use empty space in super for COW
    The super partition usually has some empty space even after
    the target partitions are created, especially for retrofit
    Virtual A/B devices. Use those empty space for COW before taking
    up userdata space.
    
    - Add a new SnapshotManager::DeviceDescriptor class, which contains
      both the path and major/minor numbers. When a DmTableTarget is
      created, major/minor numbers would be used if they exist.
    - MapCowImage only fills out one of the path and major/minor numbers
      because IImageManager does not provide both. However, with proper
      branching, init still works.
    - PartitionCowCreator computes free regions in super partition metadata
      and use that space until it is used up. It returns a pair of numbers
      (cow_partition_size, cow_file_size) and let SnapshotManager to create
      the partition / images with proper sizes.
      - A region is considered free iff it is used by NEITHER target NOR
        source partitions
      - The list is in PartitionCowCreator's return value so that
        SnapshotManager can use it as a guide when creating partitions.
      - These partitions are created under the group named "cow".
        - Avoid mapping COW partitions directly during init first stage
          mount. Init only maps them when they are needed by the top-level
          device.
    - CreateCowImage no longer zero-fills the first 4 bytes of the image.
      (See below)
    - CreateUpdatePartitions: after creating the snapshot, also maps the COW
      devices (via MapCowDevices()) and zero-fills the first 4 bytes of the
      top-level COW device.
    - Add a new SnapshotManager::MapCowDevices() function, which maps both
      the COW partition (in super) and the COW image (through
      IImageManager) (if necessary). Then, create a dm-linear target that
      concatenates them (if necessary).
    - DeleteSnapshot also unmaps the top-level COW device and COW partition
      before unmapping the COW images (before deleting them).
    
    Test: libsnapshot_test
    
    Change-Id: I0098b7e842ab48b0b4dd2a59142098b098d23d34

--- fs_mgr/libsnapshot/include/libsnapshot/snapshot.h
+++ fs_mgr/libsnapshot/include/libsnapshot/snapshot.h
@@ -20,6 +20,7 @@
 #include <map>
 #include <memory>
 #include <string>
+#include <string_view>
 #include <vector>
 
 #include <android-base/unique_fd.h>
@@ -51,6 +52,9 @@ namespace snapshot {
 struct AutoDeleteCowImage;
 struct AutoDeleteSnapshot;
 struct PartitionCowCreator;
+struct AutoDeviceList;
+
+static constexpr const std::string_view kCowGroupName = "cow";
 
 enum class UpdateState : unsigned int {
     // No update or merge is in progress.
@@ -266,9 +270,8 @@ class SnapshotManager final {
     // be mapped with two table entries: a dm-snapshot range covering
     // snapshot_size, and a dm-linear range covering the remainder.
     //
-    // All sizes are specified in bytes, and the device, snapshot and COW partition sizes
-    // must be a multiple of the sector size (512 bytes). COW file size will be rounded up
-    // to the nearest sector.
+    // All sizes are specified in bytes, and the device, snapshot, COW partition and COW file sizes
+    // must be a multiple of the sector size (512 bytes).
     bool CreateSnapshot(LockedFile* lock, const std::string& name, SnapshotStatus status);
 
     // |name| should be the base partition name (e.g. "system_a"). Create the
@@ -286,9 +289,23 @@ class SnapshotManager final {
                      const std::string& cow_device, const std::chrono::milliseconds& timeout_ms,
                      std::string* dev_path);
 
-    // Map a COW image that was previous created with CreateCowImage.
+    // A descriptor of a device-mapper device.
+    struct DeviceDescriptor {
+        // path like /dev/block/dm-1. The path may be filled in, but it may not exist if wait
+        // time is zero.
+        std::string path;
+        // major:minor number. This should be preferred when creating DmTable targets because
+        // path may not exist.
+        std::string number;
+
+        // Return a string that can be used in DmTable.
+        const std::string& GetDeviceString() const;
+    };
+
+    // Map a COW image that was previous created with CreateCowImage. Note that not all fields
+    // in descriptor are filled in even if true is returned.
     bool MapCowImage(const std::string& name, const std::chrono::milliseconds& timeout_ms,
-                     std::string* cow_image_device);
+                     DeviceDescriptor* descriptor);
 
     // Remove the backing copy-on-write image and snapshot states for the named snapshot. The
     // caller is responsible for ensuring that the snapshot is unmapped.
@@ -368,6 +385,18 @@ class SnapshotManager final {
     // Map the base device, COW devices, and snapshot device.
     bool MapPartitionWithSnapshot(LockedFile* lock, CreateLogicalPartitionParams params,
                                   std::string* path);
+
+    // Map the COW devices, including the partition in super and the images.
+    // |params|:
+    //    - |partition_name| should be the name of the top-level partition (e.g. system_b),
+    //            not system_b-cow-img
+    //    - |device_name| and |partition| is ignored
+    //    - |timeout_ms| and the rest is respected
+    // Return the path in |cow_device_path| (e.g. /dev/block/dm-1) and major:minor in
+    // |cow_device_string|
+    bool MapCowDevices(LockedFile* lock, const CreateLogicalPartitionParams& params,
+                       const SnapshotStatus& snapshot_status, AutoDeviceList* created_devices,
+                       DeviceDescriptor* cow_device);
 
     // The reverse of MapPartitionWithSnapshot.
     bool UnmapPartitionWithSnapshot(LockedFile* lock, const std::string& target_partition_name);
