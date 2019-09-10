90c97ee2f2943026b4c120332848b963e5657f8a
Tom Cherry <tomcherry@google.com>
2019-09-10 14:20:35 -0700

    init: create sockets before forking
    There is a race condition with Service::Start and socket creation.
    Since socket creation currently happens after the fork(), it's
    possible that init can continue executing other commands before the
    socket is created.  If init starts another service that relies on that
    socket, it isn't guaranteed to be available.
    
    Particularly, we've seen this with hwservicemanager starting after
    logd, but hwservicemanager's logs sometimes not showing up.
    
    Bug: 140810300
    Test: boot and logging functions correctly
    
    Change-Id: Ib2932e836d345830cd38f3b556598508fd953058

--- init/service.cpp
+++ init/service.cpp
@@ -426,6 +426,23 @@ Result<void> Service::Start() {
         pid = fork();
     }
 
+    std::vector<Descriptor> descriptors;
+    for (const auto& socket : sockets_) {
+        if (auto result = socket.Create(scon)) {
+            descriptors.emplace_back(std::move(*result));
+        } else {
+            LOG(INFO) << "Could not create socket '" << socket.name << "': " << result.error();
+        }
+    }
+
+    for (const auto& file : files_) {
+        if (auto result = file.Create()) {
+            descriptors.emplace_back(std::move(*result));
+        } else {
+            LOG(INFO) << "Could not open file '" << file.name << "': " << result.error();
+        }
+    }
+
     if (pid == 0) {
         umask(077);
 
@@ -438,16 +455,8 @@ Result<void> Service::Start() {
             setenv(key.c_str(), value.c_str(), 1);
         }
 
-        for (const auto& socket : sockets_) {
-            if (auto result = socket.CreateAndPublish(scon); !result) {
-                LOG(INFO) << "Could not create socket '" << socket.name << "': " << result.error();
-            }
-        }
-
-        for (const auto& file : files_) {
-            if (auto result = file.CreateAndPublish(); !result) {
-                LOG(INFO) << "Could not open file '" << file.name << "': " << result.error();
-            }
+        for (const auto& descriptor : descriptors) {
+            descriptor.Publish();
         }
 
         if (auto result = WritePidToFiles(&writepid_files_); !result) {
