dc9df009a4126f3fe8a841bbb41e7b56d47939f5
Joshua Duong <joshuaduong@google.com>
2019-10-08 14:57:17 -0700

    Add wifi service for adb.
    Change-Id: Idea323b50902396926e6d084c54aeceae2ed8c93

---
+++ adb/daemon/adb_wifi.cpp
@@ -0,0 +1,50 @@
+/*
+ * Copyright (C) 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#if !ADB_HOST
+
+#define TRACE_TAG ADB_WIRELESS
+
+#include "adb.h"
+#include "sysdeps.h"
+#include "transport.h"
+
+#include <unistd.h>
+
+#include <adbd_wifi.h>
+
+#define DEBUGON 1
+
+static AdbdWifiContext* sWifiCtx;
+
+void adbd_wifi_init(void) {
+    AdbdWifiCallbacks cb;
+    cb.version = 1;
+//    cb.callbacks.v1.device_authorized = ;
+    sWifiCtx = adbd_wifi_new(&cb);
+
+    std::thread([]() {
+        adb_thread_setname("adbd wifi");
+        adbd_wifi_run(sWifiCtx);
+    }).detach();
+}
+
+static void __attribute__((unused)) adb_wifi_disconnected(void* unused, atransport* t)  {
+    if (DEBUGON) LOG(INFO) << "ADB wifi disconnect";
+    adbd_wifi_notify_disconnect(sWifiCtx, t->auth_id);
+}
+
+#endif /* !HOST */
