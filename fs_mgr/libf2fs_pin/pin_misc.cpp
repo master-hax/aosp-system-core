8616ac693c89390d89322c42aa748c31239b3c73
Ramon Pantin <pantin@google.com>
2019-11-14 10:55:03 -0800

    Add libf2fs_pin library
    Privides interfaces to:
    - determine if an f2fs file system supports reliably pinning files, e.g. files
      that will not be unpinned by f2fs garbage collection.)
    - allocate space and reliably pin an f2fs file
    - determine if an f2fs file is reliably pinned
    
    libpin_test is used to create reliably pinnned files of various sizes, store
    a pattern into them (through the filesystem), ensure that the file is reliably
    pinned, and verify that the pattern can be read from the storage assigned to
    the file by reading the data through the block device.
    
    Signed-off-by: Ramon Pantin <pantin@google.com>
    Test: ./libf2fs_pin_test
    Bug: 133967059
    Change-Id: I21d9dd6dc9d5ff10a84dd7654da419ca11836f14

---
+++ fs_mgr/libf2fs_pin/pin_misc.cpp
@@ -0,0 +1,181 @@
+/*
+ * Copyright (C) 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "pin_misc.h"
+
+#include <errno.h>
+#include <iostream>
+
+void PrintError(const char* msg1, const char* msg2) {
+    std::cerr << msg1 << ": " << msg2 << "\n";
+}
+
+void PrintErrno(const char* msg1, const char* msg2) {
+    int error = errno;  // save errno in case it gets clobbered internally by output
+    std::cerr << msg1 << ": " << msg2;
+    if (error) std::cerr << ": " << strerror(error);
+    std::cerr << "\n";
+}
+
+android::f2fs_pin::Result ResultExtentMapToResult(android::f2fs_pin::ResultExtentMap r) {
+    if (!r) return r.error();
+    return {};
+}
+
+void PrintErrorResult(const char* msg, const android::f2fs_pin::Result& result) {
+    std::cerr << msg << ": " << result.error();
+    std::cerr << "\n";
+}
+
+//  Write (through the file system) a pattern of uint64_t: 0, 1, 2, ...
+
+bool WritePattern(const char* file, int file_fd, off_t file_size) {
+    SectorAlignedMemory mem;
+    constexpr size_t kBlockSize = mem.kSize;
+    uint64_t* block = mem.Address();
+    uint64_t* end = block + kBlockSize / sizeof(uint64_t);
+    uint64_t value = 0;
+
+    for (off_t remaining = file_size; remaining > 0;) {
+        for (uint64_t* p = block; p < end;) *p++ = value++;
+        size_t n = remaining > kBlockSize ? kBlockSize : remaining;
+        ssize_t written = write(file_fd, block, n);
+        if (written != n) {
+            if (written < 0)
+                PrintErrno("write() failed", file);
+            else
+                PrintError("partial write()", file);
+            return false;
+        }
+        remaining -= written;
+    }
+    if (fsync(file_fd) < 0) {
+        PrintError("fsync() failed", file);
+        return false;
+    }
+    return true;
+}
+
+//  Read file from the bdev and verify a pattern of uint64_t: 0, 1, 2, ...
+
+bool VerifyPattern(const char* bdev, int bdev_fd, const char* file, int file_fd, off_t file_size) {
+    SectorAlignedMemory mem;
+    constexpr size_t kBlockSize = mem.kSize;
+    uint64_t* block = mem.Address();
+    uint64_t value = 0;
+
+    for (off_t offset = 0; offset < file_size;) {
+        off_t leftover = file_size - offset;
+
+        auto result_extent_map = android::f2fs_pin::FileGetExtentMap(file_fd, offset, leftover);
+        if (!result_extent_map) {
+            PrintErrorResult("could not get extent map",
+                             ResultExtentMapToResult(result_extent_map));
+            return false;
+        }
+        android::f2fs_pin::ExtentMap em = result_extent_map.value();
+        android::f2fs_pin::FiemapExtent* fe = em.em_fiemap.fm_extents;
+
+        if (fe->fe_logical != offset) {
+            PrintError("file should not have holes", file);
+            return false;
+        }
+
+        off_t length = fe->fe_length;
+        off_t physical = fe->fe_physical;
+
+        while (length > 0) {
+            size_t n = length > kBlockSize ? kBlockSize : length;
+            ssize_t nread = pread(bdev_fd, block, n, physical);
+            if (nread != n) {
+                if (nread < 0)
+                    PrintErrno("pread() failed", bdev);
+                else
+                    PrintError("partial read()", bdev);
+                return false;
+            }
+            uint64_t* end = block + n / sizeof(uint64_t);
+            for (uint64_t* p = block; p < end; ++p, ++value)
+                if (*p != value) {
+                    PrintError("wrong data when reading file directly from device", file);
+                    return false;
+                }
+            physical += n;
+            length -= n;
+        }
+
+        offset += fe->fe_length;
+        leftover -= fe->fe_length;
+    }
+    return true;
+}
+
+bool EnsurePinned(const char* bdev, const char* file, bool verify_file) {
+    android::base::unique_fd bdev_fd(open(bdev, O_RDONLY | O_DIRECT | O_CLOEXEC));
+    if (bdev_fd < 0) {
+        PrintErrno("could not open block device", bdev);
+        return false;
+    }
+    android::base::unique_fd file_fd(open(file, O_RDONLY | O_CLOEXEC));
+    if (file_fd < 0) {
+        PrintErrno("could not open file", file);
+        return false;
+    }
+    std::string bdev_string(bdev);
+    auto res = android::f2fs_pin::FileEnsureReliablyPinned(file_fd, bdev_fd, bdev_string);
+    if (!res) {
+        PrintErrorResult("FileEnsureReliablyPinned() failed", res);
+        return false;
+    }
+    if (!verify_file) return true;
+
+    struct stat st;
+    if (fstat(file_fd, &st) < 0) {
+        PrintErrno("fstat() failed", file);
+        return false;
+    }
+    off_t file_size = st.st_size;
+    return VerifyPattern(bdev, bdev_fd, file, file_fd, file_size);
+}
+
+bool CreatePinned(const char* bdev, const char* file, off_t file_size, bool init_file) {
+    android::base::unique_fd bdev_fd(open(bdev, O_RDONLY | O_DIRECT | O_CLOEXEC));
+    if (bdev_fd < 0) {
+        PrintErrno("could not open block device", bdev);
+        return false;
+    }
+    android::base::unique_fd file_fd(open(file, O_RDWR | O_CREAT | O_EXCL | O_CLOEXEC, 0600));
+    if (file_fd < 0) {
+        PrintErrno("could not create file", file);
+        return false;
+    }
+    std::string bdev_string(bdev);
+    auto res = android::f2fs_pin::FileAllocateSpaceAndReliablyPin(file_fd, bdev_fd, bdev_string,
+                                                                  file_size);
+    if (!res) {
+        PrintErrorResult("FileAllocateSpaceAndReliablyPin() failed", res);
+        return false;
+    }
+    if (!init_file) return true;
+
+    if (!WritePattern(file, file_fd, file_size)) return false;
+    res = android::f2fs_pin::FileEnsureReliablyPinned(file_fd, bdev_fd, bdev_string);
+    if (!res) {
+        PrintErrorResult("file not reliably pinned after writing into it", res);
+        return false;
+    }
+    return VerifyPattern(bdev, bdev_fd, file, file_fd, file_size);
+}
---
+++ fs_mgr/libf2fs_pin/pin_misc.h
@@ -0,0 +1,50 @@
+/*
+ * Copyright (C) 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include "f2fs_pin/pin.h"
+#include "pin_impl.h"
+
+#include <sys/stat.h>
+#include <cstdint>
+
+//  Read write from some devices fails unless the memory buffer address is sector aligned
+
+class SectorAlignedMemory {
+  public:
+    static constexpr size_t kSectorSize = 4096;
+    static constexpr size_t kSize = 64 * 1024;
+    uint64_t* Address() {
+        uintptr_t va = (uintptr_t)mem;
+        va &= ~(kSectorSize - 1);  // ensure va is kSectorSize aligned
+        va += kSectorSize;         // ensure it is inside mem[]
+        return (uint64_t*)va;
+    }
+
+  private:
+    uint64_t mem[kSize / sizeof(uint64_t) + kSectorSize];  // extra sector to align
+};
+
+void PrintError(const char* msg1, const char* msg2);
+void PrintErrno(const char* msg1, const char* msg2);
+android::f2fs_pin::Result ResultExtentMapToResult(android::f2fs_pin::ResultExtentMap r);
+void PrintErrorResult(const char* msg, const android::f2fs_pin::Result& result);
+
+bool WritePattern(const char* file, int file_fd, off_t file_size);
+bool VerifyPattern(const char* bdev, int bdev_fd, const char* file, int file_fd, off_t file_size);
+bool EnsurePinned(const char* bdev, const char* file, bool verify_file);
+bool CreatePinned(const char* bdev, const char* file, off_t file_size, bool init_file);
