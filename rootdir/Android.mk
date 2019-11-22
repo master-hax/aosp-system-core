51dcc7959f98757613b1edb1c6dd26a7dd8ea75e
Kiyoung Kim <kiyoungkim@google.com>
2019-11-22 16:14:10 +0900

    Move linker config under /system/etc
    Currently linker config locates under /dev, but this makes some problem
    in case of using two system partitions with chroot. To match system
    image and configuration, linker config better stays under /system/etc as
    it was before.
    
    Bug: 144966380
    Test: m -j passed && tested from cuttelfish
    Change-Id: Iaae5af65721eee8106311c1efb4760a9db13564a

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -73,6 +73,7 @@ endif
 # create some directories (some are mount points) and symlinks
 LOCAL_POST_INSTALL_CMD := mkdir -p $(addprefix $(TARGET_ROOT_OUT)/, \
     dev proc sys system data odm oem acct config storage mnt apex debug_ramdisk $(BOARD_ROOT_EXTRA_FOLDERS)); \
+    mkdir -p $(TARGET_OUT_ETC)/linkerconfig; \
     ln -sf /system/bin $(TARGET_ROOT_OUT)/bin; \
     ln -sf /system/etc $(TARGET_ROOT_OUT)/etc; \
     ln -sf /data/user_de/0/com.android.shell/files/bugreports $(TARGET_ROOT_OUT)/bugreports; \
