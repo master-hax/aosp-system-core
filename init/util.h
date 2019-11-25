3e2cec1cc63dd35ba9adf7ceb3bcf32220ae3aab
Florian Mayer <fmayer@google.com>
2019-11-25 13:52:57 +0000

    Create shmem for atrace.
    Bug: 137366208
    
    Test: $ adb shell su root ls -lZ /dev/__atrace_shmem__
    -rwxr-xr-x 1 root root u:object_r:atrace_shmem_device:s0  8192 1970-08-31 04:05 /dev/__atrace_shmem__
    
    Change-Id: Ib66c3cae1ab9c765e73ee533d292cfd2cca6ef9e

--- init/util.h
+++ init/util.h
@@ -40,6 +40,7 @@ extern void (*trigger_shutdown)(const std::string& command);
 Result<int> CreateSocket(const std::string& name, int type, bool passcred, mode_t perm, uid_t uid,
                          gid_t gid, const std::string& socketcon);
 
+int OpenFile(const std::string& path, int flags, mode_t mode);
 Result<std::string> ReadFile(const std::string& path);
 Result<void> WriteFile(const std::string& path, const std::string& content);
 
