8d94734e1a8bfd292461075427548b269145fe95
Florian Mayer <fmayer@google.com>
2019-11-25 13:52:57 +0000

    Create shmem for atrace.
    Bug: 137366208
    
    Test: $ adb shell su root ls -lZ /dev/__atrace_shmem__
    -rwxr-xr-x 1 root root u:object_r:atrace_shmem_device:s0  8192 1970-08-31 04:05 /dev/__atrace_shmem__
    
    Change-Id: Ib66c3cae1ab9c765e73ee533d292cfd2cca6ef9e

---
+++ libcutils/include/cutils/atrace_shmem.h
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <stdatomic.h>
+#include <assert.h>
+
+struct AtraceShmemPage {
+  _Atomic(uint32_t) atrace_sequence_number;
+  _Atomic(uint64_t) atrace_requested_tags;
+  // Some words reserved for future extensions
+  char reserved[1024];
+  char enabled_cmdlines[7152];
+};
+
+static_assert(sizeof(AtraceShmemPage) % 4096 == 0, "shmem needs to be whole pages");
