fa8adc4b4d828f1f1192837032cacbbc256a97a0
Alessio Balsini <balsini@google.com>
2019-12-02 11:46:52 +0000

    PartitionCowCreator accounts for extra extents
    dm-verity and error correction use part of the partitions using these
    features to store their data.  Their data may be modified during an
    update, so the COW device for the dm-snapshot must reserve some extra
    space for them.
    This patch extends the PartitionCowCreator data structure with the
    extra_extents field that will store the (optional) additional extents,
    e.g., the hash table extent for dm-verity or the error correction
    extent.
    
    Test: OTA apply
    Bug: 145180464
    Change-Id: I387a6cc8438507ad41a85cc3400241ecaf627b8f
    Signed-off-by: Alessio Balsini <balsini@google.com>

--- fs_mgr/libsnapshot/partition_cow_creator.cpp
+++ fs_mgr/libsnapshot/partition_cow_creator.cpp
@@ -75,11 +75,28 @@ bool SourceCopyOperationIsClone(const InstallOperation& operation) {
                       });
 }
 
+void WriteExtent(DmSnapCowSizeCalculator* sc, const chromeos_update_engine::Extent& de,
+                 unsigned int sectors_per_block) {
+    const auto block_boundary = de.start_block() + de.num_blocks();
+    for (auto b = de.start_block(); b < block_boundary; ++b) {
+        for (unsigned int s = 0; s < sectors_per_block; ++s) {
+            const auto sector_id = b * sectors_per_block + s;
+            sc->WriteSector(sector_id);
+        }
+    }
+}
+
 uint64_t PartitionCowCreator::GetCowSize() {
     // WARNING: The origin partition should be READ-ONLY
     const uint64_t logical_block_size = current_metadata->logical_block_size();
     const unsigned int sectors_per_block = logical_block_size / kSectorSize;
     DmSnapCowSizeCalculator sc(kSectorSize, kSnapshotChunkSize);
+
+    // Allocate space for extra extents (if any). These extents are those that can be
+    // used for error corrections or to store verity hash trees.
+    for (const auto& de : extra_extents) {
+        WriteExtent(&sc, de, sectors_per_block);
+    }
 
     if (operations == nullptr) return sc.cow_size_bytes();
 
@@ -90,17 +107,7 @@ uint64_t PartitionCowCreator::GetCowSize() {
             continue;
 
         for (const auto& de : iop.dst_extents()) {
-            // Skip if no blocks are written
-            if (de.num_blocks() == 0) continue;
-
-            // Flag all the blocks that were written
-            const auto block_boundary = de.start_block() + de.num_blocks();
-            for (auto b = de.start_block(); b < block_boundary; ++b) {
-                for (unsigned int s = 0; s < sectors_per_block; ++s) {
-                    const auto sector_id = b * sectors_per_block + s;
-                    sc.WriteSector(sector_id);
-                }
-            }
+            WriteExtent(&sc, de, sectors_per_block);
         }
     }
 
