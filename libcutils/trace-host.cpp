60582a302f35cbf2279ee3935d6ac142a81c4038
Florian Mayer <fmayer@google.com>
2019-12-10 12:20:03 +0000

    Use sequence number to check if to reload atrace tags.
    This is to deprecate the sysprop change notification in atrace.
    After this change, processes will only update their enabled tags
    at the first atrace event. Previously we reloaded the tags as a
    result of the sysprop changed Binder notification, which woke up
    every process in the system.
    
    Test: adb shell su root atrace -t 10 ss
    Test: #define ATRACE_SHMEM 0; adb shell su root atrace -t 10 ss
    
    Change-Id: Idffba5fd4ba23fba2f6b9f594365df68ac0c1626

--- libcutils/trace-host.cpp
+++ libcutils/trace-host.cpp
@@ -19,7 +19,9 @@
 atomic_bool             atrace_is_ready      = ATOMIC_VAR_INIT(true);
 int                     atrace_marker_fd     = -1;
 uint64_t                atrace_enabled_tags  = 0;
+_Atomic(uint32_t)       last_sequence_number = ATOMIC_VAR_INIT(0);
 
+void atrace_seq_number_changed(uint32_t, uint32_t) {}
 void atrace_set_debuggable(bool /*debuggable*/) {}
 void atrace_set_tracing_enabled(bool /*enabled*/) {}
 void atrace_update_tags() { }
