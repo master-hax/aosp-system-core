f26a13c4a9dd4ce73e8055d91d5a6dbfe2c69ac4
Wenhao Wang <wenhaowang@google.com>
2019-12-11 15:22:03 -0800

    Add storageproxyd UFS support
    This CL enables storageproxyd to run on UFS device.
    The proxy prepares and sends SECURITY PROTOCOL IN/OUT commands to UFS
    device.
    
    Bug: 143636526
    Test: Trusty storage tests
    Change-Id: Ibe16578c12b978c9a95deccfb1873081e8d0e994

--- trusty/storage/proxy/rpmb.c
+++ trusty/storage/proxy/rpmb.c
@@ -31,7 +31,13 @@
 #include "ipc.h"
 #include "log.h"
 #include "rpmb.h"
+#include "rpmb_protocol.h"
+#include "sg.h"
 #include "storage.h"
+
+#ifdef RPMB_DEBUG
+#include <inttypes.h>
+#endif
 
 #define MMC_READ_MULTIPLE_BLOCK 18
 #define MMC_WRITE_MULTIPLE_BLOCK 25
@@ -50,6 +56,34 @@
 #define MMC_WRITE_FLAG_RELW (MMC_WRITE_FLAG_W | MMC_RELIABLE_WRITE_FLAG)
 
 #define MMC_BLOCK_SIZE 512
+
+/* CDB format of SECURITY PROTOCOL IN/OUT commands */
+struct sec_proto_cdb {
+    /*
+     * OPERATION CODE = A2h for SECURITY PROTOCOL IN command,
+     * OPERATION CODE = B5h for SECURITY PROTOCOL OUT command.
+     */
+    uint8_t opcode;
+    /* SECURITY PROTOCOL = ECh (JEDEC Universal Flash Storage) */
+    uint8_t sec_proto;
+    /*
+     * The SECURITY PROTOCOL SPECIFIC field specifies the RPMB Protocol ID.
+     * CDB Byte 2 = 00h and CDB Byte 3 = 01h for RPMB Region 0.
+     */
+    uint8_t cdb_byte_2;
+    uint8_t cdb_byte_3;
+    /*
+     * Byte 4 and 5 are reserved.
+     */
+    uint8_t cdb_byte_4;
+    uint8_t cdb_byte_5;
+    /* ALLOCATION/TRANSFER LENGTH in big-endian */
+    uint32_t length;
+    /* Byte 9 is reserved. */
+    uint8_t cdb_byte_10;
+    /* CONTROL = 00h. */
+    uint8_t ctrl;
+} __packed;
 
 static int rpmb_fd = -1;
 static uint8_t read_buf[4096];
@@ -132,6 +166,125 @@ static int send_mmc_rpmb_req(int mmc_fd, const struct storage_rpmb_send_req* req
     return rc;
 }
 
+static int send_ufs_rpmb_req(int sg_fd, const struct storage_rpmb_send_req* req) {
+    int rc;
+    const uint8_t* write_buf = req->payload;
+    struct sec_proto_cdb in_cdb = {0xA2, 0xEC, 0x00, 0x01, 0x00, 0x00, 0, 0x00, 0x00};
+    struct sec_proto_cdb out_cdb = {0xB5, 0xEC, 0x00, 0x01, 0x00, 0x00, 0, 0x00, 0x00};
+    unsigned char sense_buffer[32];
+
+    if (req->reliable_write_size) {
+#ifdef RPMB_DEBUG
+        ALOGW("-------------------------Beginning reliable_write_size\n");
+        ALOGW("reliable_write_size: %d", (int)req->reliable_write_size);
+        struct rpmb_packet* pp = (struct rpmb_packet*)write_buf;
+        ALOGW("write_counter: %" PRIu32, rpmb_get_u32(pp->write_counter));
+        ALOGW("address: %" PRIu16, rpmb_get_u16(pp->address));
+        ALOGW("block_count: %" PRIu16, rpmb_get_u16(pp->block_count));
+        ALOGW("result: %" PRIu16, rpmb_get_u16(pp->result));
+        ALOGW("req_resp: %" PRIu16, rpmb_get_u16(pp->req_resp));
+#endif
+        /* Prepare SECURITY PROTOCOL OUT command. */
+        out_cdb.length = __builtin_bswap32(req->reliable_write_size);
+        sg_io_hdr_t io_hdr;
+        memset(&io_hdr, 0, sizeof(sg_io_hdr_t));
+        io_hdr.interface_id = 'S';
+        io_hdr.dxfer_direction = SG_DXFER_TO_DEV;
+        io_hdr.cmd_len = sizeof(out_cdb);
+        io_hdr.mx_sb_len = sizeof(sense_buffer);
+        io_hdr.dxfer_len = req->reliable_write_size;
+        io_hdr.dxferp = (void*)write_buf;
+        io_hdr.cmdp = (unsigned char*)&out_cdb;
+        io_hdr.sbp = sense_buffer;
+        /* 20000 millisecs == 20 seconds */
+        io_hdr.timeout = 20000;
+        rc = ioctl(sg_fd, SG_IO, &io_hdr);
+        if (rc < 0) {
+            ALOGE("%s: ufs ioctl failed: %d, %s\n", __func__, rc, strerror(errno));
+        }
+        write_buf += req->reliable_write_size;
+#ifdef RPMB_DEBUG
+        ALOGW("-------------------------End of reliable_write_size\n");
+#endif
+    }
+
+    if (req->write_size) {
+        struct rpmb_packet* pp = (struct rpmb_packet*)write_buf;
+        if (rpmb_get_u16(pp->req_resp) == RPMB_REQ_DATA_READ) {
+            pp->block_count = rpmb_u16(req->read_size / 512);
+        }
+#ifdef RPMB_DEBUG
+        ALOGW("-------------------------Beginning write_size\n");
+        ALOGW("write_size: %d", (int)req->write_size);
+        ALOGW("write_counter: %" PRIu32, rpmb_get_u32(pp->write_counter));
+        ALOGW("address: %" PRIu16, rpmb_get_u16(pp->address));
+        ALOGW("block_count: %" PRIu16, rpmb_get_u16(pp->block_count));
+        ALOGW("result: %" PRIu16, rpmb_get_u16(pp->result));
+        ALOGW("req_resp: %" PRIu16, rpmb_get_u16(pp->req_resp));
+        ALOGW("block_count: %" PRIu16, rpmb_get_u16(pp->block_count));
+#endif
+        /* Prepare SECURITY PROTOCOL OUT command. */
+        out_cdb.length = __builtin_bswap32(req->write_size);
+        sg_io_hdr_t io_hdr;
+        memset(&io_hdr, 0, sizeof(sg_io_hdr_t));
+        io_hdr.interface_id = 'S';
+        io_hdr.dxfer_direction = SG_DXFER_TO_DEV;
+        io_hdr.cmd_len = sizeof(out_cdb);
+        io_hdr.mx_sb_len = sizeof(sense_buffer);
+        io_hdr.dxfer_len = req->write_size;
+        io_hdr.dxferp = (void*)write_buf;
+        io_hdr.cmdp = (unsigned char*)&out_cdb;
+        io_hdr.sbp = sense_buffer;
+        /* 20000 millisecs == 20 seconds */
+        io_hdr.timeout = 20000;
+        rc = ioctl(sg_fd, SG_IO, &io_hdr);
+        if (rc < 0) {
+            ALOGE("%s: ufs ioctl failed: %d, %s\n", __func__, rc, strerror(errno));
+        }
+        write_buf += req->write_size;
+#ifdef RPMB_DEBUG
+        ALOGW("-------------------------End of write_size\n");
+#endif
+    }
+
+    if (req->read_size) {
+#ifdef RPMB_DEBUG
+        ALOGW("-------------------------Beginning read_size\n");
+        ALOGW("read_size: %d", (int)req->read_size);
+#endif
+        /* Prepare SECURITY PROTOCOL IN command. */
+        out_cdb.length = __builtin_bswap32(req->read_size);
+        sg_io_hdr_t io_hdr;
+        memset(&io_hdr, 0, sizeof(sg_io_hdr_t));
+        io_hdr.interface_id = 'S';
+        io_hdr.dxfer_direction = SG_DXFER_FROM_DEV;
+        io_hdr.cmd_len = sizeof(in_cdb);
+        io_hdr.mx_sb_len = sizeof(sense_buffer);
+        /* TODO: figure out the difference between "data transfer associated
+         * with the command" and "ALLOCATION/TRANSFER LENGTH". */
+        io_hdr.dxfer_len = req->read_size;
+        io_hdr.dxferp = read_buf;
+        io_hdr.cmdp = (unsigned char*)&in_cdb;
+        io_hdr.sbp = sense_buffer;
+        /* 20000 millisecs == 20 seconds */
+        io_hdr.timeout = 20000;
+        rc = ioctl(sg_fd, SG_IO, &io_hdr);
+        if (rc < 0) {
+            ALOGE("%s: ufs ioctl failed: %d, %s\n", __func__, rc, strerror(errno));
+        }
+#ifdef RPMB_DEBUG
+        struct rpmb_packet* pp = (struct rpmb_packet*)read_buf;
+        ALOGW("write_counter: %" PRIu32, rpmb_get_u32(pp->write_counter));
+        ALOGW("address: %" PRIu16, rpmb_get_u16(pp->address));
+        ALOGW("block_count: %" PRIu16, rpmb_get_u16(pp->block_count));
+        ALOGW("result: %" PRIu16, rpmb_get_u16(pp->result));
+        ALOGW("req_resp: %" PRIu16, rpmb_get_u16(pp->req_resp));
+        ALOGW("-------------------------End of read_size\n");
+#endif
+    }
+    return rc;
+}
+
 static int send_virt_rpmb_req(int rpmb_fd, void* read_buf, size_t read_size, const void* payload,
                               size_t payload_size) {
     int rc;
@@ -194,6 +347,13 @@ int rpmb_send(struct storage_msg* msg, const void* r, size_t req_len) {
             msg->result = STORAGE_ERR_GENERIC;
             goto err_response;
         }
+    } else if (dev_type == UFS_RPMB) {
+        rc = send_ufs_rpmb_req(rpmb_fd, req);
+        if (rc < 0) {
+            ALOGE("send_ufs_rpmb_req failed: %d, %s\n", rc, strerror(errno));
+            msg->result = STORAGE_ERR_GENERIC;
+            goto err_response;
+        }
     } else if ((dev_type == VIRT_RPMB) || (dev_type == SOCK_RPMB)) {
         size_t payload_size = req->reliable_write_size + req->write_size;
         rc = send_virt_rpmb_req(rpmb_fd, read_buf, req->read_size, req->payload, payload_size);
@@ -233,7 +393,7 @@ err_response:
 }
 
 int rpmb_open(const char* rpmb_devname, enum dev_type open_dev_type) {
-    int rc;
+    int rc, k;
     dev_type = open_dev_type;
 
     if (dev_type != SOCK_RPMB) {
@@ -263,6 +423,15 @@ int rpmb_open(const char* rpmb_devname, enum dev_type open_dev_type) {
             return rc;
         }
     }
+
+    /* For UFS, it is prudent to check we hava a sg device by calling an ioctl */
+    if (dev_type == UFS_RPMB) {
+        if ((ioctl(rc, SG_GET_VERSION_NUM, &k) < 0) || (k < 30000)) {
+            ALOGE("%s is not a sg device, or old sg driver\n", rpmb_devname);
+            return -1;
+        }
+    }
+    rpmb_fd = rc;
     return 0;
 }
 
