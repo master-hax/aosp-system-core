ee6b947f464c5decbbf060bc9295c7302fef0908
Woody Lin <woodylin@google.com>
2019-12-26 22:22:28 +0800

    init/service_parser: Add arguments `-w=' and `-t=' for `critical'
    The critical services can now using the interface `critical [-w=<fatal
    crash window mins>] [-t=<fatal reboot target>]` to setup the timing
    window that more than 4 crashes in it is not tolerable, and the reboot
    target of fatal handler as well.
    
    Config `-w=60 -t=zygote-fatal' for all system-server services, so the
    system will restart ifself when these UI sensitive processes repeatedly
    crash.
    
    Bug: 146818493
    Change-Id: Ib2dc253616be6935ab9ab52184a1b6394665e813

--- init/service.cpp
+++ init/service.cpp
@@ -45,6 +45,7 @@
 #include <android/api-level.h>
 
 #include "mount_namespace.h"
+#include "reboot_utils.h"
 #include "selinux.h"
 #else
 #include "host_init_stubs.h"
@@ -315,10 +316,11 @@ void Service::Reap(const siginfo_t& siginfo) {
     boot_clock::time_point now = boot_clock::now();
     if (((flags_ & SVC_CRITICAL) || is_process_updatable) && !(flags_ & SVC_RESTART)) {
         bool boot_completed = android::base::GetBoolProperty("sys.boot_completed", false);
-        if (now < time_crashed_ + 4min || !boot_completed) {
+        if (now < time_crashed_ + fatal_crash_window_ || !boot_completed) {
             if (++crash_count_ > 4) {
                 if (flags_ & SVC_CRITICAL) {
-                    // Aborts into bootloader
+                    // Aborts into `fatal_reboot_target_'.
+                    SetFatalRebootTarget(fatal_reboot_target_);
                     LOG(FATAL) << "critical process '" << name_ << "' exited 4 times "
                                << (boot_completed ? "in 4 minutes" : "before boot completed");
                 } else {
