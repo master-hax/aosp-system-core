380972d7e62c0f9b0c7580c504f543eeae52f455
Shukang Zhou <shukang@google.com>
2020-02-06 15:23:30 -0800

    Make adbconnection support profileable
    Bug: 149050485
    Test: N/A
    
    Change-Id: Id1f1a920e1afc148c7e4d2add790baab796178e1

--- adb/adbconnection/include/adbconnection/process_info.h
+++ adb/adbconnection/include/adbconnection/process_info.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2019 The Android Open Source Project
+ * Copyright (C) 2020 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,13 +16,25 @@
 
 #pragma once
 
-#include <sys/types.h>
-
-#include <android-base/unique_fd.h>
-
-#include "adbconnection/process_info.h"
-
 extern "C" {
 
-void adbconnection_listen(void (*callback)(int fd, ProcessInfo process));
+struct ProcessInfo {
+  enum class Arch {
+    kUnspecified,
+    kArm32,
+    kArm64,
+    kX86,
+    kX86_64,
+    kOther,
+  };
+  uint64_t pid;
+  bool debuggable;
+  bool profileable;
+  Arch arch;
+
+  ProcessInfo() : pid(0), debuggable(false), profileable(false), arch(Arch::kUnspecified) {}
+
+  ProcessInfo(uint64_t pid, bool dbg, bool prof, Arch ar)
+      : pid(pid), debuggable(dbg), profileable(prof), arch(ar) {}
+};
 }
