9226d12774ff263c8bcf844590b78e81be3ca668
Shukang Zhou <shukang@google.com>
2020-02-13 17:01:39 -0800

    "track-app" service showing debuggable/profileable apps
    Add a "track-app" service in adbd. For every debuggable or
    profileable-from-shell process, ART sends related info to
    adbd and adbd surfaces the info through the "track-app"
    service.
    
    The output format of "track-app" is very similar to the
    existing "track-jdwp" service. For example, a process is
    shown as string "3529-d0p1\n" where
        3529 is the pid,
        d0 means it's no debuggable,
        p1 means it's profileable-from-shell.
    
    Bug: 149050485
    Test: N/A
    
    Change-Id: Id1f1a920e1afc148c7e4d2add790baab796178e1

--- adb/adbconnection/include/adbconnection/process_info.h
+++ adb/adbconnection/include/adbconnection/process_info.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2019 The Android Open Source Project
+ * Copyright (C) 2020 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,13 +16,15 @@
 
 #pragma once
 
-#include <sys/types.h>
-
-#include <android-base/unique_fd.h>
-
-#include "adbconnection/process_info.h"
-
 extern "C" {
 
-void adbconnection_listen(void (*callback)(int fd, ProcessInfo process));
+struct ProcessInfo {
+  uint64_t pid;
+  bool debuggable;
+  bool profileable;
+
+  ProcessInfo() : pid(0), debuggable(false), profileable(false) {}
+
+  ProcessInfo(uint64_t pid, bool dbg, bool prof) : pid(pid), debuggable(dbg), profileable(prof) {}
+};
 }
