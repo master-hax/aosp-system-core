18ad10f6bf609a7a004ecca2330e69b8fa2674a0
Yifan Hong <elsk@google.com>
2020-03-19 19:26:28 -0700

    libsnapshot: Add RecoveryCreateSnapshotDevices(device)
    ... that doesn't auto mount/umount /metadata.
    
    Test: builds
    Bug: 151983957
    Change-Id: If525a82c1635ddef221e7be20763bc2f27d3c417

--- fs_mgr/libsnapshot/snapshot.cpp
+++ fs_mgr/libsnapshot/snapshot.cpp
@@ -2587,6 +2587,15 @@ CreateResult SnapshotManager::RecoveryCreateSnapshotDevices() {
         LOG(ERROR) << "Couldn't mount Metadata.";
         return CreateResult::NOT_CREATED;
     }
+    return RecoveryCreateSnapshotDevices(mount);
+}
+
+CreateResult SnapshotManager::RecoveryCreateSnapshotDevices(
+        const std::unique_ptr<AutoDevice>& metadata_device) {
+    if (!device_->IsRecovery()) {
+        LOG(ERROR) << __func__ << " is only allowed in recovery.";
+        return CreateResult::NOT_CREATED;
+    }
 
     auto state_file = GetStateFilePath();
     if (access(state_file.c_str(), F_OK) != 0 && errno == ENOENT) {
