c93dec23d975dca0015a9d2cc40d0373aa131d6f
David Anderson <dvander@google.com>
2020-04-30 18:53:23 -0700

    fastboot: Fix snapshot-update merge behavior.
    When merging in recovery, the "imminent data wipe" code was used, which
    made the assumption the /metadata and /data state would be zapped. This
    caused future OTAs to error because the old snapshots were detected.
    
    This CL allows OTAs to proceed even if unexpected snapshots are present.
    It also forces the state to "MergeCompleted" after a merge in recovery,
    so that the next normal boot can perform cleanup.
    
    Bug: 155339165
    Test: fastboot snapshot-update merge, then take another OTA
          vts_libsnapshot_test
    Change-Id: Ief6dea3ba76323044e61307272dda320a4494aea

--- fs_mgr/libsnapshot/include/libsnapshot/snapshot.h
+++ fs_mgr/libsnapshot/include/libsnapshot/snapshot.h
@@ -201,6 +201,10 @@ class ISnapshotManager {
     // optional callback fires periodically to query progress via GetUpdateState.
     virtual bool HandleImminentDataWipe(const std::function<void()>& callback = {}) = 0;
 
+    // Force a merge to complete in recovery. This is similar to HandleImminentDataWipe
+    // but does not expect a data wipe after.
+    virtual bool FinishMergeInRecovery() = 0;
+
     // This method is only allowed in recovery and is used as a helper to
     // initialize the snapshot devices as a requirement to mount a snapshotted
     // /system in recovery.
@@ -289,6 +293,7 @@ class SnapshotManager final : public ISnapshotManager {
             const std::string& super_device,
             const std::chrono::milliseconds& timeout_ms = {}) override;
     bool HandleImminentDataWipe(const std::function<void()>& callback = {}) override;
+    bool FinishMergeInRecovery() override;
     CreateResult RecoveryCreateSnapshotDevices() override;
     CreateResult RecoveryCreateSnapshotDevices(
             const std::unique_ptr<AutoDevice>& metadata_device) override;
@@ -578,6 +583,7 @@ class SnapshotManager final : public ISnapshotManager {
     std::unique_ptr<IDeviceInfo> device_;
     std::unique_ptr<IImageManager> images_;
     bool has_local_image_manager_ = false;
+    bool in_factory_data_reset_ = false;
 };
 
 }  // namespace snapshot
