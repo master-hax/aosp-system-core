cac4dc5c301ee0aa983a60b056df8896d1700a20
Elliott Hughes <enh@google.com>
2020-05-27 12:24:30 -0700

    Make systemTime() abort on bad input.
    There's no CHECK in libutils, sadly.
    
    Bug: http://b/157167405
    Test: treehugger
    Change-Id: I1532bf80ba7fdafad016610be3c782b547417126

--- libutils/Timers.cpp
+++ libutils/Timers.cpp
@@ -20,31 +20,27 @@
 #include <utils/Timers.h>
 
 #include <limits.h>
+#include <stdlib.h>
 #include <time.h>
 
 // host linux support requires Linux 2.6.39+
 #if defined(__linux__)
 nsecs_t systemTime(int clock)
 {
-    static const clockid_t clocks[] = {
-            CLOCK_REALTIME,
-            CLOCK_MONOTONIC,
-            CLOCK_PROCESS_CPUTIME_ID,
-            CLOCK_THREAD_CPUTIME_ID,
-            CLOCK_BOOTTIME
-    };
-    struct timespec t;
-    t.tv_sec = t.tv_nsec = 0;
+    static constexpr clockid_t clocks[] = {CLOCK_REALTIME, CLOCK_MONOTONIC,
+                                           CLOCK_PROCESS_CPUTIME_ID, CLOCK_THREAD_CPUTIME_ID,
+                                           CLOCK_BOOTTIME};
+    if (clock < 0 || clock >= SYSTEM_TIME_MAX) abort();
+    timespec t = {};
     clock_gettime(clocks[clock], &t);
     return nsecs_t(t.tv_sec)*1000000000LL + t.tv_nsec;
 }
 #else
-nsecs_t systemTime(int /*clock*/)
-{
+nsecs_t systemTime(int clock) {
+    if (clock < 0 || clock >= SYSTEM_TIME_MAX) abort();
     // Clock support varies widely across hosts. Mac OS doesn't support
     // CLOCK_BOOTTIME, and Windows is windows.
-    struct timeval t;
-    t.tv_sec = t.tv_usec = 0;
+    timeval t = {};
     gettimeofday(&t, nullptr);
     return nsecs_t(t.tv_sec)*1000000000LL + nsecs_t(t.tv_usec)*1000LL;
 }
