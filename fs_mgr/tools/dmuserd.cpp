94bc1eada10b7389ad63c2cf0e61ac79c4f2c5a8
Palmer Dabbelt <palmerdabbelt@google.com>
2020-06-22 17:46:07 -0700

    Have dmuserd read from system_a, and run a simple benchmark
    This now reads from a block device (system_a) and is a bit more reasonable
    about responding to messages.
    
    Test: Ran dmuserd, which tests itself
    Signed-off-by: Palmer Dabbelt <palmerdabbelt@google.com>
    Change-Id: I704953975b420a093c00b99be268ad911399106b

--- fs_mgr/tools/dmuserd.cpp
+++ fs_mgr/tools/dmuserd.cpp
@@ -16,40 +16,114 @@
 
 #include <libdm/dm.h>
 
+#include <linux/types.h>
+
+#define DM_USER_MAP_READ 0
+#define DM_USER_MAP_WRITE 1
+
+#define DEBUG 0
+
+struct dm_user_message {
+    __u64 seq;
+    __u64 type;
+    __u64 flags;
+    __u64 sector;
+    __u64 len;
+    __u8 buf[];
+};
+
 using namespace android::dm;
 
 static int daemon_main(void) {
-    int fd = open("/dev/dm-user", O_RDWR);
-    if (!fd) {
+    int block_fd = open("/dev/block/mapper/system_a", O_RDWR);
+    if (!block_fd) {
+        perror("Unable to open /dev/block/mapper/system_a");
+        return 1;
+    }
+
+    int ctrl_fd = open("/dev/dm-user", O_RDWR);
+    if (!ctrl_fd) {
         perror("Unable to open /dev/dm-user");
         return 1;
     }
+
+    size_t buf_size = 1UL << 16;
+    char* buf = new char[buf_size];
 
     /* Just keeps pumping messages between userspace and the kernel.  We won't
      * actually be doing anything, but the sequence numbers line up so it'll at
      * least make forward progress. */
     while (true) {
-        unsigned long buf[1 << 16];
+        struct dm_user_message* msg = (struct dm_user_message*)(&(buf[0]));
 
-        for (size_t i = 0; i < 1024; ++i) buf[i] = -1UL;
+        memset(buf, 0, buf_size);
 
-        ssize_t readed = read(fd, &buf, sizeof(buf));
+        ssize_t readed = read(ctrl_fd, buf, buf_size);
         if (readed < 0) {
-            perror("Control read failed");
-            return 2;
+            perror("Control read failed, trying with more space");
+            delete[] buf;
+            buf_size *= 2;
+            fprintf(stderr, "Looking for %x bytes\n", buf_size);
+            buf = new char[buf_size];
+            if (buf == NULL) {
+                perror("Unable to allocate buffer");
+                return 2;
+            }
+            continue;
         }
 
+#if (DEBUG == 1)
         printf("read() from dm-user returned %d bytes:\n", readed);
-        for (size_t i = 0; i < 4; ++i) printf("%d: 0x%016lu\n", i, buf[i]);
-        printf("...\n");
+        printf("    msg->seq:    0x%016llx\n", msg->seq);
+        printf("    msg->type:   0x%016llx\n", msg->type);
+        printf("    msg->flags:  0x%016llx\n", msg->flags);
+        printf("    msg->sector: 0x%016llx\n", msg->sector);
+        printf("    msg->len:    0x%016llx\n", msg->len);
+#endif
 
-        for (size_t i = 4; i < 1 << 16; ++i) buf[i] = i;
+        switch (msg->type) {
+            case DM_USER_MAP_READ: {
+#if (DEBUG == 1)
+                printf("Responding to read of sector %lld with %lld bytes data\n", msg->sector,
+                       msg->len);
+#endif
 
-        ssize_t written = write(fd, &buf, sizeof(buf));
-        if (written < 0) {
-            perror("Control write failed");
-            return 3;
+                if ((sizeof(*msg) + msg->len) > buf_size) {
+                    auto old_buf = buf;
+                    buf_size = sizeof(*msg) + msg->len;
+                    buf = new char[buf_size];
+                    memcpy(buf, old_buf, sizeof(*msg));
+                    delete[] old_buf;
+                    msg = (struct dm_user_message*)(&(buf[0]));
+                }
+
+                ssize_t readed = 0;
+                lseek(block_fd, msg->sector * 512, SEEK_SET);
+                while (readed < msg->len) {
+                    ssize_t r = read(block_fd, msg->buf + readed, msg->len - readed);
+                    if (r < 0) {
+                        perror("Unable to read from block device");
+                        return 7;
+                    }
+                    readed += r;
+                }
+
+                ssize_t written = write(ctrl_fd, buf, sizeof(*msg) + msg->len);
+                if (written < 0) {
+                    perror("Control write failed");
+                    return 3;
+                }
+                break;
+            }
+
+            case DM_USER_MAP_WRITE:
+                abort();
+                break;
         }
+
+#if (DEBUG == 1)
+        printf("read() finished, next message\n");
+#endif
     }
 
     return 0;
@@ -60,23 +134,27 @@ int main() {
      * :) */
     system("dmctl create palmer user 0 1000000");
 
-    /* Backgrounds the daemon.. */
+    /* Backgrounds the daemon... */
     if (fork() == 0) return daemon_main();
 
-    /* Reads a single */
-    int fd = open("/dev/block/mapper/palmer", O_RDWR);
-    if (fd < 0) {
-        perror("Unable to open block device");
-        return 1;
-    }
+    /* That's probably log enough... */
+    sleep(5);
 
-    char buf[4096];
-    ssize_t readed = read(fd, &buf[0], 4096);
-    if (readed < 0) {
-        perror("Unable to read from block device");
-        return 2;
-    }
-
-    for (ssize_t i = 0; i < readed; ++i) printf("read(): palmer[%d] == 0x%02x\n", i, buf[i]);
-    return 0;
+#if (DEBUG == 1)
+    system("dd if=/dev/block/mapper/system_a bs=4K count=4 of=/mnt/system_a");
+    system("dd if=/dev/block/mapper/system_a bs=4K count=4 of=/mnt/system_a");
+    system("dd if=/dev/block/mapper/system_a bs=4K count=4 of=/mnt/system_a");
+    system("dd if=/dev/block/mapper/palmer bs=4K count=4 of=/mnt/palmer");
+    system("dd if=/dev/block/mapper/palmer bs=4K count=4 of=/mnt/palmer");
+    system("dd if=/dev/block/mapper/palmer bs=4K count=4 of=/mnt/palmer");
+    printf("dd was small, as debug is on\n");
+#else
+    system("dd if=/dev/block/mapper/system_a bs=4K count=1024 of=/mnt/system_a");
+    system("dd if=/dev/block/mapper/system_a bs=4K count=1024 of=/mnt/system_a");
+    system("dd if=/dev/block/mapper/system_a bs=4K count=1024 of=/mnt/system_a");
+    system("dd if=/dev/block/mapper/palmer bs=4K count=1024 of=/mnt/palmer");
+    system("dd if=/dev/block/mapper/palmer bs=4K count=1024 of=/mnt/palmer");
+    system("dd if=/dev/block/mapper/palmer bs=4K count=1024 of=/mnt/palmer");
+#endif
+    return system("diff /mnt/system_a /mnt/palmer");
 }
