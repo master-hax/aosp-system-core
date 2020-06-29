73a6e2f5d7e0c80b777675db8306b5ad200c7121
Jiyong Park <jiyong@google.com>
2020-06-29 10:42:03 +0900

    Move build.prop to ./etc subdir
    build.prop files are moved to the ./etc subdir. This is to be in-line
    with the established practice of having all configuration files
    under ./etc subdir. build.prop doesn't need to be an exception.
    
    The old patch (<partition>/build.prop and/or <partition>/default.prop)
    is supsported in some unbundled partitions (e.g. vendor, odm, etc.)
    because we havet to support those partitions that were built with
    previous versions of Android.
    
    Bug: 158735147
    Test: device has build.prop under ./etc and is bootable
    Change-Id: I26300574c23c78cc4e5bc20f98fc9702838465e1

--- init/property_service.cpp
+++ init/property_service.cpp
@@ -886,24 +886,26 @@ void PropertyLoadBootDefaults() {
         load_properties_from_file("/prop.default", nullptr, &properties);
     }
 
-    load_properties_from_file("/system/build.prop", nullptr, &properties);
-    load_properties_from_file("/system_ext/build.prop", nullptr, &properties);
+    // <part>/etc/build.prop is the canonical location of the build-time properties since S.
+    // Falling back to <part>/defalt.prop and <part>/build.prop when legacy partition has to be
+    // supported, i.e., vendor, odm, etc.
+    const auto load = [&properties](const std::string& partition, bool support_legacy) {
+        auto path = partition + "/etc/build.prop";
+        auto success = load_properties_from_file(path.c_str(), nullptr, &properties);
+        if (!success && support_legacy) {
+            load_properties_from_file((partition + "/default.prop").c_str(), nullptr, &properties);
+            load_properties_from_file((partition + "/build.prop").c_str(), nullptr, &properties);
+        }
+    };
 
-    // TODO(b/117892318): uncomment the following condition when vendor.imgs for
-    // aosp_* targets are all updated.
-//    if (SelinuxGetVendorAndroidVersion() <= __ANDROID_API_R__) {
-        load_properties_from_file("/vendor/default.prop", nullptr, &properties);
-//    }
-    load_properties_from_file("/vendor/build.prop", nullptr, &properties);
+    // Order matters here. The more the partition is specific to a product, the higher its
+    // precedence is.
+    load("/system", /* support_legacy */ false);
+    load("/system_ext", /* support_legacy */ false);
+    load("/vendor", /* support_legacy */ true);
+    load("/odm", /* support_legacy */ SelinuxGetVendorAndroidVersion() < __ANDROID_API_Q__);
+    load("/product", /* support_legacy */ true);
 
-    if (SelinuxGetVendorAndroidVersion() >= __ANDROID_API_Q__) {
-        load_properties_from_file("/odm/etc/build.prop", nullptr, &properties);
-    } else {
-        load_properties_from_file("/odm/default.prop", nullptr, &properties);
-        load_properties_from_file("/odm/build.prop", nullptr, &properties);
-    }
-
-    load_properties_from_file("/product/build.prop", nullptr, &properties);
     load_properties_from_file("/factory/factory.prop", "ro.*", &properties);
 
     if (access(kDebugRamdiskProp, R_OK) == 0) {
