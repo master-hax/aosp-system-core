7e7737f3d5a642db363ebc8578de6fedfacb7729
Tom Cherry <tomcherry@google.com>
2020-07-31 15:21:54 -0700

    SocketClient: don't ignore SIGPIPE
    1) All current users are better off ignoring SIGPIPE at the beginning
       of their process instead of ignoring it just for SocketClient
    2) This isn't thread safe if users did want it, since sigaction()
       ignores SIGPIPE for the entire process
    3) This costs 5-10% of logd CPU time when logcat is reading logs
    
    Test: kill logcat and see that logd doesn't crash
    Test: run simpleperf and see that no cycles are going to sigaction
    Change-Id: I6532c8a0d71338e534411707b9a9bd785145c730

--- libsysutils/src/SocketClient.cpp
+++ libsysutils/src/SocketClient.cpp
@@ -202,13 +202,8 @@ int SocketClient::sendDataLockedv(struct iovec *iov, int iovcnt) {
     }
 
     int ret = 0;
-    int e = 0; // SLOGW and sigaction are not inert regarding errno
+    int e = 0;  // SLOGW is not inert regarding errno
     int current = 0;
-
-    struct sigaction new_action, old_action;
-    memset(&new_action, 0, sizeof(new_action));
-    new_action.sa_handler = SIG_IGN;
-    sigaction(SIGPIPE, &new_action, &old_action);
 
     for (;;) {
         ssize_t rc = TEMP_FAILURE_RETRY(
@@ -238,8 +233,6 @@ int SocketClient::sendDataLockedv(struct iovec *iov, int iovcnt) {
         ret = -1;
         break;
     }
-
-    sigaction(SIGPIPE, &old_action, &new_action);
 
     if (e != 0) {
         errno = e;
