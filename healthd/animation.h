1f4c18c72c460a8e84cfe38769eb484cd1dc6bfe
Yifan Hong <elsk@google.com>
2020-08-05 16:15:15 -0700

    Load default charger resources from /system
    When charger resources are missing from /product, before this change,
    images are loaded from /res/images/charger. After this change, charger
    first checks if the resource exists at that location, and if not, use
    resource under /system/etc/res/images/charger.
    
    On devices that uses resources from /product, this change has no effect.
    On devices that uses resources from /res,
    - if an old image is mounted at /, charger continues to load resources
      from /res.
    - if a new image is mounted at /, that is, battery_fail and
      battery_scale may not exist under /res, charger loads them from /system.
      There are no font files installed by modules defined here, so any
      device-specific font files stay at /res.
    
    Also create virtual function wrappers over minui APIs to be mocked
    later.
    
    See next change on moving battery_scale and battery_fail images to
    /system.
    
    Test: build and inspect system/etc/res/images/charger
    Bug: 143907179
    Change-Id: I4895b9602274fa4a1ebb4d7d3b842622b81b7b51

--- healthd/animation.h
+++ healthd/animation.h
@@ -18,6 +18,7 @@
 #define HEALTHD_ANIMATION_H
 
 #include <inttypes.h>
+
 #include <string>
 
 class GRSurface;
@@ -52,20 +53,9 @@ struct animation {
     // - When treating paths as relative paths, it adds ".png" suffix.
     // - When treating paths as absolute paths, it doesn't add the suffix. Hence, the suffix
     //   is added here.
-    void set_resource_root(const std::string& root) {
-        if (!animation_file.empty()) {
-            animation_file = root + animation_file + ".png";
-        }
-        if (!fail_file.empty()) {
-            fail_file = root + fail_file + ".png";
-        }
-        if (!text_clock.font_file.empty()) {
-            text_clock.font_file = root + text_clock.font_file + ".png";
-        }
-        if (!text_percent.font_file.empty()) {
-            text_percent.font_file = root + text_percent.font_file + ".png";
-        }
-    }
+    // If |backup_root| is provided, additionally check if file under |root| is accessbile or not.
+    // If not accessbile, use |backup_root| instead.
+    void set_resource_root(const std::string& root, const std::string& backup_root = "");
 
     std::string animation_file;
     std::string fail_file;
