c14b3d811d1c64d9db30cfd6db7eccd911cb8731
Daniel Norman <danielnorman@google.com>
2020-11-09 17:28:24 -0800

    Returns a service parse failure for overrides that cross subcontexts.
    Also includes new --out_<partition> flags for
      system,system_ext,product,vendor,odm
    to allow host_init_verifier to work with a collection of init rc files.
    
    Test: host_init_verifier --out_system=... --out_vendor=...
          where vendor contains an init rc file that overrides a service
          present in system. Observe parse failure and non-zero exit.
    Bug: 163089173
    Change-Id: I520fef613e0036df8a7d47a98d47405eaa969110

--- init/host_init_verifier.cpp
+++ init/host_init_verifier.cpp
@@ -25,6 +25,8 @@
 #include <fstream>
 #include <iostream>
 #include <iterator>
+#include <map>
+#include <set>
 #include <string>
 #include <vector>
 
@@ -51,6 +53,7 @@
 
 using namespace std::literals;
 
+using android::base::EndsWith;
 using android::base::ParseInt;
 using android::base::ReadFileToString;
 using android::base::Split;
@@ -60,6 +63,10 @@ using android::properties::PropertyInfoArea;
 using android::properties::PropertyInfoEntry;
 
 static std::vector<std::string> passwd_files;
+
+// NOTE: Keep this in sync with the order used by init.cpp LoadBootScripts()
+static const std::vector<std::string> partition_search_order =
+        std::vector<std::string>({"system", "system_ext", "odm", "vendor", "product"});
 
 static std::vector<std::pair<std::string, int>> GetVendorPasswd(const std::string& passwd_file) {
     std::string passwd;
@@ -148,12 +155,28 @@ static Result<void> check_stub(const BuiltinArguments& args) {
 #include "generated_stub_builtin_function_map.h"
 
 void PrintUsage() {
-    std::cout << "usage: host_init_verifier [options] <init rc file>\n"
+    std::cout << "usage: host_init_verifier [options]\n"
                  "\n"
-                 "Tests an init script for correctness\n"
+                 "Tests init script(s) for correctness.\n"
                  "\n"
-                 "-p FILE\tSearch this passwd file for users and groups\n"
-                 "--property_contexts=FILE\t Use this file for property_contexts\n"
+                 "Generic options:\n"
+                 "  -p FILE\tSearch this passwd file for users and groups\n"
+                 "  --property_contexts=FILE\t Use this file for property_contexts\n"
+                 "\n"
+                 "Single script mode options:\n"
+                 "  [init rc file]\tPositional argument; test this init script.\n"
+                 "\n"
+                 "Multiple script mode options:\n"
+                 "  --out_system=DIR\tPath to the output product directory for the "
+                 "system partition.\n"
+                 "  --out_system_ext=DIR\tPath to the output product directory for the "
+                 "system_ext partition.\n"
+                 "  --out_odm=DIR\tPath to the output product directory for the "
+                 "odm partition.\n"
+                 "  --out_vendor=DIR\tPath to the output product directory for the "
+                 "vendor partition.\n"
+                 "  --out_product=DIR\tPath to the output product directory for the "
+                 "product partition.\n"
               << std::endl;
 }
 
@@ -176,6 +199,18 @@ Result<InterfaceInheritanceHierarchyMap> ReadInterfaceInheritanceHierarchy() {
     }
 
     return result;
+}
+
+// Parses services by scanning partition directories for init rc files.
+// partition_map is a map from partition_name -> directory.
+size_t ParseServicesFromPartitions(const std::map<std::string, std::string>& partition_map,
+                                   Parser& parser) {
+    for (const auto& p : partition_search_order) {
+        if (partition_map.find(p) != partition_map.end()) {
+            parser.ParseConfig(partition_map.at(p) + "etc/init");
+        }
+    }
+    return parser.parse_error_count();
 }
 
 const PropertyInfoArea* property_info_area;
@@ -203,12 +238,18 @@ int main(int argc, char** argv) {
     android::base::SetMinimumLogSeverity(android::base::ERROR);
 
     auto property_infos = std::vector<PropertyInfoEntry>();
+    std::map<std::string, std::string> partition_map;
 
     while (true) {
         static const char kPropertyContexts[] = "property-contexts=";
         static const struct option long_options[] = {
                 {"help", no_argument, nullptr, 'h'},
                 {kPropertyContexts, required_argument, nullptr, 0},
+                {"out_system", required_argument, nullptr, 0},
+                {"out_system_ext", required_argument, nullptr, 0},
+                {"out_odm", required_argument, nullptr, 0},
+                {"out_vendor", required_argument, nullptr, 0},
+                {"out_product", required_argument, nullptr, 0},
                 {nullptr, 0, nullptr, 0},
         };
 
@@ -223,6 +264,16 @@ int main(int argc, char** argv) {
             case 0:
                 if (long_options[option_index].name == kPropertyContexts) {
                     HandlePropertyContexts(optarg, &property_infos);
+                }
+                for (const auto& p : partition_search_order) {
+                    if (long_options[option_index].name == "out_" + p) {
+                        if (partition_map.find(p) != partition_map.end()) {
+                            PrintUsage();
+                            return EXIT_FAILURE;
+                        }
+                        partition_map[p] =
+                                EndsWith(optarg, "/") ? optarg : std::string(optarg) + "/";
+                    }
                 }
                 break;
             case 'h':
@@ -240,7 +291,9 @@ int main(int argc, char** argv) {
     argc -= optind;
     argv += optind;
 
-    if (argc != 1) {
+    // If provided, use the partition map to check multiple init rc files.
+    // Otherwise, check a single init rc file.
+    if ((!partition_map.empty() && argc != 0) || (partition_map.empty() && argc != 1)) {
         PrintUsage();
         return EXIT_FAILURE;
     }
@@ -262,26 +315,47 @@ int main(int argc, char** argv) {
 
     property_info_area = reinterpret_cast<const PropertyInfoArea*>(serialized_contexts.c_str());
 
+    if (!partition_map.empty()) {
+        std::vector<std::string> vendor_prefixes;
+        for (const auto& partition : {"vendor", "odm"}) {
+            if (partition_map.find(partition) != partition_map.end()) {
+                vendor_prefixes.push_back(partition_map.at(partition));
+            }
+        }
+        InitializeHostSubcontext(vendor_prefixes);
+    }
+
     const BuiltinFunctionMap& function_map = GetBuiltinFunctionMap();
     Action::set_function_map(&function_map);
     ActionManager& am = ActionManager::GetInstance();
     ServiceList& sl = ServiceList::GetInstance();
     Parser parser;
-    parser.AddSectionParser("service", std::make_unique<ServiceParser>(
-                                               &sl, nullptr, *interface_inheritance_hierarchy_map));
+    parser.AddSectionParser("service",
+                            std::make_unique<ServiceParser>(&sl, GetSubcontext(),
+                                                            *interface_inheritance_hierarchy_map));
     parser.AddSectionParser("on", std::make_unique<ActionParser>(&am, nullptr));
     parser.AddSectionParser("import", std::make_unique<HostImportParser>());
 
-    if (!parser.ParseConfigFileInsecure(*argv)) {
-        LOG(ERROR) << "Failed to open init rc script '" << *argv << "'";
-        return EXIT_FAILURE;
+    if (!partition_map.empty()) {
+        size_t failures = ParseServicesFromPartitions(partition_map, parser);
+        if (failures > 0) {
+            LOG(ERROR) << "Failed to parse init scripts with " << failures << " errors";
+            return EXIT_FAILURE;
+        }
+    } else {
+        if (!parser.ParseConfigFileInsecure(*argv)) {
+            LOG(ERROR) << "Failed to open init rc script '" << *argv << "'";
+            return EXIT_FAILURE;
+        }
+        size_t failures =
+                parser.parse_error_count() + am.CheckAllCommands() + sl.CheckAllCommands();
+        if (failures > 0) {
+            LOG(ERROR) << "Failed to parse init script '" << *argv << "' with " << failures
+                       << " errors";
+            return EXIT_FAILURE;
+        }
     }
-    size_t failures = parser.parse_error_count() + am.CheckAllCommands() + sl.CheckAllCommands();
-    if (failures > 0) {
-        LOG(ERROR) << "Failed to parse init script '" << *argv << "' with " << failures
-                   << " errors";
-        return EXIT_FAILURE;
-    }
+
     return EXIT_SUCCESS;
 }
 
