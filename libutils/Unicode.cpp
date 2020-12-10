aa7f9ec805ac4fd4eb9de7cec5618b871dfc3ae2
Richard Townsend <Richard.Townsend@arm.com>
2020-12-10 19:28:27 +0000

    fix: undefined shift
    Clang 12's undefined behaviour sanitizer is smarter and now picks
    up a sign change.
    
    Test: lunch fvp-eng && mm ; Clang 12 toolchain
    Test: mm libutils_test    ; Clang 11 toolchain (all tests pass on host)
    Change-Id: I9d6c590d22f9b063b8354d24062ccefe3779ea39

--- libutils/Unicode.cpp
+++ libutils/Unicode.cpp
@@ -141,14 +141,14 @@ static inline int32_t utf32_at_internal(const char* cur, size_t *num_read)
         return *cur;
     }
     cur++;
-    char32_t mask, to_ignore_mask;
+    uint64_t mask, to_ignore_mask;
     size_t num_to_read = 0;
-    char32_t utf32 = first_char;
+    uint32_t utf32 = first_char;
     for (num_to_read = 1, mask = 0x40, to_ignore_mask = 0xFFFFFF80;
          (first_char & mask);
          num_to_read++, to_ignore_mask |= mask, mask >>= 1) {
         // 0x3F == 00111111
-        utf32 = (utf32 << 6) + (*cur++ & 0x3F);
+        utf32 = ((utf32 & 0x3ffffff) << 6) + (*cur++ & 0x3F);
     }
     to_ignore_mask |= mask;
     utf32 &= ~(to_ignore_mask << (6 * (num_to_read - 1)));
