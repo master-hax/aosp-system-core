7028611c0955472575a227fc27b7f26b9103b355
Wenhao Wang <wenhaowang@google.com>
2020-12-10 14:02:13 -0800

    ConfirmationUI Fuzzing
    Not for submission
    
    Test: /data/fuzz/arm64/trusty_confirmationui_fuzzer/trusty_confirmationui_fuzzer
    Change-Id: I2e287281e7c8100f0d48413fbe0ff99d397a74c1

--- trusty/confirmationui/fuzz/fuzz.cpp
+++ trusty/confirmationui/fuzz/fuzz.cpp
@@ -17,18 +17,48 @@
 #undef NDEBUG
 
 #include <assert.h>
+#include <iostream>
 #include <log/log.h>
 #include <stdlib.h>
+#include <trusty/coverage/coverage.h>
+#include <trusty/fuzz/counters.h>
 #include <trusty/fuzz/utils.h>
 #include <unistd.h>
 
+using android::trusty::coverage::CoverageRecord;
+using android::trusty::fuzz::ExtraCounters;
 using android::trusty::fuzz::TrustyApp;
 
 #define TIPC_DEV "/dev/trusty-ipc-dev0"
 #define CONFIRMATIONUI_PORT "com.android.trusty.confirmationui"
+#define CONFUI_MAX_MSG_SIZE 8192 + 8
+
+/* ConfirmationUI TA's UUID is 7dee2364-c036-425b-b086-df0f6c233c1b */
+static struct uuid confirmationui_uuid = {
+    0x7dee2364,
+    0xc036,
+    0x425b,
+    {0xb0, 0x86, 0xdf, 0x0f, 0x6c, 0x23, 0x3c, 0x1b},
+};
+
+static CoverageRecord record(TIPC_DEV, &confirmationui_uuid);
+
+extern "C" int LLVMFuzzerInitialize(int* /* argc */, char*** /* argv */) {
+    auto ret = record.Open();
+    assert(ret.ok());
+    return 0;
+}
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+    if (size < CONFUI_MAX_MSG_SIZE) {
+        return -1;
+    }
+
     static uint8_t buf[TIPC_MAX_MSG_SIZE];
+    size_t data_idx = 0;
+
+    ExtraCounters counters(&record);
+    counters.Reset();
 
     TrustyApp ta(TIPC_DEV, CONFIRMATIONUI_PORT);
     auto ret = ta.Connect();
@@ -36,16 +66,23 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
         android::trusty::fuzz::Abort();
     }
 
-    /* Send message to confirmationui server */
-    ret = ta.Write(data, size);
-    if (!ret.ok()) {
-        return -1;
-    }
+    std::cout << "size " << size << "\n";
+    while (data_idx < size) {
+        size_t write_size = std::min<size_t>(CONFUI_MAX_MSG_SIZE, size - data_idx);
+        data_idx += write_size;
+        std::cout << "data_idx " << data_idx << "\n";
+        std::cout << "write_size " << write_size << "\n";
+        /* Send message to confirmationui server */
+        ret = ta.Write(&data[data_idx], write_size);
+        if (!ret.ok()) {
+            return -1;
+        }
 
-    /* Read message from confirmationui server */
-    ret = ta.Read(&buf, sizeof(buf));
-    if (!ret.ok()) {
-        return -1;
+        /* Read message from confirmationui server */
+        ret = ta.Read(&buf, sizeof(buf));
+        if (!ret.ok()) {
+            return -1;
+        }
     }
 
     return 0;
