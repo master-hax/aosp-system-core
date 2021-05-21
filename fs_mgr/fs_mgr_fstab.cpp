ce964bcd522f3869dbb727e45acf6e4c00897b2f
Yi-Yo Chiang <yochiang@google.com>
2021-05-22 00:35:09 +0800

    fs_mgr_fstab: Allowlist overlayfs mount flags
    Only allow "lowerdir=" and ignore other flags, such as "upperdir".
    Ignore "override_creds=" as it is automatically appended by fs_mgr.
    Ignore "seclabel" as it is automatically appended by the selinux driver.
    
    "lowerdir=" can be specified in the fs mount flags as well as
    fs_mgr_flags, and have the same effect:
    
      overlay /mnt/product/test1 overlay lowerdir=/1:/2 first_stage_mount
      overlay /mnt/product/test1 overlay ro first_stage_mount,lowerdir=/1:/2
    
    both results in:
    
      mount(source=overlay,target=/mnt/product/test1,type=overlay,lowerdir=/1:/2,override_creds=off)
    
    Bug: 188862155
    Test: Boot to normal with overlayfs mount entries in first stage fstab
    Change-Id: I6d6abd44ab32afadec428005f4aece834f9c8905

--- fs_mgr/fs_mgr_fstab.cpp
+++ fs_mgr/fs_mgr_fstab.cpp
@@ -121,6 +121,25 @@ void ParseMountFlags(const std::string& flags, FstabEntry* entry) {
     std::string fs_options;
     for (const auto& flag : Split(flags, ",")) {
         if (!SetMountFlag(flag, entry)) {
+            std::string arg;
+            if (auto equal_sign = flag.find('='); equal_sign != std::string::npos) {
+                arg = flag.substr(equal_sign + 1);
+            }
+
+            // Allowlist overlayfs flags.
+            if (entry->fs_type == "overlay") {
+                if (StartsWith(flag, "lowerdir=")) {
+                    entry->lowerdir = arg;
+                } else if (StartsWith(flag, "override_creds=") || flag == "seclabel") {
+                    // Do nothing, don't even log any warning. This prevents
+                    // log spam when parsing fstab from /proc/mounts.
+                } else {
+                    LWARNING << "Warning: ignored overlayfs flag: " << flag;
+                }
+                // Skip appending to fs_options.
+                continue;
+            }
+
             // Unknown flag, so it must be a filesystem specific option.
             if (!fs_options.empty()) {
                 fs_options.append(",");  // appends a comma if not the first
@@ -128,10 +147,6 @@ void ParseMountFlags(const std::string& flags, FstabEntry* entry) {
             fs_options.append(flag);
 
             if (entry->fs_type == "f2fs" && StartsWith(flag, "reserve_root=")) {
-                std::string arg;
-                if (auto equal_sign = flag.find('='); equal_sign != std::string::npos) {
-                    arg = flag.substr(equal_sign + 1);
-                }
                 if (!ParseInt(arg, &entry->reserved_size)) {
                     LWARNING << "Warning: reserve_root= flag malformed: " << arg;
                 } else {
