646ee488180bd97c658726db71b80870012cda64
Elliott Hughes <enh@google.com>
2021-07-13 17:14:20 -0700

    Lose convertToResPath to aapt.
    aapt (not aapt2) is the only user.
    
    Test: treehugger
    Change-Id: Ie69f84f4f805c69f838e345b44755a316b9f9b06

--- libutils/String8.cpp
+++ libutils/String8.cpp
@@ -582,20 +582,4 @@ String8& String8::appendPath(const char* name)
     }
 }
 
-String8& String8::convertToResPath()
-{
-#if OS_PATH_SEPARATOR != RES_PATH_SEPARATOR
-    size_t len = length();
-    if (len > 0) {
-        char * buf = lockBuffer(len);
-        for (char * end = buf + len; buf < end; ++buf) {
-            if (*buf == OS_PATH_SEPARATOR)
-                *buf = RES_PATH_SEPARATOR;
-        }
-        unlockBuffer(len);
-    }
-#endif
-    return *this;
-}
-
 }; // namespace android
