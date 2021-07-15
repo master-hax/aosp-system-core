884831fa98f48bf4349155bf27307f4b627e03bb
Jaegeuk Kim <jaegeuk@google.com>
2021-07-15 08:49:22 -0700

    fs_mgr: add --no-kernel-check to avoid frequent full scan
    Let's rely on anroid/f2fs for full scan.
    
    Bug: 193676701
    Signed-off-by: Jaegeuk Kim <jaegeuk@google.com>
    Change-Id: I8b1b5006a8d9caf2bd025edc1c28d82028d79d7d

--- fs_mgr/fs_mgr.cpp
+++ fs_mgr/fs_mgr.cpp
@@ -258,8 +258,9 @@ static void check_fs(const std::string& blk_device, const std::string& fs_type,
             }
         }
     } else if (is_f2fs(fs_type)) {
-        const char* f2fs_fsck_argv[] = {F2FS_FSCK_BIN,     "-a", "-c", "10000", "--debug-cache",
-                                        blk_device.c_str()};
+        const char* f2fs_fsck_argv[] = {
+                F2FS_FSCK_BIN,     "-a", "-c", "10000", "--debug-cache", "--no-kernel-check",
+                blk_device.c_str()};
         const char* f2fs_fsck_forced_argv[] = {
                 F2FS_FSCK_BIN, "-f", "-c", "10000", "--debug-cache", blk_device.c_str()};
 
@@ -269,7 +270,7 @@ static void check_fs(const std::string& blk_device, const std::string& fs_type,
             ret = logwrap_fork_execvp(ARRAY_SIZE(f2fs_fsck_forced_argv), f2fs_fsck_forced_argv,
                                       &status, false, LOG_KLOG | LOG_FILE, false, FSCK_LOG_FILE);
         } else {
-            LINFO << "Running " << F2FS_FSCK_BIN << " -a -c 10000 --debug-cache "
+            LINFO << "Running " << F2FS_FSCK_BIN << " -a -c 10000 --debug-cache --no-kernel-check"
                   << realpath(blk_device);
             ret = logwrap_fork_execvp(ARRAY_SIZE(f2fs_fsck_argv), f2fs_fsck_argv, &status, false,
                                       LOG_KLOG | LOG_FILE, false, FSCK_LOG_FILE);
