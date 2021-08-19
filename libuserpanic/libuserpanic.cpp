6cb5b00338ab4d766a5c48a8c7ea34d6eb9629b0
Woody Lin <woodylin@google.com>
2021-08-19 15:56:44 +0800

    Add libuserpanic
    Provide a tiny library implementing system panic functionality
    for Android's core services (init, llkd, system_server) to request a
    kernel panic with informative headline.
    
    Bug: 188777408
    Change-Id: I85881b9f721d40382173469749f46ce510e1047b

---
+++ libuserpanic/libuserpanic.cpp
@@ -0,0 +1,59 @@
+/*
+ * Copyright (C) 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#define LOG_TAG "libuserpanic"
+
+#include <errno.h>
+#include <fcntl.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <android-base/file.h>
+#include <android-base/logging.h>
+#include <android/userpanic.h>
+
+#include <sys/ioctl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+#include <string>
+
+static void write_to_file(const std::string& content, const std::string& path)
+{
+    if (!android::base::WriteStringToFile(content, path)) {
+        PLOG(ERROR) << "Failed to write " << path;
+    }
+}
+
+void android_panic_kernel(const char* title)
+{
+    std::string req_buf;
+    struct {
+      char cmd;
+      char version;
+      uint32_t title_len;
+    } req_header = {
+      'c', 0, static_cast<uint32_t>(strlen(title))
+    };
+
+    req_buf.append(reinterpret_cast<char*>(&req_header), sizeof(req_header));
+    req_buf.append(title);
+
+    write_to_file(req_buf, "/dev/userspace_panic");
+    write_to_file("c", "/proc/sysrq-trigger");
+    /* No return */
+    while (true) pause();
+}
