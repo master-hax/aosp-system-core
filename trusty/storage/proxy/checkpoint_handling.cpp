790be04e25f8d7ecf81cd075fc103f112fb59ce5
Thurston Dang <thurston@google.com>
2021-10-07 23:08:56 +0000

    storageproxyd: stall on RPMB writes when checkpointing, if necessary
    If a checkpointing operation is in progress, stall any write operations
    that are flagged as STORAGE_MSG_FLAG_PRE_COMMIT_CHECKPOINT. In tandem
    with trusty-side changes that set the flag appropriately, this avoids
    the awkward case where the superblock is committed, but the data is not
    because the checkpoint is rolled back.
    
    Based on Stephen's CL/1845477 "Add helper to check checkpoint state of
    mounts".
    
    Test: m storageproxyd
    Bug: 194313068
    Change-Id: Ib6a432db1bc1b034f803b743b0d7322e3f31d814

---
+++ trusty/storage/proxy/checkpoint_handling.cpp
@@ -0,0 +1,102 @@
+/*
+ * Copyright (C) 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "checkpoint_handling.h"
+#include "log.h"
+
+#include <fstab/fstab.h>
+#include <cstring>
+#include <string>
+
+namespace {
+
+bool isCheckpointing = false;
+
+bool checkpointStateCached = false;
+
+}  // namespace
+
+int is_data_checkpoint_active(bool* active) {
+    if (!active) {
+        ALOGE("active out parameter is null");
+        return -1;
+    }
+
+    if (checkpointStateCached && !isCheckpointing) {
+        *active = false;
+        return 0;
+    }
+
+    android::fs_mgr::Fstab procMounts;
+    bool success = android::fs_mgr::ReadFstabFromFile("/proc/mounts", &procMounts);
+    if (!success) {
+        ALOGE("Could not parse /proc/mounts");
+        return -1;
+    }
+
+    android::fs_mgr::FstabEntry* dataEntry =
+            android::fs_mgr::GetEntryForMountPoint(&procMounts, "/data");
+    if (dataEntry == NULL) {
+        isCheckpointing = false;
+        checkpointStateCached = false;
+
+        *active = false;
+        return 0;
+    }
+
+    if (dataEntry->fs_type != "f2fs") {
+        ALOGW("Checkpoint status not supported for filesystem %s", dataEntry->fs_type.c_str());
+        return -1;
+    }
+
+    size_t checkpointPos = dataEntry->fs_options.find("checkpoint=");
+    if (checkpointPos == std::string::npos) {
+        isCheckpointing = false;
+        checkpointStateCached = true;
+
+        *active = false;
+        return 0;
+    }
+    size_t checkpointValueStart = checkpointPos + strlen("checkpoint=");
+
+    size_t checkpointValueEnd = dataEntry->fs_options.find(",", checkpointValueStart);
+    if (checkpointValueEnd == std::string::npos) {
+        checkpointValueEnd = dataEntry->fs_options.length();
+    }
+
+    /*
+     * The data entry looks like "... foo,checkpoint=disable:0,bar ..."
+     *                                               ^        ^
+     *                                               |        |
+     *                                               |    checkpointValueEnd
+     *                                               |
+     *                                        checkpointValueStart
+     *
+     * We want to know if the checkpoint value is prefixed by "disable", but we
+     * only scan up to the first comma (or end of the string).
+     */
+    size_t checkpointValueSize =
+            std::min(checkpointValueEnd - checkpointValueStart, strlen("disable"));
+
+    std::string checkpointValue =
+            dataEntry->fs_options.substr(checkpointValueStart, checkpointValueSize);
+
+    isCheckpointing = (checkpointValue == std::string("disable"));
+    checkpointStateCached = true;
+
+    *active = isCheckpointing;
+    return 0;
+}
