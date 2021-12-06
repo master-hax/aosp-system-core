8246868319c12de628590416e0c54d064202cd6a
David Anderson <dvander@google.com>
2021-12-05 22:48:37 -0800

    libsnapshot: Test more configurations in presubmit.
    Bug: 208944665
    Test: presubmit
    Change-Id: I994a93e4e364bc210ddd2eb60025c656caa86b1c

--- fs_mgr/libsnapshot/run_snapshot_tests.sh
+++ fs_mgr/libsnapshot/run_snapshot_tests.sh
@@ -1,26 +1,28 @@
-#!/system/bin/sh
+#!/bin/bash
+
+###############################################################################
+# This is a helper script intended for Cuttlefish. It tests each supported
+# configuration of vts_libsnapshot_test. It is run on presubmit and should be
+# run manually after material changes to libsnapshot.
+#
+# This script is not intended to be run on real devices. For devices under
+# test, use vts_libsnapshot_test, which only tests the device's actual shipping
+# configuration.
+###############################################################################
 
 # Detect host or AOSP.
-getprop ro.build.version.sdk > /dev/null 2>&1
-if [ $? -eq 0 ]; then
-    cmd_prefix=""
-    local_root=""
-else
-    cmd_prefix="adb shell"
-    local_root="${ANDROID_PRODUCT_OUT}"
-    set -e
-    set -x
-    adb root
-    adb sync data
-    set +x
-    set +e
-fi
+set -e
+set -x
+adb root
+adb sync data
+set +x
+set +e
 
 testpath64="/data/nativetest64/vts_libsnapshot_test/vts_libsnapshot_test"
 testpath32="/data/nativetest/vts_libsnapshot_test/vts_libsnapshot_test"
-if [ -f "${local_root}/${testpath64}" ]; then
+if [ -f "${ANDROID_PRODUCT_OUT}/${testpath64}" ]; then
     testpath="${testpath64}"
-elif [ -f "${local_root}/${testpath32}" ]; then
+elif [ -f "${ANDROID_PRODUCT_OUT}/${testpath32}" ]; then
     testpath="${testpath32}"
 else
     echo "ERROR: vts_libsnapshot_test not found." 1>&2
@@ -32,4 +34,14 @@ fi
 set -x
 set -e
 
-time ${cmd_prefix} ${testpath}
+echo "Testing VAB with userspace snapshots"
+time adb shell ${testpath}
+
+echo "Testing VAB with dm-snapshot and compression"
+time adb shell ${testpath} -force_mode vabc-legacy
+
+echo "Testing VAB with dm-snapshot"
+time adb shell ${testpath} -force_mode vab-legacy
+
+echo "Testing VAB with userspace snapshots and no compression"
+time adb shell ${testpath} -compression_method none
