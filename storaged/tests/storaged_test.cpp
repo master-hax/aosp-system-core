bf4fbc3b3ec3589ad26a6278bb3e1e0ca0bc521b
Yifan Hong <elsk@google.com>
2021-12-06 23:04:12 -0800

    storaged: skip disk_stats test if not supported.
    If HAL does not support disk stats, do not run
    corresponding storaged tests.
    
    Also add storaged-unit-tests to Treehugger.
    
    Test: storaged-unit-tests
    Bug: 208543110
    Change-Id: Ia2acf30ca3e259e0f0744385da69d92f9c00f223

--- storaged/tests/storaged_test.cpp
+++ storaged/tests/storaged_test.cpp
@@ -249,6 +249,13 @@ TEST(storaged_test, disk_stats_monitor) {
     // testing if detect() will return the right value
     disk_stats_monitor dsm_detect{healthService};
     ASSERT_TRUE(dsm_detect.enabled());
+
+    // Even if enabled(), healthService may not support disk stats. Check if it is supported.
+    std::vector<aidl::android::hardware::health::DiskStats> halStats;
+    if (healthService->getDiskStats(&halStats).getExceptionCode() == EX_UNSUPPORTED_OPERATION) {
+        GTEST_SKIP();
+    }
+
     // feed monitor with constant perf data for io perf baseline
     // using constant perf is reasonable since the functionality of stream_stats
     // has already been tested
