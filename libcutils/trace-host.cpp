5d7abc674d864111aa42ad891368a896c99fa660
Ray Ye <kongxincai@google.com>
2022-03-21 20:21:53 +0000

    Added async trace functions with a track name argument
    Bug: 221119585
    Test: atest TraceDevTest
    Change-Id: I48d9e858ce588e3735ddbbe14b1bd7c8f264cd83

--- libcutils/trace-host.cpp
+++ libcutils/trace-host.cpp
@@ -28,6 +28,10 @@ void atrace_begin_body(const char* /*name*/) {}
 void atrace_end_body() { }
 void atrace_async_begin_body(const char* /*name*/, int32_t /*cookie*/) {}
 void atrace_async_end_body(const char* /*name*/, int32_t /*cookie*/) {}
+void atrace_async_for_track_begin_body(const char* /*track_name*/, const char* /*name*/,
+                                       int32_t /*cookie*/) {}
+void atrace_async_for_track_end_body(const char* /*track_name*/, const char* /*name*/,
+                                     int32_t /*cookie*/) {}
 void atrace_instant_body(const char* /*name*/) {}
 void atrace_instant_for_track_body(const char* /*trackName*/,
                                    const char* /*name*/) {}
