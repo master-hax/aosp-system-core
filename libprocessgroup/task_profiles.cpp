386147f5867d24f5062d7d9a9ee57a64ba755016
Bart Van Assche <bvanassche@google.com>
2022-03-22 16:15:00 -0700

    Fix support for optional cgroup attributes
    The Linux kernel returns error code EACCES for cgroup attributes that do
    not exist. This patch makes lines like the following disappear from
    logcat for optional cgroup attributes:
    
    03-22 23:16:04.816   616   649 E libprocessgroup: Failed to write '0' to /sys/fs/cgroup/./uid_10077/p
    id_1695/io.group_idle: Permission denied
    
    Bug: 213617178
    Test: Booted Android in Cuttlefish and inspected logcat.
    Change-Id: I6e041dfc34a52c9bdb75a8c70d99ad79b06eee06
    Signed-off-by: Bart Van Assche <bvanassche@google.com>

--- libprocessgroup/task_profiles.cpp
+++ libprocessgroup/task_profiles.cpp
@@ -207,7 +207,7 @@ bool SetAttributeAction::ExecuteForTask(int tid) const {
     }
 
     if (!WriteStringToFile(value_, path)) {
-        if (errno == ENOENT) {
+        if (errno == EACCES) {
             if (optional_) {
                 return true;
             } else {
