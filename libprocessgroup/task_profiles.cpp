7a6b1cd96c8b2b36fd8b2fc67845da5ea8832ca2
Bart Van Assche <bvanassche@google.com>
2022-03-22 16:15:00 -0700

    Fix support for optional cgroup attributes
    The Linux kernel returns error code EACCES (Permission denied) if either
    a cgroup attribute does not exist or if the process that is trying to
    write into a cgroup attribute does not have permission to write. In
    other words, it is not possible to tell the difference between these two
    scenarios by checking the value of 'errno'. Hence this patch that adds a
    stat() call to check whether or not a cgroup attribute exists. This
    patch makes lines like the following disappear from logcat for optional
    cgroup attributes:
    
    03-22 23:16:04.816   616   649 E libprocessgroup: Failed to write '0' to /sys/fs/cgroup/./uid_10077/p
    id_1695/io.group_idle: Permission denied
    
    Bug: 213617178
    Test: Booted Android in Cuttlefish and inspected logcat.
    Change-Id: I6e041dfc34a52c9bdb75a8c70d99ad79b06eee06
    Signed-off-by: Bart Van Assche <bvanassche@google.com>

--- libprocessgroup/task_profiles.cpp
+++ libprocessgroup/task_profiles.cpp
@@ -207,7 +207,7 @@ bool SetAttributeAction::ExecuteForTask(int tid) const {
     }
 
     if (!WriteStringToFile(value_, path)) {
-        if (errno == ENOENT) {
+        if (struct stat statbuf; stat(path.c_str(), &statbuf) < 0) {
             if (optional_) {
                 return true;
             } else {
