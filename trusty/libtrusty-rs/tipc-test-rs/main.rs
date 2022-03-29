06ff73c07b7c2a4672400a83a82ef6e8ebd4c303
David LeGare <legare@google.com>
2022-03-29 22:36:15 +0000

    Setup tipc-test-rs
    Test: manual using tipc-test-rs
    Bug: 226659377
    Change-Id: I998013b2f8b304299acb09d58beb49330747802a

---
+++ trusty/libtrusty-rs/tipc-test-rs/main.rs
@@ -0,0 +1,43 @@
+//! Utility for testing libtrusty-rs.
+
+use std::io::prelude::*;
+use std::str;
+use structopt::StructOpt;
+use trusty::{TipcStream, TIPC_DEFAULT_DEVNAME};
+
+const ECHO_NAME: &str = "com.android.ipc-unittest.srv.echo";
+
+fn main() {
+    let opt = Opt::from_args();
+
+    for test in opt.tests {
+        match test.as_str() {
+            "echo" => run_echo(),
+
+            _ => todo!("Implement test for {:?}", test),
+        }
+    }
+}
+
+fn run_echo() {
+    let mut connection = TipcStream::connect(TIPC_DEFAULT_DEVNAME, ECHO_NAME)
+        .expect("Failed to connect to Trusty service");
+
+    write!(connection, "Hello, world?").expect("Failed to write to connection");
+
+    let mut read_buf = [0u8; 1024];
+    let read_len = connection
+        .read(&mut read_buf[..])
+        .expect("Failed to read from connection");
+
+    match str::from_utf8(&read_buf[..read_len]) {
+        Ok(msg) => println!("Service responded with {:?}", msg),
+        Err(_) => println!("Service responded with {} non-utf8 bytes", read_len),
+    }
+}
+
+#[derive(Debug, StructOpt)]
+struct Opt {
+    #[structopt(long = "test", short, required(true))]
+    tests: Vec<String>,
+}
