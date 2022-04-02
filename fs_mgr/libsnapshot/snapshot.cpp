1b71c210d2858f72c467f11e8df3fcbfada6f0e6
Akilesh Kailash <akailash@google.com>
2022-04-02 04:54:26 +0000

    libsnapshot: OTA upgrade when vendor partition is S
    In Android S, snapuserd binary was on vendor partition.
    When there is an OTA update from S -> T, it is possible
    that vendor partitions are not updated. In that case,
    successive OTA updates T1 -> T2 will continue to have
    snapuserd from Android S as vendor partition wasn't updated
    to T. All this means, we should disable user-space snapshots.
    
    When installing OTA during runtime, check for property
    ro.vendor.build.version.release_or_codename; if the property
    is set to "12", then skip userspace-snapshots.
    
    Bug: 227614163
    
    Test: Simulate OTA test on Pixel 6 from T1 -> T2 by forcefully
    setting the property to 12 and verify OTA is applied
    successfully by falling back to dm-snapshot.
    
    Signed-off-by: Akilesh Kailash <akailash@google.com>
    Change-Id: I95f29145e5cd9ffb8d03d28ae414f0037b88be90

--- fs_mgr/libsnapshot/snapshot.cpp
+++ fs_mgr/libsnapshot/snapshot.cpp
@@ -3205,9 +3205,21 @@ Return SnapshotManager::CreateUpdateSnapshots(const DeltaArchiveManifest& manife
     status.set_compression_enabled(cow_creator.compression_enabled);
     if (cow_creator.compression_enabled) {
         if (!device()->IsTestDevice()) {
+            bool userSnapshotsEnabled = IsUserspaceSnapshotsEnabled();
+            const std::string UNKNOWN = "unknown";
+            std::string vendor_release = android::base::GetProperty(
+                    "ro.vendor.build.version.release_or_codename", UNKNOWN);
+
+            // No user-space snapshots if vendor partition is on Android 12
+            if (vendor_release.find("12") != std::string::npos) {
+                LOG(INFO) << "User-space snapshots disabled as vendor partition is on Android : "
+                          << vendor_release;
+                userSnapshotsEnabled = false;
+            }
+
             // Userspace snapshots is enabled only if compression is enabled
-            status.set_userspace_snapshots(IsUserspaceSnapshotsEnabled());
-            if (IsUserspaceSnapshotsEnabled()) {
+            status.set_userspace_snapshots(userSnapshotsEnabled);
+            if (userSnapshotsEnabled) {
                 is_snapshot_userspace_ = true;
                 status.set_io_uring_enabled(IsIouringEnabled());
                 LOG(INFO) << "User-space snapshots enabled";
