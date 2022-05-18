ce9d8194e2bd167abd374314c1fb9398b6810cf5
Armelle Laine <armellel@google.com>
2022-05-18 05:38:49 +0000

    trusty: add cast-auth hal test
    Bug: None
    Change-Id: I9d8a2d6cbe490b649208f7c21070ff842f7a41d4

---
+++ trusty/test/cast-auth/aidl-cpp/ICastAuth.h
@@ -0,0 +1,44 @@
+#pragma once
+//#include <array>
+#include <stdint.h>
+#include <string.h>
+#include <sys/types.h>
+#ifdef __TRUSTY__
+#include <lk/compiler.h>
+#else
+#define __PACKED __attribute__((packed))
+#endif
+
+#include <lib/binder/Binder.h>
+#include <lib/binder/Errors.h>
+
+namespace aidl {
+class ICastAuth {
+public:
+  virtual ~ICastAuth() {}
+  virtual int ProvisionKey(const ::trusty::aidl::Payload& req_payload) = 0;
+  virtual int SignHash(const ::trusty::aidl::Payload& req_payload, ::trusty::aidl::Payload* resp_payload) = 0;
+  static constexpr char PORT[] = "com.android.trusty.cast_auth";
+  enum : uint32_t {
+    REQ_SHIFT = 1,
+    RESP_BIT = 1,
+    CMD_ProvisionKey = (0 << REQ_SHIFT),
+    CMD_SignHash = (1 << REQ_SHIFT),
+  };
+  struct __PACKED Request_ProvisionKey {
+    static constexpr uint32_t num_handles = 0U;
+    void send_handles(::trusty::aidl::Handle*&) {
+    }
+    void recv_handles(::trusty::aidl::Handle*&) {
+    }
+  };
+  struct __PACKED Request_SignHash {
+    static constexpr uint32_t num_handles = 0U;
+    void send_handles(::trusty::aidl::Handle*&) {
+    }
+    void recv_handles(::trusty::aidl::Handle*&) {
+    }
+  };
+  virtual void destroy() {}
+};
+}  // namespace aidl
