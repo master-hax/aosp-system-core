9adf5de1d0547939b280ed88ca4a71a8dd936899
Akilesh Kailash <akailash@google.com>
2022-06-30 19:01:23 +0000

    Reduce priority of merge threads
    Currently, when daemon is spin up, it runs at the highest
    priority with nice value set to -20. This can potetially
    lead to a problem in a busy system especially after OTA
    reboot when all the merge threads are running in parallel.
    
    Now that we reduced the number of merge threads in-flight
    to 2, we reduce the priority as well by setting the nice
    value to -5. The other threads which serve I/O's
    from dm-user (from root filesystem) still runs at higher
    priority. We need this because post OTA reboot, these
    threads serve I/O's until merge is completed.
    
    Merge threads on the other hand can run at a relatively
    lower priority. We need to make sure that there
    is always forward progress even in a busy system
    and hence set the priority to -5 as compared
    to default value of 0.
    
    No boot time regressions observed.
    
    Output of NICE value of merge and worker threads post OTA reboot:
    
    1 S     0   427   451     1 0  39 -20  64 2314640 dev_r+ ?      00:00:00 8
    1 S     0   427   486     1 4  39 -20  64 2314640 dev_r+ ?      00:00:02 8
    1 S     0   427   487     1 4  39 -20  64 2314640 dev_r+ ?      00:00:02 8
    1 S     0   427   488     1 3  39 -20  64 2314640 dev_r+ ?      00:00:02 8
    5 R     0   427   634     1 1  24  -5  64 2314640 0    ?        00:00:00 8
    5 R     0   427   935     1 5  24  -5  64 2314640 0    ?        00:00:02 8
    
    Bug: 237490659
    Test: Full and incremental OTA
    Signed-off-by: Akilesh Kailash <akailash@google.com>
    Change-Id: I6791dd72ccd8cd5bba6eff663bb3f9598bce7ed2

--- fs_mgr/libsnapshot/snapuserd/user-space-merge/snapuserd_readahead.cpp
+++ fs_mgr/libsnapshot/snapuserd/user-space-merge/snapuserd_readahead.cpp
@@ -727,6 +727,10 @@ bool ReadAhead::RunThread() {
 
     InitializeIouring();
 
+    if (setpriority(PRIO_PROCESS, gettid(), kNiceValueForMergeThreads)) {
+        SNAP_PLOG(ERROR) << "Failed to priority for TID: " << gettid();
+    }
+
     while (!RAIterDone()) {
         if (!ReadAheadIOStart()) {
             break;
