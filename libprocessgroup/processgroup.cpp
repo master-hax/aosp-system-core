9536de37ebd5526e4d99ff3d3d4d92262c543ece
Bart Van Assche <bvanassche@google.com>
2022-08-02 13:18:12 -0700

    libprocessgroup: Provide SetProcessProfiles() and SetTaskProfiles() alternatives
    Provide alternative versions that do not force callers to create
    std::string objects. This patch has the intended side-effect that all
    callers that pass a {string} initializer list to the 'profiles' argument
    now call an std::initializer_list<> overload instead of the const
    std::vector<std::string>& overload.
    
    Bug: 213617178
    Change-Id: Ica61e944a66a17178ee43a113b8ca082f7eb834b
    Signed-off-by: Bart Van Assche <bvanassche@google.com>

--- libprocessgroup/processgroup.cpp
+++ libprocessgroup/processgroup.cpp
@@ -151,12 +151,36 @@ bool SetProcessProfiles(uid_t uid, pid_t pid, const std::vector<std::string>& pr
     return TaskProfiles::GetInstance().SetProcessProfiles(uid, pid, profiles, false);
 }
 
+bool SetProcessProfiles(uid_t uid, pid_t pid, std::initializer_list<const char*> profiles) {
+    if (profiles.size() <= 4) {
+        std::array<std::string_view, 4> profile_a;
+        std::copy(profiles.begin(), profiles.end(), profile_a.begin());
+        return TaskProfiles::GetInstance().SetProcessProfiles(
+                uid, pid, std::span(profile_a.begin(), profiles.size()), false);
+    }
+    std::vector<std::string_view> profile_v(profiles.size());
+    std::copy(profiles.begin(), profiles.end(), profile_v.begin());
+    return TaskProfiles::GetInstance().SetProcessProfiles(uid, pid, profile_v, false);
+}
+
 bool SetProcessProfilesCached(uid_t uid, pid_t pid, const std::vector<std::string>& profiles) {
     return TaskProfiles::GetInstance().SetProcessProfiles(uid, pid, profiles, true);
 }
 
 bool SetTaskProfiles(int tid, const std::vector<std::string>& profiles, bool use_fd_cache) {
     return TaskProfiles::GetInstance().SetTaskProfiles(tid, profiles, use_fd_cache);
+}
+
+bool SetTaskProfiles(int tid, std::initializer_list<const char*> profiles, bool use_fd_cache) {
+    if (profiles.size() <= 4) {
+        std::array<std::string_view, 4> profile_a;
+        std::copy(profiles.begin(), profiles.end(), profile_a.begin());
+        return TaskProfiles::GetInstance().SetTaskProfiles(
+                tid, std::span(profile_a.begin(), profiles.size()), use_fd_cache);
+    }
+    std::vector<std::string_view> profile_v(profiles.size());
+    std::copy(profiles.begin(), profiles.end(), profile_v.begin());
+    return TaskProfiles::GetInstance().SetTaskProfiles(tid, profile_v, use_fd_cache);
 }
 
 // C wrapper for SetProcessProfiles.
