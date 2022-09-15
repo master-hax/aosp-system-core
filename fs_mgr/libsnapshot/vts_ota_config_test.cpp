0b770a840db7b73dc3d040d909c22b9c57e8bc88
biandonglei <biandonglei@vivo.com.cn>
2022-09-15 14:40:39 +0800

    Some device already doesn't support A/B update, it looks virtual A/B is also not mandatory. In order to pass this test case on such devices, this test case should check ro.vendor.api_level first.
    test:
     run vts -m vts_ota_config_test -t VAB#Enabled
    
    issue:
     245441425
    
    Change-Id: Id8f7dac8c247ac7e95febf232655fc2cf27192e2

--- fs_mgr/libsnapshot/vts_ota_config_test.cpp
+++ fs_mgr/libsnapshot/vts_ota_config_test.cpp
@@ -22,9 +22,9 @@ static int GetVsrLevel() {
 }
 
 TEST(VAB, Enabled) {
-    ASSERT_TRUE(android::base::GetBoolProperty("ro.virtual_ab.enabled", false));
     if (GetVsrLevel() < __ANDROID_API_T__) {
         GTEST_SKIP();
     }
+    ASSERT_TRUE(android::base::GetBoolProperty("ro.virtual_ab.enabled", false));
     ASSERT_TRUE(android::base::GetBoolProperty("ro.virtual_ab.userspace.snapshots.enabled", false));
 }
