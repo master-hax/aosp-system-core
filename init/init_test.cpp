a5c37a79769d6eb4e8314dc5c75b2171a79f5979
Bart Van Assche <bvanassche@google.com>
2022-11-14 09:30:51 -0800

    init: Add a unit test for the "start console" action
    Treehugger starts Cuttlefish with the console disabled. Add a test that
    enables console. The purpose of this test is to trigger the code paths
    in Service::Start() that are unique to processes associated with a
    console.
    
    Bug: 213617178
    Change-Id: I834632ce6ec5c237c9c2c3f5b1aa7bc98c3ef260
    Signed-off-by: Bart Van Assche <bvanassche@google.com>

--- init/init_test.cpp
+++ init/init_test.cpp
@@ -193,6 +193,30 @@ service A something
     EXPECT_TRUE(service->is_override());
 }
 
+TEST(init, StartConsole) {
+    std::string init_script = R"init(
+service console /system/bin/sh
+    class core
+    console console
+    disabled
+    user root
+    group root shell log readproc
+    seclabel u:r:su:s0
+    setenv HOSTNAME console
+)init";
+
+    ActionManager action_manager;
+    ServiceList service_list;
+    TestInitText(init_script, BuiltinFunctionMap(), {}, &action_manager, &service_list);
+    ASSERT_EQ(std::distance(service_list.begin(), service_list.end()), 1);
+
+    auto service = service_list.begin()->get();
+    ASSERT_NE(service, nullptr);
+    service->Start();
+    EXPECT_EQ(getsid(service->pid()), service->pid());
+    service->Stop();
+}
+
 static std::string GetSecurityContext() {
     char* ctx;
     if (getcon(&ctx) == -1) {
