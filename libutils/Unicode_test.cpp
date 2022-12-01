f3455fdfdef33892580c6b2980314e548575fd24
Eric Miao <ericymiao@google.com>
2022-11-30 16:04:55 -0800

    libutils: Add more tests for Unicode
    This CL added additional tests for converting between utf16 and utf8,
    specifically tests that:
    
      1. check utf16_to_utf8_length() returns 0 if input is an
         empty UTF16 string
    
      2. check utf16_to_utf8_length() returns 1 if input is a
         single ASCII character UTF16 string
    
      3. check utf16_to_utf8_length() returns 3 if input is a
         single UTF-16 character between U+0800 - U+FFFF
    
      4. check utf16_to_utf8_length() returns 4 if input has
         a surrogate pair
    
      5. check unpaired UTF-16 surrogate is handled correctly
         (skipped)
    
      6. check utf16_to_utf8_length(0 handles invalid surrogate
         case correctly, by skipping the first but handling the
         rest correctly
    
      7. check a normal string with a mix of 1/2/3/4-byte UTF8
         characters is correctly converted by utf16_to_utf8()
    
      8. check conversion from invalid utf8 sequence with invalid
         leading byte and/or invalid trailing byte(s) should still
         work and not crash
    
    Change-Id: If68e514af0e84ddebf5900b2e140e76ba4f44553

--- libutils/Unicode_test.cpp
+++ libutils/Unicode_test.cpp
@@ -94,27 +94,151 @@ TEST_F(UnicodeTest, UTF8toUTF16TruncatedUTF8) {
 
 TEST_F(UnicodeTest, UTF8toUTF16Normal) {
     const uint8_t str[] = {
-        0x30, // U+0030, 1 UTF-16 character
-        0xC4, 0x80, // U+0100, 1 UTF-16 character
-        0xE2, 0x8C, 0xA3, // U+2323, 1 UTF-16 character
+        0x30,                   // U+0030, 1 UTF-16 character
+        0xC4, 0x80,             // U+0100, 1 UTF-16 character
+        0xE2, 0x8C, 0xA3,       // U+2323, 1 UTF-16 character
         0xF0, 0x90, 0x80, 0x80, // U+10000, 2 UTF-16 character
     };
 
     char16_t output[1 + 1 + 1 + 2 + 1];  // Room for null
 
-    utf8_to_utf16(str, sizeof(str), output, sizeof(output) / sizeof(output[0]));
+    utf8_to_utf16(str, sizeof(str), output, std::size(output));
 
-    EXPECT_EQ(0x0030, output[0])
-            << "should be U+0030";
-    EXPECT_EQ(0x0100, output[1])
-            << "should be U+0100";
-    EXPECT_EQ(0x2323, output[2])
-            << "should be U+2323";
-    EXPECT_EQ(0xD800, output[3])
-            << "should be first half of surrogate U+10000";
-    EXPECT_EQ(0xDC00, output[4])
-            << "should be second half of surrogate U+10000";
+    EXPECT_EQ(0x0030, output[0]) << "should be U+0030";
+    EXPECT_EQ(0x0100, output[1]) << "should be U+0100";
+    EXPECT_EQ(0x2323, output[2]) << "should be U+2323";
+    EXPECT_EQ(0xD800, output[3]) << "should be first half of surrogate U+10000";
+    EXPECT_EQ(0xDC00, output[4]) << "should be second half of surrogate U+10000";
     EXPECT_EQ(0, output[5]) << "should be null terminated";
+}
+
+TEST_F(UnicodeTest, UTF8toUTF16Invalid) {
+    const uint8_t str[] = {
+        0xf8,                   // invalid leading byte
+        0xc4, 0x00,             // U+0100 with invalid trailing byte
+        0xe2, 0x0c, 0x23,       // U+2323 with invalid trailing bytes
+        0xf0, 0x10, 0x00, 0x00, // U+10000 with invalid trailing bytes
+    };
+    char16_t output[1 + 1 + 1 + 2 + 1]; // Room for null
+
+    // invalid input shouldn't crash this test
+    utf8_to_utf16(str, sizeof(str), output, std::size(output));
+
+    // invalid leading byte will be ignored and an invalid UTF-16 character
+    // 0xFFFF will be produced in the output
+    EXPECT_EQ(0xFFFF, output[0]) << "should be U+FFFF";
+
+    // invalid trailing byte(s) will be ignored
+    EXPECT_EQ(0x0100, output[1]) << "should be U+0100";
+    EXPECT_EQ(0x2323, output[2]) << "should be U+2323";
+    EXPECT_EQ(0xD800, output[3]) << "should be first half of surrogate U+10000";
+    EXPECT_EQ(0xDC00, output[4]) << "should be second half of surrogate U+10000";
+    EXPECT_EQ(0, output[5]) << "should be null terminated";
+}
+
+TEST_F(UnicodeTest, UTF16toUTF8ZeroLength) {
+    ssize_t measured;
+
+    const char16_t str[] = {};
+    measured = utf16_to_utf8_length(str, 0);
+    EXPECT_EQ(0, measured)
+            << "Zero length input should return zero length output.";
+}
+
+TEST_F(UnicodeTest, UTF16toUTF8ASCIILength) {
+    ssize_t measured;
+
+    const char16_t str[] = { 0x0030 };
+
+    measured = utf16_to_utf8_length(str, 1);
+    EXPECT_EQ(1, measured)
+            << "ASCII glyphs in UTF16 should give a length of 1 in UTF8";
+}
+
+TEST_F(UnicodeTest, UTF16toUTF8Plane1Length) {
+    ssize_t measured;
+
+    // U+2323 SMILE
+    const char16_t str[] = { 0x2323 };
+
+    measured = utf16_to_utf8_length(str, 1);
+    EXPECT_EQ(3, measured)
+            << "Plane 1 glyphs should have a length of 3 char in UTF-8";
+}
+
+TEST_F(UnicodeTest, UTF16toUTF8SurrogateLength) {
+    ssize_t measured;
+
+    // U+10000
+    const char16_t str[] = { 0xd800, 0xdc00 };
+
+    measured = utf16_to_utf8_length(str, 2);
+    EXPECT_EQ(4, measured)
+            << "Surrogate pairs should have a length of 4 chars";
+}
+
+TEST_F(UnicodeTest, UTF16toUTF8UnpairedSurrogateLength) {
+    ssize_t measured;
+
+    // U+10000 (but with half surrogate pair)
+    const char16_t str[] = { 0xd800 };
+
+    // the half surrogate will be converted as a normal 3-byte UTF8
+    measured = utf16_to_utf8_length(str, 1);
+    EXPECT_EQ(0, measured)
+            << "A single unpaired surrogate should have a length of 0 chars";
+
+    // U+0030, U+0100, U+10000 (but with half surrogate pair), U+2323
+    const char16_t str2[] = { 0x0030, 0x0100, 0xd800, 0x2323 };
+    measured = utf16_to_utf8_length(str2, 4);
+    EXPECT_EQ(6, measured)
+            << "Unpaired surrogate should be skipped";
+}
+
+TEST_F(UnicodeTest, UTF16toUTF8CorrectInvalidSurrogateLength) {
+    ssize_t measured;
+
+    // http://b/29250543
+    // d841d8 is an invalid start for a surrogate pair. Make sure this is handled by ignoring the
+    // first character in the pair and handling the rest correctly.
+    const char16_t str[] = { 0xd841, 0xd841, 0xdc41 };
+
+    measured = utf16_to_utf8_length(str, 3);
+    EXPECT_EQ(4, measured)
+            << "Surrogate pairs should have a length of 4 chars";
+}
+
+TEST_F(UnicodeTest, UTF16toUTF8Normal) {
+    const char16_t str[] = {
+        0x0024, // U+0024 ($) --> 0x24,           1 UTF-8 character
+        0x00A3, // U+00A3 (£) --> 0xC2 0xA3,      2 UTF-8 characters
+        0x0939, // U+0939 (ह) --> 0xE0 0xA4 0xB9, 3 UTF-8 characters
+        0x20AC, // U+20AC (€) --> 0xE2 0x82 0xAC, 3 UTF-8 characters
+        0xD55C, // U+D55C (한)--> 0xED 0x95 0x9C, 3 UTF-8 characters
+        0xD801, 0xDC37, // U+10437 (𐐷) --> 0xF0 0x90 0x90 0xB7, 4 UTF-8 characters
+    };
+
+    char output[1 + 2 + 3 + 3 + 3 + 4 + 1];     // Room for null
+
+    utf16_to_utf8(str, 7, output, sizeof(output));
+
+    EXPECT_EQ('\x24', output[0]) << "should be 0x24";
+    EXPECT_EQ('\xC2', output[1]) << "should be 0xC2";
+    EXPECT_EQ('\xA3', output[2]) << "should be 0xA3";
+    EXPECT_EQ('\xE0', output[3]) << "should be 0xE0";
+    EXPECT_EQ('\xA4', output[4]) << "should be 0xA4";
+    EXPECT_EQ('\xB9', output[5]) << "should be 0xB9";
+    EXPECT_EQ('\xE2', output[6]) << "should be 0xE2";
+    EXPECT_EQ('\x82', output[7]) << "should be 0x82";
+    EXPECT_EQ('\xAC', output[8]) << "should be 0xAC";
+    EXPECT_EQ('\xED', output[9]) << "should be 0xED";
+    EXPECT_EQ('\x95', output[10]) << "should be 0x95";
+    EXPECT_EQ('\x9C', output[11]) << "should be 0x9C";
+    EXPECT_EQ('\xF0', output[12]) << "should be 0xF0";
+    EXPECT_EQ('\x90', output[13]) << "should be 0x90";
+    EXPECT_EQ('\x90', output[14]) << "should be 0x90";
+    EXPECT_EQ('\xB7', output[15]) << "should be 0xB7";
+    EXPECT_EQ('\x00', output[16]) << "should be 0x00";
 }
 
 TEST_F(UnicodeTest, strstr16EmptyTarget) {
