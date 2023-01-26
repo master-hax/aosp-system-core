a9381ef821073be96de04ed1fad1541ced7ab5ee
Daniel Zheng <zhengdaniel@google.com>
2023-01-26 00:45:00 +0000

    changed flash {partition_name} to work in a task oriented way.
    Test: tested flash {partition_name} on raven device.
    Change-Id: I5988b3bbca2f1bed6c4453ca7ff3996c39c490f4

--- fastboot/fastboot.cpp
+++ fastboot/fastboot.cpp
@@ -44,8 +44,12 @@
 #include <unistd.h>
 
 #include <chrono>
+#include <fstream>
 #include <functional>
+#include <iostream>
+#include <memory>
 #include <regex>
+#include <sstream>
 #include <string>
 #include <thread>
 #include <utility>
@@ -1406,6 +1410,42 @@ class ImageSource {
     virtual ~ImageSource(){};
     virtual bool ReadFile(const std::string& name, std::vector<char>* out) const = 0;
     virtual unique_fd OpenFile(const std::string& name) const = 0;
+};
+
+class Task {
+  public:
+    Task() = default;
+    virtual void Run() = 0;
+    virtual bool Parse(std::string& text) = 0;
+    virtual ~Task() = default;
+};
+
+class FlashTask : public Task {
+  public:
+    FlashTask(std::string& slot) : slot_(slot) {}
+    void Run() override {
+        auto flash = [&](const std::string& partition) {
+            do_flash(partition.c_str(), fname_.c_str());
+        };
+        do_for_partitions(pname_, slot_, flash, true);
+    }
+    bool Parse(std::string& text) override {
+        std::stringstream ss(text);
+        ss >> pname_;
+        if (!ss.eof()) {
+            ss >> fname_;
+        } else {
+            fname_ = find_item(pname_);
+            if (fname_.empty()) die("cannot determine image filename for '%s'", pname_.c_str());
+        }
+        return true;
+    }
+    ~FlashTask() {}
+
+  private:
+    std::string pname_;
+    std::string fname_;
+    std::string slot_;
 };
 
 class FlashAllTool {
