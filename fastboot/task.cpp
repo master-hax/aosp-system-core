eecff051c871dd0df8542b5fc5e1e3d881e8c489
Daniel Zheng <zhengdaniel@google.com>
2023-01-30 17:43:00 +0000

    Added support for reboot task
    Test: tested on raven device
    Change-Id: I4935d720f567e70da95ea8da37f3404b80b313c8
    Bug: 194686221
    
    Changed reboot {target} to work off tasks. reboot-{target} commands are
    also supported.
    
    Test: tested reboot on raven device
    Change-Id: I05aed269d121a5d651c1ab1180a1b4878ae213fd
    
    Modified load_buf to be able to find images in $OUT directory
    
    Test: tested flash {partition} and flash {partition} {img_name} on raven
    device
    
    Change-Id: I3879792d11ad15bc910670853d2a7fe200fcc66f

--- fastboot/task.cpp
+++ fastboot/task.cpp
@@ -14,8 +14,11 @@
 // limitations under the License.
 //
 #include "task.h"
+#include "fastboot.h"
+#include "util.h"
 
 FlashTask::FlashTask(std::string& _slot) : slot_(_slot){};
+
 FlashTask::FlashTask(std::string& _slot, bool& _force_flash)
     : slot_(_slot), force_flash_(_force_flash) {}
 
@@ -44,5 +47,39 @@ bool FlashTask::Parse(const std::string& text) {
         fname_ = find_item(pname_);
         if (fname_.empty()) die("cannot determine image filename for '%s'", pname_.c_str());
     }
+    return true;
+}
+RebootTask::RebootTask(fastboot::FastBootDriver* _fb) : fb_(_fb){};
+
+RebootTask::RebootTask(fastboot::FastBootDriver* _fb, std::string _reboot_target)
+    : reboot_target_(std::move(_reboot_target)), fb_(_fb){};
+
+void RebootTask::Run() {
+    if ((reboot_target_ == "userspace" || reboot_target_ == "fastboot")) {
+        if (!is_userspace_fastboot()) {
+            reboot_to_userspace_fastboot();
+            fb_->WaitForDisconnect();
+        }
+    } else if (reboot_target_ == "recovery") {
+        fb_->RebootTo("recovery");
+        fb_->WaitForDisconnect();
+    } else if (reboot_target_ == "bootloader") {
+        fb_->RebootTo("bootloader");
+        fb_->WaitForDisconnect();
+    } else if (reboot_target_ == "") {
+        fb_->Reboot();
+        fb_->WaitForDisconnect();
+    } else {
+        syntax_error("unknown reboot target %s", reboot_target_.c_str());
+    }
+}
+
+bool RebootTask::Parse(const std::string& text) {
+    std::stringstream ss(text);
+    if (!ss.eof()) {
+        ss >> reboot_target_;
+    }
+    // invalid arguments
+    if (!ss.eof()) return false;
     return true;
 }
