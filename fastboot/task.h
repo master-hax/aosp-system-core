59203c21e1b98ef25b008051145217317138a8d5
Daniel Zheng <zhengdaniel@google.com>
2023-01-31 21:07:53 +0000

    Added support for flash task
    Added Flash Task
    
    Test: tested on raven
    Change-Id: I4a6c22f795440afeba18497745dddf8d5c570f65
    Bug: 194686221
    
    changed flash {partition_name} to work with tasks
    
    Test: tested flash {partition_name} on raven
    Change-Id: I1fa45b949ad8e5017026dd542dfe1389279a9e64
    Bug: 194686221

--- fastboot/task.h
+++ fastboot/task.h
@@ -26,6 +26,10 @@
  * SUCH DAMAGE.
  */
 #pragma once
+#include <fastboot.h>
+#include <fastboot_driver.h>
+#include <util.h>
+#include <sstream>
 #include <string>
 
 class Task {
@@ -34,4 +38,44 @@ class Task {
     virtual void Run() = 0;
     virtual bool Parse(std::string& text) = 0;
     virtual ~Task() = default;
+};
+
+class FlashTask : public Task {
+  public:
+    FlashTask(std::string& _slot) : slot_(_slot) {}
+    FlashTask(std::string& _slot, bool& _force_flash) : slot_(_slot), force_flash_(_force_flash) {}
+
+    void Run() override {
+        auto flash = [&](const std::string& partition) {
+            if (should_flash_in_userspace(partition) && !is_userspace_fastboot() && !force_flash_) {
+                die("The partition you are trying to flash is dynamic, and "
+                    "should be flashed via fastbootd. Please run:\n"
+                    "\n"
+                    "    fastboot reboot fastboot\n"
+                    "\n"
+                    "And try again. If you are intentionally trying to "
+                    "overwrite a fixed partition, use --force.");
+            }
+            do_flash(partition.c_str(), fname_.c_str());
+        };
+        do_for_partitions(pname_, slot_, flash, true);
+    }
+    bool Parse(std::string& text) override {
+        std::stringstream ss(text);
+        ss >> pname_;
+        if (!ss.eof()) {
+            ss >> fname_;
+        } else {
+            fname_ = find_item(pname_);
+            if (fname_.empty()) die("cannot determine image filename for '%s'", pname_.c_str());
+        }
+        return true;
+    }
+    ~FlashTask() {}
+
+  private:
+    std::string pname_;
+    std::string fname_;
+    std::string slot_;
+    bool force_flash_ = false;
 };
