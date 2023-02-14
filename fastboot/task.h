8f446c699bf75ebf492930992c4a20ee66d91613
Daniel Zheng <zhengdaniel@google.com>
2023-02-14 17:44:40 +0000

    Added support for FlashSuperLayoutTask
    Test: testing flashall code with this change
    Bug: 194686221
    Change-Id: Ibb96980402db51b48c9a296338f29195f68c65bf

--- fastboot/task.h
+++ fastboot/task.h
@@ -20,6 +20,8 @@
 
 #include "fastboot.h"
 #include "fastboot_driver.h"
+#include "super_flash_helper.h"
+#include "util.h"
 
 class Task {
   public:
@@ -52,4 +54,18 @@ class RebootTask : public Task {
   private:
     const std::string reboot_target_ = "";
     FlashingPlan* fp_;
+};
+
+class FlashSuperLayoutTask : public Task {
+  public:
+    FlashSuperLayoutTask(const std::string& _super_name, SuperFlashHelper* _helper);
+    static std::unique_ptr<FlashSuperLayoutTask> Initialize(FlashingPlan* _fp,
+                                                            std::vector<ImageEntry>& os_images);
+    using ImageEntry = std::pair<const Image*, std::string>;
+    void Run() override;
+    ~FlashSuperLayoutTask(){};
+
+  private:
+    std::string super_name_;
+    SuperFlashHelper* helper_ = nullptr;
 };
