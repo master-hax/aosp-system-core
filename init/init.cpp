5e305f148ef57002ae77dff237b3735d39ee2a40
Bart Van Assche <bvanassche@google.com>
2023-02-17 12:33:36 -0800

    init: Test the shutdown state directly from the main loop
    Prepare for removing the UnSetExec() calls from EnterShutdown().
    
    Bug: 266255006
    Change-Id: I131f568dc0e9dcacaae2384208e7d9cb769540f8
    Signed-off-by: Bart Van Assche <bvanassche@google.com>

--- init/init.cpp
+++ init/init.cpp
@@ -1119,7 +1119,10 @@ int SecondStageMain(int argc, char** argv) {
             HandlePowerctlMessage(*shutdown_command);
         }
 
-        if (!(prop_waiter_state.MightBeWaiting() || Service::is_exec_service_running())) {
+        // Examine the pending actions if not waiting for a state transition and if no service is
+        // running or if shutting down.
+        if (IsShuttingDown() ||
+            !(prop_waiter_state.MightBeWaiting() || Service::is_exec_service_running())) {
             am.ExecuteOneCommand();
             // If there's more work to do, wake up again immediately.
             if (am.HasMoreCommands()) {
