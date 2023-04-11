574eec0958ed8acc5c6846adf89775f65af5a7b0
Daniel Zheng <zhengdaniel@google.com>
2023-04-11 09:09:35 -0700

    Added Mock function call tests
    Checking flash partition is called correct number of times
    
    Test: tested fastboot_test
    Change-Id: I8a04fb0e30e794641f3e2fbe2ab666274be18b85

--- fastboot/task_test.cpp
+++ fastboot/task_test.cpp
@@ -136,3 +136,19 @@ TEST_F(ParseTest, CORRECT_TASK_FORMED) {
     ASSERT_TRUE(tasks[3]->AsUpdateSuperTask());
     ASSERT_TRUE(tasks[4]->AsWipeTask());
 }
+
+TEST_F(ParseTest, CORRECT_CALLS) {
+    fastboot::MockFastbootDriver fb;
+    fp->fb = &fb;
+
+    EXPECT_CALL(fb, RebootTo(_, _, _)).Times(1);
+    EXPECT_CALL(fb, Reboot(_, _)).Times(1);
+    EXPECT_CALL(fb, WaitForDisconnect()).Times(2);
+
+    std::vector<std::string> commands = {"reboot bootloader", "reboot"};
+    std::vector<std::unique_ptr<Task>> tasks = collectTasks(fp.get(), commands);
+
+    for (auto& task : tasks) {
+        task->Run();
+    }
+}
