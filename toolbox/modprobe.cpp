aosp-mirror/platform_system_core

commit 83207784251c560809c06fe391362b312bfb3923

toolbox/modprobe: Fallback to /lib/modules/<uname>
Make the module directory optional by reading the kernel release
version. This path is where the kernel installs modules by default.
Similar behaviour can be found in several modprobe implementations.

Bug: 254835242
Change-Id: I61707636705e5b4d9bd8ccf6351e7057eae6bcf5

toolbox/modprobe.cpp
@@ -23,7 +23,10 @@
 #include <android-base/file.h>
 #include <android-base/logging.h>
 #include <android-base/strings.h>
+#include <android-base/stringprintf.h>
 #include <modprobe/modprobe.h>
+
+#include <sys/utsname.h>
 
 namespace {
 
@@ -37,9 +40,8 @@ enum modprobe_mode {
 void print_usage(void) {
     LOG(INFO) << "Usage:";
     LOG(INFO);
-    // -d option is required on Android
-    LOG(INFO) << "  modprobe [options] -d DIR [--all=FILE|MODULE]...";
-    LOG(INFO) << "  modprobe [options] -d DIR MODULE [symbol=value]...";
+    LOG(INFO) << "  modprobe [options] [-d DIR] [--all=FILE|MODULE]...";
+    LOG(INFO) << "  modprobe [options] [-d DIR] MODULE [symbol=value]...";
     LOG(INFO);
     LOG(INFO) << "Options:";
     LOG(INFO) << "  --all=FILE: FILE to acquire module names from";
@@ -187,6 +189,12 @@ extern "C" int modprobe_main(int argc, char** argv) {
                 module_parameters = module_parameters + " " + argv[opt];
             }
         }
+    }
+
+    if (mod_dirs.empty()) {
+        utsname uts;
+        uname(&uts);
+        mod_dirs.emplace_back(android::base::StringPrintf("/lib/modules/%s", uts.release));
     }
 
     LOG(DEBUG) << "mode is " << mode;

master-hax/aosp-system-core

commit 1c37c994e005ea59ff610354246700dd5e052dee

toolbox/modprobe: Fix fallback path when mod_dirs is empty
Due to GKI, the kernel UTS release string will not always (if ever)
match the vendor's UTS release string that is used to create the
initramfs file structure -- /lib/modules/<vendor uname>. This causes
module load failures when `-d DIR` is omitted. To fix this, we can
include all of the versions under /lib/modules that match the kernel's
major and minor version instead of directly using the value of uname().

Test: verified boot using GKI kernel + initramfs with different UTS strings
Fixes: 83207784251c ("toolbox/modprobe: Fallback to /lib/modules/<uname> ")
Bug: 282917063
Bug: 254835242
Change-Id: I5368f5cff139ba3165323a6a91066be38bfa0736
Signed-off-by: Will McVicker <willmcvicker@google.com>

toolbox/modprobe.cpp
@@ -85,6 +85,26 @@ void MyLogger(android::base::LogId id, android::base::LogSeverity severity, cons
     }
 }
 
+// Find directories in format of "/lib/modules/x.y*".
+static int KernelVersionNameFilter(const dirent* de) {
+    std::vector<std::string> lines;
+    std::string kernel_version;
+    utsname uts;
+
+    // Grab the kernel major version and minor version, e.g. 5.15
+    uname(&uts);
+    lines = android::base::Split(uts.release, ".");
+    if (lines.size() < 2) {
+        return 0;
+    }
+    kernel_version = std::string(lines[0] + "." + lines[1]);
+
+    if (android::base::StartsWith(de->d_name, kernel_version)) {
+        return 1;
+    }
+    return 0;
+}
+
 }  // anonymous namespace
 
 extern "C" int modprobe_main(int argc, char** argv) {
@@ -192,9 +212,19 @@ extern "C" int modprobe_main(int argc, char** argv) {
     }
 
     if (mod_dirs.empty()) {
-        utsname uts;
-        uname(&uts);
-        mod_dirs.emplace_back(android::base::StringPrintf("/lib/modules/%s", uts.release));
+        static constexpr auto LIB_MODULES_PREFIX = "/lib/modules/";
+        dirent** kernel_dirs = NULL;
+        int n;
+
+        n = scandir(LIB_MODULES_PREFIX, &kernel_dirs, KernelVersionNameFilter, NULL);
+        if (n == -1) {
+            LOG(ERROR) << "Failed to scan dir " << LIB_MODULES_PREFIX;
+        } else if (n > 0) {
+            while (n--) {
+                mod_dirs.emplace_back(LIB_MODULES_PREFIX + std::string(kernel_dirs[n]->d_name));
+            }
+        }
+        free(kernel_dirs);
     }
 
     LOG(DEBUG) << "mode is " << mode;