f1f74fd8bc74500ce0e8d6e4d94c6fa5dd53ce4b
Tze-nan, Wu <Tze-nan.Wu@mediatek.com>
2023-05-25 19:00:09 +0800

    Android Init: Fsync single file but not directory in WritePersistentPropertyFile
    The persist property setting might experience a timeout due to 'fsync()'.
    
    In-order to reduce the time spent on 'fsync()' in persist property write flow,
    Perform 'fsync()' on the persist property file itself rather than its parent folder.
    
    Bud: 284911737
    Signed-off-by: Tze-nan, Wu <Tze-nan.Wu@mediatek.com>
    Change-Id: Idf788313382d5efd91628a4bb143413cccce58c8

--- init/persistent_properties.cpp
+++ init/persistent_properties.cpp
@@ -211,12 +211,11 @@ Result<void> WritePersistentPropertyFile(const PersistentProperties& persistent_
     // directories must be fsync()'ed otherwise, the rename is not necessarily written to storage.
     // Note in this case, that the source and destination directories are the same, so only one
     // fsync() is required.
-    auto dir = Dirname(persistent_property_filename);
-    auto dir_fd = unique_fd{open(dir.c_str(), O_DIRECTORY | O_RDONLY | O_CLOEXEC)};
-    if (dir_fd < 0) {
-        return ErrnoError() << "Unable to open persistent properties directory for fsync()";
+    auto dist_fd = unique_fd{open(persistent_property_filename.c_str(), O_RDONLY | O_CLOEXEC)};
+    if (dist_fd < 0) {
+        return ErrnoError() << "Unable to open persistent properties file for fsync()";
     }
-    fsync(dir_fd.get());
+    fsync(dist_fd.get());
 
     return {};
 }
