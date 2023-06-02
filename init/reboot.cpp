eda6dc336115b666b5c05938dc7cd21c9b14067e
David Anderson <dvander@google.com>
2023-06-02 12:00:12 -0700

    init: Fix ramdump when enabling shutdown animations.
    Fix a bug where services weren't stopped properly if shutdown animations
    were enabled.
    
    Bug: 285241485
    Test: Pixel w/ ro.init.shutdown_animation=true
    Change-Id: I7f35572b5223f03f3f5a341fa7b5e90c01d56ce3

--- init/reboot.cpp
+++ init/reboot.cpp
@@ -680,14 +680,14 @@ static void DoReboot(unsigned int cmd, const std::string& reason, const std::str
                            << "': " << result.error();
             }
             s->SetShutdownCritical();
-        } else if (do_shutdown_animation) {
-            continue;
         } else if (s->IsShutdownCritical()) {
             // Start shutdown critical service if not started.
             if (auto result = s->Start(); !result.ok()) {
                 LOG(ERROR) << "Could not start shutdown critical service '" << s->name()
                            << "': " << result.error();
             }
+        } else if (do_shutdown_animation && s->classnames().count("animation") > 0) {
+            // Need these for shutdown animations.
         } else {
             stop_first.insert(s->name());
         }
