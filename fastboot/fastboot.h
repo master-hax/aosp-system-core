3e9975053488a8fdb7fa619677af41bdd1175be1
Daniel Zheng <zhengdaniel@google.com>
2023-06-05 13:23:57 -0700

    Adding test for comparison
    Adding test to compare task list formed from fastboot-info vs list
    formed from image list. Looks like we might need to manually compare
    since we need a device connected on fastboot to generate the task
    list
    
    Test: fastboot task-list-compare
    Bug: 194686221
    Change-Id: I5f60b7dd58fff211275db69c792a85b5fd48c18e

--- fastboot/fastboot.h
+++ fastboot/fastboot.h
@@ -109,6 +109,8 @@ class FlashAllTool {
     FlashAllTool(FlashingPlan* fp);
 
     void Flash();
+    // used for testing purposes
+    bool CompareTaskLists();
 
   private:
     void CheckRequirements();
@@ -117,6 +119,8 @@ class FlashAllTool {
     void AddFlashTasks(const std::vector<std::pair<const Image*, std::string>>& images,
                        std::vector<std::unique_ptr<Task>>& tasks);
     std::vector<std::unique_ptr<Task>> CollectTasksFromImageList();
+    std::vector<std::unique_ptr<Task>> CollecTasksFromFastbootInfo();
+
     std::vector<ImageEntry> boot_images_;
     std::vector<ImageEntry> os_images_;
     std::vector<std::unique_ptr<Task>> tasks_;
