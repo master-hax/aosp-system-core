16d1e3f9dac51eda273a0323603f1e4e2d4ab31e
Daniel Zheng <zhengdaniel@google.com>
2023-06-05 14:24:05 -0700

    Moving source class declaration
    Moving to fastboot.h to be used in task_test
    
    Test: m fastboot, fastboot flashall
    Bug: 194686221
    Change-Id: I9a70aca6c612ff9342515748728bbc511a06d298

--- fastboot/fastboot.cpp
+++ fastboot/fastboot.cpp
@@ -1893,16 +1893,6 @@ void FlashAllTool::AddFlashTasks(const std::vector<std::pair<const Image*, std::
     }
 }
 
-class ZipImageSource final : public ImageSource {
-  public:
-    explicit ZipImageSource(ZipArchiveHandle zip) : zip_(zip) {}
-    bool ReadFile(const std::string& name, std::vector<char>* out) const override;
-    unique_fd OpenFile(const std::string& name) const override;
-
-  private:
-    ZipArchiveHandle zip_;
-};
-
 bool ZipImageSource::ReadFile(const std::string& name, std::vector<char>* out) const {
     return UnzipToMemory(zip_, name, out);
 }
@@ -1925,12 +1915,6 @@ static void do_update(const char* filename, FlashingPlan* fp) {
 
     CloseArchive(zip);
 }
-
-class LocalImageSource final : public ImageSource {
-  public:
-    bool ReadFile(const std::string& name, std::vector<char>* out) const override;
-    unique_fd OpenFile(const std::string& name) const override;
-};
 
 bool LocalImageSource::ReadFile(const std::string& name, std::vector<char>* out) const {
     auto path = find_item_given_name(name);
