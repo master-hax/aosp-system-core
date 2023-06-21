823e654b72d4ce825405cf4040bdf2e561df1709
Daniel Zheng <zhengdaniel@google.com>
2023-06-21 14:41:11 -0700

    use FlashingPlan sparse_limit for testing
    Have get_sparse_limit() return FlashingPlan's sparse limit if set. Used
    for testing to compare task lists
    
    Test: fastboot_test
    Change-Id: I7ac1eb4dc83a53b3d7aa59aa668a780b4b550664

--- fastboot/task.h
+++ fastboot/task.h
@@ -82,7 +82,7 @@ class RebootTask : public Task {
 class OptimizedFlashSuperTask : public Task {
   public:
     OptimizedFlashSuperTask(const std::string& super_name, std::unique_ptr<SuperFlashHelper> helper,
-                            SparsePtr sparse_layout, uint64_t super_size);
+                            SparsePtr sparse_layout, uint64_t super_size, const FlashingPlan* fp);
     static std::unique_ptr<OptimizedFlashSuperTask> Initialize(const FlashingPlan* fp,
                                                                std::vector<ImageEntry>& os_images);
     static std::unique_ptr<OptimizedFlashSuperTask> InitializeFromTasks(
@@ -96,6 +96,7 @@ class OptimizedFlashSuperTask : public Task {
     std::unique_ptr<SuperFlashHelper> helper_;
     SparsePtr sparse_layout_;
     uint64_t super_size_;
+    const FlashingPlan* fp_;
 };
 
 class UpdateSuperTask : public Task {
