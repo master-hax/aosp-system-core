44fa87d2877c13b77abf1c5346ab13c0f3ab05b2
Daniel Zheng <zhengdaniel@google.com>
2023-06-21 14:49:52 -0700

    Adding parsing for fastboot-info
    Adding back the parsing for fastboot info
    
    Test: m fastboot, fastboot flashall
    Change-Id: I0075266bad5d45dcb99dbf91aa431008ca336216

--- fastboot/fastboot.h
+++ fastboot/fastboot.h
@@ -111,6 +111,8 @@ class FlashAllTool {
     FlashAllTool(FlashingPlan* fp);
 
     void Flash();
+    std::vector<std::unique_ptr<Task>> CollectTasks(bool use_fastboot_info = false,
+                                                    bool exclude_non_flash_tasks = false);
 
   private:
     void CheckRequirements();
@@ -118,7 +120,11 @@ class FlashAllTool {
     void CollectImages();
     void AddFlashTasks(const std::vector<std::pair<const Image*, std::string>>& images,
                        std::vector<std::unique_ptr<Task>>& tasks);
-    std::vector<std::unique_ptr<Task>> CollectTasksFromImageList();
+
+    std::vector<std::unique_ptr<Task>> CollectTasksFromFastbootInfo(
+            bool exclude_non_flash_tasks = false);
+    std::vector<std::unique_ptr<Task>> CollectTasksFromImageList(
+            bool exclude_non_flash_tasks = false);
 
     std::vector<ImageEntry> boot_images_;
     std::vector<ImageEntry> os_images_;
@@ -143,6 +149,7 @@ class LocalImageSource final : public ImageSource {
     unique_fd OpenFile(const std::string& name) const override;
 };
 
+char* get_android_product_out();
 bool should_flash_in_userspace(const std::string& partition_name);
 bool is_userspace_fastboot();
 void do_flash(const char* pname, const char* fname, const bool apply_vbmeta,
