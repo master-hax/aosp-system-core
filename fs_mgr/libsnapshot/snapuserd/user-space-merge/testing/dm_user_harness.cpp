af57b017ef6d56a6f80e832a726f159ffd013af7
David Anderson <dvander@google.com>
2023-06-28 12:55:36 -0700

    snapuserd: Add a harness to run tests without dm-user specific code.
    This patch adds an abstraction layer around Tempdevice (which wraps
    device-mapper), and around file descriptors, for calls to
    ReadFullyAtOffset. It also adds a layer to replace hardcoding of
    DmUserBlockServer.
    
    The only implementation of the new layer, currently, is for dm-user.
    However this will allow the harness to run with a backend chosen at
    runtime, making testing on the host or of ublk much easier.
    
    Bug: 288273605
    Test: snapuserd_test
    Change-Id: I8735ef6c373f3e5c5cdf3df461668ddd8e551f63

---
+++ fs_mgr/libsnapshot/snapuserd/user-space-merge/testing/dm_user_harness.cpp
@@ -0,0 +1,77 @@
+// Copyright (C) 2023 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "dm_user_harness.h"
+
+#include <fcntl.h>
+
+#include <android-base/file.h>
+#include <fs_mgr/file_wait.h>
+#include <libdm/dm.h>
+#include "../../block_server_dm_user.h"
+
+namespace android {
+namespace snapshot {
+
+using namespace std::chrono_literals;
+using android::base::unique_fd;
+
+DmUserDeviceFd::DmUserDeviceFd(unique_fd&& fd) : fd_(std::move(fd)) {}
+
+bool DmUserDeviceFd::ReadFullyAtOffset(void* buffer, size_t size, off_t offset) {
+    return android::base::ReadFullyAtOffset(fd_, buffer, size, offset);
+}
+
+DmUserDevice::DmUserDevice(std::unique_ptr<Tempdevice>&& dev) : dev_(std::move(dev)) {}
+
+std::unique_ptr<IUserDeviceFd> DmUserDevice::OpenFd() {
+    unique_fd fd(open(dev_->path().c_str(), O_RDONLY | O_CLOEXEC));
+    if (fd < 0) {
+        return nullptr;
+    }
+    return std::make_unique<DmUserDeviceFd>(std::move(fd));
+}
+
+bool DmUserDevice::Destroy() {
+    return dev_->Destroy();
+}
+
+DmUserTestHarness::DmUserTestHarness() {
+    block_server_factory_ = std::make_unique<DmUserBlockServerFactory>();
+}
+
+std::unique_ptr<IUserDevice> DmUserTestHarness::CreateDevice(const std::string& dev_name,
+                                                             const std::string& misc_name,
+                                                             uint64_t num_sectors) {
+    android::dm::DmTable dmuser_table;
+    dmuser_table.Emplace<android::dm::DmTargetUser>(0, num_sectors, misc_name);
+    auto dev = std::make_unique<Tempdevice>(dev_name, dmuser_table);
+    if (!dev->valid()) {
+        return nullptr;
+    }
+
+    auto misc_device = "/dev/dm-user/" + misc_name;
+    if (!android::fs_mgr::WaitForFile(misc_device, 10s)) {
+        return nullptr;
+    }
+
+    return std::make_unique<DmUserDevice>(std::move(dev));
+}
+
+IBlockServerFactory* DmUserTestHarness::GetBlockServerFactory() {
+    return block_server_factory_.get();
+}
+
+}  // namespace snapshot
+}  // namespace android
