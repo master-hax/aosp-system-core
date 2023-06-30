f69fad837104d4e3578178fb74665077a8f7e70a
Snehal <snehalreddy@google.com>
2023-06-30 14:49:48 +0000

    Coverage library on the NS side for the coverage controller
    Bug: 289520358
    
    Change-Id: I6c2c0e5db9e8d9a0edec93d2fb123d185c7c4416

---
+++ trusty/line-coverage/include/trusty/line-coverage/uuid.h
@@ -0,0 +1,26 @@
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <stdint.h>
+
+struct uuid {
+    uint32_t time_low;
+    uint16_t time_mid;
+    uint16_t time_hi_and_version;
+    uint8_t clock_seq_and_node[8];
+};
