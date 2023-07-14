0fce3aff841c4e4b664e58984ed4245753c3c908
Kalesh Singh <kaleshsingh@google.com>
2023-07-14 14:29:42 -0700

    16KB: Reduce x86 mmap rand bits
    Change-Id: I9efd3bceff97b9372f803cc503b5d2a213b2be75
    Signed-off-by: Kalesh Singh <kaleshsingh@google.com>

--- init/security.cpp
+++ init/security.cpp
@@ -126,7 +126,7 @@ Result<void> SetMmapRndBitsAction(const BuiltinArguments&) {
 #elif defined(__x86_64__)
     // x86_64 supports 28 - 32 rnd bits, but Android wants to ensure that the
     // theoretical maximum of 32 bits is always supported and used.
-    if (SetMmapRndBitsMin(32, 32, false) && (!Has32BitAbi() || SetMmapRndBitsMin(16, 16, true))) {
+    if (SetMmapRndBitsMin(30, 30, false) && (!Has32BitAbi() || SetMmapRndBitsMin(14, 14, true))) {
         return {};
     }
 #elif defined(__arm__) || defined(__i386__)
