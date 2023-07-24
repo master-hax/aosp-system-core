06944fafa5937f896dce48ab0987cbea97408abf
Yi-Yo Chiang <yochiang@google.com>
2023-07-24 20:03:01 +0800

    fs_mgr: Split libfs_mgr and libfstab
    The goal is to make the header definitions of the two curiously
    intertwined libraries less chaotic.
    
    After this change, libfstab's header would be self contained. In the
    sense that all symbols exported by its headers are defined in its
    compilation units.
    libfs_mgr would still embed libfstab like before, it can use internal
    symbols (not exported by public headers) of libfstab through the
    libfstab/boot_config.h private header.
    
    Keep include_fstab as a symbolic link pointing to its new location.
    This is a temporary workaround as there are still some bad build rules
    (incorrectly) depending on the old include path with Android.bp
    `include_dirs` directive.
    
    Test: build
    Change-Id: Ib70a84984ac2cbfca5f5b27fadebf6a16e58146a

--- fs_mgr/libfstab/boot_config.h
+++ fs_mgr/libfstab/boot_config.h
@@ -14,10 +14,8 @@
  * limitations under the License.
  */
 
-#ifndef __CORE_FS_MGR_PRIV_BOOTCONFIG_H
-#define __CORE_FS_MGR_PRIV_BOOTCONFIG_H
+#pragma once
 
-#include <sys/cdefs.h>
 #include <string>
 #include <utility>
 #include <vector>
@@ -33,5 +31,3 @@ std::vector<std::pair<std::string, std::string>> fs_mgr_parse_proc_bootconfig(
 bool fs_mgr_get_boot_config_from_bootconfig(const std::string& bootconfig, const std::string& key,
                                             std::string* out_val);
 bool fs_mgr_get_boot_config_from_bootconfig_source(const std::string& key, std::string* out_val);
-
-#endif /* __CORE_FS_MGR_PRIV_BOOTCONFIG_H */
---
+++ fs_mgr/libfstab/fstab_priv.h
@@ -0,0 +1,63 @@
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <string>
+
+#include <android-base/logging.h>
+#include <fstab/fstab.h>
+
+#include "boot_config.h"
+
+#define FSTAB_TAG "[libfstab] "
+
+/* The CHECK() in logging.h will use program invocation name as the tag.
+ * Thus, the log will have prefix "init: " when libfs_mgr is statically
+ * linked in the init process. This might be opaque when debugging.
+ * Append a library name tag at the end of the abort message to aid debugging.
+ */
+#define FSTAB_CHECK(x) CHECK(x) << "in " << FSTAB_TAG
+
+// Logs a message to kernel
+#define LINFO LOG(INFO) << FSTAB_TAG
+#define LWARNING LOG(WARNING) << FSTAB_TAG
+#define LERROR LOG(ERROR) << FSTAB_TAG
+#define LFATAL LOG(FATAL) << FSTAB_TAG
+
+// Logs a message with strerror(errno) at the end
+#define PINFO PLOG(INFO) << FSTAB_TAG
+#define PWARNING PLOG(WARNING) << FSTAB_TAG
+#define PERROR PLOG(ERROR) << FSTAB_TAG
+#define PFATAL PLOG(FATAL) << FSTAB_TAG
+
+bool fs_mgr_update_for_slotselect(android::fs_mgr::Fstab* fstab);
+const std::string& get_android_dt_dir();
+bool is_dt_compatible();
+
+namespace android {
+namespace fs_mgr {
+
+bool InRecovery();
+
+// Exported for testability.
+std::string GetFstabPath();
+bool ParseFstabFromString(const std::string& fstab_str, bool proc_mounts, Fstab* fstab_out);
+bool SkipMountWithConfig(const std::string& skip_config, Fstab* fstab, bool verbose);
+// Exported for testability.
+
+}  // namespace fs_mgr
+}  // namespace android
