929f7467802a753303b661904d4ac214ea3b3342
Daniel Zheng <zhengdaniel@google.com>
2023-09-12 13:34:07 -0700

    Adding block sz to compressors
    Adding block sz to compressor classes to prepare for variable block size
    compression
    
    Test: m libsnapshot
    Change-Id: I84db20c80c0f95188f79ccc73b5c30678bd75e78

--- fs_mgr/libsnapshot/include/libsnapshot/cow_compress.h
+++ fs_mgr/libsnapshot/include/libsnapshot/cow_compress.h
@@ -24,24 +24,26 @@ namespace snapshot {
 
 class ICompressor {
   public:
-    explicit ICompressor(uint32_t compression_level) : compression_level_(compression_level) {}
+    explicit ICompressor(uint32_t compression_level, uint32_t compression_block_sz)
+        : compression_level_(compression_level), compression_block_sz_(compression_block_sz) {}
 
     virtual ~ICompressor() {}
     // Factory methods for compression methods.
-    static std::unique_ptr<ICompressor> Gz(uint32_t compression_level);
-    static std::unique_ptr<ICompressor> Brotli(uint32_t compression_level);
-    static std::unique_ptr<ICompressor> Lz4(uint32_t compression_level);
+    static std::unique_ptr<ICompressor> Gz(uint32_t compression_level, const int32_t BLOCK_SZ);
+    static std::unique_ptr<ICompressor> Brotli(uint32_t compression_level, const int32_t BLOCK_SZ);
+    static std::unique_ptr<ICompressor> Lz4(uint32_t compression_level, const int32_t BLOCK_SZ);
     static std::unique_ptr<ICompressor> Zstd(uint32_t compression_level, const int32_t BLOCK_SZ);
 
     static std::unique_ptr<ICompressor> Create(CowCompression compression, const int32_t BLOCK_SZ);
 
     uint32_t GetCompressionLevel() const { return compression_level_; }
-
+    uint8_t GetMaxCompressionSize() const { return compression_block_sz_; }
     [[nodiscard]] virtual std::basic_string<uint8_t> Compress(const void* data,
                                                               size_t length) const = 0;
 
   private:
     uint32_t compression_level_;
+    uint32_t compression_block_sz_;
 };
 }  // namespace snapshot
 }  // namespace android
