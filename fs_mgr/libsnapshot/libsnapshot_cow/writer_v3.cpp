8a391e124a5af9118c57b28546b07f13e2cc77c5
Daniel Zheng <zhengdaniel@google.com>
2023-09-14 16:50:47 -0700

    Add GetCowSize
    CowSize in our new format will be calculated the same as before but
    without the footer.
    
    Test: m libsnapshot
    Change-Id: I24b0048665d0c0c338495cda5782b1b929056a9b

--- fs_mgr/libsnapshot/libsnapshot_cow/writer_v3.cpp
+++ fs_mgr/libsnapshot/libsnapshot_cow/writer_v3.cpp
@@ -139,9 +139,11 @@ bool CowWriterV3::Finalize() {
 }
 
 uint64_t CowWriterV3::GetCowSize() {
-    LOG(ERROR) << __LINE__ << " " << __FILE__
-               << " <- Get Cow Size function here should never be called";
-    return 0;
+    if (current_data_size_ > 0) {
+        return next_data_pos_;
+    } else {
+        return next_op_pos_;
+    }
 }
 
 bool CowWriterV3::GetDataPos(uint64_t* pos) {
