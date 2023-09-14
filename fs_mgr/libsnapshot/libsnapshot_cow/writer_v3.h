e3740f4385504b197e7a1463e5aaffe512b7352a
Daniel Zheng <zhengdaniel@google.com>
2023-09-14 16:50:47 -0700

    Add GetCowSize
    CowSize in our new format will be calculated the same as before but
    without the footer.
    
    Test: m libsnapshot
    Change-Id: I24b0048665d0c0c338495cda5782b1b929056a9b

--- fs_mgr/libsnapshot/libsnapshot_cow/writer_v3.h
+++ fs_mgr/libsnapshot/libsnapshot_cow/writer_v3.h
@@ -51,6 +51,10 @@ class CowWriterV3 : public CowWriterBase {
 
     uint64_t next_op_pos_ = 0;
     uint64_t next_data_pos_ = 0;
+    uint64_t current_data_pos_ = 0;
+    uint64_t current_data_size_ = 0;
+    uint64_t current_op_pos_ = 0;
+    ssize_t total_data_written_ = 0;
 
     int num_compress_threads_ = 1;
 };
