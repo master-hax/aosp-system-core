7b58ac9d427a118c9527c5740d68737e5f2e052c
Daniel Zheng <zhengdaniel@google.com>
2023-10-05 14:01:56 -0700

    Shove type into source_info
    We can shove type into source info to save 8 bits in per cow operation.
    We only need 4 bits inside of source_info to enumerate all the types of
    Cow Operation:
    
    static constexpr uint8_t kCowCopyOp = 1;
    static constexpr uint8_t kCowReplaceOp = 2;
    static constexpr uint8_t kCowZeroOp = 3;
    static constexpr uint8_t kCowLabelOp = 4;
    static constexpr uint8_t kCowClusterOp = 5;
    static constexpr uint8_t kCowXorOp = 6;
    static constexpr uint8_t kCowSequenceOp = 7;
    static constexpr uint8_t kCowFooterOp = -1;
    
    Test: critical ota paths on pixel
    Change-Id: I22049db0b39a55bd8f863339f3751d3146d5c1e9

--- fs_mgr/libsnapshot/libsnapshot_cow/cow_format.cpp
+++ fs_mgr/libsnapshot/libsnapshot_cow/cow_format.cpp
@@ -82,8 +82,9 @@ std::ostream& operator<<(std::ostream& os, CowOperationV2 const& op) {
 
 std::ostream& operator<<(std::ostream& os, CowOperation const& op) {
     os << "CowOperation(";
-    EmitCowTypeString(os, op.type);
-    if (op.type == kCowReplaceOp || op.type == kCowXorOp || op.type == kCowSequenceOp) {
+    EmitCowTypeString(os, GetCowOpSourceInfoType(&op));
+    if (GetCowOpSourceInfoType(&op) == kCowReplaceOp || GetCowOpSourceInfoType(&op) == kCowXorOp ||
+        GetCowOpSourceInfoType(&op) == kCowSequenceOp) {
         if (op.source_info & (op.data_length < BLOCK_SZ)) {
             os << ", compressed";
         } else {
@@ -91,12 +92,15 @@ std::ostream& operator<<(std::ostream& os, CowOperation const& op) {
         }
         os << ", data_length:" << op.data_length;
     }
-    if (op.type != kCowClusterOp && op.type != kCowSequenceOp && op.type != kCowLabelOp) {
+    if (GetCowOpSourceInfoType(&op) != kCowClusterOp &&
+        GetCowOpSourceInfoType(&op) != kCowSequenceOp &&
+        GetCowOpSourceInfoType(&op) != kCowLabelOp) {
         os << ", new_block:" << op.new_block;
     }
-    if (op.type == kCowXorOp || op.type == kCowReplaceOp || op.type == kCowCopyOp) {
+    if (GetCowOpSourceInfoType(&op) == kCowXorOp || GetCowOpSourceInfoType(&op) == kCowReplaceOp ||
+        GetCowOpSourceInfoType(&op) == kCowCopyOp) {
         os << ", source:" << (op.source_info & kCowOpSourceInfoDataMask);
-    } else if (op.type == kCowClusterOp) {
+    } else if (GetCowOpSourceInfoType(&op) == kCowClusterOp) {
         os << ", cluster_data:" << (op.source_info & kCowOpSourceInfoDataMask);
     } else {
         os << ", label:0x" << android::base::StringPrintf("%" PRIx64, op.source_info);
@@ -126,7 +130,7 @@ int64_t GetNextDataOffset(const CowOperationV2& op, uint32_t cluster_ops) {
 }
 
 bool IsMetadataOp(const CowOperation& op) {
-    switch (op.type) {
+    switch (GetCowOpSourceInfoType(&op)) {
         case kCowLabelOp:
         case kCowClusterOp:
         case kCowFooterOp:
@@ -138,7 +142,7 @@ bool IsMetadataOp(const CowOperation& op) {
 }
 
 bool IsOrderedOp(const CowOperation& op) {
-    switch (op.type) {
+    switch (GetCowOpSourceInfoType(&op)) {
         case kCowCopyOp:
         case kCowXorOp:
             return true;
