c5cf5be3a4d9b3f81fd8a7ecfcfc0681cad101ba
Kiyoung Kim <kiyoungkim@google.com>
2023-11-03 16:00:13 +0900

    Use /proc/self/cmdline to check if process is from vendor
    Current llndk function android_is_in_vendor_process checks if the
    process is vendor process by trying to open vndk namespace as the
    namespace is shared only from system (and unrestricted) section.
    However, this would no longer work along with VNDK deprecation. To keep
    this llndk function work, use a different logic - check
    /proc/self/cmdline and /linkerconfig/system.apexes.txt to find out of
    the process is from system or vendor (and other else).
    
    Bug: 300366609
    Test: Function worked as expected with binary from APEX and atest
    Change-Id: Id1348b51d07a35aa2aa0ea1149dbd2bcc156a65e

--- libvndksupport/linker.cpp
+++ libvndksupport/linker.cpp
@@ -18,13 +18,17 @@
 
 #include "linker.h"
 
+#include <android-base/file.h>
+#include <android-base/strings.h>
 #include <android/dlext.h>
 #include <dlfcn.h>
 #include <log/log.h>
 #include <sys/types.h>
 #include <unistd.h>
 
+#include <fstream>
 #include <initializer_list>
+#include <string>
 
 extern "C" android_namespace_t* android_get_exported_namespace(const char*);
 
@@ -35,6 +39,73 @@ struct VendorNamespace {
     const char* name = nullptr;
 };
 
+int IsInVendorProcess() {
+    // Special case init
+    if (getpid() == 1) {
+        return 0;
+    }
+
+    std::string argv;
+    if (!android::base::ReadFileToString("/proc/self/cmdline", &argv)) {
+        // If it fails to read /proc/self/cmdline, assume as vendor process.
+        return 1;
+    }
+
+    // Check if APEX is in system if cmdline starts with /apex/
+    if (android::base::StartsWith(argv, "/apex/")) {
+        std::string apex_path = argv.substr(6);
+        std::ifstream system_apexes("/linkerconfig/system.apexes.txt", std::ifstream::in);
+        if (!system_apexes) {
+            ALOGE("IsInVendorProcess : failed to read /linkerconfig/system.apexes.txt");
+            return 1;
+        }
+
+        std::string system_apex;
+        while (std::getline(system_apexes, system_apex)) {
+            if (android::base::StartsWith(apex_path, system_apex)) {
+                return 0;
+            }
+        }
+
+        return 1;
+    }
+
+    // Read dir - section map from ld.config.txt
+    std::ifstream linker_config("/linkerconfig/ld.config.txt", std::ifstream::in);
+    if (!linker_config) {
+        ALOGE("IsInVendorProcess : failed to read /linkerconfig/ld.config.txt");
+        return 1;
+    }
+
+    std::string config_line;
+    while (std::getline(linker_config, config_line)) {
+        size_t found = config_line.find('#');
+        config_line = android::base::Trim(config_line.substr(0, found));
+
+        if (config_line.empty()) {
+            continue;
+        }
+
+        if (config_line[0] == '[') {
+            // Section starts. Stop reading ld.config.txt.
+            break;
+        }
+
+        size_t found_assign = config_line.find('=');
+
+        if (found_assign != std::string::npos) {
+            std::string section = android::base::Trim(config_line.substr(0, found_assign));
+            std::string path_prefix = android::base::Trim(config_line.substr(found_assign + 1));
+
+            if (android::base::StartsWith(argv, path_prefix)) {
+                return (section == "dir.system" || section == "dir.unrestricted") ? 0 : 1;
+            }
+        }
+    }
+
+    // Any of match failed. Assume as vendor process.
+    return 1;
+}
 }  // anonymous namespace
 
 static VendorNamespace get_vendor_namespace() {
@@ -50,15 +121,8 @@ static VendorNamespace get_vendor_namespace() {
 }
 
 int android_is_in_vendor_process() {
-    // Special case init, since when init runs, ld.config.<ver>.txt hasn't been
-    // loaded (sysprop service isn't up for init to know <ver>).
-    if (getpid() == 1) {
-        return 0;
-    }
-
-    // In vendor process, 'vndk' namespace is not visible, whereas in system
-    // process, it is.
-    return android_get_exported_namespace("vndk") == nullptr;
+    static bool is_in_vendor_process = IsInVendorProcess();
+    return is_in_vendor_process;
 }
 
 void* android_load_sphal_library(const char* name, int flag) {
