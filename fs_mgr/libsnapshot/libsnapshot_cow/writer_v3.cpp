8c81864af5a2630caf90346eee63617047ce35c1
Daniel Zheng <zhengdaniel@google.com>
2023-11-09 12:19:11 -0800

    prototypelibsnapshot: turn on v3 cow
    DO NOT MERGE: more testing is needed. this is just for testing
    enable v3 cow
    
    Test: ota
    Change-Id: I815e8efa4f1b232ca0e7f9f73f0dbab96e1f679a

--- fs_mgr/libsnapshot/libsnapshot_cow/writer_v3.cpp
+++ fs_mgr/libsnapshot/libsnapshot_cow/writer_v3.cpp
@@ -80,7 +80,7 @@ void CowWriterV3::SetupHeaders() {
     header_.sequence_buffer_offset = 0;
     header_.resume_buffer_size = kNumResumePoints;
     header_.op_count = 0;
-    header_.op_count_max = 0;
+    header_.op_count_max = 10000;
     header_.compression_algorithm = kCowCompressNone;
     return;
 }
@@ -181,6 +181,7 @@ bool CowWriterV3::OpenForAppend(uint64_t label) {
     }
 
     header_ = header_v3;
+    CHECK(header_.op_count_max == 100000);
 
     CHECK(label >= 0);
     CowParserV3 parser;
@@ -265,7 +266,6 @@ bool CowWriterV3::EmitBlocks(uint64_t new_block_start, const void* data, size_t 
             return false;
         }
     }
-
     return true;
 }
 
@@ -338,7 +338,6 @@ bool CowWriterV3::WriteOperation(const CowOperationV3& op, const void* data, siz
         next_data_pos_ += op.data_length;
         return true;
     }
-
     if (header_.op_count + 1 > header_.op_count_max) {
         LOG(ERROR) << "Maximum number of ops reached: " << header_.op_count_max;
         return false;
