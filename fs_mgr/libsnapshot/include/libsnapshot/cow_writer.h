d72dc36ed0da9e6bea8106e75751cc7f8c254eb1
Daniel Zheng <zhengdaniel@google.com>
2023-11-09 12:19:11 -0800

    libsnapshot: turn on v3 cow
    enable v3 cow
    
    Test: ota
    Change-Id: I815e8efa4f1b232ca0e7f9f73f0dbab96e1f679a

--- fs_mgr/libsnapshot/include/libsnapshot/cow_writer.h
+++ fs_mgr/libsnapshot/include/libsnapshot/cow_writer.h
@@ -56,7 +56,7 @@ struct CowOptions {
     bool batch_write = false;
 
     // Size of the cow operation buffer; used in v3 only.
-    uint32_t op_count_max = 0;
+    uint32_t op_count_max = 500000;
 };
 
 // Interface for writing to a snapuserd COW. All operations are ordered; merges
