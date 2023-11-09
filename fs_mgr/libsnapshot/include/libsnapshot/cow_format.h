9052f4a6188aaddfa024d759e267d1efe31de650
Daniel Zheng <zhengdaniel@google.com>
2023-11-09 11:14:07 -0800

    libsnapshot: add sequence data
    v3 writer to write sequence data. Sequence data will be written after
    the scratch space and before the resume space. Since this is just a list
    of integers, writing and reading should be trivial
    
    Test: cow_api_test
    Change-Id: If3b6b1cfa155aeb65bf693263fc373154ba8e81d

--- fs_mgr/libsnapshot/include/libsnapshot/cow_format.h
+++ fs_mgr/libsnapshot/include/libsnapshot/cow_format.h
@@ -105,8 +105,8 @@ struct ResumePoint {
 static constexpr uint8_t kNumResumePoints = 4;
 
 struct CowHeaderV3 : public CowHeader {
-    // Location of sequence buffer in COW.
-    uint64_t sequence_buffer_offset;
+    // number of sequence points in sequence buffer
+    uint64_t sequence_buffer_size;
     // number of currently written resume points
     uint32_t resume_point_count;
     // Size, in bytes, of the CowResumePoint buffer.
@@ -232,17 +232,23 @@ static inline uint64_t GetCowOpSourceInfoData(const CowOperation& op) {
     return op.source_info & kCowOpSourceInfoDataMask;
 }
 
+static inline off_t GetSequenceOffset(const CowHeaderV3 header) {
+    return header.prefix.header_size + header.buffer_size;
+}
+
+static inline off_t GetResumeOffset(const CowHeaderV3 header) {
+    return header.prefix.header_size + header.buffer_size + header.sequence_buffer_size;
+}
+
 static inline off_t GetOpOffset(uint32_t op_index, const CowHeaderV3 header) {
-    return header.prefix.header_size + header.buffer_size +
+    return header.prefix.header_size + header.buffer_size + header.sequence_buffer_size +
            (header.resume_point_max * sizeof(ResumePoint)) + (op_index * sizeof(CowOperationV3));
 }
+
 static inline off_t GetDataOffset(const CowHeaderV3 header) {
-    return header.prefix.header_size + header.buffer_size +
+    return header.prefix.header_size + header.buffer_size + header.sequence_buffer_size +
            (header.resume_point_max * sizeof(ResumePoint)) +
            header.op_count_max * sizeof(CowOperation);
-}
-static inline off_t GetResumeOffset(const CowHeaderV3 header) {
-    return header.prefix.header_size + header.buffer_size;
 }
 
 struct CowFooter {
