101f99e4ba91d297623b78c8f8c948b64b52916b
Mohamad Mahmoud <mohamadmahmoud@google.com>
2023-12-04 11:08:41 +0000

    Add libdebugstore for Future Use by ART and Frameworks
    Add libdebugstore, designed for integration with ART and frameworks. The primary purpose of it is to provide an internal API that allows in-process storage of specific events, enabling these processes to be dumped into the ANR (Application Not Responding) dump file.
    
    Bug: 314735374
    Test: atest && Tested manually
    Change-Id: I40b7e4e251248c08ac5beb4bbe83add531e40fdc

---
+++ debugstore/include/debugstore/debug_store.h
@@ -0,0 +1,63 @@
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef _ANDROID_DEBUGSTORE_H
+#define _ANDROID_DEBUGSTORE_H
+
+#include <atomic>
+#include <chrono>
+#include <map>
+#include "debug_store_events.h"
+#include "debug_store_sotrage.h"
+
+namespace android {
+class DebugStore {
+  public:
+    static constexpr size_t DEFAULT_EVENT_LIMIT = 32;
+    static constexpr uint64_t MAX_DELAY_MS = 20;
+    static constexpr uint64_t NON_CLOSABLE_ID = 0;
+
+    static DebugStore* Create(size_t event_limit = DEFAULT_EVENT_LIMIT,
+                              uint64_t max_delay_ms = MAX_DELAY_MS);
+    static bool IsEnabled();
+    static DebugStore* GetInstance();
+
+    DebugStore(const DebugStore&) = delete;
+    DebugStore& operator=(const DebugStore&) = delete;
+    uint64_t begin(const std::string& name,
+                   const std::vector<DebugStoreEvent::KeyValuePair>& attributes =
+                           std::vector<DebugStoreEvent::KeyValuePair>());
+    void record(const std::string& name,
+                const std::vector<DebugStoreEvent::KeyValuePair>& attributes =
+                        std::vector<DebugStoreEvent::KeyValuePair>());
+    void end(uint64_t id, const std::vector<DebugStoreEvent::KeyValuePair>& attributes =
+                                  std::vector<DebugStoreEvent::KeyValuePair>());
+
+    std::string to_string() const;
+
+  private:
+    DebugStore(size_t event_limit, uint64_t max_delay_ms);
+
+    uint64_t generate_id();
+    static std::atomic<DebugStore*> instance;
+
+    std::atomic<uint64_t> id_generator;
+    std::atomic<uint64_t> total_entries;
+
+    DebugStoreStorage<DebugStoreEvent> event_store;
+};
+}  // namespace android
+#endif  // _ANDROID_DEBUGSTORE_H
