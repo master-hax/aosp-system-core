5a9e3c92d51fb5386e371d89bbd3666938dae17e
David Anderson <dvander@google.com>
2023-12-12 11:25:40 -0800

    remount: Detect when flashall has happened in the bootloader.
    This adds a new metadata header flag to the super partition. This flag
    is set when "adb remount" is used, and is implicitly cleared when
    flashing.
    
    If there is a scratch partition present on /data, we require that the
    flag be set in order to proceed using overlays. If not set, scratch is
    not mapped in first-stage init, and scratch images are removed later
    during startup.
    
    Bug: 297923468
    Test: adb remount -R, touch file in out/, sync, flashall
    Change-Id: I9cc411a1632101b5fc043193b38db8ffb9c20e7f

--- fs_mgr/fs_mgr_overlayfs_control.cpp
+++ fs_mgr/fs_mgr_overlayfs_control.cpp
@@ -219,6 +219,35 @@ OverlayfsTeardownResult TeardownDataScratch(IImageManager* images,
     return OverlayfsTeardownResult::Ok;
 }
 
+bool GetOverlaysActiveFlag() {
+    auto slot_number = fs_mgr_overlayfs_slot_number();
+    const auto super_device = kPhysicalDevice + fs_mgr_get_super_partition_name();
+
+    auto metadata = ReadMetadata(super_device, slot_number);
+    if (!metadata) {
+        return false;
+    }
+    return !!(metadata->header.flags & LP_HEADER_FLAG_OVERLAYS_ACTIVE);
+}
+
+bool SetOverlaysActiveFlag(bool flag) {
+    // Mark overlays as active in the partition table, to detect re-flash.
+    auto slot_number = fs_mgr_overlayfs_slot_number();
+    const auto super_device = kPhysicalDevice + fs_mgr_get_super_partition_name();
+    auto builder = MetadataBuilder::New(super_device, slot_number);
+    if (!builder) {
+        LERROR << "open " << super_device << " metadata";
+        return false;
+    }
+    builder->SetOverlaysActiveFlag(flag);
+    auto metadata = builder->Export();
+    if (!metadata || !UpdatePartitionTable(super_device, *metadata.get(), slot_number)) {
+        LERROR << "update super metadata";
+        return false;
+    }
+    return true;
+}
+
 OverlayfsTeardownResult fs_mgr_overlayfs_teardown_scratch(const std::string& overlay,
                                                           bool* change) {
     // umount and delete kScratchMountPoint storage if we have logical partitions
@@ -242,7 +271,9 @@ OverlayfsTeardownResult fs_mgr_overlayfs_teardown_scratch(const std::string& ove
     auto images = IImageManager::Open("remount", 10s);
     if (images && images->BackingImageExists(partition_name)) {
         // No need to check super partition, if we knew we had a scratch device
-        // in /data.
+        // in /data. Note: we don't care if SetOverlaysActiveFlag fails, since
+        // the overlays are removed no matter what.
+        SetOverlaysActiveFlag(false);
         return TeardownDataScratch(images.get(), partition_name, was_mounted);
     }
 
@@ -260,6 +291,7 @@ OverlayfsTeardownResult fs_mgr_overlayfs_teardown_scratch(const std::string& ove
         return OverlayfsTeardownResult::Ok;
     }
     builder->RemovePartition(partition_name);
+    builder->SetOverlaysActiveFlag(false);
     auto metadata = builder->Export();
     if (metadata && UpdatePartitionTable(super_device, *metadata.get(), slot_number)) {
         if (change) *change = true;
@@ -448,6 +480,9 @@ static bool CreateDynamicScratch(std::string* scratch_device, bool* partition_ex
             }
         }
     }
+
+    builder->SetOverlaysActiveFlag(true);
+
     // land the update back on to the partition
     if (changed) {
         auto metadata = builder->Export();
@@ -537,6 +572,9 @@ static bool CreateScratchOnData(std::string* scratch_device, bool* partition_exi
         LERROR << "could not map scratch image";
         // If we cannot use this image, then remove it.
         TeardownDataScratch(images.get(), partition_name, false /* was_mounted */);
+        return false;
+    }
+    if (!SetOverlaysActiveFlag(true)) {
         return false;
     }
     return true;
@@ -856,7 +894,7 @@ void MapScratchPartitionIfNeeded(Fstab* fstab,
         return;
     }
 
-    if (ScratchIsOnData()) {
+    if (ScratchIsOnData() && GetOverlaysActiveFlag()) {
         if (auto images = IImageManager::Open("remount", 0ms)) {
             images->MapAllImages(init);
         }
@@ -878,7 +916,11 @@ void CleanupOldScratchFiles() {
         return;
     }
     if (auto images = IImageManager::Open("remount", 0ms)) {
-        images->RemoveDisabledImages();
+        if (GetOverlaysActiveFlag()) {
+            images->RemoveDisabledImages();
+        } else {
+            fs_mgr_overlayfs_teardown_scratch(kScratchMountPoint, nullptr);
+        }
     }
 }
 
