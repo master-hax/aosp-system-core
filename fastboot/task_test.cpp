cfc95779a5bedb042ce5ca348a4c071c8bbb1f4f
Manish Singh <psych@google.com>
2023-12-15 12:36:47 +0000

    Revert "remount: Detect when flashall has happened in the bootlo..."
    Revert submission 2871216-b297923468
    
    Reason for revert: DroidMonitor-triggered revert due to breakage https://android-build.corp.google.com/builds/quarterdeck?branch=git_main-throttled-monitored&target=adt4-trunk_staging-userdebug&lkgb=11223468&lkbb=11224707&fkbb=11223653, bug https://b.corp.google.com/issues/316464083
    
    BUG: 316464083
    
    Reverted changes: /q/submissionid:2871216-b297923468
    
    Change-Id: I5c20d61201841ed2b4294e8a9de7948cb2af41d6

--- fastboot/task_test.cpp
+++ fastboot/task_test.cpp
@@ -233,7 +233,7 @@ TEST_F(ParseTest, CorrectTaskLists) {
             << "size of fastboot-info task list: " << fastboot_info_tasks.size()
             << " size of hardcoded task list: " << hardcoded_tasks.size();
 }
-TEST_F(ParseTest, IsDynamicPartitiontest) {
+TEST_F(ParseTest, IsDynamicParitiontest) {
     if (!get_android_product_out()) {
         GTEST_SKIP();
     }
@@ -258,7 +258,7 @@ TEST_F(ParseTest, IsDynamicPartitiontest) {
                 ParseFastbootInfoLine(fp.get(), android::base::Tokenize(test.first, " "));
         auto flash_task = task->AsFlashTask();
         ASSERT_FALSE(flash_task == nullptr);
-        ASSERT_EQ(FlashTask::IsDynamicPartition(fp->source.get(), flash_task), test.second);
+        ASSERT_EQ(FlashTask::IsDynamicParitition(fp->source.get(), flash_task), test.second);
     }
 }
 
@@ -358,7 +358,7 @@ TEST_F(ParseTest, OptimizedFlashSuperPatternMatchTest) {
                     contains_optimized_task = true;
                 }
                 if (auto flash_task = task->AsFlashTask()) {
-                    if (FlashTask::IsDynamicPartition(fp->source.get(), flash_task)) {
+                    if (FlashTask::IsDynamicParitition(fp->source.get(), flash_task)) {
                         return false;
                     }
                 }
