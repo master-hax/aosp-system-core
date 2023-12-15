1877692a82eec58de96f19e8996f8e8073758f8c
Seungjae Yoo <seungjaeyoo@google.com>
2023-12-15 15:44:47 +0900

    Support dm-verity with verification based on root digest
    Currently the only ways to enable dm-verity were relying on its built-in
    vbmeta image or containing its public key on standalone vbmeta image.
    Merging this change will support enabling dm-verity based on hashtree
    descriptor root digest for standalone vbmeta image.
    
    Bug: 285855436
    Test: Presubmit
    Test: adb shell /apex/com.android.virt/bin/vm run-microdroid --vendor /vendor/etc/avf/microdroid/microdroid_vendor.img
    Change-Id: I51eb64cae2ca8b4e97f1c6419b35d45e6f51cacb

--- init/first_stage_mount.cpp
+++ init/first_stage_mount.cpp
@@ -735,9 +735,13 @@ bool FirstStageMountVBootV2::GetDmVerityDevices(std::set<std::string>* devices) 
 bool FirstStageMountVBootV2::SetUpDmVerity(FstabEntry* fstab_entry) {
     AvbHashtreeResult hashtree_result;
 
-    // It's possible for a fstab_entry to have both avb_keys and avb flag.
-    // In this case, try avb_keys first, then fallback to avb flag.
-    if (!fstab_entry->avb_keys.empty()) {
+    if (IsMicrodroid() && !fstab_entry->avb_hashtree_descriptor_digest.empty()) {
+        auto avb_standalone_handle = AvbHandle::LoadAndVerifyVbmeta(*fstab_entry);
+        hashtree_result = avb_standalone_handle->SetUpAvbHashtree(fstab_entry,
+                                                                  false /* wait_for_verity_dev */);
+    } else if (!fstab_entry->avb_keys.empty()) {
+        // It's possible for a fstab_entry to have both avb_keys and avb flag.
+        // In this case, try avb_keys first, then fallback to avb flag.
         if (!InitAvbHandle()) return false;
         // Checks if hashtree should be disabled from the top-level /vbmeta.
         if (avb_handle_->status() == AvbHandleStatus::kHashtreeDisabled ||
