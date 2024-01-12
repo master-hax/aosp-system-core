4949ac957c5bb2acc715e9914a09a0af0b1d77ab
Michael Hoisie <hoisie@google.com>
2024-01-12 19:18:19 +0000

    Add support for ashmem-host for host Windows
    Migrate to tmpfile and fileno for temp file operations. These calls are
    supported on MinGW, and the temp files are automatically cleaned up.
    
    A Windows variant of ashmem-host is needed to support CursorWindows on
    host Windows.
    
    In Windows, it is not possible to unlink an open file, so the nlink
    check in ashmem_validate_stat must be made Unix-only.
    
    Test: SQLiteDatabaseTest in Google3
    Test: libcutils_test_static on Windows
    
    Bug: 317884162
    Change-Id: I7fc0f1f49406b01549b7f4d7e138cb3e4d79be72

---
+++ libcutils/ashmem_base_test.cpp
@@ -0,0 +1,47 @@
+/*
+ * Copyright (C) 2024 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <gtest/gtest.h>
+
+#include <unistd.h>
+
+#include <android-base/mapped_file.h>
+#include <android-base/unique_fd.h>
+#include <cutils/ashmem.h>
+
+using android::base::unique_fd;
+
+/*
+ * These are basic tests for ashmem that work on both Android and host
+ * platforms (Linux, Mac, Windows).
+ */
+TEST(AshmemHostTest, TestCreate) {
+    int size = 4096;
+    uint8_t data[size];
+    std::memset(data, 0xff, size);
+    unique_fd fd;
+    fd = unique_fd(ashmem_create_region(nullptr, size));
+    ASSERT_TRUE(fd >= 0);
+    ASSERT_TRUE(ashmem_valid(fd));
+    ASSERT_EQ(size, ashmem_get_size_region(fd));
+
+    std::unique_ptr<android::base::MappedFile> mapped =
+            android::base::MappedFile::FromFd(fd, 0, size, PROT_READ | PROT_WRITE);
+
+    void* addr = mapped->data();
+    memcpy(addr, &data, size);
+    ASSERT_EQ(0, memcmp(addr, &data, size));
+}
