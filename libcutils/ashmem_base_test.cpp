e9baf644ec447aed3daae1908a8e44d28835b4b2
Michael Hoisie <hoisie@google.com>
2024-01-12 19:18:19 +0000

    Add support for ashmem-host for host Windows
    A Windows variant of ashmem-host is needed to support CursorWindows on
    host Windows.
    
    In Windows GetTempPath can be used to get the temporary path directory
    instead of hardcoding /tmp.
    
    Test: SQLiteDatabaseTest in Google3
    Test: libcutils_test_static on Windows
    
    Bug: 317884162
    Change-Id: I7fc0f1f49406b01549b7f4d7e138cb3e4d79be72

---
+++ libcutils/ashmem_base_test.cpp
@@ -0,0 +1,54 @@
+/*
+ * Copyright (C) 2024 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <gtest/gtest.h>
+
+#include <unistd.h>
+
+#include <android-base/unique_fd.h>
+#include <cutils/ashmem.h>
+
+#ifdef _WIN32
+#include <windows.h>
+#endif
+using android::base::unique_fd;
+
+/*
+ * These are basic tests for ashmem that work on both Android and host
+ * platforms (Linux, Mac, Windows).
+ */
+TEST(AshmemHostTest, TestCreate) {
+#ifdef _WIN32
+    SYSTEM_INFO systemInfo;
+    GetSystemInfo(&systemInfo);
+    int size = systemInfo.dwPageSize;
+#else
+    int size = getpagesize();
+#endif
+
+    uint8_t data[size];
+    std::memset(data, 0xff, size);
+    unique_fd fd;
+    fd = unique_fd(ashmem_create_region(nullptr, size));
+    ASSERT_TRUE(fd >= 0);
+    ASSERT_TRUE(ashmem_valid(fd));
+    ASSERT_EQ(size, ashmem_get_size_region(fd));
+}
+
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
