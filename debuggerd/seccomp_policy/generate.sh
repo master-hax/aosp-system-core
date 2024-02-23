256bc86a90bb94c7076f936bbd60cb73a16be00f
Christopher Ferris <cferris@google.com>
2024-02-23 14:06:36 -0800

    Add no mmap/mprotect versions of policy files.
    The mediaswcodec process requires a less restrictive policy for
    mmap/mprotect. Since the policy files have no flow control, generate
    versions of the policies that can be included but only do not have
    the mmap/mprotect rules.
    
    Test: Ran debuggerd <PID_OF_media.swcodec> on 64 bit arm device.
    Test: Ran debuggerd <PID_OF_media.swcodec> on 32 bit arm device.
    Change-Id: I3b611a5a0be1cb0bf53295209fc9e8e956f8392a

--- debuggerd/seccomp_policy/generate.sh
+++ debuggerd/seccomp_policy/generate.sh
@@ -1,11 +1,31 @@
 #!/bin/bash
 
-set -ex
+set -e
 
 cd "$(dirname "$0")"
 CPP='cpp -undef -E -P crash_dump.policy.def'
-$CPP -D__arm__ -o crash_dump.arm.policy
-$CPP -D__aarch64__ -D__LP64__ -o crash_dump.arm64.policy
-$CPP -D__riscv -D__LP64__ -o crash_dump.riscv64.policy
-$CPP -D__i386__ -o crash_dump.x86.policy
-$CPP -D__x86_64__ -D__LP64__ -o crash_dump.x86_64.policy
+arches=( \
+  "arm"      "-D__arm__" \
+  "arm64"    "-D__aarch64__ -D__LP64__" \
+  "riscv64"  "-D__riscv -D__LP64__" \
+  "x86"      "-D__i386__" \
+  "x86_64"   "-D__x86_64__ -D__LP64__" \
+)
+
+# Normal pass
+for ((i = 0; i < ${#arches[@]}; i = i + 2)); do
+  arch=${arches[$i]}
+  arch_defines=${arches[$((i+1))]}
+  echo "Generating normal policy for ${arch}"
+  ${CPP} ${arch_defines} -o crash_dump.${arch}.policy
+done
+
+# Generate version without mmap/mprotect rules
+# This is needed for swcodec to be able to include the policy file since that
+# process requires a more permissive version of these syscalls.
+for ((i = 0; i < ${#arches[@]}; i = i + 2)); do
+  arch=${arches[$i]}
+  arch_defines=${arches[$((i+1))]}
+  echo "Generating no mmap/mprotect policy for ${arch}"
+  ${CPP} ${arch_defines} -DNO_MMAP_MPROTECT_RULES -o crash_dump.no_mmap_mprotect.${arch}.policy
+done
