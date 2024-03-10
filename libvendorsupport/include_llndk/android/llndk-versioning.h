e24e2e768d219031e34d05f9342f6a8baccf0d0e
Justin Yun <justinyun@google.com>
2024-03-10 10:16:42 +0900

    Define macros for LLNDK versioning
    __INTRODUCED_IN_LLNDK(vendor_api_level) is for the LLNDK only APIs
    introduced in the vendor_api_level.
    
    __INTRODUCED_IN_NDK_LLNDK(sdk_api_level, vendor_api_level) is for both
    NDK and LLNDK APIs.
    
    ANDROID_NDK_LLNDK_AT_LEAST(sdk_api_level, vendor_api_level) is used
    a statement for 'if' to guard the API callers. For example:
    
    if ANDROID_NDK_LLNDK_AT_LEAST(__ANDROID_API_V__, 202404) {
      new_api_for_V_and_202404();
    } else {
      fallback_function();
    }
    
    Use ANDROID_LLNDK_AT_LEAST(vendor_api_level) to call LLNDK only APIs.
    
    Bug: 302113279
    Test: build trunk-staging and next configurations
    Change-Id: I60d40655b1f8afdfa5c69b2ac4be2205f8057bea

--- libvendorsupport/include_llndk/android/llndk-versioning.h
+++ libvendorsupport/include_llndk/android/llndk-versioning.h
@@ -23,6 +23,7 @@ __BEGIN_DECLS
 // LLNDK (https://source.android.com/docs/core/architecture/vndk/build-system#ll-ndk) is similar to
 // NDK, but uses its own versioning of YYYYMM format for vendor builds. The LLNDK symbols are
 // enabled when the vendor api level is equal to or newer than the ro.board.api_level.
+// Use __INTRODUCED_IN_LLNDK if the symbol is available only to vendor modules as LLNDK.
 #define __INTRODUCED_IN_LLNDK(vendor_api_level)                                             \
     _Pragma("clang diagnostic push") _Pragma("clang diagnostic ignored \"-Wgcc-compat\"")   \
             __attribute__((enable_if(                                                       \
@@ -32,19 +33,41 @@ __BEGIN_DECLS
                     "call with '#if (__ANDROID_VENDOR_API__ >= " #vendor_api_level ")'."))) \
             _Pragma("clang diagnostic pop")
 
-// For the vendor libraries, __INTRODUCED_IN must be ignored because they are only for NDKs but not
-// for LLNDKs.
-#undef __INTRODUCED_IN
-#define __INTRODUCED_IN(x)
+// If the symbol is available to both NDK and LLNDK use __INTRODUCED_IN_NDK_LLNDK with both api
+// levels. Symbols added in Android U or older can use __INTRODUCED_IN in any cases.
+#define __INTRODUCED_IN_NDK_LLNDK(sdk_api_level, vendor_api_level) \
+    __INTRODUCED_IN_LLNDK(vendor_api_level)
+
+// Use this macro as an `if` statement to call an API that are available to both NDK and LLNDK.
+// This returns true for the vendor modules if the vendor_api_level is less than or equal to the
+// ro.board.api_level.
+#define ANDROID_NDK_LLNDK_AT_LEAST(sdk_api_level, vendor_api_level) \
+    constexpr(__ANDROID_VENDOR_API__ >= vendor_api_level)
 
 #else  // __ANDROID_VENDOR__
 
-// For non-vendor libraries, __INTRODUCED_IN_LLNDK must be ignored because it must not change
-// symbols of NDK or the system side of the treble boundary. It leaves a no-op annotation for ABI
-// analysis.
-#define __INTRODUCED_IN_LLNDK(vendor_api_level) \
-    __attribute__((annotate("introduced_in_llndk=" #vendor_api_level)))
+// __INTRODUCED_IN_LLNDK is for LLNDK only but not for NDK. This is replaced with
+// __INTRODUCED_IN(__ANDROID_API_FUTURE__) for non-vendor modules., It leaves a no-op annotation
+// for ABI analysis.
+#define __INTRODUCED_IN_LLNDK(vendor_api_level)                         \
+    __attribute__((annotate("introduced_in_llndk=" #vendor_api_level))) \
+    __INTRODUCED_IN(__ANDROID_API_FUTURE__)
+
+// For non-vendor modules, replace __INTRODUCED_IN_NDK_LLNDK with __INTRODUCED_IN. It leaves a no-op
+// annotation for ABI analysis.
+#define __INTRODUCED_IN_NDK_LLNDK(sdk_api_level, vendor_api_level)      \
+    __attribute__((annotate("introduced_in_llndk=" #vendor_api_level))) \
+    __INTRODUCED_IN(sdk_api_level)
+
+// For non-vendor modules, it is replaced with __builtin_available(__builtin_available) to guard the
+// API for __INTRODUCED_IN.
+#define ANDROID_NDK_LLNDK_AT_LEAST(sdk_api_level, vendor_api_level) \
+    (__builtin_available(android sdk_api_level, *))
 
 #endif  // __ANDROID_VENDOR__
+
+// Use ANDROID_LLNDK_AT_LEAST to guard the LLNDK only symbols
+#define ANDROID_LLNDK_AT_LEAST(vendor_api_level) \
+    ANDROID_NDK_LLNDK_AT_LEAST(__ANDROID_API_FUTURE__, vendor_api_level)
 
 __END_DECLS
