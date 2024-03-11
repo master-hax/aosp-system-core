70dba5e452ad1aa123c61e0d683df9a3e312ebf5
T.J. Mercier <tjmercier@google.com>
2024-03-11 20:47:41 +0000

    Add read-only aconfig flag to split the cgroup v2 hierarchy into apps/system
    This flag adds "apps" and "system" cgroups underneath the v2 hierarchy
    root. Applications with UIDs < 10000 (AID_APP_START) will be placed
    under "system" and others will be placed under "apps".
    
    Bug: 327480673
    Change-Id: I40837dee27a59691f81fef48e66a86c5eacda892

--- libprocessgroup/task_profiles.cpp
+++ libprocessgroup/task_profiles.cpp
@@ -127,7 +127,17 @@ void ProfileAttribute::Reset(const CgroupController& controller, const std::stri
     file_v2_name_ = file_v2_name;
 }
 
+static bool isSystemApp(uid_t uid) {
+    return uid < AID_APP_START;
+}
+
 std::string ConvertUidToPath(const char* root_cgroup_path, uid_t uid) {
+    if (android::libprocessgroup_flags::cgroup_v2_sys_app_isolation()) {
+        if (isSystemApp(uid))
+            return StringPrintf("%s/system/uid_%u", root_cgroup_path, uid);
+        else
+            return StringPrintf("%s/apps/uid_%u", root_cgroup_path, uid);
+    }
     return StringPrintf("%s/uid_%u", root_cgroup_path, uid);
 }
 
