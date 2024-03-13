9d6cd6b9d9cb7247e2ce829c0b470ed8917c0eea
Akilesh Kailash <akailash@google.com>
2024-03-13 14:17:25 -0700

    libsnapshot: Move snapshot metadata to super partition.
    snapshot metadata files are stored in /metadata. This means, we cannot
    wipe after installing any update.
    
    This patch does the following:
    
    1: Create a scratch space in super partition. The scratch space for ota
       metadata is just about 1MB.
    
    2: Create ext4 filesystem on top of scratch block device.
    
    3: Mount the scratch on /mnt/scratch_super
    
    4: When snapshot-manager instance is created, point the /mnt/scratch/ota
    to metadata_dir_ so that all the snapshot files are stored in the new
    path.
    
    All the logic of OTA remains the same. This flow is enabled only on userdebug builds for now and the only consumer would be snapshotctl
    
    $snapshotctl apply-update /data/nbd/ -w
    
    During init, we would have to mount the scratch partition to detect
    if there is any pending updates.
    
    With this, we would now be able to wipe the device along with the update flow. This will help incremental flashing wherein we would end up saving ~35-40 seconds on Pixel devices.
    
    With this flow, the end-to-end update for incremental builds takes
    ~20-30 seconds.
    
    Bug: 330744468
    Test: Pixel 6 incremental flashing with wipe, Full OTA, vts_libsnapshot
    Change-Id: Iac6ce2cf37b70ea221cd18175c8962988d03d95b
    Signed-off-by: Akilesh Kailash <akailash@google.com>

--- fs_mgr/libsnapshot/snapshot.cpp
+++ fs_mgr/libsnapshot/snapshot.cpp
@@ -48,6 +48,7 @@
 #include <libsnapshot/snapshot_stats.h>
 #include "device_info.h"
 #include "partition_cow_creator.h"
+#include "scratch_super.h"
 #include "snapshot_metadata_updater.h"
 #include "utility.h"
 
@@ -117,7 +118,21 @@ std::unique_ptr<SnapshotManager> SnapshotManager::New(IDeviceInfo* info) {
         info = new DeviceInfo();
     }
 
-    return std::unique_ptr<SnapshotManager>(new SnapshotManager(info));
+    std::string scratch_device = android::snapshot::GetScratchDevice();
+    std::string scratch_metadata;
+    if (!scratch_device.empty()) {
+        scratch_metadata = android::snapshot::MapScratchDevice(scratch_device);
+        if (!scratch_metadata.empty()) {
+            info->SetMetadataDir(scratch_metadata);
+        }
+    }
+
+    auto sm = std::unique_ptr<SnapshotManager>(new SnapshotManager(info));
+    if (!scratch_metadata.empty()) {
+        sm->SetScratchMetadata();
+        LOG(INFO) << "Using scratch metadata";
+    }
+    return sm;
 }
 
 std::unique_ptr<SnapshotManager> SnapshotManager::NewForFirstStageMount(IDeviceInfo* info) {
@@ -1110,6 +1125,13 @@ UpdateState SnapshotManager::ProcessUpdateState(const std::function<bool()>& cal
         if (result.state == UpdateState::MergeFailed) {
             AcknowledgeMergeFailure(result.failure_code);
         }
+
+        if (result.state == UpdateState::MergeCompleted) {
+            if (is_scratch_metadata_) {
+                CleanupMetadataScratch();
+            }
+        }
+
         if (result.state != UpdateState::Merging) {
             // Either there is no merge, or the merge was finished, so no need
             // to keep waiting.
@@ -2310,7 +2332,27 @@ bool SnapshotManager::ListSnapshots(LockedFile* lock, std::vector<std::string>* 
 }
 
 bool SnapshotManager::IsSnapshotManagerNeeded() {
-    return access(kBootIndicatorPath, F_OK) == 0;
+    if (access(kBootIndicatorPath, F_OK) == 0) {
+        return true;
+    }
+
+    if (IsScratchPresent()) {
+        return true;
+    }
+
+    return false;
+}
+
+bool SnapshotManager::MapScratchPartitionIfNeeded(
+        const std::function<bool(const std::string&)>& init) {
+    auto device = android::snapshot::GetScratchDevice();
+    if (!device.empty()) {
+        init(device);
+        if (android::snapshot::MapScratchDevice(device).empty()) {
+            return false;
+        }
+    }
+    return true;
 }
 
 std::string SnapshotManager::GetGlobalRollbackIndicatorPath() {
@@ -2394,6 +2436,12 @@ bool SnapshotManager::MapAllPartitions(LockedFile* lock, const std::string& supe
         if (GetPartitionGroupName(metadata->groups[partition.group_index]) == kCowGroupName) {
             LOG(INFO) << "Skip mapping partition " << GetPartitionName(partition) << " in group "
                       << kCowGroupName;
+            continue;
+        }
+
+        if (GetPartitionName(partition) ==
+            android::base::Basename(android::snapshot::kOtaMetadataMount)) {
+            LOG(INFO) << "Partition: " << GetPartitionName(partition) << " skipping";
             continue;
         }
 
@@ -3282,7 +3330,7 @@ Return SnapshotManager::CreateUpdateSnapshots(const DeltaArchiveManifest& manife
     // TODO(b/134949511): remove this check. Right now, with overlayfs mounted, the scratch
     // partition takes up a big chunk of space in super, causing COW images to be created on
     // retrofit Virtual A/B devices.
-    if (device_->IsOverlayfsSetup()) {
+    if (!is_scratch_metadata_ && device_->IsOverlayfsSetup()) {
         LOG(ERROR) << "Cannot create update snapshots with overlayfs setup. Run `adb enable-verity`"
                    << ", reboot, then try again.";
         return Return::Error();
