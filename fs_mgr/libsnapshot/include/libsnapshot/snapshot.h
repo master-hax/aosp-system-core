e132e2bdffb01ed7d2558f65c3d93fe241bf30e4
Akilesh Kailash <akailash@google.com>
2024-03-13 14:17:25 -0700

    libsnapshot: Move snapshot metadata to super partition.
    snapshot metadata files are stored in /metadata. This means, we cannot
    wipe after installing any update.
    
    This patch does the following:
    
    1: Create a scratch space in super partition. The scratch space for ota
       metadata is just about 1MB.
    
    2: Create ext4 filesystem on top of scratch block device.
    
    3: Mount the scratch on /mnt/scratch_super
    
    4: When snapshot-manager instance is created, point the /mnt/scratch/ota
    to metadata_dir_ so that all the snapshot files are stored in the new
    path.
    
    All the logic of OTA remains the same. This flow is enabled only on userdebug builds for now and the only consumer would be snapshotctl
    
    $snapshotctl apply-update /data/nbd/ -w
    
    
    During init, we would have to mount the scratch partition to detect
    if there is any pending updates.
    
    With this, we would now be able to wipe the device along with the update flow. This will help incremental flashing wherein we would end up saving ~35-40 seconds on Pixel devices.
    
    With this flow, the end-to-end update for incremental builds takes
    ~20-30 seconds.
    
    Bug: 330744468
    Test: Pixel 6 incremental flashing with wipe, Full OTA, vts_libsnapshot
    Change-Id: Iac6ce2cf37b70ea221cd18175c8962988d03d95b
    Signed-off-by: Akilesh Kailash <akailash@google.com>

--- fs_mgr/libsnapshot/include/libsnapshot/snapshot.h
+++ fs_mgr/libsnapshot/include/libsnapshot/snapshot.h
@@ -108,6 +108,7 @@ class ISnapshotManager {
         virtual bool IsRecovery() const = 0;
         virtual bool IsTestDevice() const { return false; }
         virtual bool IsFirstStageInit() const = 0;
+        virtual void SetMetadataDir(std::string value) = 0;
         virtual std::unique_ptr<IImageManager> OpenImageManager() const = 0;
         virtual android::dm::IDeviceMapper& GetDeviceMapper() = 0;
 
@@ -328,6 +329,18 @@ class SnapshotManager final : public ISnapshotManager {
     // might be needed to perform first-stage mounts.
     static bool IsSnapshotManagerNeeded();
 
+    // Map the scratch partition which contains metadata of snapshots
+    static bool MapScratchPartitionIfNeeded(const std::function<bool(const std::string&)>& init);
+
+    // Create OTA metadata on super partition
+    static bool CreateOtaMetadataOnSuper(IDeviceInfo* info = nullptr);
+
+    // Clean up OTA metadata scratch device partition on super
+    static bool CleanupScratch(IDeviceInfo* info = nullptr);
+
+    // Helper function to create scratch device on super for OTA metadata
+    static bool CreateDynamicScratch(IDeviceInfo* info, std::string* scratch_device);
+
     // Helper function for second stage init to restorecon on the rollback indicator.
     static std::string GetGlobalRollbackIndicatorPath();
 
@@ -392,6 +405,8 @@ class SnapshotManager final : public ISnapshotManager {
     void SetUeventRegenCallback(std::function<bool(const std::string&)> callback) {
         uevent_regen_callback_ = callback;
     }
+
+    void SetScratchMetadata() { is_scratch_metadata_ = true; }
 
     // If true, compression is enabled for this update. This is used by
     // first-stage to decide whether to launch snapuserd.
@@ -849,6 +864,7 @@ class SnapshotManager final : public ISnapshotManager {
     std::unique_ptr<IImageManager> images_;
     bool use_first_stage_snapuserd_ = false;
     bool in_factory_data_reset_ = false;
+    bool is_scratch_metadata_ = false;
     std::function<bool(const std::string&)> uevent_regen_callback_;
     std::unique_ptr<SnapuserdClient> snapuserd_client_;
     std::unique_ptr<LpMetadata> old_partition_metadata_;
