0ea38776bb0f3650dfde8f8fbfc14cb04a42a8d7
Sijie Chen <sijiec@google.com>
2024-03-25 20:06:18 +0000

    [Berberis][CrashReporting] Retrieve TLS guest state header slot
    This CL is to retrieve the TLS slot information.
    
    Bug: b/321799516
    Test: m
    Manual testing by: 1. crash the jni tests to produce tombstones file 2.
    get the signature field of guest state header 3. verified it is the same
    value as NATIVE_BRIDGE_GUEST_STATE_SIGNATURE
    
    Change-Id: I23386a032b1d641df7aae6d53752379d4f39b37c

--- debuggerd/crash_dump.cpp
+++ debuggerd/crash_dump.cpp
@@ -42,6 +42,7 @@
 #include <android-base/unique_fd.h>
 #include <bionic/macros.h>
 #include <bionic/reserved_signals.h>
+#include <bionic/tls_defines.h>
 #include <cutils/sockets.h>
 #include <log/log.h>
 #include <private/android_filesystem_config.h>
@@ -53,6 +54,8 @@
 #include <unwindstack/AndroidUnwinder.h>
 #include <unwindstack/Error.h>
 #include <unwindstack/Regs.h>
+
+#include <native_bridge_support/guest_state_accessor/accessor.h>
 
 #include "libdebuggerd/backtrace.h"
 #include "libdebuggerd/tombstone.h"
@@ -401,6 +404,30 @@ static void InstallSigPipeHandler() {
   action.sa_handler = SIG_IGN;
   action.sa_flags = SA_RESTART;
   sigaction(SIGPIPE, &action, nullptr);
+}
+
+static NativeBridgeGuestStateHeader* GetGuestStateTlsPointer([[maybe_unused]] pid_t tid) {
+#if defined(__x86_64__)
+  {
+    void** tp_reg = nullptr;
+    ErrnoRestorer errno_restorer;
+    errno = 0;
+    uintptr_t fs_base = ptrace(PTRACE_PEEKUSER, tid, offsetof(user_regs_struct, fs_base), nullptr);
+    if (errno == 0) {
+      tp_reg = reinterpret_cast<void**>(fs_base);
+    }
+    if (tp_reg == nullptr) {
+      PLOG(ERROR) << "failed to read thread register for thread " << tid;
+      return nullptr;
+    }
+    auto ptr_to_guest_slot = reinterpret_cast<NativeBridgeGuestStateHeader**>(
+        &tp_reg[TLS_SLOT_NATIVE_BRIDGE_GUEST_STATE]);
+    uintptr_t header_ptr = ptrace(PTRACE_PEEKDATA, tid, ptr_to_guest_slot, 0);
+    return reinterpret_cast<NativeBridgeGuestStateHeader*>(header_ptr);
+  }
+#else
+  return nullptr;
+#endif
 }
 
 int main(int argc, char** argv) {
