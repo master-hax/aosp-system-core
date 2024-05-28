1e6ad06a2cea63ffdca358254e4067d554985645
Daniel Zheng <zhengdaniel@google.com>
2024-05-28 16:36:52 -0700

    libsnapshot: cap merge op count
    Set op processing window during snapshot merge to be read from the build
    configurations. A lower window will reduce memory strain. With variable
    block sized compression, this number might not be accurate, and might
    need some more tinkering with.
    
    Test: th
    Change-Id: Ic7770115bca963d923a7a276897c5deac30f9faf

--- fs_mgr/libsnapshot/snapuserd/snapuserd_daemon.cpp
+++ fs_mgr/libsnapshot/snapuserd/snapuserd_daemon.cpp
@@ -106,7 +106,6 @@ bool Daemon::StartServerForUserspaceSnapshots(int arg_start, int argc, char** ar
         }
         return user_server_.Run();
     }
-
     for (int i = arg_start; i < argc; i++) {
         auto parts = android::base::Split(argv[i], ",");
 
@@ -114,8 +113,9 @@ bool Daemon::StartServerForUserspaceSnapshots(int arg_start, int argc, char** ar
             LOG(ERROR) << "Malformed message, expected at least four sub-arguments.";
             return false;
         }
-        auto handler =
-                user_server_.AddHandler(parts[0], parts[1], parts[2], parts[3], FLAGS_o_direct);
+        auto handler = user_server_.AddHandler(
+                parts[0], parts[1], parts[2], parts[3], FLAGS_o_direct,
+                android::base::GetUintProperty<uint32_t>("ro.virtual_ab.cow_op_merge_size", 0));
         if (!handler || !user_server_.StartHandler(parts[0])) {
             return false;
         }
