0f6495d15b6f58278d1c75c30b648acd63d384b6
Grzegorz Jaszczyk <jaszczyk@google.com>
2024-06-06 15:27:08 +0000

    libmodprobe: add support for dynamic module options
    Hitherto libmodprobe was supporting modules.options which allows passing
    extra module options. This commit extends that support and beside
    `options`, modules.options can now contain `dyn_options` entries.
    
    Syntax example:
    dyn_options snd_sof_pci system "/vendor/bin/mod_options --sof-fw"
    
    This allows to determine extra module options, with help of external
    program, at runtime which can be useful for unibuilds, supporting
    various device flavors and therefore requiring different module options.
    
    Using external program to customize modules parameters are inspired by
    Android's external firmware handler:
    https://android.googlesource.com/platform/system/core/+/master/init/README.ueventd.md#firmware-loading
    and the same RunExternalHandler is reused for both dynamic module
    handling and firmware handling.
    
    Bug: 335619610
    Test: Test audio on systems using snd_sof_pci module and verify
    correctness of custom module parameters.
    
    Change-Id: Ibd1a942d4ea022519676435ed525cd7c3e720d31

--- libmodprobe/include/modprobe/modprobe.h
+++ libmodprobe/include/modprobe/modprobe.h
@@ -23,7 +23,9 @@
 #include <unordered_set>
 #include <vector>
 
+#include <android-base/result.h>
 #include <android-base/thread_annotations.h>
+using android::base::Result;
 
 class Modprobe {
   public:
@@ -59,6 +61,7 @@ class Modprobe {
     bool ParseSoftdepCallback(const std::vector<std::string>& args);
     bool ParseLoadCallback(const std::vector<std::string>& args);
     bool ParseOptionsCallback(const std::vector<std::string>& args);
+    bool ParseDynOptionsCallback(const std::vector<std::string>& args);
     bool ParseBlocklistCallback(const std::vector<std::string>& args);
     void ParseKernelCmdlineOptions();
     void ParseCfg(const std::string& cfg, std::function<bool(const std::vector<std::string>&)> f);
