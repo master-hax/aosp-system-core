9039983c71c406d430c0977bbe7ed9f50d48a7cb
Daniel Zheng <zhengdaniel@google.com>
2024-06-27 13:51:13 -0700

    Disable images in recovery
    If a previous snapshot was not cleaned up, we want to mark the snapshots
    for cleanup upon the next reboot (in recovery mode). Since we cannot
    mount /data in recovery, attempting to delete the snapshots alone will
    return an error.
    
    Test: apply OTA -> reboot recovery -> install sideload OTA (confirm
    success) -> reboot and
    confirm that no snapshots exist under /data/gsi/ota
    
    Change-Id: I2894f5fbc1dd534510bc2b6581f54e53133602d7

--- fs_mgr/libfiemap/image_manager.cpp
+++ fs_mgr/libfiemap/image_manager.cpp
@@ -655,6 +655,23 @@ bool ImageManager::RemoveAllImages() {
     return ok && RemoveAllMetadata(metadata_dir_);
 }
 
+bool ImageManager::DisableAllImages() {
+    if (!MetadataExists(metadata_dir_)) {
+        return true;
+    }
+    auto metadata = OpenMetadata(metadata_dir_);
+    if (!metadata) {
+        return RemoveAllMetadata(metadata_dir_);
+    }
+
+    bool ok = true;
+    for (const auto& partition : metadata->partitions) {
+        auto partition_name = GetPartitionName(partition);
+        ok &= DisableImage(partition_name);
+    }
+    return ok && RemoveAllMetadata(metadata_dir_);
+}
+
 bool ImageManager::Validate() {
     auto metadata = OpenMetadata(metadata_dir_);
     if (!metadata) {
