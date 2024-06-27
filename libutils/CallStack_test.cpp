85756fd4d9a3ae59c4c6667791b5ec00cdbce1f7
Pechetty Sravani <pechetty@google.com>
2024-06-27 05:17:08 +0000

    Revert "libutils: CallStackTest log_stack check more log"
    This reverts commit 47fde793b111aecdb34edba598b5697ba9c4701c.
    
    Reason for revert: Droidmonitor triggered revert due to build breakage in b/349658817. Will be verifying through ABTD before submission.
    
    Change-Id: I8511ecacc0b17e28e2129f7bd86acfff22c9853b

--- libutils/CallStack_test.cpp
+++ libutils/CallStack_test.cpp
@@ -72,8 +72,7 @@ TEST(CallStackTest, thread_backtrace) {
 TEST(CallStackTest, log_stack) {
     android::CallStack::logStack("callstack_test");
     auto logger_list = android_logger_list_open(android_name_to_log_id("main"),
-                                                ANDROID_LOG_NONBLOCK,
-                                                10000 /* tail */, getpid());
+                                                ANDROID_LOG_NONBLOCK, 1000, getpid());
     ASSERT_NE(nullptr, logger_list);
     std::string log;
     while (true) {
