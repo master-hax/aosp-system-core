ec1424e936909e6e02e33db4d4c6112df09ac70c
T.J. Mercier <tjmercier@google.com>
2024-08-08 16:11:21 +0000

    Don't write cgroup.rc
    Change-Id: Ic3f01fdf7fda89a56ab80657e1cf4573156273e6

---
+++ libprocessgroup/util/cgroup_descriptor.cpp
@@ -0,0 +1,22 @@
+#include <processgroup/cgroup_descriptor.h>
+
+#include <processgroup/util.h> // For flag values
+
+CgroupDescriptor::CgroupDescriptor(uint32_t version, const std::string& name,
+                                   const std::string& path, mode_t mode, const std::string& uid,
+                                   const std::string& gid, uint32_t flags,
+                                   uint32_t max_activation_depth)
+    : controller_(version, flags, name, path, max_activation_depth),
+      mode_(mode),
+      uid_(uid),
+      gid_(gid) {}
+
+void CgroupDescriptor::set_mounted(bool mounted) {
+    uint32_t flags = controller_.flags();
+    if (mounted) {
+        flags |= CGROUPRC_CONTROLLER_FLAG_MOUNTED;
+    } else {
+        flags &= ~CGROUPRC_CONTROLLER_FLAG_MOUNTED;
+    }
+    controller_.set_flags(flags);
+}
