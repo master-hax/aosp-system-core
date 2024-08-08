c89d4c9be9a7b1cda7c96d4ff0e801f9fbbceb09
T.J. Mercier <tjmercier@google.com>
2024-08-08 16:11:21 +0000

    Don't write cgroup.rc
    Change-Id: Ic3f01fdf7fda89a56ab80657e1cf4573156273e6

--- libprocessgroup/util/include/processgroup/cgroup_controller.h
+++ libprocessgroup/util/include/processgroup/cgroup_controller.h
@@ -20,11 +20,7 @@
 #include <cstdint>
 #include <string>
 
-namespace android {
-namespace cgrouprc {
-namespace format {
-
-// Minimal controller description to be mmapped into process address space
+// Minimal controller description
 struct CgroupController {
   public:
     CgroupController() = default;
@@ -40,7 +36,7 @@ struct CgroupController {
     void set_flags(uint32_t flags);
 
   private:
-    static constexpr size_t CGROUP_NAME_BUF_SZ = 16;
+    static constexpr size_t CGROUP_NAME_BUF_SZ = 16; // TODO cgroup.rc depended on these being a constant size, but now that is no longer necessary.
     static constexpr size_t CGROUP_PATH_BUF_SZ = 32;
 
     uint32_t version_ = 0;
@@ -49,7 +45,3 @@ struct CgroupController {
     char name_[CGROUP_NAME_BUF_SZ] = {};
     char path_[CGROUP_PATH_BUF_SZ] = {};
 };
-
-}  // namespace format
-}  // namespace cgrouprc
-}  // namespace android
