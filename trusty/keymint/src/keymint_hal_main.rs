238653dd370fbee6d9b3f965ee167f2f8678e2e7
Android Culprit Assistant <boq-android-culprit-assistant@system.gserviceaccount.com>
2024-08-11 21:40:07 +0000

    Revert "trusty: keymint: Add commandline option to specify devic..."
    This is an Android Culprit Assistant revert, created as part of a culprit search session (http://go/aca-get/bf8361e3-5465-46a6-893d-4f983f8c13f8). It will not be committed, and can be ignored. If you need to commit this revert, please don't use this - create a new revert of the original topic.
    
    Change-Id: Icd1cd8d8388ea453e91d46f941ec4c421a0a19d1
    Signed-off-by: boq-android-culprit-assistant@system.gserviceaccount.com

--- trusty/keymint/src/keymint_hal_main.rs
+++ trusty/keymint/src/keymint_hal_main.rs
@@ -14,7 +14,6 @@
 // limitations under the License.
 
 //! This module implements the HAL service for Keymint (Rust) in Trusty.
-use clap::Parser;
 use kmr_hal::{
     extract_rsp, keymint, rpc, secureclock, send_hal_info, sharedsecret, SerializedChannel,
 };
@@ -82,13 +81,6 @@ impl SerializedChannel for TipcChannel {
     }
 }
 
-#[derive(Parser, Debug)]
-struct Args {
-    /// Tipc device path
-    #[arg(short, long, default_value_t = DEFAULT_DEVICE.to_string())]
-    dev: String,
-}
-
 fn main() {
     if let Err(HalServiceError(e)) = inner_main() {
         panic!("HAL service failed: {:?}", e);
@@ -96,7 +88,6 @@ fn main() {
 }
 
 fn inner_main() -> Result<(), HalServiceError> {
-    let args = Args::parse();
     // Initialize Android logging.
     android_logger::init_once(
         android_logger::Config::default()
@@ -115,15 +106,10 @@ fn inner_main() -> Result<(), HalServiceError> {
     binder::ProcessState::start_thread_pool();
 
     // Create connection to the TA
-    let connection =
-        trusty::TipcChannel::connect(args.dev.as_str(), TRUSTY_KEYMINT_RUST_SERVICE_NAME).map_err(
-            |e| {
-                HalServiceError(format!(
-                    "Failed to connect to Trusty Keymint TA at {} because of {:?}.",
-                    args.dev, e
-                ))
-            },
-        )?;
+    let connection = trusty::TipcChannel::connect(DEFAULT_DEVICE, TRUSTY_KEYMINT_RUST_SERVICE_NAME)
+        .map_err(|e| {
+            HalServiceError(format!("Failed to connect to Trusty Keymint TA because of {:?}.", e))
+        })?;
     let tipc_channel = Arc::new(Mutex::new(TipcChannel(connection)));
 
     // Register the Keymint service
