7bea35ea629a749254f3ce3466c4e236d259627e
Yi-Yo Chiang <yochiang@google.com>
2024-10-15 14:51:01 +0800

    remount: Make scratch filesystem with "nodiscard" flag
    According to "man mkfs.ext4", the "discard" flag is useful for "SSD and
    sparse / thin-provisioned storage".
    For remount/overlayfs we use IImageManager to allocate and manage the
    data blocks so the "discard" option don't benefit us.
    If the scratch device image is not mounted via dm-linear, but with
    loopdev (direct file access), then the mkfs discard option could cause
    file extents be deallocated and reallocated. This would break
    IImageManager because IImageManager bookkeeps its allocated file block
    extents.
    
    Note that we already pass "nodiscard" when mounting the scratch
    filesystem.
    As a result, also explicitly pass nodiscard when calling mkfs.
    
    Bug: 359464822
    Change-Id: I57294d242700a923a31c84798d042a437ac57eba

--- fs_mgr/fs_mgr_overlayfs.cpp
+++ fs_mgr/fs_mgr_overlayfs.cpp
@@ -939,10 +939,11 @@ bool MakeScratchFilesystem(const std::string& scratch_device) {
     auto command = ""s;
     if (!access(kMkF2fs.c_str(), X_OK) && fs_mgr_filesystem_available("f2fs")) {
         fs_type = "f2fs";
-        command = kMkF2fs + " -w 4096 -f -d1 -l" + android::base::Basename(kScratchMountPoint);
+        command = kMkF2fs + " -w 4096 -f -d1 -t 0 -l" + android::base::Basename(kScratchMountPoint);
     } else if (!access(kMkExt4.c_str(), X_OK) && fs_mgr_filesystem_available("ext4")) {
         fs_type = "ext4";
-        command = kMkExt4 + " -F -b 4096 -t ext4 -m 0 -O has_journal -M " + kScratchMountPoint;
+        command = kMkExt4 + " -F -b 4096 -t ext4 -m 0 -O has_journal -E nodiscard -M " +
+                  kScratchMountPoint;
     } else {
         LERROR << "No supported mkfs command or filesystem driver available, supported filesystems "
                   "are: f2fs, ext4";
@@ -951,8 +952,8 @@ bool MakeScratchFilesystem(const std::string& scratch_device) {
     command += " " + scratch_device + " >/dev/null 2>/dev/null </dev/null";
     fs_mgr_set_blk_ro(scratch_device, false);
     auto ret = system(command.c_str());
+    LINFO << "make " << fs_type << " filesystem on " << scratch_device << " return=" << ret;
     if (ret) {
-        LERROR << "make " << fs_type << " filesystem on " << scratch_device << " return=" << ret;
         return false;
     }
     return true;
